7ad41756bbf74f64098280787e5825948a70f78f
beedee <beedee@993157c7-ee19-0410-b2c4-bb4e9862e678>
2007-06-23 18:21:01 +0000

    git-svn-id: svn://svn.ijw.co.nz/svn/OpenRa@1064 993157c7-ee19-0410-b2c4-bb4e9862e678

--- OpenRa.FileFormats/Palette.cs
+++ OpenRa.FileFormats/Palette.cs
@@ -28,6 +28,9 @@ namespace OpenRa.FileFormats
 					colors.Add(Color.FromArgb(r, g, b));
 				}
 			}
+			colors[0] = Color.FromArgb(0, 0, 0, 0);
+			colors[3] = Color.FromArgb(178, 0, 0, 0);
+			colors[4] = Color.FromArgb(140, 0, 0, 0);
 		}
 	}
 }
