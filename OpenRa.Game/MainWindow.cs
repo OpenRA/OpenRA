6490ed86128358311b9d0a55524b476d7ff1f97a
chrisf <chrisf@993157c7-ee19-0410-b2c4-bb4e9862e678>
2007-07-05 22:46:58 +0000

    tiny bit of progress
    git-svn-id: svn://svn.ijw.co.nz/svn/OpenRa@1105 993157c7-ee19-0410-b2c4-bb4e9862e678

--- OpenRa.Game/MainWindow.cs
+++ OpenRa.Game/MainWindow.cs
@@ -4,6 +4,8 @@ using System.Text;
 using System.Windows.Forms;
 using System.Drawing;
 using BluntDirectX.Direct3D;
+using OpenRa.FileFormats;
+using System.IO;
 
 namespace OpenRa.Game
 {
@@ -11,14 +13,20 @@ namespace OpenRa.Game
 	{
 		GraphicsDevice device;
 
+		const string mapName = "scm12ea.ini";
+
 		public MainWindow()
 		{
-			Text = "OpenRA";
 			ClientSize = new Size(640, 480);
 
 			Visible = true;
 
 			device = GraphicsDevice.Create(this, ClientSize.Width, ClientSize.Height, true, false);
+
+			IniFile mapFile = new IniFile(File.OpenRead("../../../" + mapName));
+			Map map = new Map(mapFile);
+
+			Text = string.Format("OpenRA - {0} - {1}", map.Title, mapName);
 		}
 
 		internal void Run()
