91cc485201ca1159fc21bd54ad9eb0341f0a69c2
chrisf <chrisf@993157c7-ee19-0410-b2c4-bb4e9862e678>
2007-07-10 07:38:33 +0000

    git-svn-id: svn://svn.ijw.co.nz/svn/OpenRa@1161 993157c7-ee19-0410-b2c4-bb4e9862e678

---
+++ OpenRa.FileFormats/Tuple.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenRa.FileFormats
+{
+	public class Tuple<A>
+	{
+		public A a;
+
+		public Tuple(A a) { this.a = a; }
+	}
+
+	public class Tuple<A, B>
+	{
+		public A a;
+		public B b;
+
+		public Tuple(A a, B b) { this.a = a; this.b = b; }
+	}
+
+	public class Tuple<A, B, C>
+	{
+		public A a;
+		public B b;
+		public C c;
+
+		public Tuple(A a, B b, C c) { this.a = a; this.b = b; this.c = c; }
+	}
+
+	public class Tuple<A, B, C, D>
+	{
+		public A a;
+		public B b;
+		public C c;
+		public D d;
+
+		public Tuple(A a, B b, C c, D d) { this.a = a; this.b = b; this.c = c; this.d = d; }
+	}
+}
