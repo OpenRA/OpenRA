c74c7b5deba8111775ef3df42f9e6365c5b9f68c
chrisf <chrisf@993157c7-ee19-0410-b2c4-bb4e9862e678>
2007-07-12 13:58:15 +0000

    git-svn-id: svn://svn.ijw.co.nz/svn/OpenRa@1179 993157c7-ee19-0410-b2c4-bb4e9862e678

---
+++ OpenRa.Game/Mcv.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenRa.FileFormats;
+
+namespace OpenRa.Game
+{
+	class Mcv : Actor
+	{
+		int facing;
+
+		static Package unitsPackage = new Package("../../../conquer.mix");
+
+		public Mcv()
+		{
+
+		}
+	}
+}
