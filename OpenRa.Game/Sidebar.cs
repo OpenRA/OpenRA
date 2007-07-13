b0f4f08938e6c288796d1770f66f3c40533ca090
chrisf <chrisf@993157c7-ee19-0410-b2c4-bb4e9862e678>
2007-07-13 15:36:55 +0000

    git-svn-id: svn://svn.ijw.co.nz/svn/OpenRa@1206 993157c7-ee19-0410-b2c4-bb4e9862e678

--- OpenRa.Game/Sidebar.cs
+++ OpenRa.Game/Sidebar.cs
@@ -9,55 +9,47 @@ using System.Drawing;
 namespace OpenRa.Game
 {
 	using Sprite = SheetRectangle<Sheet>;
+
 	class Sidebar
 	{
 		TechTree.TechTree techTree = new TechTree.TechTree();
 		Renderer renderer;
 
-		FvfVertexBuffer<Vertex> vertexBuffer;
-		IndexBuffer indexBuffer;
-		const int spritesPerBatch = 1024;
+		SpriteRenderer spriteRenderer;
+		Package package;
+
 		Dictionary<string, Sprite> sprites = new Dictionary<string,Sprite>();
 
+		void LoadSprite(string name)
+		{
+			sprites.Add(name, SpriteSheetBuilder.LoadSprite(package, name + "icon.shp"));
+		}
 
 		public Sidebar(Race race, Renderer renderer)
 		{
-			Package package = new Package("../../../hires.mix");
-			sprites.Add("e7", SpriteSheetBuilder.LoadSprite(package, "e7icon.shp"));
-			sprites.Add("e6", SpriteSheetBuilder.LoadSprite(package, "e6icon.shp"));
-			techTree.CurrentRace = race;
 			this.renderer = renderer;
-			vertexBuffer = new FvfVertexBuffer<Vertex>(renderer.Device, 4 * spritesPerBatch, Vertex.Format);
-			indexBuffer = new IndexBuffer(renderer.Device, 6 * spritesPerBatch);
+			this.spriteRenderer = new SpriteRenderer(renderer);
+
+			package = new Package("../../../hires.mix");
+			LoadSprite("E7");
+			LoadSprite("E6");
+			LoadSprite("POWR");
+			techTree.CurrentRace = race;
 		}
 
 		public void Paint(PointF scrollOffset)
 		{
-			List<Vertex> vertices = new List<Vertex>();
-			List<ushort> indicies = new List<ushort>();
-
 			int x = 0, y = 0;
-
-			//foreach (SheetRectangle<Sheet> sprite in sprites.Values)
 			foreach (Item i in techTree.BuildableBuildings)
 			{
 				Sprite sprite;
 				if (!sprites.TryGetValue(i.tag, out sprite)) continue;
 				PointF location = new PointF(x + scrollOffset.X, y + scrollOffset.Y);
-				Util.CreateQuad(vertices, indicies, location, sprite, 0);
+				spriteRenderer.DrawSprite(sprite, location);
 				y += 48;
 			}
 
-			if (vertices.Count == 0)
-				return;
-
-			renderer.DrawWithShader(ShaderQuality.Low, delegate 
-			{
-				vertexBuffer.SetData(vertices.ToArray());
-				indexBuffer.SetData(indicies.ToArray());
-				foreach (Sprite sprite in sprites.Values)
-					renderer.DrawBatch(vertexBuffer, indexBuffer, new Range<int>(0, vertices.Count), new Range<int>(0, indicies.Count), sprite.sheet.Texture);
-			});
+			spriteRenderer.Flush();
 		}
 	}
 }
---
+++ OpenRa.Game/SpriteRenderer.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using BluntDirectX.Direct3D;
+using OpenRa.FileFormats;
+using System.Drawing;
+
+namespace OpenRa.Game
+{
+	class SpriteRenderer
+	{
+		FvfVertexBuffer<Vertex> vertexBuffer;
+		IndexBuffer indexBuffer;
+		Renderer renderer;
+
+		const int spritesPerBatch = 1024;
+
+		List<Vertex> vertices = new List<Vertex>();
+		List<ushort> indicies = new List<ushort>();
+		Sheet currentSheet = null;
+		int sprites = 0;
+
+		public SpriteRenderer(Renderer renderer)
+		{
+			this.renderer = renderer;
+
+			vertexBuffer = new FvfVertexBuffer<Vertex>(renderer.Device, 4 * spritesPerBatch, Vertex.Format);
+			indexBuffer = new IndexBuffer(renderer.Device, 6 * spritesPerBatch);
+		}
+
+		public void Flush()
+		{
+			if (sprites > 0)
+			{
+				renderer.DrawWithShader(ShaderQuality.Low, delegate
+				{
+					vertexBuffer.SetData(vertices.ToArray());
+					indexBuffer.SetData(indicies.ToArray());
+					renderer.DrawBatch(vertexBuffer, indexBuffer,
+						new Range<int>(0, vertices.Count),
+						new Range<int>(0, indicies.Count),
+						currentSheet.Texture);
+				});
+
+				vertices = new List<Vertex>();
+				indicies = new List<ushort>();
+				currentSheet = null;
+				sprites = 0;
+			}
+		}
+
+		public void DrawSprite(SheetRectangle<Sheet> s, PointF location)
+		{
+			if (s.sheet != currentSheet)
+				Flush();
+
+			currentSheet = s.sheet;
+			Util.CreateQuad(vertices, indicies, location, s, 0);
+
+			if (++sprites >= spritesPerBatch)
+				Flush();
+		}
+	}
+}
