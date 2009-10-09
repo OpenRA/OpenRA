382ff42397b7584a81d196bb19830f4c2e471096
Bob <Bob@nameless.(none)>
2009-10-09 18:09:30 +1300

    updated sequences, and some small fixes

--- OpenRa.Game/Controller.cs
+++ OpenRa.Game/Controller.cs
@@ -17,12 +17,16 @@ namespace OpenRa.Game
 			this.game = game;
 		}
 
-		public void WorldClicked(object sender, MouseEventArgs e)
+		public void WorldClicked( object sender, MouseEventArgs e )
 		{
-			var xy = (1 / 24.0f) * (new float2(e.Location) + game.viewport.Location);
-			if (orderGenerator != null)
-				orderGenerator.Order(game, new int2((int)xy.X, (int)xy.Y)).Apply(game);
 			// todo: route all orders through netcode
+			var xy = ( 1 / 24.0f ) * ( new float2( e.Location ) + game.viewport.Location );
+			if( orderGenerator != null )
+			{
+				var order = orderGenerator.Order( game, new int2( (int)xy.X, (int)xy.Y ) );
+				if( order != null )
+					order.Apply( game );
+			}
 		}
 	}
 }
