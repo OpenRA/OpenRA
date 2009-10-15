3deacd8062510be104cdaeb0f136f93bd70f1007
Matthew Bowra-Dean <matthew@ijw.co.nz>
2009-10-15 18:37:24 +1300

    Beginnings of cursor support.

---
+++ OpenRa.Game/Cursor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRa.Game.Graphics;
+using System.IO;
+
+namespace OpenRa.Game
+{
+	public class Cursor
+	{
+		CursorSequence sequence;
+		Cursor(string cursor)
+		{
+			sequence = SequenceProvider.GetCursorSequence(cursor);
+			
+		}
+
+		public static Cursor Default
+		{
+			get { return new Cursor("default"); }
+		}
+
+		public static Cursor Move
+		{
+			get { return new Cursor("move"); }
+		}
+	}
+}
