5fd7f477c1ceb39b4a67fe2e42a963fed6d53cde
Chris Forbes <chrisf@ijw.co.nz>
2009-11-08 19:42:59 +1300

    map buildings no longer get free harvesters

--- OpenRa.Game/Traits/AcceptsOre.cs
+++ OpenRa.Game/Traits/AcceptsOre.cs
@@ -9,16 +9,16 @@ namespace OpenRa.Game.Traits
 	{
 		public AcceptsOre(Actor self)
 		{
-			/* create the free harvester! */
-			Game.world.AddFrameEndTask(
-				w =>
-				{
-					var harvester = new Actor("harv", self.Location + new int2(1, 2), self.Owner);
-					var mobile = harvester.traits.Get<Mobile>();
-					mobile.facing = 64;
-					mobile.InternalSetActivity( new Harvest() );
-					w.Add(harvester);
-				});
+			if (!Game.skipMakeAnims)
+				Game.world.AddFrameEndTask(
+					w =>
+					{		/* create the free harvester! */
+						var harvester = new Actor("harv", self.Location + new int2(1, 2), self.Owner);
+						var mobile = harvester.traits.Get<Mobile>();
+						mobile.facing = 64;
+						mobile.InternalSetActivity(new Harvest());
+						w.Add(harvester);
+					});
 		}
 	}
 }
