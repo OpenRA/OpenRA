8c2194c90304f5b705f86013c2808ffb177bab5a
Alli <alzeih@gmail.com>
2009-12-19 23:36:49 +1300

    Forgot Fake.cs and project file

---
+++ OpenRa.Game/Traits/Fake.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenRa.Game.Traits
+{
+	class Fake
+	{
+	
+	public Fake(Actor self){}
+	
+	}
+}
