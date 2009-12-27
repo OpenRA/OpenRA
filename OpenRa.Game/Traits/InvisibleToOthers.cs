ea7638adfa598f996b0457258d248354d46f69e6
Chris Forbes <chrisf@ijw.co.nz>
2009-12-28 10:23:44 +1300

    mines are invisible to others

---
+++ OpenRa.Game/Traits/InvisibleToOthers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenRa.Game.Traits
+{
+	class InvisibleToOthers : IRenderModifier
+	{
+		public InvisibleToOthers(Actor self) { }
+
+		public IEnumerable<Renderable> ModifyRender(Actor self, IEnumerable<Renderable> r)
+		{
+			return Game.LocalPlayer == self.Owner
+				? r : new Renderable[] { };
+		}
+	}
+}
