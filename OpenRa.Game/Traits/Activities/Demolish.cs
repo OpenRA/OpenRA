ce69c98270424f33bb6956d0fb3e27a5425ef29a
Chris Forbes <chrisf@ijw.co.nz>
2009-12-28 18:58:40 +1300

    start of demolition support

---
+++ OpenRa.Game/Traits/Activities/Demolish.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenRa.Game.Traits.Activities
+{
+	class Demolish : IActivity
+	{
+		Actor target;
+		public IActivity NextActivity { get; set; }
+
+		public Demolish( Actor target )
+		{
+			this.target = target;
+		}
+
+		public IActivity Tick(Actor self)
+		{
+			if (target == null || target.IsDead) return NextActivity;
+
+			// 1. run to adj tile
+			// 2. spawn timed demolition (for +3/4s)
+			// 3. run away --- where?
+			return this;
+		}
+
+		public void Cancel(Actor self) { target = null; NextActivity = null; }
+	}
+}
---
+++ OpenRa.Game/Traits/C4Demolition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRa.Game.Traits.Activities;
+
+namespace OpenRa.Game.Traits
+{
+	class C4Demolition : IOrder
+	{
+		public Order IssueOrder(Actor self, int2 xy, MouseInput mi, Actor underCursor)
+		{
+			if (underCursor == null) return null;
+			if (underCursor.Owner == self.Owner && !mi.Modifiers.HasModifier(Modifiers.Ctrl)) return null;
+			if (!underCursor.traits.Contains<Building>()) return null;
+
+			return new Order("C4", self, underCursor, int2.Zero, null);
+		}
+
+		public void ResolveOrder(Actor self, Order order)
+		{
+			if (order.OrderString == "C4")
+			{
+				self.CancelActivity();
+				self.QueueActivity(new Demolish(order.TargetActor));
+			}
+		}
+	}
+}
