8054292e1718d0d045451c2cecc185137654ac8a
Chris Forbes <chrisf@ijw.co.nz>
2009-12-28 19:49:33 +1300

    engineer stuff

---
+++ OpenRa.Game/Traits/Activities/CaptureBuilding.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenRa.Game.Traits.Activities
+{
+	class CaptureBuilding : IActivity
+	{
+		Actor target;
+
+		public CaptureBuilding(Actor target) { this.target = target; }
+
+		public IActivity NextActivity { get; set; }
+
+		public IActivity Tick(Actor self)
+		{
+			if (target == null || target.IsDead) return NextActivity;
+			// todo: don't waste engineers on things that have been captured 
+			// between order issue time and the time we hit the building.
+
+			target.Health -= EngineerCapture.EngineerDamage;
+			if (target.Health <= 0)
+			{
+				target.Owner = self.Owner;
+				target.Health = EngineerCapture.EngineerDamage;
+			}
+
+			// the engineer is sacrificed.
+			Game.world.AddFrameEndTask(w => w.Remove(self));
+
+			return NextActivity;
+		}
+
+		public void Cancel(Actor self) { target = null; NextActivity = null; }
+	}
+}
---
+++ OpenRa.Game/Traits/EngineerCapture.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRa.Game.Traits.Activities;
+
+namespace OpenRa.Game.Traits
+{
+	class EngineerCapture : IOrder
+	{
+		public const int EngineerDamage = 300;
+
+		public Order IssueOrder(Actor self, int2 xy, MouseInput mi, Actor underCursor)
+		{
+			if (underCursor == null) return null;
+			if (!underCursor.traits.Contains<Building>()) return null;
+			
+			// todo: other bits
+
+			return new Order(underCursor.Health <= EngineerDamage ? "Capture" : "Enter",
+				self, underCursor, int2.Zero, null);
+		}
+
+		public void ResolveOrder(Actor self, Order order)
+		{
+			if (order.OrderString == "Enter" || order.OrderString == "Capture")
+			{
+				self.CancelActivity();
+				self.QueueActivity(new Move(order.TargetActor, 2));
+				self.QueueActivity(new CaptureBuilding(order.TargetActor));
+			}
+		}
+	}
+}
