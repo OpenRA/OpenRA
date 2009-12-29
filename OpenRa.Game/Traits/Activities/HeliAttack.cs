f061c82f4ca2aeb0e06b58232d7c0056d620e541
Chris Forbes <chrisf@ijw.co.nz>
2009-12-29 21:08:23 +1300

    wired up so it actually works

---
+++ OpenRa.Game/Traits/Activities/HeliAttack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenRa.Game.Traits.Activities
+{
+	class HeliAttack : IActivity
+	{
+		Actor target;
+		const int CruiseAltitude = 20;
+		public HeliAttack( Actor target ) { this.target = target; }
+
+		public IActivity NextActivity { get; set; }
+
+		public IActivity Tick(Actor self)
+		{
+			if (target == null || target.IsDead)
+				return NextActivity;
+
+			var limitedAmmo = self.traits.GetOrDefault<LimitedAmmo>();
+			if (limitedAmmo != null && !limitedAmmo.HasAmmo())
+				return NextActivity;
+
+			var unit = self.traits.Get<Unit>();
+
+			if (unit.Altitude != CruiseAltitude)
+			{
+				unit.Altitude += Math.Sign(CruiseAltitude - unit.Altitude);
+				return this;
+			}
+
+			var range = Rules.WeaponInfo[ self.Info.Primary ].Range;
+			var dist = target.CenterLocation - self.CenterLocation;
+
+			var desiredFacing = Util.GetFacing(dist, unit.Facing);
+			Util.TickFacing(ref unit.Facing, desiredFacing, self.Info.ROT);
+
+			if (!float2.WithinEpsilon(float2.Zero, dist, range * Game.CellSize))
+			{
+				var rawSpeed = .2f * Util.GetEffectiveSpeed(self);
+				self.CenterLocation += (rawSpeed / dist.Length) * dist;
+				self.Location = ((1 / 24f) * self.CenterLocation).ToInt2();
+			}
+
+			/* todo: maintain seperation wrt other helis */
+			return this;
+		}
+
+		public void Cancel(Actor self) { target = null; NextActivity = null; }
+	}
+}
---
+++ OpenRa.Game/Traits/AttackHeli.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRa.Game.Traits.Activities;
+
+namespace OpenRa.Game.Traits
+{
+	class AttackHeli : AttackBase
+	{
+		public AttackHeli(Actor self) : base(self) { }
+
+		const int facingTolerance = 20;
+		public override void Tick(Actor self)
+		{
+			base.Tick(self);
+
+			if (target == null) return;
+
+			var unit = self.traits.Get<Unit>();
+			var facingToTarget = Util.GetFacing(target.CenterLocation - self.CenterLocation, unit.Facing);
+
+			if (Math.Abs(facingToTarget - unit.Facing) % 256 < facingTolerance)
+				DoAttack(self);
+		}
+
+		protected override void QueueAttack(Actor self, Order order)
+		{
+			self.CancelActivity();
+			self.QueueActivity(new HeliAttack(order.TargetActor));
+			target = order.TargetActor;
+			// todo: fly home
+		}
+	}
+}
