1694a9c43a898b50058dc2440a4a3916d8077d64
Paul Chote <paul@chote.net>
2010-01-02 02:21:05 -0800

    forgot a file

---
+++ OpenRa.Game/GameRules/UserSettings.cs
@@ -0,0 +1,21 @@
+﻿
+namespace OpenRa.Game.GameRules
+{
+	class UserSettings
+	{
+		public readonly bool UnitDebug = false;
+		public readonly bool BuildingDebug = false;
+		public readonly bool PathDebug = false;
+		public readonly int Timestep = 40;
+		public readonly string Replay = "";
+		public readonly bool UseAftermath = false;
+		public readonly string NetworkHost = "";
+		public readonly int NetworkPort = 0;
+		public readonly int SheetSize = 512;
+		public readonly bool Fullscreen = false;
+		public readonly string Map = "scm12ea.ini";
+		public readonly int Player = 1;
+		public readonly int Width = 0;
+		public readonly int Height = 0;
+	}
+}
