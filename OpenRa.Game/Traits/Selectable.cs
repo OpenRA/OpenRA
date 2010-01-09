68fcd2981620a376a9857bded785edc4060ad20f
Chris Forbes <chrisf@ijw.co.nz>
2010-01-10 12:45:23 +1300

    SelectableInfo

--- OpenRa.Game/Traits/Selectable.cs
+++ OpenRa.Game/Traits/Selectable.cs
@@ -5,6 +5,15 @@ using System.Text;
 
 namespace OpenRa.Game.Traits
 {
+	class SelectableInfo : ITraitInfo
+	{
+		public readonly int Priority = 10;
+		public readonly int[] Bounds = null;
+		public readonly string Voice = "GenericVoice";
+
+		public object Create(Actor self) { return new Selectable(self); }
+	}
+
 	class Selectable
 	{
 		public Selectable( Actor self ) { }
