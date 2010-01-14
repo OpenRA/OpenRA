c338876b3483915035262f6cedaa041e227ae22f
Chris Forbes <chrisf@ijw.co.nz>
2010-01-14 20:17:45 +1300

    session objects

---
+++ OpenRa.FileFormats/Session.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenRa.FileFormats
+{
+	public class Session
+	{
+		public List<Client> Clients = new List<Client>();
+		public string Map = "scm12ea.ini";
+		// public string[] Mods = { };
+	}
+
+	public enum ClientState
+	{
+		NotReady,
+		// Downloading,
+		// Uploading,
+		Ready
+	}
+
+	public class Client
+	{
+		public int Index;
+		public int Palette;
+		public int Race;
+		// public int SpawnPoint;
+		public string Name;
+		public ClientState State;
+	}
+}
