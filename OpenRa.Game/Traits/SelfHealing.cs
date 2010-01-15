3521f1cc45155cf1821fd1ae1172d1562726aead
Chris Forbes <chrisf@ijw.co.nz>
2010-01-16 11:30:31 +1300

    SelfHealing trait

---
+++ OpenRa.Game/Traits/SelfHealing.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRa.Game.GameRules;
+
+namespace OpenRa.Game.Traits
+{
+	class SelfHealingInfo : ITraitInfo
+	{
+		public readonly int Step = 5;
+		public readonly int Ticks = 5;
+		public readonly float HealIfBelow = .5f;
+
+		public object Create(Actor self) { return new SelfHealing(); }
+	}
+
+	class SelfHealing : ITick
+	{
+		int ticks;
+
+
+		public void Tick(Actor self)
+		{
+			var info = self.Info.Traits.Get<SelfHealingInfo>();
+
+			if ((float)self.Health / self.GetMaxHP() >= info.HealIfBelow)
+				return;
+
+			if (--ticks <= 0)
+			{
+				ticks = info.Ticks;
+				self.InflictDamage(self, -info.Step, Rules.WarheadInfo["Super"]);
+			}
+		}
+	}
+}
