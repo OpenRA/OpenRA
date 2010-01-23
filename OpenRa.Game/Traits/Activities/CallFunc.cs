0d7a1ef2519fa5947f67bf6d95aef21ff0604128
Chris Forbes <chrisf@ijw.co.nz>
2010-01-23 13:14:12 +1300

    Added CallFunc activity

---
+++ OpenRa.Game/Traits/Activities/CallFunc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenRa.Traits.Activities
+{
+	public class CallFunc : IActivity
+	{
+		Action a;
+		public IActivity NextActivity { get; set; }
+
+		public IActivity Tick(Actor self)
+		{
+			if (a != null) a();
+			return NextActivity;
+		}
+
+		public void Cancel(Actor self) { a = null; NextActivity = null; }
+	}
+}
