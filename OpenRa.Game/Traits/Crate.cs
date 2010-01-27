d474f06ea96260b6170c8523de5809daac88f3d6
Paul Chote <paul@chote.net>
2010-01-27 19:46:52 +1300

    Crates: Part 1

---
+++ OpenRa.Game/Traits/Crate.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OpenRa.Effects;
+using OpenRa.Traits;
+
+namespace OpenRa.Traits
+{
+	class CrateInfo : ITraitInfo
+	{
+		public readonly int Lifetime = 5; // Seconds
+		public object Create(Actor self) { return new Crate(self); }
+	}
+
+	class Crate : ICrushable, IOccupySpace, ITick
+	{
+		readonly Actor self;
+		int ticks;
+		public Crate(Actor self)
+		{
+			this.self = self;
+			self.World.UnitInfluence.Add(self, this);
+		}
+
+		public void OnCrush(Actor crusher)
+		{
+			// TODO: Do Stuff
+			
+			
+			self.World.AddFrameEndTask(w =>	w.Remove(self));
+		}
+
+		public bool IsPathableCrush(UnitMovementType umt, Player player)
+		{
+			return true;
+		}
+
+		public bool IsCrushableBy(UnitMovementType umt, Player player)
+		{
+			return true;
+		}
+
+		public IEnumerable<int2> OccupiedCells() { yield return self.Location; }
+		
+		public void Tick(Actor self)
+		{
+			if (++ticks >= self.Info.Traits.Get<CrateInfo>().Lifetime * 25)
+			{
+				self.World.AddFrameEndTask(w =>	w.Remove(self));
+			}
+		}
+	}
+}
---
+++ OpenRa.Game/Traits/CrateSpawnPower.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRa.Orders;
+
+namespace OpenRa.Traits
+{
+	class CrateSpawnPowerInfo : SupportPowerInfo
+	{
+		public readonly float Duration = 0f;
+		public override object Create(Actor self) { return new CrateSpawnPower(self, this); }
+	}
+
+	class CrateSpawnPower : SupportPower, IResolveOrder
+	{
+		public CrateSpawnPower(Actor self, CrateSpawnPowerInfo info) : base(self, info) { }
+
+		protected override void OnBeginCharging() {}
+		protected override void OnFinishCharging() {}
+		protected override void OnActivate()
+		{
+			Game.controller.orderGenerator = new SelectTarget();
+		}
+
+		public void ResolveOrder(Actor self, Order order)
+		{
+			if (order.OrderString == "SpawnCrate")
+			{
+				self.World.AddFrameEndTask(
+					w => w.CreateActor("crate", order.TargetLocation, self.Owner));
+					
+				Game.controller.CancelInputMode();
+				FinishActivate();
+			}
+		}
+
+		class SelectTarget : IOrderGenerator
+		{
+			public SelectTarget() { }
+
+			public IEnumerable<Order> Order(World world, int2 xy, MouseInput mi)
+			{
+				if (mi.Button == MouseButton.Right)
+					Game.controller.CancelInputMode();
+
+				return OrderInner(world, xy, mi);
+			}
+
+			IEnumerable<Order> OrderInner(World world, int2 xy, MouseInput mi)
+			{
+				if (mi.Button == MouseButton.Left)
+				{
+					var loc = mi.Location + Game.viewport.Location;
+					var underCursor = world.FindUnits(loc, loc).FirstOrDefault();
+
+					if (underCursor == null)
+						yield return new Order("SpawnCrate", world.LocalPlayer.PlayerActor, xy);
+				}
+
+				yield break;
+			}
+
+			public void Tick(World world) {	}
+
+			public void Render(World world) { }
+
+			public Cursor GetCursor(World world, int2 xy, MouseInput mi)
+			{
+				mi.Button = MouseButton.Left;
+				return OrderInner(world, xy, mi).Any()
+					? Cursor.Ability : Cursor.MoveBlocked;
+			}
+		}
+	}
+}
