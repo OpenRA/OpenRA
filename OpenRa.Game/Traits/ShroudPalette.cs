302af4edce6034c2c7148aa971804919fb737233
Paul Chote <paul@chote.net>
2010-02-03 23:33:02 +1300

    Forgot a file

---
+++ OpenRa.Game/Traits/ShroudPalette.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using OpenRa.FileFormats;
+
+namespace OpenRa.Traits
+{
+	class ShroudPaletteInfo : ITraitInfo
+	{
+		public object Create(Actor self) { return new ShroudPalette(self); }
+	}
+
+	class ShroudPalette
+	{
+		public ShroudPalette(Actor self)
+		{
+				// TODO: This shouldn't rely on a base palette
+				var wr = self.World.WorldRenderer;
+				var pal = wr.GetPalette("terrain");
+				wr.AddPalette("shroud", new Palette(pal, new ShroudPaletteRemap()));
+		}
+	}
+}
