c78ce442c6531df263a612a0af641fb150ce4ef6
Chris Forbes <chrisf@ijw.co.nz>
2010-02-06 09:42:54 +1300

    rude hack to make snow maps for c&c work

--- OpenRa.Game/Traits/PaletteFromFile.cs
+++ OpenRa.Game/Traits/PaletteFromFile.cs
@@ -8,7 +8,7 @@ namespace OpenRa.Traits
 	class PaletteFromFileInfo : ITraitInfo
 	{
 		public readonly string Name = null;
-		public readonly string Theatre = null;
+		public readonly string Theater = null;
 		public readonly string Filename = null;
 		public object Create(Actor self) { return new PaletteFromFile(self, this); }
 	}
@@ -17,8 +17,8 @@ namespace OpenRa.Traits
 	{
 		public PaletteFromFile(Actor self, PaletteFromFileInfo info)
 		{
-			if (info.Theatre == null || 
-				info.Theatre.ToLowerInvariant() == self.World.Map.Theater.ToLowerInvariant())
+			if (info.Theater == null || 
+				info.Theater.ToLowerInvariant() == self.World.Map.Theater.ToLowerInvariant())
 			{
 				//Log.Write("Loading palette {0} from file {1}", info.Name, info.Filename);
 				self.World.WorldRenderer.AddPalette(info.Name, new Palette(FileSystem.Open(info.Filename)));
