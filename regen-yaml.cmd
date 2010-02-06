da930fbc140df2cff70e43a481826174fc547b94
Chris Forbes <chrisf@ijw.co.nz>
2010-02-07 01:21:16 +1300

    RC is dead.

--- regen-yaml.cmd
+++
@@ -1,6 +0,0 @@
-pushd mods\ra\
-..\..\RulesConverter\bin\debug\RulesConverter.exe units.ini rules.ini trees.ini campaignUnits.ini rules.yaml
-popd
-pushd mods\aftermath\
-..\..\RulesConverter\bin\debug\RulesConverter.exe aftermathUnits.ini aftrmath.ini rules.yaml
-popd
