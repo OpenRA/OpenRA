b7ade669fa6790e32181cc371184ebae7372884d
Chris Forbes <chrisf@ijw.co.nz>
2010-02-10 14:51:26 +1300

    added Country/CountryInfo trait for World

---
+++ OpenRa.Game/Traits/World/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenRa.Traits
+{
+	class CountryInfo : ITraitInfo
+	{
+		public readonly string Name = null;
+		public readonly string Race = null;
+
+		/* todo: icon,... */
+
+		public object Create(Actor self) { return new CountryInfo(); }
+	}
+
+	class Country { /* we're only interested in the Info */ }
+}
