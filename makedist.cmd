496e8f29e659504514d1fedcb2315a167d41eede
Chris Forbes <chrisf@ijw.co.nz>
2010-02-16 20:09:24 +1300

    cleaning up old dead bits

--- makedist.cmd
+++ makedist.cmd
@@ -4,17 +4,13 @@
 @copy *.mix ..\openra-bin\
 :nomix
 @copy *.ini ..\openra-bin\
-@copy *.rem ..\openra-bin\
 @copy *.til ..\openra-bin\
 @copy INSTALL ..\openra-bin\
 @copy LICENSE ..\openra-bin\
 @copy *.fx ..\openra-bin\
-@copy *.xml ..\openra-bin\
-@copy specialbin.png ..\openra-bin\
 @copy OpenRA.Server\bin\debug\OpenRA.Server.exe ..\openra-bin\
 @copy SequenceEditor\bin\debug\SequenceEditor.exe ..\openra-bin\
 @copy OpenRA.Game\bin\debug\*.dll ..\openra-bin\
 @copy OpenRA.Game\bin\debug\OpenRa.Game.exe ..\openra-bin\
 @xcopy /E mods ..\openra-bin\mods\
-@copy *.yaml ..\openra-bin\
 @copy bogus.* ..\openra-bin\
