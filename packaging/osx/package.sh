043429a5f33d3658792b11d143da02c34a58b778
pchote <paul@chote.net>
2010-02-21 23:15:54 +1300

    Package Cg and SDL frameworks inside the app bundle; Only external dependency is mono itself

--- packaging/osx/package.sh
+++ packaging/osx/package.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # OpenRA Packaging script for osx
 #   Creates a .app bundle for OpenRA game, and a command line app for OpenRa server
-#   Statically links all custom dlls into the executable, but requires the Mono and
-#   Cg frameworks installed on the target machine for these binaries to run
+#   Statically links all custom dlls into the executable; only requires Mono
+#   to run on a non-development machine
 
 # List of game files to copy into the app bundle
 # TODO: This will be significantly shorter once we move the ra files into its mod dir
@@ -17,8 +17,13 @@ export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/Library/Frameworks/Mono.framework/Versi
 mkbundle --deps --static -z -o openra_server OpenRA.Server.exe OpenRa.FileFormats.dll
 
 # Package the game binary
-mkbundle --deps --static -z -o OpenRA OpenRa.Game.exe OpenRa.FileFormats.dll thirdparty/Tao/Tao.Cg.dll thirdparty/Tao/Tao.OpenGl.dll thirdparty/Tao/Tao.OpenAl.dll thirdparty/Tao/Tao.FreeType.dll thirdparty/Tao/Tao.Sdl.dll thirdparty/Tao.Externals.dll thirdparty/ISE.FreeType.dll OpenRa.Gl.dll
+mkbundle --deps --static -z -o OpenRA OpenRa.Game.exe OpenRa.Gl.dll OpenRa.FileFormats.dll thirdparty/Tao/Tao.Cg.dll thirdparty/Tao/Tao.OpenGl.dll thirdparty/Tao/Tao.OpenAl.dll thirdparty/Tao/Tao.FreeType.dll thirdparty/Tao/Tao.Sdl.dll thirdparty/Tao.Externals.dll thirdparty/ISE.FreeType.dll
 
-# Copy everything into our game bundle template
+# Copy game files into our game bundle template
 cp -R packaging/osx/OpenRA.app .
 cp -R $GAME_FILES OpenRA.app/Contents/Resources/
+
+# Copy frameworks into our game bundle template
+mkdir OpenRa.app/Contents/Frameworks/
+cp -R /Library/Frameworks/Cg.Framework OpenRa.app/Contents/Frameworks/
+cp -R /Library/Frameworks/SDL.Framework OpenRa.app/Contents/Frameworks/
