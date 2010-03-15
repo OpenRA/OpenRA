f1841021c4681e8de8a7a8a7cc491c9ec2398030
Chris Forbes <chrisf@ijw.co.nz>
2010-03-15 20:51:57 +1300

    added MasterServerQuery. downloads the server list and constructs IEnumerable<GameServer> from it.

---
+++ OpenRA.Game/Server/MasterServerQuery.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using OpenRA.FileFormats;
+
+namespace OpenRA.Server
+{
+	static class MasterServerQuery
+	{
+		public static IEnumerable<GameServer> GetGameList(string masterServerUrl)
+		{
+			var wc = new WebClient();
+			var data = wc.DownloadData(masterServerUrl + "list.php");
+			var str = Encoding.UTF8.GetString(data);
+
+			var yaml = MiniYaml.FromString(str);
+			return yaml.Select(a => { var gs = new GameServer(); FieldLoader.Load(gs, a.Value); return gs; });
+		}
+	}
+
+	class GameServer
+	{
+		public readonly string Name = null;
+		public readonly string Address = null;
+		public readonly int State = 0;
+		public readonly int Players = 0;
+		public readonly string Map = null;
+		public readonly string[] Mods = { };
+		public readonly int TTL = 0;
+	}
+}
