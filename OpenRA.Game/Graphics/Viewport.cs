32b0ea796829da402752c4d0fbb00346f6ce7b80
Chris Forbes <chrisf@ijw.co.nz>
2010-03-16 20:41:10 +1300

    fix artifacts from screen shake

--- OpenRA.Game/Graphics/Viewport.cs
+++ OpenRA.Game/Graphics/Viewport.cs
@@ -68,7 +68,7 @@ namespace OpenRA.Graphics
 			float2 r1 = new float2(2, -2) / screenSize;
 			float2 r2 = new float2(-1, 1);
 
-			renderer.BeginFrame(r1, r2, scrollPosition);
+			renderer.BeginFrame(r1, r2, scrollPosition.ToInt2());
 
 			if( Game.orderManager.GameStarted )
 			{
