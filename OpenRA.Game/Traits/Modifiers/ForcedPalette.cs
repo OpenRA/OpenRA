12bec40b9ae2110817091e84ba1887d707f989bb
Chris Forbes <chrisf@ijw.co.nz>
2010-03-17 18:49:52 +1300

    add ForcedPalette trait (RenderModifier). this can be used for things like c17/mine/trees

---
+++ OpenRA.Game/Traits/Modifiers/ForcedPalette.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenRA.Traits
+{
+	class ForcedPaletteInfo : ITraitInfo
+	{
+		public readonly string Palette = null;
+		public object Create(Actor self) { return new ForcedPalette(this); }
+	}
+
+	class ForcedPalette : IRenderModifier
+	{
+		string palette;
+
+		public ForcedPalette(ForcedPaletteInfo info) { palette = info.Palette; }
+
+		public IEnumerable<Renderable> ModifyRender(Actor self, IEnumerable<Renderable> r)
+		{
+			return r.Select(rr => rr.WithPalette(palette));
+		}
+	}
+}
