687b8547f543cd28004e5e5a0c0919a829d87ea2
alzeih <alzeih@gmail.com>
2010-03-18 18:31:11 +1300

    Unit on Build is Generic. (half way there)

--- OpenRA.Game/Traits/OreRefinery.cs
+++ OpenRA.Game/Traits/OreRefinery.cs
@@ -1,4 +1,4 @@
-﻿#region Copyright & License Information
+#region Copyright & License Information
 /*
  * Copyright 2007,2009,2010 Chris Forbes, Robert Pepperell, Matthew Bowra-Dean, Paul Chote, Alli Witheford.
  * This file is part of OpenRA.
@@ -33,15 +33,6 @@ namespace OpenRA.Traits
 		public OreRefinery(Actor self)
 		{
 			this.self = self;
-			self.World.AddFrameEndTask(
-				w =>
-				{		/* create the free harvester! */
-					var harvester = w.CreateActor("harv", self.Location 
-						+ new int2(1, 2), self.Owner);
-					var unit = harvester.traits.Get<Unit>();
-					unit.Facing = 64;
-					harvester.QueueActivity(new Harvest());
-				});
 		}
 		public int2 DeliverOffset {	get { return new int2(1, 2); } }
 		public void OnDock(Actor harv, DeliverOre dockOrder)
