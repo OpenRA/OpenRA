df336aee700c695c0360ca4b5eb0b3cb82790b72
alzeih <alzeih@gmail.com>
2010-04-02 13:03:59 +1300

    Move default values into GlobalDefaultsInfo, add to cnc.

--- OpenRA.Game/Traits/World/GlobalDefaults.cs
+++ OpenRA.Game/Traits/World/GlobalDefaults.cs
@@ -6,79 +6,79 @@ namespace OpenRA.Traits
 	public class GlobalDefaultsInfo : StatelessTraitInfo<GlobalDefaults>
 	{
 		/* Special Weapons */
-		public readonly float GapRegenInterval =0;
+		public readonly float GapRegenInterval = 0.1f;
 		public readonly int BadgerBombCount = 1;
 
 		/* Chrono Side Effects */
-		public readonly float QuakeChance = 0;
-		public readonly float QuakeDamage = 0;		/* percent */
-		public readonly float VortexChance = 0;
-		public readonly int VortexDamage = 0;
-		public readonly int VortexRange = 0;
-		public readonly int VortexSpeed = 0;
+		public readonly float QuakeChance = 0.2f;
+		public readonly float QuakeDamage = 0.33f;		/* percent */
+		public readonly float VortexChance = 0.2f;
+		public readonly int VortexDamage = 200;
+		public readonly int VortexRange = 10;
+		public readonly int VortexSpeed = 10;
 		
 		/* Repair & Refit */
-		public readonly float RefundPercent = 0;
-		public readonly float ReloadRate = 0;
-		public readonly float RepairPercent = 0;
-		public readonly float RepairRate = 0;
-		public readonly int RepairStep = 0;
-		public readonly float URepairPercent = 0;
-		public readonly int URepairStep = 0;
+		public readonly float RefundPercent = 0.5f;
+		public readonly float ReloadRate = 0.04f;
+		public readonly float RepairPercent = 0.2f;
+		public readonly float RepairRate = 0.016f;
+		public readonly int RepairStep = 7;
+		public readonly float URepairPercent = 0.2f;
+		public readonly int URepairStep = 10;
 		
 		/* Combat & Damage */
 		public readonly float TurboBoost = 1.5f;
-		public readonly float BallisticScatter = 0;
-		public readonly float ExpSpread = 0;
-		public readonly int FireSupress = 0;
-		public readonly float HomingScatter = 0;
-		public readonly int MaxDamage = 0;
-		public readonly int MinDamage = 0;
+		public readonly float BallisticScatter = 1.0f;
+		public readonly float ExpSpread = 0.3f;
+		public readonly int FireSupress = 1;
+		public readonly float HomingScatter = 2.0f;
+		public readonly int MaxDamage = 1000;
+		public readonly int MinDamage = 1;
 		public readonly bool OreExplosive = false;
 		public readonly bool PlayerAutoCrush = false;
 		public readonly bool PlayerReturnFire = false;
 		public readonly bool PlayerScatter = false;
 		public readonly bool TreeTargeting = false;
-		public readonly int Incoming = 0;
+		public readonly int Incoming = 10;
 		
 		/* Income & Production */
-		public readonly float BuildSpeed = 0;
-		public readonly float BuildupTime = 0;
-		public readonly float OreTruckRate = 0;
-		public readonly bool SeparateAircraft = true;
-		public readonly float SurvivorRate = 0;
+		public readonly float BuildSpeed = 0.4f;
+		public readonly float BuildupTime = 0.06f;
+		public readonly float OreTruckRate = 1;
+		public readonly bool SeparateAircraft = false;
+		public readonly float SurvivorRate = 0.4f;
 		
 		/* Audo/Visual Map Controls */
 		public readonly bool AllyReveal = true;
-		public readonly float ConditionRed = 0;
-		public readonly float ConditionYellow = 0;
-		public readonly int DropZoneRadius = 0;
+		public readonly float ConditionRed = 0.25f;
+		public readonly float ConditionYellow = 0.5f;
+		public readonly int DropZoneRadius = 4;
 		public readonly bool EnemyHealth = true;
-		public readonly int Gravity = 0;
-		public readonly float IdleActionFrequency = 0;
-		public readonly float MessageDelay = 0;
-		public readonly float MovieTime = 0;
+		public readonly int Gravity = 3;
+		public readonly float IdleActionFrequency = 0.1f;
+		public readonly float MessageDelay = 0.6f;
+		public readonly float MovieTime = 0.06f;
 		public readonly bool NamedCivilians = false;
-		public readonly float SavourDelay = 0;
+		public readonly float SavourDelay = 0.03f;
 		
-		public readonly int SpeakDelay = 0;
-		public readonly int TimerWarning = 0;
-		public readonly bool FlashLowPower = false;
+		public readonly int SpeakDelay = 2;
+		public readonly int TimerWarning = 2;
+		public readonly bool FlashLowPower = true;
 
 		/* Computer & Movement Controls */
 		public readonly bool CurleyShuffle = false;
-		public readonly float BaseBias = 0;
-		public readonly float BaseDefenseDelay = 0;
-		public readonly float CloseEnough = 0;
-		public readonly int DamageDelay = 0;
-		public readonly int GameSpeeBias = 0;
-		public readonly int LZScanRadius = 0;
-		public readonly bool MineAware = false;
-		public readonly float Stray = 0;
-		public readonly float SubmergeDelay = 0;
-		public readonly float SuspendDelay = 0;
-		public readonly int SuspendPriority = 0;
-		public readonly float TeamDelay = 0;
+		public readonly float BaseBias = 2.0f;
+		public readonly float BaseDefenseDelay = 0.25f;
+		public readonly float CloseEnough = 2.75f;
+		public readonly int DamageDelay = 1;
+		public readonly int GameSpeeBias = 1;
+		public readonly int LZScanRadius = 16;
+		public readonly bool MineAware = true;
+		public readonly float Stray = 2.0f;
+		public readonly float SubmergeDelay = 0.02f;
+		public readonly float SuspendDelay = 2.0f;
+		public readonly int SuspendPriority = 20;
+		public readonly float TeamDelay = 0.6f;
 		
 		public readonly int LowPowerSlowdown = 3;
 	}
