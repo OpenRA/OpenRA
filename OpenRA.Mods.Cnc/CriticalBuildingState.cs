c14654fc6c0858f2c58ee925055c160194dc5039
Chris Forbes <chrisf@ijw.co.nz>
2010-04-05 12:41:16 +1200

    #87 CNC buildings should show Critical anim while dying

---
+++ OpenRA.Mods.Cnc/CriticalBuildingState.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRA.Traits;
+using OpenRA.Effects;
+
+namespace OpenRA.Mods.Cnc
+{
+	class CriticalBuildingStateInfo : ITraitInfo
+	{
+		public readonly int LingerTime = 20;
+		public object Create(Actor self) { return new CriticalBuildingState(self, this); }
+	}
+
+	class CriticalBuildingState : INotifyDamage
+	{
+		CriticalBuildingStateInfo info;
+
+		public CriticalBuildingState(Actor self, CriticalBuildingStateInfo info)
+		{
+			this.info = info;
+			self.RemoveOnDeath = false;
+		}
+
+		public void Damaged(Actor self, AttackInfo e)
+		{
+			if (e.DamageStateChanged && e.DamageState == DamageState.Dead)
+			{
+				self.traits.Get<RenderSimple>().anim.PlayRepeating("critical-idle");
+				self.World.AddFrameEndTask(
+					w => w.Add(
+						new DelayedAction(info.LingerTime,
+							() => w.Remove(self))));
+			}
+		}
+	}
+}
