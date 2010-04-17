645362fe9375a417a833719eb6faef640d7f4ad4
Chris Forbes <chrisf@ijw.co.nz>
2010-04-17 12:28:32 +1200

    map actor id -> live actor dict for map scripting purposes

--- OpenRA.Game/Traits/World/SpawnMapActors.cs
+++ OpenRA.Game/Traits/World/SpawnMapActors.cs
@@ -9,12 +9,14 @@ namespace OpenRA.Traits
 
 	class SpawnMapActors : IGameStarted
 	{
+		public Dictionary<string, Actor> MapActors = new Dictionary<string, Actor>();
+
 		public void GameStarted(World world)
 		{
 			Game.skipMakeAnims = true;		// rude hack
-			// TODO: Keep a dictionary of actor reference -> actor somewhere for scripting purposes
+
 			foreach (var actorReference in world.Map.Actors)
-				world.CreateActor(actorReference.Value.Name, actorReference.Value.Location,
+				MapActors[actorReference.Key] = world.CreateActor(actorReference.Value.Name, actorReference.Value.Location,
 					world.players.Values.FirstOrDefault(p => p.InternalName == actorReference.Value.Owner)
 					?? world.NeutralPlayer);
 
