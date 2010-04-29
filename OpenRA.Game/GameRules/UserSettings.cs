0c31e1c8a1f28ec3534735c909d7b2f289891ec5
Chris Forbes <chrisf@ijw.co.nz>
2010-04-29 17:21:01 +1200

    sync report on by default

--- OpenRA.Game/GameRules/UserSettings.cs
+++ OpenRA.Game/GameRules/UserSettings.cs
@@ -27,7 +27,7 @@ namespace OpenRA.GameRules
 		public bool PathDebug = false;
 		public bool PerfDebug = true;
 		public bool IndexDebug = false;
-		public bool RecordSyncReports = false;
+		public bool RecordSyncReports = true;
 		
 		// Window settings
 		public readonly int Width = 0;
