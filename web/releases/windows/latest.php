4ddc63f6bff9a479fa4eca1ccd543a758dc60c8d
Matthew Bowra-Dean <matthew@ijw.co.nz>
2010-05-03 14:04:42 +1200

    Some client side stats and platform detection + updates to server side script to support download stats.

--- web/releases/windows/latest.php
+++ web/releases/windows/latest.php
@@ -1,4 +1,7 @@
 <?php
 $latest = "OpenRA-20100425.exe";
 header("Location: $latest");
+$file = file_get_contents("../../downloads.txt");
+$new_downloads = $file + 1;
+file_put_contents("../../downloads.txt", $new_downloads);
 ?>
