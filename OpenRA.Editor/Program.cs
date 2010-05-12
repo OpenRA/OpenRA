8cd38812d90a221e798a1bc717c23a979337887f
Chris Forbes <chrisf@ijw.co.nz>
2010-05-12 17:41:37 +1200

    make editor work in non-en locales

--- OpenRA.Editor/Program.cs
+++ OpenRA.Editor/Program.cs
@@ -1,18 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Globalization;
 using System.Windows.Forms;
 
 namespace OpenRA.Editor
 {
 	static class Program
 	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
 		[STAThread]
 		static void Main( string[] args )
 		{
+			Application.CurrentCulture = CultureInfo.InvariantCulture;
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new Form1(args));
