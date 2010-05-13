8aad7f8350d5fe9e09d657a4083d480c922cf509
Bob <ytinasni@gmail.com>
2010-05-13 17:55:04 +1200

    make infantry die instantly to tesla

---
+++ OpenRA.Mods.RA/TeslaInstantKills.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.RA
+{
+	class TeslaInstantKills : INotifyDamage
+	{
+		public void Damaged( Actor self, AttackInfo e )
+		{
+			if( e.Warhead.InfDeath == 5 )
+				self.Health = 0;
+		}
+	}
+}
