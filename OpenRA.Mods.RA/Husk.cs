7e754a5590a69c2e0b6fd215e7b3dff02a3d72b3
Chris Forbes <chrisf@ijw.co.nz>
2010-05-16 16:30:49 +1200

    husks should not be mobile

---
+++ OpenRA.Mods.RA/Husk.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.RA
+{
+	class HuskInfo : ITraitInfo { public object Create(Actor self) { return new Husk(self); } }
+
+	class Husk : IOccupySpace
+	{
+		Actor self;
+		public Husk(Actor self) 
+		{
+			this.self = self;
+			self.World.WorldActor.traits.Get<UnitInfluence>().Add(self, this);
+		}
+
+		public IEnumerable<int2> OccupiedCells() { yield return self.Location; }
+	}
+}
