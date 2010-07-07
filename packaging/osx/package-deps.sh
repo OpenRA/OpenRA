9d70bb24a6b7334b02c8b6e861020227ebd754c8
Paul Chote <paul@chote.net>
2010-07-07 21:45:46 +1200

    Package osx dependencies separately from the main download; Game is now run from inside app bundle; Support dir used to cache downloads only

--- packaging/osx/package-deps.sh
+++ packaging/osx/package-deps.sh
@@ -1,30 +1,26 @@
 #!/bin/sh
 # OpenRA Packaging script for osx
-#    Creates a self contained app bundle that contains
-#    all the dependencies required to run the game.
+#    Packages all the dependencies required to run the game.
 #       This script assumes that it is being run on osx >= 10.5
 #    and that all the required dependencies are installed
 #    and the dependant dlls exist in the system GAC.
-#       $GAME_PATH in OpenRA.app/Contents/MacOS/OpenRA
-#    specifies the game directory to load. This can point
-#    anywhere on the filesystem.
 
 # A list of the binaries that we want to be able to run
 DEPS_LOCAL="OpenRA.Game.exe OpenRA.Gl.dll OpenRA.FileFormats.dll"
-PACKAGING_DIR="osxbuild/launcher"
+PWD=`pwd`
+PACKAGING_PATH="$PWD/osxbuild"
+BINARY_PATH="$PACKAGING_PATH/deps"
+LIB_PATH="$BINARY_PATH/lib"
 SYSTEM_MONO="/Library/Frameworks/Mono.framework/Versions/2.6.3"
 
 # dylibs referred to by dlls in the gac; won't show up to otool
 GAC_DYLIBS="$SYSTEM_MONO/lib/libMonoPosixHelper.dylib $SYSTEM_MONO/lib/libgdiplus.dylib "
 
 ####################################################################################
-EXE_DIR="$PACKAGING_DIR/OpenRA.app/Contents/MacOS"
-LIB_DIR="$EXE_DIR/lib"
 
 function patch_mono {
 	echo "Patching binary: "$1
 	LIBS=$( otool -L $1 | grep /Library/Frameworks/Mono.framework/ | awk {'print $1'} )
-	# Todo: fix the -id?
 	for i in $LIBS; do
 		install_name_tool -change $i @executable_path/lib/`basename $i` $1
 	done
@@ -37,9 +33,9 @@ function patch_mono {
 	
 	for i in $LIBS; do
 		FILE=`basename $i`
-		if [ ! -e $LIB_DIR/$FILE ]; then
-			cp $i $LIB_DIR
-			patch_mono $LIB_DIR/$FILE
+		if [ ! -e $LIB_PATH/$FILE ]; then
+			cp $i $LIB_PATH
+			patch_mono $LIB_PATH/$FILE
 		fi
 	done
 }
@@ -51,36 +47,29 @@ export CC="gcc -arch i386 -mmacosx-version-min=10.5 -isysroot /Developer/SDKs/Ma
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/Library/Frameworks/Mono.framework/Versions/Current/lib/pkgconfig/
 export PATH=/sw/bin:/sw/sbin:$PATH
 
-# Copy and patch mono
-echo "Creating app bundle..."
-
 
 # Create the directory tree and copy in our existing files
-mkdir -p $PACKAGING_DIR/OpenRA.app
-cp -r ./packaging/osx/OpenRA.app/* $PACKAGING_DIR/OpenRA.app/
-mkdir -p $LIB_DIR
+mkdir -p "$LIB_PATH"
+cp "$SYSTEM_MONO/bin/mono" "$BINARY_PATH"
+patch_mono "$BINARY_PATH/mono"
 
-
-cp "$SYSTEM_MONO/bin/mono" $EXE_DIR
-patch_mono "$EXE_DIR/mono"
-
-# Copy the gac dylibs into the app bundle
+# Copy the gac dylibs=
 for i in $GAC_DYLIBS; do
-	cp $i $LIB_DIR
-	patch_mono $LIB_DIR/`basename $i`
+	cp $i $LIB_PATH
+	patch_mono $LIB_PATH/`basename $i`
 done
 
-
 # Find the dlls that are used by the game; copy them into the app bundle and patch/package any dependencies
-echo "Searching for dlls..."
+echo "Searching for dlls... (this will take a while)"
 
 # This is a huge hack, but it works
-DLLS=`mkbundle --deps --static -z -c -o OpenRA $DEPS_LOCAL | grep "embedding: "`
+DLLS=`mkbundle --deps -c -o "$PACKAGING_PATH/bogus" $DEPS_LOCAL | grep "embedding: "`
+rm "$PACKAGING_PATH/bogus"
 for i in $DLLS; do
   	if [ "$i" != "embedding:" ]; then
-		cp $i $LIB_DIR
+		cp $i $LIB_PATH
 		if [ -e "$i.config" ]; then
-			CONFIG=$LIB_DIR/"`basename $i`.config"
+			CONFIG=$LIB_PATH/"`basename $i`.config"
 			
 			echo "Patching config: $CONFIG"
 			# Remove any references to the hardcoded mono framework; the game will look in the right location anyway
@@ -94,18 +83,22 @@ done
 
 # Remove the files that we want to run that we accidentally copied over
 for i in $DEPS_LOCAL; do
-	rm "$LIB_DIR/$i"
+	rm "$LIB_PATH/$i"
 done
 
 
 # Copy external frameworks
 echo "Copying Cg..."
-cp /Library/Frameworks/Cg.framework/Cg $LIB_DIR
-chmod 755 $LIB_DIR/Cg
+cp -X /Library/Frameworks/Cg.framework/Cg $LIB_PATH
+chmod 755 $LIB_PATH/Cg
 
 echo "Copying SDL..."
-cp /Library/Frameworks/SDL.framework/SDL $LIB_DIR
-xattr -d com.apple.quarantine $LIB_DIR/SDL
-chmod 755 $LIB_DIR/SDL
+cp -X /Library/Frameworks/SDL.framework/SDL $LIB_PATH
+chmod 755 $LIB_PATH/SDL
+
+cd "$BINARY_PATH"
+zip osx-deps-v1 -r -9 *
+mv osx-deps-v1.zip "$PACKAGING_PATH"
+rm -rf "$BINARY_PATH"
 
 echo "All Done!"
--- packaging/osx/package-game.sh
+++ packaging/osx/package-game.sh
@@ -5,35 +5,29 @@
 
 PWD=`pwd`
 PACKAGING_PATH="$PWD/osxbuild"
-LAUNCHER_PATH="$PACKAGING_PATH/launcher/OpenRA.app"
 SOURCE_PATH="$PWD/."
+BUNDLE_PATH="$SOURCE_PATH/packaging/osx/OpenRA.app"
+TARGET_PATH="$PACKAGING_PATH/OpenRA.app/Contents/Resources"
 
 FILES="OpenRA.Game.exe OpenRA.Gl.dll OpenRA.FileFormats.dll FreeSans.ttf FreeSansBold.ttf titles.ttf shaders maps mods/ra mods/cnc"
 EXCLUDE="*.mdb ./mods/cnc/packages/*.mix ./mods/ra/packages/*.mix  ./mods/cnc/packages/*.MIX ./mods/ra/packages/*.MIX"
 
 # Copy source files into packaging dir
-PAYLOAD="$PACKAGING_PATH/payload"
-mkdir -p $PAYLOAD
-mkdir -p "$PAYLOAD/mods"
+mkdir -p $PACKAGING_PATH
+cp -r "$BUNDLE_PATH" "$PACKAGING_PATH/OpenRA.app"
+mkdir -p "$TARGET_PATH/mods"
 
 for i in $FILES; do
-	cp -RX "$i" "$PAYLOAD/$i"
+	cp -R "$i" "$TARGET_PATH/$i"
 done
 
 # Delete unwanted files
-cd $PAYLOAD
+cd $TARGET_PATH
 for i in $EXCLUDE; do
 	find . -path "$i" -delete
 done
-date "+%Y%m%d%H%M" > "VERSION"
-zip payload -r -9 *
+
+git describe --tags > "VERSION"
 cd $PACKAGING_PATH
-
-# Move everything into the app bundle
-cp -r "$LAUNCHER_PATH" .
-cp  "$PAYLOAD/payload.zip" "OpenRA.app/Contents/Resources/"
-cp "$PAYLOAD/VERSION" "OpenRA.app/Contents/Resources/"
-
-rm -rf $PAYLOAD
-zip OpenRA -r -9 OpenRA.app 
+#zip OpenRA -r -9 OpenRA.app 
 echo "Done!"
