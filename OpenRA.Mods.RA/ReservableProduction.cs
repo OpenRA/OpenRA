97e43cfe407f9e72f843fba21d150dc348f6ee47
Chris Forbes <chrisf@ijw.co.nz>
2010-08-01 11:57:33 +1200

    add ReservableProduction, to start fixing afld behavior.

---
+++ OpenRA.Mods.RA/ReservableProduction.cs
@@ -0,0 +1,33 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2007-2010 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made 
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation. For more information,
+ * see LICENSE.
+ */
+#endregion
+
+
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.RA
+{
+	// a small hack to teach Production about Reservable.
+
+	public class ReservableProductionInfo : ProductionInfo, ITraitPrerequisite<ReservableInfo>
+	{
+		public override object Create(ActorInitializer init) { return new ReservableProduction(); }
+	}
+
+	class ReservableProduction : Production
+	{
+		public override bool Produce(Actor self, OpenRA.GameRules.ActorInfo producee)
+		{
+			if (Reservable.IsReserved(self))
+				return false;
+
+			return base.Produce(self, producee);
+		}
+	}
+}
