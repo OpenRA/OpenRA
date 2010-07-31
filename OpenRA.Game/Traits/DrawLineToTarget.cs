13244d17f10e7af87afbfbfb00bed96ad86512db
Chris Forbes <chrisf@ijw.co.nz>
2010-08-01 11:32:09 +1200

    format and clean up DrawLineToTarget

--- OpenRA.Game/Traits/DrawLineToTarget.cs
+++ OpenRA.Game/Traits/DrawLineToTarget.cs
@@ -20,45 +20,43 @@ namespace OpenRA.Traits
 		public virtual object Create(ActorInitializer init) { return new DrawLineToTarget(this); }
 	}
 
-	public class DrawLineToTarget :IPostRenderSelection
+	public class DrawLineToTarget : IPostRenderSelection
 	{
 		DrawLineToTargetInfo Info;
 		public DrawLineToTarget(DrawLineToTargetInfo info)
 		{
 			this.Info = info;
 		}
-		
+
 		Target target;
 		int lifetime;
 		Color c;
-		
+
 		public void SetTarget(Actor self, Target target, Color c)
 		{
 			this.target = target;
 			lifetime = Info.Ticks;
 			this.c = c;
 		}
-		
+
 		public void SetTargetSilently(Actor self, Target target, Color c)
 		{
 			this.target = target;
 			this.c = c;
-		}	
-		
-		public void RenderAfterWorld (Actor self)
+		}
+
+		public void RenderAfterWorld(Actor self)
 		{
-			var activity = self.GetCurrentActivity();
-			if (activity == null || activity is Idle)
-				return;
-			
+			if (self.IsIdle) return;
+
 			var force = Game.GetModifierKeys().HasModifier(Modifiers.Alt);
 			if ((lifetime <= 0 || --lifetime <= 0) && !force)
 				return;
-			
+
 			var p = target.CenterLocation;
-			
+
 			Game.Renderer.LineRenderer.DrawLine(self.CenterLocation, p, c, c);
-			for (bool b = false; !b; p = self.CenterLocation, b = true) 
+			for (bool b = false; !b; p = self.CenterLocation, b = true)
 			{
 				Game.Renderer.LineRenderer.DrawLine(p + new float2(-1, -1), p + new float2(-1, 1), c, c);
 				Game.Renderer.LineRenderer.DrawLine(p + new float2(-1, 1), p + new float2(1, 1), c, c);
