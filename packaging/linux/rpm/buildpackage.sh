6e045e864ad4d65b900da7ff0757a71576f0990c
Matthew <matthew@ijw.co.nz>
2010-08-14 23:15:34 +1200

    Further RPM fixes

--- packaging/linux/rpm/buildpackage.sh
+++ packaging/linux/rpm/buildpackage.sh
@@ -12,19 +12,17 @@ PKGVERSION=`echo $5 | sed "s/-/\\./g"`
 sed -i "s/%define version [0-9\\.]\+/%define version $PKGVERSION/" openra.spec
 cp openra.spec "$6/SPECS/"
 
-pushd "$6"
+cd "$6"
 
 rpmbuild --target noarch -bb SPECS/openra.spec
 
-pushd RPMS/noarch/
+cd RPMS/noarch/
 PACKAGEFILE=openra-$PKGVERSION-1.noarch.rpm
 size=`stat -c "%s" $PACKAGEFILE`
-popd
 
 echo "$5,$size,$PACKAGEFILE" > /tmp/rpmlatest.txt
 
 wput $PACKAGEFILE "ftp://$3:$4@$1/$2/"
-pushd /tmp
+cd /tmp
 wput -u rpmlatest.txt "ftp://$3:$4@$1/$2/"
-popd
-popd
+
