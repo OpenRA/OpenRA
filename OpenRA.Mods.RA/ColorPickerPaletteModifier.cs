f6a880e524a066a76897e0dcb5a80cb16001715e
Chris Forbes <chrisf@ijw.co.nz>
2010-08-17 20:32:10 +1200

    line way too long.

--- OpenRA.Mods.RA/ColorPickerPaletteModifier.cs
+++ OpenRA.Mods.RA/ColorPickerPaletteModifier.cs
@@ -8,10 +8,9 @@
  */
 #endregion
 
-using System.Drawing;
-using OpenRA.Traits;
 using System.Collections.Generic;
 using OpenRA.FileFormats;
+using OpenRA.Traits;
 using OpenRA.Widgets.Delegates;
 
 namespace OpenRA.Mods.RA
@@ -28,11 +27,12 @@ namespace OpenRA.Mods.RA
 			SplitPlayerPalette = info.SplitRamp;
 			w.WorldRenderer.AddPalette("colorpicker", w.WorldRenderer.GetPalette(info.BasePalette));
 		}
-		
-		public void AdjustPalette(Dictionary<string,Palette> palettes)
+
+		public void AdjustPalette(Dictionary<string, Palette> palettes)
 		{
 			palettes["colorpicker"] = new Palette(palettes["colorpicker"],
-			                              new PlayerColorRemap(LobbyDelegate.CurrentColorPreview1, LobbyDelegate.CurrentColorPreview2, SplitPlayerPalette));
+			  new PlayerColorRemap(LobbyDelegate.CurrentColorPreview1,
+				  LobbyDelegate.CurrentColorPreview2, SplitPlayerPalette));
 		}
 	}
 }
