1e758ec3e03c964e539b00130ec7bc4c088eb9b0
Matthew Bowra-Dean <matthew@ijw.co.nz>
2010-09-19 01:47:00 +1200

    Removed website from main repository.
    Moved to http://github.com/beedee/OpenRAWebsite/

--- web/releases/windows/latest.php
+++
@@ -1,7 +0,0 @@
-<?php
-$latest = "OpenRA-20100425.exe";
-header("Location: $latest");
-$file = file_get_contents("../../downloads.txt");
-$new_downloads = $file + 1;
-file_put_contents("../../downloads.txt", $new_downloads);
-?>
