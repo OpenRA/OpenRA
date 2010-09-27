47950c9113e1af81f1dadec41c0d0e7a06c0562f
Chris Forbes <chrisf@ijw.co.nz>
2010-09-28 07:45:14 +1300

    Revert "remove setters on Mobile.{from,to}Cell. use SetLocation instead"
    This reverts commit 911db3feb17c30791661ceda5485168fee25ea16.

--- OpenRA.Game/Traits/Activities/Move.cs
+++ OpenRA.Game/Traits/Activities/Move.cs
@@ -156,7 +156,7 @@ namespace OpenRA.Traits.Activities
 			}
 			else
 			{
-				mobile.SetLocation( mobile.fromCell, nextCell.Value );
+				mobile.toCell = nextCell.Value;
 				var move = new MoveFirstHalf(
 					this,
 					Util.CenterOfCell( mobile.fromCell ),
@@ -350,7 +350,8 @@ namespace OpenRA.Traits.Activities
 							mobile.Facing,
 							Util.GetNearestFacing( mobile.Facing, Util.GetFacing( nextCell.Value - mobile.toCell, mobile.Facing ) ),
 							moveFraction - moveFractionTotal );
-						mobile.SetLocation( mobile.toCell, nextCell.Value );
+						mobile.fromCell = mobile.toCell;
+						mobile.toCell = nextCell.Value;
 						return ret;
 					}
 					else
@@ -363,7 +364,7 @@ namespace OpenRA.Traits.Activities
 					mobile.Facing,
 					mobile.Facing,
 					moveFraction - moveFractionTotal );
-				mobile.SetLocation( mobile.toCell, mobile.toCell );
+				mobile.fromCell = mobile.toCell;
 				return ret2;
 			}
 		}
@@ -378,7 +379,7 @@ namespace OpenRA.Traits.Activities
 			protected override MovePart OnComplete( Actor self, Mobile mobile, Move parent )
 			{
 				self.CenterLocation = Util.CenterOfCell( mobile.toCell );
-				mobile.SetLocation( mobile.toCell, mobile.toCell );
+				mobile.fromCell = mobile.toCell;
 				mobile.FinishedMoving(self);
 				return null;
 			}
