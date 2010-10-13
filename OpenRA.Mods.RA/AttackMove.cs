8aa180c1f463a367c7c2c147272e92c15dc3b35b
Chris Forbes <chrisf@ijw.co.nz>
2010-10-13 19:20:17 +1300

    cleaned up unused bits in attackmove

--- OpenRA.Mods.RA/AttackMove.cs
+++ OpenRA.Mods.RA/AttackMove.cs
@@ -1,13 +1,21 @@
-﻿using System;
+﻿#region Copyright & License Information
+/*
+ * Copyright 2007-2010 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made 
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation. For more information,
+ * see LICENSE.
+ */
+#endregion
+
 using System.Drawing;
-using OpenRA.Traits;
 using OpenRA.Effects;
+using OpenRA.Traits;
 
 namespace OpenRA.Mods.RA
 {
 	class AttackMoveInfo : TraitInfo<AttackMove>
 	{
-		//public object Create(ActorInitializer init) { return new AttackMove(init.self); }
 		public readonly bool JustMove = false;
 	}
 
@@ -18,9 +26,8 @@ namespace OpenRA.Mods.RA
 		public string VoicePhraseForOrder(Actor self, Order order)
 		{
 			if (order.OrderString == "AttackMove")
-			{
 				return "AttackMove";
-			}
+
 			return null;
 		}
 
