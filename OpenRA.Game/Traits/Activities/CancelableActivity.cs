25582cb9f8466a5a6f4836750682310f2f5a3812
Bob <ytinasni@gmail.com>
2010-10-23 20:56:05 +1300

    move CancelableActivity into its own file

---
+++ OpenRA.Game/Traits/Activities/CancelableActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenRA.Traits
+{
+	public abstract class CancelableActivity : IActivity
+	{
+		protected IActivity NextActivity { get; private set; }
+		protected bool IsCanceled { get; private set; }
+
+		public abstract IActivity Tick( Actor self );
+		protected virtual bool OnCancel( Actor self ) { return true; }
+
+		public void Cancel( Actor self )
+		{
+			IsCanceled = OnCancel( self );
+			if( IsCanceled )
+				NextActivity = null;
+			else if (NextActivity != null)
+				NextActivity.Cancel( self );
+		}
+
+		public void Queue( IActivity activity )
+		{
+			if( NextActivity != null )
+				NextActivity.Queue( activity );
+			else
+				NextActivity = activity;
+		}
+
+		public virtual IEnumerable<float2> GetCurrentPath()
+		{
+			yield break;
+		}
+	}
+}
--- OpenRA.Game/Traits/TraitsInterfaces.cs
+++ OpenRA.Game/Traits/TraitsInterfaces.cs
@@ -180,37 +180,6 @@ namespace OpenRA.Traits
 		IEnumerable<float2> GetCurrentPath();
 	}
 
-	public abstract class CancelableActivity : IActivity
-	{
-		protected IActivity NextActivity { get; private set; }
-		protected bool IsCanceled { get; private set; }
-
-		public abstract IActivity Tick( Actor self );
-		protected virtual bool OnCancel( Actor self ) { return true; }
-
-		public void Cancel( Actor self )
-		{
-			IsCanceled = OnCancel( self );
-			if( IsCanceled )
-				NextActivity = null;
-			else if (NextActivity != null)
-				NextActivity.Cancel( self );
-		}
-
-		public void Queue( IActivity activity )
-		{
-			if( NextActivity != null )
-				NextActivity.Queue( activity );
-			else
-				NextActivity = activity;
-		}
-
-		public virtual IEnumerable<float2> GetCurrentPath()
-		{
-			yield break;
-		}
-	}
-
 	public interface IRenderOverlay { void Render( WorldRenderer wr ); }
 	public interface INotifyIdle { void Idle(Actor self); }
 
