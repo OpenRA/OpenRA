a1bc31883b328b0736568da194190dbce6851d8d
Matthew Bowra-Dean <matthew@ijw.co.nz>
2010-11-12 21:44:09 +1300

    Changed shaders dir to renderer specific shader dirs in file list.

--- packaging/package-all.sh
+++ packaging/package-all.sh
@@ -26,7 +26,7 @@ find . -path "*.mdb" -delete
 # Note that the Tao dlls are shipped on all platforms except osx and that
 # they are now installed to the game directory instead of placed in the gac
 FILES="OpenRA.Game.exe OpenRA.Editor.exe OpenRA.Utility.exe OpenRA.Renderer.Cg.dll OpenRA.Renderer.Gl.dll OpenRA.Renderer.Null.dll \
-OpenRA.FileFormats.dll FreeSans.ttf FreeSansBold.ttf titles.ttf shaders mods/ra mods/cnc VERSION COPYING HACKING INSTALL"
+OpenRA.FileFormats.dll FreeSans.ttf FreeSansBold.ttf titles.ttf cg glsl mods/ra mods/cnc VERSION COPYING HACKING INSTALL"
 
 for i in $FILES; do
 	cp -R "$i" "packaging/built/$i" || exit 3
