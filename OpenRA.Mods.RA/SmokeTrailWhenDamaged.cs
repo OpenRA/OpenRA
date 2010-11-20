060d5443901f4c38be0980d6ad96c124e488f7a8
Chris Forbes <chrisf@ijw.co.nz>
2010-11-21 11:48:09 +1300

    add smoke trails for damaged aircraft

---
+++ OpenRA.Mods.RA/SmokeTrailWhenDamaged.cs
@@ -0,0 +1,31 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2007-2010 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made 
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation. For more information,
+ * see LICENSE.
+ */
+#endregion
+
+using System;
+using OpenRA.Traits;
+using OpenRA.Mods.RA.Effects;
+
+namespace OpenRA.Mods.RA
+{
+	class SmokeTrailWhenDamagedInfo : TraitInfo<SmokeTrailWhenDamaged> { }
+
+	class SmokeTrailWhenDamaged : ITick
+	{
+		public void Tick(Actor self)
+		{
+			if (self.GetDamageState() >= DamageState.Heavy)
+				self.World.AddFrameEndTask(
+					w => { if (!self.Destroyed) w.Add(
+						new Smoke(w, self.CenterLocation.ToInt2() 
+							- new int2(0, self.Trait<IMove>().Altitude), 
+							"smokey")); });
+		}
+	}
+}
