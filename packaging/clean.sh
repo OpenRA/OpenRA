58797c0d3714cc5194fcfcab3e4eddb3bc8e7145
Paul Chote <paul@chote.net>
2010-12-29 17:52:16 +1300

    packaging cleanup script.

---
+++ packaging/clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -rf packaging/linux/root
+rm packaging/linux/package.log
+rm packaging/linux/deb/package.log
+rm packaging/linux/rpm/package.log
+rm packaging/linux/pkgbuild/package.log
+rm packaging/osx/package.log
+rm packaging/windows/package.log
