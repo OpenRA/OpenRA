5665d40e29dbe5c909c9f3e378623b19e896a81c
Matthew Bowra-Dean <matthew@ijw.co.nz>
2011-01-05 13:55:27 +1300

    Separate root dirs for different architectures.

--- packaging/linux/buildpackage.sh
+++ packaging/linux/buildpackage.sh
@@ -9,44 +9,45 @@ fi
 VERSION=$1
 BUILTDIR=$2
 PACKAGEDIR=$3
+ROOTDIR=root$4
 
 # Clean up
-rm -rf root
+rm -rf $ROOTDIR
 
 # Game files
-mkdir -p root/usr/bin/
+mkdir -p $ROOTDIR/usr/bin/
 if [ $4 = "x64" ]; then
-	cp -T $BUILTDIR/gtklaunch root/usr/bin/openra
+	cp -T $BUILTDIR/gtklaunch $ROOTDIR/usr/bin/openra
 else
-	cp -T $BUILTDIR/gtklaunch32 root/usr/bin/openra
+	cp -T $BUILTDIR/gtklaunch32 $ROOTDIR/usr/bin/openra
 fi
 
-mkdir -p root/usr/share/openra/
-cp -R $BUILTDIR/* "root/usr/share/openra/" || exit 3
+mkdir -p $ROOTDIR/usr/share/openra/
+cp -R $BUILTDIR/* "$ROOTDIR/usr/share/openra/" || exit 3
 
 # Remove unneeded files
-rm root/usr/share/openra/OpenRA.Launcher.exe
-rm root/usr/share/openra/gtklaunch
-rm root/usr/share/openra/gtklaunch32
+rm $ROOTDIR/usr/share/openra/OpenRA.Launcher.exe
+rm $ROOTDIR/usr/share/openra/gtklaunch
+rm $ROOTDIR/usr/share/openra/gtklaunch32
 
 # Desktop Icons
-mkdir -p root/usr/share/applications/
-sed "s/{VERSION}/$VERSION/" openra.desktop > root/usr/share/applications/openra.desktop
+mkdir -p $ROOTDIR/usr/share/applications/
+sed "s/{VERSION}/$VERSION/" openra.desktop > $ROOTDIR/usr/share/applications/openra.desktop
 
 # Menu entries
-mkdir -p root/usr/share/menu/
-cp openra root/usr/share/menu/
+mkdir -p $ROOTDIR/usr/share/menu/
+cp openra $ROOTDIR/usr/share/menu/
 
 # Icon images
-mkdir -p root/usr/share/pixmaps/
-cp openra.32.xpm root/usr/share/pixmaps/
-mkdir -p root/usr/share/icons/
-cp -r hicolor root/usr/share/icons/
+mkdir -p $ROOTDIR/usr/share/pixmaps/
+cp openra.32.xpm $ROOTDIR/usr/share/pixmaps/
+mkdir -p $ROOTDIR/usr/share/icons/
+cp -r hicolor $ROOTDIR/usr/share/icons/
 
 (
     echo "Building Debian package."
     cd deb
-    ./buildpackage.sh "$VERSION" ../root "$PACKAGEDIR" $4 &> package.log
+    ./buildpackage.sh "$VERSION" ../$ROOTDIR "$PACKAGEDIR" $4 &> package.log
     if [ $? -ne 0 ]; then
         echo "Debian package build failed, refer to $PWD/package.log."
     fi
@@ -55,7 +56,7 @@ cp -r hicolor root/usr/share/icons/
 (
     echo "Building Arch-Linux package."
     cd pkgbuild
-    sh buildpackage.sh "$VERSION" ../root "$PACKAGEDIR" $4 &> package.log
+    sh buildpackage.sh "$VERSION" ../$ROOTDIR "$PACKAGEDIR" $4 &> package.log
     if [ $? -ne 0 ]; then
         echo "Arch-Linux package build failed, refer to $PWD/package.log."
     fi
@@ -64,7 +65,7 @@ cp -r hicolor root/usr/share/icons/
 (
     echo "Building RPM package."
     cd rpm
-    sh buildpackage.sh "$VERSION" ../root ~/rpmbuild "$PACKAGEDIR" $4 &> package.log
+    sh buildpackage.sh "$VERSION" ../$ROOTDIR ~/rpmbuild "$PACKAGEDIR" $4 &> package.log
     if [ $? -ne 0 ]; then
         echo "RPM package build failed, refer to $PWD/package.log."
     fi
