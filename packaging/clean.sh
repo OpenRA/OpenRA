2f31928a53ed9096a33c3a93aa51f51a217352a5
Paul Chote <paul@chote.net>
2011-01-15 13:51:38 +1300

    Fix packaging clean script.

--- packaging/clean.sh
+++ packaging/clean.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-rm -rf packaging/linux/root
+rm -rf packaging/linux/rootx64
+rm -rf packaging/linux/rootx86
 rm -rf packaging/built
-rm packaging/linux/package.log
+rm packaging/linux/package32.log
+rm packaging/linux/package64.log
 rm packaging/linux/deb/package.log
 rm packaging/linux/rpm/package.log
 rm packaging/linux/pkgbuild/package.log
