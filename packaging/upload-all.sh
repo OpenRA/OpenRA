695ca4a23ba8c59d7493bb5d7af3477657cf0f55
Paul Chote <paul@chote.net>
2011-02-08 16:21:48 +1300

    Simplify the uploader script. Untested.

--- packaging/upload-all.sh
+++ packaging/upload-all.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 FTPSERVER=openra.res0l.net
+FTPPATHBASE="openra.res0l.net"
+FTP="ftp://${FTPSERVER}/${FTPPATHBASE}/assets/downloads/"
+
+upload () {
+    PLATFORM=$1
+    FILENAME=$2
+    wput -u "${FTP}${PLATFORM}/" "${FILENAME}"
+}
+
 TAG=$1
 PKGDIR=$2
+TYPE=`echo ${TAG} | grep -o "^[a-z]\\+"`
+VERSION=`echo ${TAG} | grep -o "[0-9]\\+-\\?[0-9]\\?"`
+LINUXVERSION=`echo ${TAG} | sed "s/-/\\./g"`
 
-TYPE=`echo $TAG | grep -o "^[a-z]\\+"`
-VERSION=`echo $TAG | grep -o "[0-9]\\+-\\?[0-9]\\?"`
-LINUXVERSION=`echo $TAG | sed "s/-/\\./g"`
+cd ${PKGDIR}
+upload windows OpenRA-${TAG}.exe
+upload mac OpenRA-${TAG}.zip
+upload linux/deb openra_${LINUXVERSION}_all.deb
 
-FTPPATHBASE="openra.res0l.net"
-FTPPATH="$FTPPATHBASE/assets/downloads"
+mv openra-${VERSION}-1.noarch.rpm openra-${TYPE}${VERSION}-1.noarch.rpm
+upload linux/rpm openra-${TYPE}${VERSION}-1.noarch.rpm
+mv openra-${VERSION}-1-any.pkg.tar.xz openra-${TYPE}${VERSION}-1-any.pkg.tar.xz
+upload linux/arch openra-${TYPE}${VERSION}-1-any.pkg.tar.xz
 
-./uploader.sh windows $PKGDIR/OpenRA-$TAG.exe "$FTPSERVER" "$FTPPATH"
-./uploader.sh mac $PKGDIR/OpenRA-$TAG.zip "$FTPSERVER" "$FTPPATH"
-./uploader.sh linux/deb $PKGDIR/openra_${LINUXVERSION}_all.deb "$FTPSERVER" "$FTPPATH"
-
-mv $PKGDIR/openra-$LINUXVERSION-1.noarch.rpm $PKGDIR/openra-$TYPE$VERSION-1.noarch.rpm
-./uploader.sh linux/rpm $PKGDIR/openra-$TYPE$VERSION-1.noarch.rpm "$FTPSERVER" "$FTPPATH"
-mv $PKGDIR/openra-$LINUXVERSION-1-any.pkg.tar.xz $PKGDIR/openra-$TYPE$VERSION-1-any.pkg.tar.xz
-./uploader.sh linux/arch $PKGDIR/openra-$TYPE$VERSION-1-any.pkg.tar.xz "$FTPSERVER" "$FTPPATH"
-
-if [ "$TYPE" = "release" ]; then
-    wput --basename=../ -u ../VERSION ftp://$FTPSERVER/$FTPPATHBASE/master/
-fi
-
-wget http://$FTPSERVER/home/syncdownloads
+wget http://${FTPSERVER}/home/syncdownloads
