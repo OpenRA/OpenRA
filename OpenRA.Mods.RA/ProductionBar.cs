c62e025c45bcb242654d8199700760f287e5652f
Chris Forbes <chrisf@ijw.co.nz>
2011-03-02 22:20:34 +1300

    add temp hacked ProductionBar -- not wired to ProductionQueue properly yet.

---
+++ OpenRA.Mods.RA/ProductionBar.cs
@@ -0,0 +1,24 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2007-2011 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made 
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation. For more information,
+ * see COPYING.
+ */
+#endregion
+
+using System.Drawing;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.RA
+{
+	class ProductionBarInfo : TraitInfo<ProductionBar> { }
+
+	class ProductionBar : ISelectionBar
+	{
+		public float GetValue() { return .5f; }
+
+		public Color GetColor() { return Color.CadetBlue; }
+	}
+}
