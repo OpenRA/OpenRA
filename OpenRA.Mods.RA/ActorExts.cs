981e57bde87c9826201c70bb65c8e2fdf645f99c
Chris Forbes <chrisf@ijw.co.nz>
2011-03-13 18:10:23 +1300

    some more overloads etc

--- OpenRA.Mods.RA/ActorExts.cs
+++ OpenRA.Mods.RA/ActorExts.cs
@@ -4,48 +4,58 @@ namespace OpenRA.Mods.RA
 {
 	public static class ActorExts
 	{
-		public static bool AppearsFriendlyTo(this Actor self, Actor toActor) 
+		public static bool AppearsFriendlyTo(this Actor self, Player toPlayer) 
 		{ 
 			if (self.HasTrait<Spy>())
 			{
 				if (self.Trait<Spy>().Disguised)
 				{
 					//TODO: check if we can see through disguise
-					if ( toActor.Owner.Stances[self.Trait<Spy>().disguisedAsPlayer] == Stance.Ally)
+					if ( toPlayer.Stances[self.Trait<Spy>().disguisedAsPlayer] == Stance.Ally)
 						return true;
 				}
 				else 
 				{
-					if (toActor.Owner.Stances[self.Owner] == Stance.Ally)
+					if (toPlayer.Stances[self.Owner] == Stance.Ally)
 						return true;
 				}
 				return false;
 			}
-			return toActor.Owner.Stances[self.Owner] == Stance.Ally;
+			return toPlayer.Stances[self.Owner] == Stance.Ally;
 		}
 		
-		public static bool AppearsHostileTo(this Actor self, Actor toActor) 
+		public static bool AppearsHostileTo(this Actor self, Player toPlayer) 
 		{ 
 			if (self.HasTrait<Spy>())
 			{
-				if (toActor.Owner.Stances[self.Owner] == Stance.Ally)
+				if (toPlayer.Stances[self.Owner] == Stance.Ally)
 					return false;
 				
 				if (self.Trait<Spy>().Disguised)
 				{
 					//TODO: check if we can see through disguise
-					if ( toActor.Owner.Stances[self.Trait<Spy>().disguisedAsPlayer] == Stance.Enemy)
+                    if (toPlayer.Stances[self.Trait<Spy>().disguisedAsPlayer] == Stance.Enemy)
 						return true;
 				}
 				else 
 				{
-					if (toActor.Owner.Stances[self.Owner] == Stance.Enemy)
+                    if (toPlayer.Stances[self.Owner] == Stance.Enemy)
 						return true;
 				}
 				return false;
 			}
-			return toActor.Owner.Stances[self.Owner] == Stance.Enemy;
+            return toPlayer.Stances[self.Owner] == Stance.Enemy;
 		}
+
+        public static bool AppearsHostileTo(this Actor self, Actor toActor)
+        {
+            return AppearsHostileTo(self, toActor.Owner);
+        }
+
+        public static bool AppearsFriendlyTo(this Actor self, Actor toActor)
+        {
+            return AppearsFriendlyTo(self, toActor.Owner);
+        }
 	}
 }
 
