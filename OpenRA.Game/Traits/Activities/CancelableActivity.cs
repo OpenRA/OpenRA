820f67e46b691fce00047ff9632522f918bdd56c
alzeih <alzeih@gmail.com>
2011-04-05 20:45:58 +1200

    more for Activity

--- OpenRA.Game/Traits/Activities/CancelableActivity.cs
+++ OpenRA.Game/Traits/Activities/CancelableActivity.cs
@@ -1,14 +1,4 @@
-﻿#region Copyright & License Information
-/*
- * Copyright 2007-2011 The OpenRA Developers (see AUTHORS)
- * This file is part of OpenRA, which is free software. It is made 
- * available to you under the terms of the GNU General Public License
- * as published by the Free Software Foundation. For more information,
- * see COPYING.
- */
-#endregion
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,7 +16,7 @@ namespace OpenRA.Traits.Activities
 		}
 		protected virtual bool OnCancel( Actor self ) { return true; }
 
-		public void Cancel( Actor self )
+		public virtual void Cancel( Actor self )
 		{
 			IsCanceled = OnCancel( self );
 			if( IsCanceled )
