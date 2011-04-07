b0425aff3b7928417bd4a9a8e1929c28c780f5cb
Paul Chote <paul@chote.net>
2011-04-07 21:15:42 +1200

    Renormalize line endings and fix copyright headers again.

--- OpenRA.TilesetBuilder/Form1.Designer.cs
+++ OpenRA.TilesetBuilder/Form1.Designer.cs
@@ -1,4 +1,14 @@
-﻿namespace OpenRA.TilesetBuilder
+﻿#region Copyright & License Information
+/*
+ * Copyright 2007-2011 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made 
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation. For more information,
+ * see COPYING.
+ */
+#endregion
+
+namespace OpenRA.TilesetBuilder
 {
 	partial class Form1
 	{
--- SequenceEditor/Properties/AssemblyInfo.cs
+++ SequenceEditor/Properties/AssemblyInfo.cs
@@ -1,4 +1,14 @@
-﻿using System.Reflection;
+﻿#region Copyright & License Information
+/*
+ * Copyright 2007-2011 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made 
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation. For more information,
+ * see COPYING.
+ */
+#endregion
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
