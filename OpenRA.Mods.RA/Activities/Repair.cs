f851e6a21295c4d6766ebb4730d6021a12f584c0
Chris Forbes <chrisf@ijw.co.nz>
2011-04-09 15:14:19 +1200

    fix a silly glitch in unit repairing

--- OpenRA.Mods.RA/Activities/Repair.cs
+++ OpenRA.Mods.RA/Activities/Repair.cs
@@ -33,7 +33,7 @@ namespace OpenRA.Mods.RA.Activities
 
 				var repairsUnits = host.Info.Traits.Get<RepairsUnitsInfo>();
 				var unitCost = self.Info.Traits.Get<ValuedInfo>().Cost;
-				var hpToRepair = Math.Min(repairsUnits.HpPerStep, health.MaxHP - health.HP);
+                var hpToRepair = repairsUnits.HpPerStep;
 				var cost = (hpToRepair * unitCost * repairsUnits.ValuePercentage) / (health.MaxHP * 100);
 
 				if (!self.Owner.PlayerActor.Trait<PlayerResources>().TakeCash(cost))
