8a4f49485b8c9c67a226e922e2c827f497ec275f
Chris Forbes <chrisf@ijw.co.nz>
2011-04-16 11:24:18 +1200

    remove defunct IOrderCursor interface

--- OpenRA.Game/Traits/TraitsInterfaces.cs
+++ OpenRA.Game/Traits/TraitsInterfaces.cs
@@ -52,7 +52,6 @@ namespace OpenRA.Traits
 
     public interface IResolveOrder { void ResolveOrder(Actor self, Order order); }
     public interface IValidateOrder { bool OrderValidation(OrderManager orderManager, World world, int clientId, Order order); }
-    public interface IOrderCursor { string CursorForOrder(Actor self, Order order); }
     public interface IOrderVoice { string VoicePhraseForOrder(Actor self, Order order); }
     public interface INotifySold { void Selling(Actor self); void Sold(Actor self); }
     public interface INotifyDamage { void Damaged(Actor self, AttackInfo e); }
