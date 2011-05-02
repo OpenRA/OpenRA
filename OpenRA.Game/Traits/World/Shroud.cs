64782fb2548cbee4c4bfccaed91d3b0ebbcf0571
Chris Forbes <chrisf@ijw.co.nz>
2011-05-02 22:23:36 +1200

    use ToList rather than List<T>.ctor

--- OpenRA.Game/Traits/World/Shroud.cs
+++ OpenRA.Game/Traits/World/Shroud.cs
@@ -120,7 +120,7 @@ namespace OpenRA.Traits
 			// No longer our ally; remove unit vis
 			if (oldStance == Stance.Ally)
 			{
-				var toRemove = new List<Actor>(vis.Select(a => a.Key).Where(a => a.Owner == player));
+				var toRemove = vis.Select(a => a.Key).Where(a => a.Owner == player).ToList();
 				foreach (var a in toRemove)
 					RemoveActor(a);
 			}
