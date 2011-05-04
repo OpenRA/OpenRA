820895bf85dfed9a8191cfb8cfb18ba60e43dd15
Paul Chote <paul@chote.net>
2011-05-04 20:22:44 +1200

    Update zenity wrapper - untested.

--- packaging/linux/OpenRA.Utility.sh
+++ packaging/linux/OpenRA.Utility.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
-# OpenRA utility wrapper for linux systems
-
-authenticate () {
-    if command -v gksudo > /dev/null; then
-        gksudo --description "OpenRA Installer" -- mono OpenRA.Utility.exe $@ || echo "Error: Permission denied."
-    elif command -v kdesudo > /dev/null; then
-        kdesudo -d -- mono OpenRA.Utility.exe $@ || echo "Error: Permission denied."
-    else
-        # Try running without escalating
-        mono OpenRA.Utility.exe $@
-    fi
-}
+# OpenRA utility wrapper for linux systems with zenity
 
 if [[ "$1" == "--display-filepicker" ]]; then
     if command -v zenity > /dev/null ; then
         zenity --file-selection --title $2
     else
-        mono OpenRA.Utility.exe $@
+        mono OpenRA.Utility.exe "$@"
     fi
-else
-    # Everything else requires root running an -inner variant
-    authenticate "$1-inner" ${@:2}
 fi
