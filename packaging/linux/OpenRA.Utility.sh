52f79f232e9db39e5f697b13cb0e9b9be47336c8
Chris Forbes <chrisf@ijw.co.nz>
2011-05-06 08:31:19 +1200

    correctly requote arguments to zenity

--- packaging/linux/OpenRA.Utility.sh
+++ packaging/linux/OpenRA.Utility.sh
@@ -3,7 +3,7 @@
 
 if [[ "$1" == "--display-filepicker" ]]; then
     if command -v zenity > /dev/null ; then
-        zenity --file-selection --title $2
+        zenity --file-selection --title "$2"
     else
         mono OpenRA.Utility.exe "$@"
     fi
