a12ecb57d83555643c1f5a40a9596b0f1ab0e647
crazybutcher <crazybutcher@luxinia.de>
2011-05-23 12:03:46 +0200

    opencl missing convert / astype and image functions added

--- spec/opencl.lua
+++ spec/opencl.lua
@@ -1,6 +1,41 @@
 -- authors: Luxinia Dev (Eike Decker & Christoph Kubisch)
 ---------------------------------------------------------
 
+local convtypes = [[bool char uchar short ushort int uint long ulong float double]]
+local convout = {}
+for i in convtypes:gmatch("([%w_]+)") do
+	local suffix = {"","_rte","_rtz","_rtp","_rtn"}
+	for k,t in ipairs(suffix) do
+		table.insert(convout,"convert_"..i..t)
+		table.insert(convout,"convert_"..i.."_sat"..t)
+		local vectors = {2,4,8,16}
+		for n,v in ipairs(vectors) do
+			table.insert(convout,"convert_"..i..v..t)
+			table.insert(convout,"convert_"..i..v.."_sat"..t)
+		end
+	end
+end
+convout = table.concat(convout, " ")
+
+local astypes = [[int uint uchar ushort float double size_t ptrdiff_t intptr_t uintptr_t
+		long ulong char short unsigned 
+		float2 float4 float8 float16
+		double2 double4 double8 double16
+		char2 char4 char8 char16
+		uchar2 uchar4 uchar8 uchar16
+		short2 short4 short8 short16
+		ushort2 ushort4 ushort8 ushort16
+		int2 int4 int8 int16
+		uint2 uint4 uint8 uint16
+		long2 long4 long8 long16
+		ulong2 ulong4 ulong8 ulong16]]
+
+local astypeout = {}
+for i in astypes:gmatch("([%w_]+)") do
+	table.insert(astypeout, "as_"..i)
+end
+astypeout = table.concat(astypeout, " ")
+
 return {
 	exts = {"cl","ocl","clh",},
 	lexer = wxstc.wxSTC_LEX_CPP,
@@ -47,9 +82,9 @@ return {
 		},
 	
 	keywords = {
-		[[int uint uchar ushort half float bool double size_t ptrdiff_t intptr_t uintptr_t void
+		[[int uint uchar ushort float double size_t ptrdiff_t intptr_t uintptr_t
 		long ulong char short unsigned 
-		half2 half4 half8 half16
+		
 		float2 float4 float8 float16
 		double2 double4 double8 double16
 		char2 char4 char8 char16
@@ -60,9 +95,12 @@ return {
 		uint2 uint4 uint8 uint16
 		long2 long4 long8 long16
 		ulong2 ulong4 ulong8 ulong16
+		
+		half2 half4 half8 half16
+		void half bool
 		image2d_t image3d_t sampler_t event_t cl_image_format
 		
-		struct typedef void const
+		struct typedef void const inline
 		return switch case for do while if else break continue volatile
 		CLK_A CLK_R CLK_RG CLK_RGB CLK_RGBA CLK_ARGB CLK_BGRA CLK_INTENSITY CLK_LUMINANCE
 		
@@ -90,17 +128,19 @@ return {
 		[[__kernel kernel __attribute__ __read_only __write_only read_only write_only
 		__constant constant __local local __global global __private private
 		vec_type_hint work_group_size_hint reqd_work_group_size
-		aligned packed endian host device
+		aligned packed endian host device 
 		
 		async_work_group_copy wait_group_events prefetch 
 		clamp min max degrees radians sign smoothstep step mix
 		mem_fence read_mem_fence write_mem_fence
 		cross prod distance dot length normalize fast_distance fast_length fast_normalize
-		read_image write_image get_image_width get_image_height get_image_depth
+		get_image_width get_image_height get_image_depth
 		get_image_channel_data_type get_image_channel_order
 		get_image_dim
 		abs abs_diff add_sat clz hadd mad24 mad_hi mad_sat
 		mul24 mul_hi rhadd rotate sub_sat upsample
+		read_imagei write_imagei read_imageui write_imageui
+		read_imagef write_imagef 
 		
 		isequal isnotequal isgreater isgreaterequal isless islessequal islessgreater
 		isfinite isinf isnan isnormal isordered isunordered signbit any all bitselect select
@@ -152,7 +192,7 @@ return {
 		wzyz wzyw wzzx wzzy wzzz wzzw wzwx wzwy wzwz wzww 
 		wwxx wwxy wwxz wwxw wwyx wwyy wwyz wwyw wwzx wwzy 
 		wwzz wwzw wwwx wwwy wwwz wwww xy xz yz xyz 
-		xw yw xyw zw xzw yzw xyzw ]],
+		xw yw xyw zw xzw yzw xyzw ]]..convout.." "..astypeout,
 
 		},
 	}
