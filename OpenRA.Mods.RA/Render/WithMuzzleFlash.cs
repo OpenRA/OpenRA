a3601b12149fa97b5221dd51a31fb4edf75af8f3
Chris Forbes <chrisf@ijw.co.nz>
2011-06-25 18:12:05 +1200

    drop redundant virtuals from WithMuzzleFlash

--- OpenRA.Mods.RA/Render/WithMuzzleFlash.cs
+++ OpenRA.Mods.RA/Render/WithMuzzleFlash.cs
@@ -57,14 +57,14 @@ namespace OpenRA.Mods.RA.Render
 				mf.Animation.PlayThen("muzzle", () => isShowing = false);
 		}
 
-		public virtual IEnumerable<Renderable> Render(Actor self)
+		public IEnumerable<Renderable> Render(Actor self)
 		{
 			foreach (var a in muzzleFlashes.Values)
 				if (a.DisableFunc == null || !a.DisableFunc())
 					yield return a.Image(self, "effect");
 		}
 
-		public virtual void Tick(Actor self)
+		public void Tick(Actor self)
 		{
 			foreach (var a in muzzleFlashes.Values)
 				a.Animation.Tick();
