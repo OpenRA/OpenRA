68fd2c9744cb3e2054e9d095fc76c52e3a7b00a7
Chris Forbes <chrisf@ijw.co.nz>
2011-07-17 20:07:47 +1200

    clean up usings after prev

--- OpenRA.Mods.RA/Lint/CheckActorReferences.cs
+++ OpenRA.Mods.RA/Lint/CheckActorReferences.cs
@@ -10,9 +10,7 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
 using OpenRA.Traits;
 
 namespace OpenRA.Mods.RA
--- RALint/RALint.cs
+++ RALint/RALint.cs
@@ -9,9 +9,6 @@
 #endregion
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using OpenRA;
 using OpenRA.FileFormats;
 using OpenRA.Traits;
