70dfcffaea1438fee78f9ae3f57447af03a2f5d8
Chris Forbes <chrisf@ijw.co.nz>
2011-07-19 19:51:24 +1200

    add another lint pass to ensure that every (non-template) actor type is actually constructible

---
+++ OpenRA.Mods.RA/Lint/CheckTraitPrerequisites.cs
@@ -0,0 +1,36 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2011 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made 
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation. For more information,
+ * see COPYING.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.RA
+{
+	public class CheckTraitPrerequisites : ILintPass
+	{
+		public void Run(Action<string> emitError, Action<string> emitWarning)
+		{
+			foreach (var actorInfo in Rules.Info.Where(a => !a.Key.StartsWith("^")))
+				try
+				{
+					var traits = actorInfo.Value.TraitsInConstructOrder().ToArray();
+					if (traits.Length == 0)
+						emitWarning("Actor {0} has no traits. Is this intended?".F(actorInfo.Key));
+				}
+				catch(Exception e)
+				{
+					emitError("Actor {0} is not constructible; failure: {1}".F(actorInfo.Key, e.Message));
+				}
+		}
+	}
+}
