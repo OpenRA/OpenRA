ea190fcf861398735fb5abe74e231efb2a38881a
Chris Forbes <chrisf@ijw.co.nz>
2011-09-11 17:25:24 +1200

    combine LobbyCommands and PlayerCommands

--- OpenRA.Mods.RA/ServerTraits/LobbyCommands.cs
+++ OpenRA.Mods.RA/ServerTraits/LobbyCommands.cs
@@ -315,6 +315,107 @@ namespace OpenRA.Mods.RA.Server
 						server.SyncLobbyInfo();
 						return true;
 					}},
+				{ "name",
+					s =>
+					{
+						Log.Write("server", "Player@{0} is now known as {1}", conn.socket.RemoteEndPoint, s);
+						client.Name = s;
+						server.SyncLobbyInfo();
+						return true;
+					}},
+				{ "race",
+					s =>
+					{
+						var parts = s.Split(' ');
+						var targetClient = server.lobbyInfo.ClientWithIndex(int.Parse(parts[0]));
+
+						// Only the host can change other client's info
+						if (targetClient.Index != client.Index && conn.PlayerIndex != 0)
+							return true;
+
+						// Map has disabled race changes
+						if (server.lobbyInfo.Slots[targetClient.Slot].LockRace)
+							return true;
+
+						targetClient.Country = parts[1];
+						server.SyncLobbyInfo();
+						return true;
+					}},
+				{ "team",
+					s =>
+					{
+						var parts = s.Split(' ');
+						var targetClient = server.lobbyInfo.ClientWithIndex(int.Parse(parts[0]));
+
+						// Only the host can change other client's info
+						if (targetClient.Index != client.Index && conn.PlayerIndex != 0)
+							return true;
+
+						// Map has disabled team changes
+						if (server.lobbyInfo.Slots[targetClient.Slot].LockTeam)
+							return true;
+
+						int team;
+						if (!int.TryParse(parts[1], out team)) { Log.Write("server", "Invalid team: {0}", s ); return false; }
+
+						targetClient.Team = team;
+						server.SyncLobbyInfo();
+						return true;
+					}},
+				{ "spawn",
+					s =>
+					{
+						var parts = s.Split(' ');
+						var targetClient = server.lobbyInfo.ClientWithIndex(int.Parse(parts[0]));
+
+						// Only the host can change other client's info
+						if (targetClient.Index != client.Index && conn.PlayerIndex != 0)
+							return true;
+
+						// Spectators don't need a spawnpoint
+						if (targetClient.Slot == null)
+							return true;
+
+						// Map has disabled spawn changes
+						if (server.lobbyInfo.Slots[targetClient.Slot].LockSpawn)
+							return true;
+
+						int spawnPoint;
+						if (!int.TryParse(parts[1], out spawnPoint) || spawnPoint < 0 || spawnPoint > server.Map.SpawnPoints.Count())
+						{
+							Log.Write("server", "Invalid spawn point: {0}", parts[1]);
+							return true;
+						}
+
+						if (server.lobbyInfo.Clients.Where( cc => cc != client ).Any( cc => (cc.SpawnPoint == spawnPoint) && (cc.SpawnPoint != 0) ))
+						{
+							server.SendChatTo( conn, "You can't be at the same spawn point as another player" );
+							return true;
+						}
+
+						targetClient.SpawnPoint = spawnPoint;
+						server.SyncLobbyInfo();
+						return true;
+					}},
+				{ "color",
+					s =>
+					{
+						var parts = s.Split(' ');
+						var targetClient = server.lobbyInfo.ClientWithIndex(int.Parse(parts[0]));
+
+						// Only the host can change other client's info
+						if (targetClient.Index != client.Index && conn.PlayerIndex != 0)
+							return true;
+
+						// Map has disabled color changes
+						if (targetClient.Slot != null && server.lobbyInfo.Slots[targetClient.Slot].LockColor)
+							return true;
+
+						var ci = parts[1].Split(',').Select(cc => int.Parse(cc)).ToArray();
+						targetClient.ColorRamp = new ColorRamp((byte)ci[0], (byte)ci[1], (byte)ci[2], (byte)ci[3]);
+						server.SyncLobbyInfo();
+						return true;
+					}}
 			};
 			
 			var cmdName = cmd.Split(' ').First();
@@ -343,7 +444,7 @@ namespace OpenRA.Mods.RA.Server
 			};
 		}
 
-		public static void LoadMap(S server)
+		static void LoadMap(S server)
 		{
 			server.Map = new Map(server.ModData.AvailableMaps[server.lobbyInfo.GlobalSettings.Map].Path);
 			server.lobbyInfo.Slots = server.Map.Players
