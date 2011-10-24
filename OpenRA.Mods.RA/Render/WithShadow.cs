35b02317467ea3707dfeaf4b2296a601097e8dfa
Chris Forbes <chrisf@ijw.co.nz>
2011-10-24 17:04:05 +1300

    fixed #669 -- helicopters bob slightly in flight

--- OpenRA.Mods.RA/Render/WithShadow.cs
+++ OpenRA.Mods.RA/Render/WithShadow.cs
@@ -8,9 +8,11 @@
  */
 #endregion
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using OpenRA.Traits;
+using OpenRA.Mods.RA.Air;
 
 namespace OpenRA.Mods.RA.Render
 {
@@ -22,9 +24,13 @@ namespace OpenRA.Mods.RA.Render
 		{
 			var move = self.Trait<IMove>();
 
+			/* rude hack */
+			var visualOffset = (move is Helicopter && move.Altitude > 0)
+				? Math.Abs((self.ActorID + Game.LocalTick) / 5 % 4 - 1) - 1 : 0;
+
 			var shadowSprites = r.Select(a => a.WithPalette("shadow"));
 			var flyingSprites = (move.Altitude <= 0) ? r
-				: r.Select(a => a.WithPos(a.Pos - new float2(0, move.Altitude)).WithZOffset(move.Altitude));
+				: r.Select(a => a.WithPos(a.Pos - new float2(0, move.Altitude + visualOffset)).WithZOffset(move.Altitude));
 
 			return shadowSprites.Concat(flyingSprites);
 		}
