6f675b60cb158e0e73bd1a1fcf1d96dde4fb4f42
Paul Kulchenko <paul@kulchenko.com>
2011-12-11 22:03:54 -0800

    Ran files with preferences through a beautifier

--- src/preferences/editor.lua
+++ src/preferences/editor.lua
@@ -2,24 +2,22 @@
 ---------------------------------------------------------
 
 preferencesDialog.addCategory {
-	category = "editor";
-	title = "Editor";
+  category = "editor";
+  title = "Editor";
 }
 
 preferencesDialog.addPage {
-	title = "Basic preferences";
-	category = "editor";
-	layout = {
-		{type = 'group',title="Sessions"; minheight = 100; minwidth = 100};
-			{type = "checkbox"; title = "Reopen files";name = 'session_restore'};
-		{type = 'finishgroup'};
-		{type = 'space'; space = 4};
-		
-		
-	};
-	onload = function ()
-		return {testbox = true}
-	end;
-	onsave = function (values)
-	end
+  title = "Basic preferences";
+  category = "editor";
+  layout = {
+    {type = 'group',title="Sessions"; minheight = 100; minwidth = 100};
+    {type = "checkbox"; title = "Reopen files";name = 'session_restore'};
+    {type = 'finishgroup'};
+    {type = 'space'; space = 4};
+  };
+  onload = function ()
+    return {testbox = true}
+  end;
+  onsave = function (values)
+  end
 }
