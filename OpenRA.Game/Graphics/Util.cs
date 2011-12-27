f63cdf37d7922faf00a0c628fc5c2452a3035658
Chris Forbes <chrisf@ijw.co.nz>
2011-12-27 14:53:18 +1300

    tidy usings in Graphics.Util

--- OpenRA.Game/Graphics/Util.cs
+++ OpenRA.Game/Graphics/Util.cs
@@ -8,11 +8,6 @@
  */
 #endregion
 
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
 using OpenRA.FileFormats.Graphics;
 
 namespace OpenRA.Graphics
