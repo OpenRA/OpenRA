97c52f15f329cd4f61e1c7446d80b6fa4c74bd59
Paul Kulchenko <paul@kulchenko.com>
2012-02-07 23:41:29 -0800

    Added highlight style to markup

--- src/editor/markup.lua
+++ src/editor/markup.lua
@@ -7,16 +7,18 @@ local MD_MARK_BOLD = '*' -- bold
 local MD_MARK_LINK = '~' -- link
 local MD_MARK_HEAD = '!' -- header
 local MD_MARK_CODE = '@' -- code
+local MD_MARK_BOXD = '|' -- highlight
 local MD_MARK_LSEP = ';' -- link separator (between text and link)
 local MD_MARK_MARK = ' ' -- separator
 local MD_LINK_NEWWINDOW = '+' -- indicator to open a new window for links
 local markup = {
+  [MD_MARK_BOXD] = {st=25, fg={127,0,127}, bg=comment.bg, b=true},
   [MD_MARK_CODE] = {st=26, fg={127,127,127}, bg=comment.bg, fs=9},
-  [MD_MARK_HEAD] = {st=27, fg=comment.fg, bg=comment.bg, fn="Lucida Console", fs=10, b=true},
+  [MD_MARK_HEAD] = {st=27, fg=comment.fg, bg=comment.bg, fn="Lucida Console", b=true},
   [MD_MARK_LINK] = {st=28, fg=comment.fg, bg=comment.bg, u=true, hs={0, 0, 127}},
   [MD_MARK_BOLD] = {st=29, fg=comment.fg, bg=comment.bg, b=true, fs=11},
   [MD_MARK_ITAL] = {st=30, fg=comment.fg, bg=comment.bg, i=true},
-  [MD_MARK_MARK] = {st=31, fg=comment.fg, bg=comment.bg, v=false}, 
+  [MD_MARK_MARK] = {st=31, fg=comment.fg, bg=comment.bg, v=false},
 }
 
 local MD_MARK_PTRN = ''  -- combination of all markup marks that can start styling
@@ -90,11 +92,12 @@ local function ismarkup (tx)
       s,e,cap = string.find(tx,"^(%"..MD_MARK_HEAD..".+[%w%p])", start)
     elseif sep == MD_MARK_LINK then
       local any = "[^%"..MD_MARK_LINK.."%"..MD_MARK_LSEP.."]-"
-      s,e,cap = string.find(tx,"(%"..MD_MARK_LINK.."[%w%p]"..any.."[%w%p]"..
-                                "%"..MD_MARK_LSEP.."[%w%p]"..any.."[%w%p]"..
-                                "%"..MD_MARK_LINK..")", start)
+      s,e,cap = string.find(tx,"^(%"..MD_MARK_LINK.."[%w%p]"..any.."[%w%p]"..
+                                 "%"..MD_MARK_LSEP.."[%w%p]"..any.."[%w%p]"..
+                                 "%"..MD_MARK_LINK..")", st)
     else
-      s,e,cap = string.find(tx,"(%"..sep.."[%w%p][^%"..sep.."]-[%w%p]%"..sep..")", start)
+      s,e,cap = string.find(tx,"^(%"..sep.."[%w%p][^%"..sep.."]-[%w%p]%"..sep..")", st)
+      if not s then s,e,cap = string.find(tx,"^(%"..sep.."[^%s%"..sep.."]%"..sep..")", st) end
     end
     if s then return s,e,cap,sep end
     start = st+1
