9f4393c83e52fa1aada0f0cf36b3a02e00421869
James Dunne <james.jdunne@gmail.com>
2012-06-18 18:55:23 -0500

    Improvements to Harvester AI.

--- OpenRA.Mods.RA/Activities/FindResources.cs
+++ OpenRA.Mods.RA/Activities/FindResources.cs
@@ -24,19 +24,48 @@ namespace OpenRA.Mods.RA.Activities
 			if (IsCanceled || NextActivity != null) return NextActivity;
 
 			var harv = self.Trait<Harvester>();
+
 			if (harv.IsFull)
 				return Util.SequenceActivities(new DeliverResources(), NextActivity);
 
 			var harvInfo = self.Info.Traits.Get<HarvesterInfo>();
 			var mobile = self.Trait<Mobile>();
 			var mobileInfo = self.Info.Traits.Get<MobileInfo>();
-			var res = self.World.WorldActor.Trait<ResourceLayer>();
-			var path = self.World.WorldActor.Trait<PathFinder>().FindPath(PathSearch.Search(self.World, mobileInfo, self.Owner, true)
-						.WithHeuristic(loc => (res.GetResource(loc) != null && harvInfo.Resources.Contains(res.GetResource(loc).info.Name)) ? 0 : 1)
-						.FromPoint(self.Location));
+			var resLayer = self.World.WorldActor.Trait<ResourceLayer>();
+
+			// Find harvestable resources nearby:
+			var path = self.World.WorldActor.Trait<PathFinder>().FindPath(
+				PathSearch.Search(self.World, mobileInfo, self.Owner, true)
+						  .WithHeuristic(loc =>
+							{
+								// Get the resource at this location:
+								var resType = resLayer.GetResource(loc);
+
+								if (resType == null) return 1;
+								// Harvester can only collect resources of one type at a time:
+								if (!harvInfo.Resources.Contains(resType.info.Name)) return 1;
+								// Another harvester has claimed this resource:
+								if (resLayer.IsClaimedBy(harv, loc)) return 1;
+
+								//// Avoid less dense resources:
+								//if (resLayer.GetResourceDensity(loc) < 1) return 1;
+
+								return 0;
+							})
+				// Start searching either from the last harvested cell or our current position:
+						  .FromPoint(harv.LastHarvestedCell ?? self.Location)
+			);
 
 			if (path.Count == 0)
 				return NextActivity;
+
+			// NOTE(jsd): do not iterate through path[n] because the current location
+			// may be included in the list and moving to the current location will
+			// cause the activity code to go into an infinite loop.
+
+			// Attempt to claim a resource as ours:
+			if (!harv.ClaimResource(self, path[0]))
+				return this;
 
 			self.SetTargetLine(Target.FromCell(path[0]), Color.Red, false);
 			return Util.SequenceActivities(mobile.MoveTo(path[0], 1), new HarvestResource(), this);
@@ -55,23 +84,38 @@ namespace OpenRA.Mods.RA.Activities
 		public override Activity Tick(Actor self)
 		{
 			if (isHarvesting) return this;
-			if (IsCanceled) return NextActivity;
+
 			var harv = self.Trait<Harvester>();
+			if (IsCanceled)
+			{
+				harv.UnclaimResource(self);
+				return NextActivity;
+			}
+
 			harv.LastHarvestedCell = self.Location;
 
+			var resLayer = self.World.WorldActor.Trait<ResourceLayer>();
+
 			if (harv.IsFull)
+			{
+				harv.UnclaimResource(self);
 				return NextActivity;
+			}
 
 			var renderUnit = self.Trait<RenderUnit>();	/* better have one of these! */
-			var resource = self.World.WorldActor.Trait<ResourceLayer>().Harvest(self.Location);
+			var resource = resLayer.Harvest(self.Location);
 			if (resource == null)
+			{
+				harv.UnclaimResource(self);
 				return NextActivity;
+			}
 
 			if (renderUnit.anim.CurrentSequence.Name != "harvest")
 			{
 				isHarvesting = true;
 				renderUnit.PlayCustomAnimation(self, "harvest", () => isHarvesting = false);
 			}
+
 			harv.AcceptResource(resource);
 			return this;
 		}
