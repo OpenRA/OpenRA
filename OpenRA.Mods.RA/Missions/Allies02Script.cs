c505954b07c897994d153b57d7232e16dd6d7fef
Scott_NZ <Scott_NZ>
2012-08-08 10:38:53 +1200

    bleh, tabify

--- OpenRA.Mods.RA/Missions/Allies02Script.cs
+++ OpenRA.Mods.RA/Missions/Allies02Script.cs
@@ -13,35 +13,35 @@ using OpenRA.Traits;
 
 namespace OpenRA.Mods.RA.Missions
 {
-    class Allies02ScriptInfo : TraitInfo<Allies02Script>, Requires<SpawnMapActorsInfo> { }
+	class Allies02ScriptInfo : TraitInfo<Allies02Script>, Requires<SpawnMapActorsInfo> { }
 
-    class Allies02Script : IWorldLoaded, ITick
-    {
-        Actor chinookHusk;
-        Actor sam1;
-        Actor sam2;
-        Actor sam3;
+	class Allies02Script : IWorldLoaded, ITick
+	{
+		Actor chinookHusk;
+		Actor sam1;
+		Actor sam2;
+		Actor sam3;
 
-        Player allies;
-        Player soviets;
+		Player allies;
+		Player soviets;
 
-        public void Tick(Actor self)
-        {
+		public void Tick(Actor self)
+		{
 
-        }
+		}
 
-        public void WorldLoaded(World w)
-        {
-            allies = w.Players.Single(p => p.InternalName == "Allies");
-            soviets = w.Players.Single(p => p.InternalName == "Soviets");
-            var actors = w.WorldActor.Trait<SpawnMapActors>().Actors;
-            chinookHusk = actors["ChinookHusk"];
-            sam1 = actors["SAM1"];
-            sam2 = actors["SAM2"];
-            sam3 = actors["SAM3"];
-            w.WorldActor.Trait<Shroud>().Explore(w, sam1.Location, 3);
-            w.WorldActor.Trait<Shroud>().Explore(w, sam2.Location, 3);
-            w.WorldActor.Trait<Shroud>().Explore(w, sam3.Location, 3);
-        }
-    }
+		public void WorldLoaded(World w)
+		{
+			allies = w.Players.Single(p => p.InternalName == "Allies");
+			soviets = w.Players.Single(p => p.InternalName == "Soviets");
+			var actors = w.WorldActor.Trait<SpawnMapActors>().Actors;
+			chinookHusk = actors["ChinookHusk"];
+			sam1 = actors["SAM1"];
+			sam2 = actors["SAM2"];
+			sam3 = actors["SAM3"];
+			w.WorldActor.Trait<Shroud>().Explore(w, sam1.Location, 3);
+			w.WorldActor.Trait<Shroud>().Explore(w, sam2.Location, 3);
+			w.WorldActor.Trait<Shroud>().Explore(w, sam3.Location, 3);
+		}
+	}
 }
