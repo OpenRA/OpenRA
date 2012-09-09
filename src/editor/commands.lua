66fdef608819403799d4871b58200b4daa26b88e
Paul Kulchenko <paul@kulchenko.com>
2012-09-08 23:33:56 -0700

    Fixed stopping the debugger when an editor tab where debugging happens is closed.
    Closing tab didn't work when the debugging session was initiated
    externally and not by the IDE.

--- src/editor/commands.lua
+++ src/editor/commands.lua
@@ -263,7 +263,7 @@ function ClosePage(selection)
     -- abort the debugger if the current marker is in the window being closed
     -- also abort the debugger if it is running, as we don't know what
     -- window will need to be activated when the debugger is paused
-    if debugger and debugger.pid and
+    if debugger and debugger.server and
       (debugger.running or editor:MarkerNext(0, CURRENT_LINE_MARKER_VALUE) >= 0) then
       debugger.terminate()
     end
