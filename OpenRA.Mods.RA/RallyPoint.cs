cbe5d9f18a400d4f08ed6407ac4dc8f12103940f
Vladimir Komarov <vovandr90@mail.ru>
2012-10-20 01:33:15 +0300

    adding new ai functional

--- OpenRA.Mods.RA/RallyPoint.cs
+++ OpenRA.Mods.RA/RallyPoint.cs
@@ -1,4 +1,4 @@
-#region Copyright & License Information
+﻿#region Copyright & License Information
 /*
  * Copyright 2007-2011 The OpenRA Developers (see AUTHORS)
  * This file is part of OpenRA, which is free software. It is made
@@ -23,15 +23,16 @@ namespace OpenRA.Mods.RA
 
 	public class RallyPoint : IIssueOrder, IResolveOrder, ISync
 	{
-		[Sync] public CPos rallyPoint;
-		public int nearEnough = 1;
+        [Sync]
+        public CPos rallyPoint;
+        public int nearEnough = 1;
 
-		public RallyPoint(Actor self)
-		{
-			var info = self.Info.Traits.Get<RallyPointInfo>();
-			rallyPoint = self.Location + new CVec(info.RallyPoint[0], info.RallyPoint[1]);
-			self.World.AddFrameEndTask(w => w.Add(new Effects.RallyPoint(self)));
-		}
+        public RallyPoint(Actor self)
+        {
+            var info = self.Info.Traits.Get<RallyPointInfo>();
+            rallyPoint = self.Location + new CVec(info.RallyPoint[0], info.RallyPoint[1]);
+            self.World.AddFrameEndTask(w => w.Add(new Effects.RallyPoint(self)));
+        }
 
 		public IEnumerable<IOrderTargeter> Orders
 		{
