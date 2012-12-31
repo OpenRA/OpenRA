c8e84b3a11e3ee9d63fb8284bd1bc9fe06af7cf5
Scott_NZ <Scott_NZ>
2012-12-31 16:31:04 +1300

    Add license notice

--- OpenRA.Mods.RA/Missions/Soviet01ClassicScript.cs
+++ OpenRA.Mods.RA/Missions/Soviet01ClassicScript.cs
@@ -1,16 +1,23 @@
-using System;
+#region Copyright & License Information
+/*
+ * Copyright 2007-2012 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation. For more information,
+ * see COPYING.
+ */
+#endregion
+
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using OpenRA.Traits;
-using System.Drawing;
-using OpenRA.Mods.RA.Move;
+using OpenRA.FileFormats;
+using OpenRA.Mods.RA.Activities;
+using OpenRA.Mods.RA.Air;
 using OpenRA.Mods.RA.Buildings;
+using OpenRA.Mods.RA.Move;
 using OpenRA.Network;
 using OpenRA.Scripting;
-using OpenRA.FileFormats;
-using OpenRA.Mods.RA.Air;
-using OpenRA.Mods.RA.Activities;
+using OpenRA.Traits;
 
 namespace OpenRA.Mods.RA.Missions
 {
