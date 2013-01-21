9f47e754928dd6855f58de85296bcc112ecf33ab
Scott_NZ <Scott_NZ>
2013-01-21 16:49:48 +1300

    Allies 01: only set eligible units to defensive stance

--- OpenRA.Mods.RA/Missions/Allies01Script.cs
+++ OpenRA.Mods.RA/Missions/Allies01Script.cs
@@ -281,7 +281,7 @@ namespace OpenRA.Mods.RA.Missions
 
 		void SetAlliedUnitsToDefensiveStance()
 		{
-			foreach (var actor in world.Actors.Where(a => a.IsInWorld && a.Owner == allies && !a.IsDead()))
+			foreach (var actor in world.Actors.Where(a => a.IsInWorld && a.Owner == allies && !a.IsDead() && a.HasTrait<AutoTarget>()))
 			{
 				actor.Trait<AutoTarget>().stance = UnitStance.Defend;
 			}
