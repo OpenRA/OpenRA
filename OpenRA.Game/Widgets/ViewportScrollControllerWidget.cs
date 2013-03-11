239e15e1a56aa890b1708e79b1f9822427ba1d72
Bugra Cuhadaroglu <bugrac@live.com>
2013-03-10 22:26:00 -0400

    Diagonal scrolling area in viewport corners are too small
    I added two options, one is for EdgeScrollTreshold and the other one is
    EdgeCornerScrollThreshold. You can modify these threshold as much as you
    want.
    
    Note: This code is related with the 2720 numbered issue.
    URL:https://github.com/OpenRA/OpenRA/issues/2720

--- OpenRA.Game/Widgets/ViewportScrollControllerWidget.cs
+++ OpenRA.Game/Widgets/ViewportScrollControllerWidget.cs
@@ -18,6 +18,7 @@ namespace OpenRA.Widgets
 	public class ViewportScrollControllerWidget : Widget
 	{
 		public int EdgeScrollThreshold = 15;
+        public int EdgeCornerScrollThreshold = 35;
 
 		ScrollDirection Keyboard;
 		ScrollDirection Edge;
@@ -94,6 +95,49 @@ namespace OpenRA.Widgets
 			Edge = ScrollDirection.None;
 			if (Game.Settings.Game.ViewportEdgeScroll && Game.HasInputFocus)
 			{
+                // First let's check if the mouse is on the corners:
+                if (Viewport.LastMousePos.X >= Game.viewport.Width - EdgeCornerScrollThreshold &&
+                    Viewport.LastMousePos.Y >= Game.viewport.Height - EdgeCornerScrollThreshold) //Bottom Right
+                {
+                    Edge = Edge.Set(ScrollDirection.Right, true);
+                    Scroll();
+                    Edge = Edge.Set(ScrollDirection.Down, true);
+                    Scroll();
+                    return;
+                }
+                if (Viewport.LastMousePos.X < EdgeCornerScrollThreshold &&
+                   Viewport.LastMousePos.Y >= Game.viewport.Height - EdgeCornerScrollThreshold) //Bottom Left
+                {
+                    Edge = Edge.Set(ScrollDirection.Left, true);
+                    Scroll();
+                    Edge = Edge.Set(ScrollDirection.Down, true);
+                    Scroll();
+                    return;
+                }
+
+                if (Viewport.LastMousePos.X >= Game.viewport.Width - EdgeCornerScrollThreshold &&
+                   Viewport.LastMousePos.Y < EdgeCornerScrollThreshold) //Top Right
+                {
+                    Edge = Edge.Set(ScrollDirection.Right, true);
+                    Scroll();
+                    Edge = Edge.Set(ScrollDirection.Up, true);
+                    Scroll();
+                    return;
+                }
+
+                if (Viewport.LastMousePos.X < EdgeCornerScrollThreshold  &&
+                    Viewport.LastMousePos.Y < EdgeCornerScrollThreshold) //Top Left
+                {
+                    Edge = Edge.Set(ScrollDirection.Left, true);
+                    Scroll();
+                    Edge = Edge.Set(ScrollDirection.Up, true);
+                    Scroll();
+                    return;
+                }
+
+                
+                //Check for corner ends here now let's check the edges:
+
 				// Check for edge-scroll
 				if (Viewport.LastMousePos.X < EdgeScrollThreshold)
 					Edge = Edge.Set(ScrollDirection.Left, true);
@@ -103,28 +147,36 @@ namespace OpenRA.Widgets
 					Edge = Edge.Set(ScrollDirection.Right, true);
 				if (Viewport.LastMousePos.Y >= Game.viewport.Height - EdgeScrollThreshold)
 					Edge = Edge.Set(ScrollDirection.Down, true);
+
+                Scroll();
+                //Check for edge-scroll ends here.
 			}
 
-			if(Keyboard != ScrollDirection.None || Edge != ScrollDirection.None)
-			{
-				var scroll = new float2(0, 0);
-
-				if (Keyboard.Includes(ScrollDirection.Up) || Edge.Includes(ScrollDirection.Up))
-					scroll += new float2(0, -1);
-				if (Keyboard.Includes(ScrollDirection.Right) || Edge.Includes(ScrollDirection.Right))
-					scroll += new float2(1, 0);
-				if (Keyboard.Includes(ScrollDirection.Down) || Edge.Includes(ScrollDirection.Down))
-					scroll += new float2(0, 1);
-				if (Keyboard.Includes(ScrollDirection.Left) || Edge.Includes(ScrollDirection.Left))
-					scroll += new float2(-1, 0);
-
-				float length = Math.Max(1, scroll.Length);
-				scroll.X = (scroll.X / length) * Game.Settings.Game.ViewportEdgeScrollStep;
-				scroll.Y = (scroll.Y / length) * Game.Settings.Game.ViewportEdgeScrollStep;
-
-				Game.viewport.Scroll(scroll);
-			}
+			
 		}
+
+        public void Scroll()
+        {
+            if (Keyboard != ScrollDirection.None || Edge != ScrollDirection.None)
+            {
+                var scroll = new float2(0, 0);
+
+                if (Keyboard.Includes(ScrollDirection.Up) || Edge.Includes(ScrollDirection.Up))
+                    scroll += new float2(0, -1);
+                if (Keyboard.Includes(ScrollDirection.Right) || Edge.Includes(ScrollDirection.Right))
+                    scroll += new float2(1, 0);
+                if (Keyboard.Includes(ScrollDirection.Down) || Edge.Includes(ScrollDirection.Down))
+                    scroll += new float2(0, 1);
+                if (Keyboard.Includes(ScrollDirection.Left) || Edge.Includes(ScrollDirection.Left))
+                    scroll += new float2(-1, 0);
+
+                float length = Math.Max(1, scroll.Length);
+                scroll.X = (scroll.X / length) * Game.Settings.Game.ViewportEdgeScrollStep;
+                scroll.Y = (scroll.Y / length) * Game.Settings.Game.ViewportEdgeScrollStep;
+
+                Game.viewport.Scroll(scroll);
+            }
+        }
 
 		public override Widget Clone() { return new ViewportScrollControllerWidget(this); }
 	}
