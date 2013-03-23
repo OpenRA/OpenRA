10ec82d1bb07c6f221c0fcc2033182b726f335f2
Igor Popov <ihptru@gmail.com>
2013-03-23 19:46:41 +0400

    give user info that new map has been downloaded (being in lobby). so he is not confused with the reason of black screen

--- OpenRA.Mods.RA/Widgets/Logic/LobbyLogic.cs
+++ OpenRA.Mods.RA/Widgets/Logic/LobbyLogic.cs
@@ -321,7 +321,10 @@ namespace OpenRA.Mods.RA.Widgets.Logic
 
 			if (!Game.modData.AvailableMaps.ContainsKey (MapUid))
 				if (Game.Settings.Game.AllowDownloading)
+				{
 					Game.DownloadMap (MapUid);
+					Game.Debug("A new map has been downloaded...");
+				}
 				else
 					throw new InvalidOperationException("Server's new map doesn't exist on your system and Downloading turned off");
 			Map = new Map(Game.modData.AvailableMaps[MapUid].Path);
