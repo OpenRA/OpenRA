da127f24d301d70bae6edd39727c6c762bda2206
Matthias Mailänder <matthias@mailaender.name>
2013-04-06 09:45:49 +0200

    separate wiki update script

--- packaging/upload-all.sh
+++ packaging/upload-all.sh
@@ -22,12 +22,3 @@ upload linux/rpm openra-${LINUXVERSION}-1.noarch.rpm
 upload linux/arch openra-${LINUXVERSION}-1-any.pkg.tar.xz
 
 curl http://${SERVER}/home/syncdownloads
-
-echo "Updating https://github.com/OpenRA/OpenRA/wiki/Traits"
-rm -rf openra-wiki
-git clone git@github.com:OpenRA/OpenRA.wiki.git openra-wiki
-cp -fr ../DOCUMENTATION.md openra-wiki/Traits.md
-cd openra-wiki
-git add Traits.md
-git commit -m "Update trait documentation"
-git push origin master
