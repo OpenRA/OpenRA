52797e8b79f0d690b7c3c648f1f0886b1883d065
Matthias Mailänder <matthias@mailaender.name>
2013-04-28 08:05:02 +0200

    added a new d2k title font
    from http://www.d2kplus.com/font.php
    by http://forum.dune2k.com/user/2251-d2k-sardaukar/

--- packaging/package-all.sh
+++ packaging/package-all.sh
@@ -30,7 +30,7 @@ markdown DOCUMENTATION.md > DOCUMENTATION.html
 # they are now installed to the game directory instead of placed in the gac
 FILES="OpenRA.Game.exe OpenRA.Editor.exe OpenRA.Utility.exe OpenRA.FileFormats.dll \
 OpenRA.Renderer.SdlCommon.dll OpenRA.Renderer.Cg.dll OpenRA.Renderer.Gl.dll OpenRA.Renderer.Null.dll \
-FreeSans.ttf FreeSansBold.ttf titles.ttf cg glsl mods/ra mods/cnc mods/d2k \
+FreeSans.ttf FreeSansBold.ttf titles.ttf Dune2k.ttf cg glsl mods/ra mods/cnc mods/d2k \
 README.html CONTRIBUTING.html DOCUMENTATION.html COPYING HACKING INSTALL CHANGELOG"
 
 echo "Copying files..."
