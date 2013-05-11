05d187d327a336df21f95fd2887f941ab48021b8
Matthias Mailänder <matthias@mailaender.name>
2013-05-11 12:56:04 +0200

    consistent spy disguise radar color override in spectator mode

--- OpenRA.Mods.RA/Spy.cs
+++ OpenRA.Mods.RA/Spy.cs
@@ -115,8 +115,7 @@ namespace OpenRA.Mods.RA
 
 		public Color RadarColorOverride(Actor self)
 		{
-			if (!Disguised || self.World.LocalPlayer == null ||
-				self.Owner.Stances[self.World.LocalPlayer] == Stance.Ally)
+			if (!Disguised || self.Owner.IsAlliedWith(self.World.RenderPlayer))
 				return self.Owner.Color.RGB;
 
 			return disguisedAsPlayer.Color.RGB;
