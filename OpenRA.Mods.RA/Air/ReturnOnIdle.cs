d3255fe6a49226f4a4cc97841e5a24c4cc8958aa
Matthias Mailänder <matthias@mailaender.name>
2013-05-11 09:44:46 +0200

    don't try to return on idle when aircraft is dead

--- OpenRA.Mods.RA/Air/ReturnOnIdle.cs
+++ OpenRA.Mods.RA/Air/ReturnOnIdle.cs
@@ -1,6 +1,6 @@
 ﻿#region Copyright & License Information
 /*
- * Copyright 2007-2011 The OpenRA Developers (see AUTHORS)
+ * Copyright 2007-2013 The OpenRA Developers (see AUTHORS)
  * This file is part of OpenRA, which is free software. It is made
  * available to you under the terms of the GNU General Public License
  * as published by the Free Software Foundation. For more information,
@@ -23,6 +23,8 @@ namespace OpenRA.Mods.RA.Air
 	{
 		public void TickIdle(Actor self)
 		{
+			if (self.IsDead()) return;
+
 			var altitude = self.Trait<Aircraft>().Altitude;
 			if (altitude == 0) return;	// we're on the ground, let's stay there.
 
