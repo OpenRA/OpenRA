da8202a15e8b4005e0312eded80bd56ea69821dc
Paul Chote <paul@chote.net>
2013-06-11 18:48:11 +1200

    Clean up LineRenderer whitespace (no code changes).

--- OpenRA.Game/Graphics/LineRenderer.cs
+++ OpenRA.Game/Graphics/LineRenderer.cs
@@ -21,10 +21,10 @@ namespace OpenRA.Graphics
 		Renderer renderer;
 		IShader shader;
 
-		Vertex[] vertices = new Vertex[ Renderer.TempBufferSize ];
+		Vertex[] vertices = new Vertex[Renderer.TempBufferSize];
 		int nv = 0;
 
-		public LineRenderer( Renderer renderer, IShader shader )
+		public LineRenderer(Renderer renderer, IShader shader)
 		{
 			this.renderer = renderer;
 			this.shader = shader;
@@ -32,49 +32,49 @@ namespace OpenRA.Graphics
 
 		public void Flush()
 		{
-			if( nv > 0 )
+			if (nv > 0)
 			{
-				shader.Render( () =>
+				shader.Render(() =>
 				{
 					var vb = renderer.GetTempVertexBuffer();
-					vb.SetData( vertices, nv );
+					vb.SetData(vertices, nv);
 					renderer.SetLineWidth(LineWidth * Game.viewport.Zoom);
-					renderer.DrawBatch( vb,	0, nv, PrimitiveType.LineList );
-				} );
+					renderer.DrawBatch(vb, 0, nv, PrimitiveType.LineList);
+				});
 
 				nv = 0;
 			}
 		}
 
-		public void DrawRect( float2 tl, float2 br, Color c )
+		public void DrawRect(float2 tl, float2 br, Color c)
 		{
-			var tr = new float2( br.X, tl.Y );
-			var bl = new float2( tl.X, br.Y );
-			DrawLine( tl, tr, c, c );
-			DrawLine( tl, bl, c, c );
-			DrawLine( tr, br, c, c );
-			DrawLine( bl, br, c, c );
+			var tr = new float2(br.X, tl.Y);
+			var bl = new float2(tl.X, br.Y);
+			DrawLine(tl, tr, c, c);
+			DrawLine(tl, bl, c, c);
+			DrawLine(tr, br, c, c);
+			DrawLine(bl, br, c, c);
 		}
 
-		public void DrawLine( float2 start, float2 end, Color startColor, Color endColor )
+		public void DrawLine(float2 start, float2 end, Color startColor, Color endColor)
 		{
 			Renderer.CurrentBatchRenderer = this;
 
-			if( nv + 2 > Renderer.TempBufferSize )
+			if (nv + 2 > Renderer.TempBufferSize)
 				Flush();
 
-			vertices[ nv++ ] = new Vertex( start + offset,
-				new float2( startColor.R / 255.0f, startColor.G / 255.0f ),
-				new float2( startColor.B / 255.0f, startColor.A / 255.0f ) );
+			vertices[nv++] = new Vertex(start + offset,
+				new float2(startColor.R / 255.0f, startColor.G / 255.0f),
+				new float2(startColor.B / 255.0f, startColor.A / 255.0f));
 
-			vertices[ nv++ ] = new Vertex( end + offset,
-				new float2( endColor.R / 255.0f, endColor.G / 255.0f ),
-				new float2( endColor.B / 255.0f, endColor.A / 255.0f ) );
+			vertices[nv++] = new Vertex(end + offset,
+				new float2(endColor.R / 255.0f, endColor.G / 255.0f),
+				new float2(endColor.B / 255.0f, endColor.A / 255.0f));
 		}
 
-		public void FillRect( RectangleF r, Color color )
+		public void FillRect(RectangleF r, Color color)
 		{
-			for (float y = r.Top; y < r.Bottom; y++)
+			for (var y = r.Top; y < r.Bottom; y++)
 				DrawLine(new float2(r.Left, y), new float2(r.Right, y), color, color);
 		}
 
