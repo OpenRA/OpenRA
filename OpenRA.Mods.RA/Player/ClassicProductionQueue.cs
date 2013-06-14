06375fe1a3306b68839102cde5fc59f3a26525c9
Curtis Shmyr <cjshmyr@gmail.com>
2013-06-13 18:31:30 -0600

    Fix BuildTimeSpeedReduction being one step (building) too early with build speed reduction

--- OpenRA.Mods.RA/Player/ClassicProductionQueue.cs
+++ OpenRA.Mods.RA/Player/ClassicProductionQueue.cs
@@ -106,7 +106,7 @@ namespace OpenRA.Mods.RA
 					.Where(p => p.Trait.Info.Produces.Contains(unit.Traits.Get<BuildableInfo>().Queue))
 						.Where(p => p.Actor.Owner == self.Owner).ToArray();
 
-				var speedModifier = Math.Min(selfsameBuildings.Count(), Info.BuildTimeSpeedReduction.Length - 1);
+				var speedModifier = Math.Min(selfsameBuildings.Count(), Info.BuildTimeSpeedReduction.Length) - 1;
 				time = (time * Info.BuildTimeSpeedReduction[speedModifier]) / 100;
 			}
 
