6259918648906d9eabf1e7c640c2f8ad54968f80
Paul Kulchenko <paul@kulchenko.com>
2013-06-24 21:03:07 -0700

    Added an example of styling individual keywords.

--- cfg/user-snippet.lua
+++ cfg/user-snippet.lua
@@ -57,3 +57,17 @@ ide.app.postinit = function()
   end
 end
 --]]
+
+--[[ An example of how individual keywords can be styled
+local G = ... -- this now points to the global environment in the script
+local luaspec = G.ide.specs['lua']
+
+local num = #luaspec.keywords
+-- take a new slot in the list of keywords (starting from 1)
+luaspec.keywords[num+1] = 'return'
+-- remove 'return' from the list of "regular" keywords
+luaspec.keywords[1] = luaspec.keywords[1]:gsub(' return', '')
+
+-- assign new style to the added slot (starting from 0)
+styles["keywords"..num] = {fg = {240, 0, 0}, b = true}
+--]]
