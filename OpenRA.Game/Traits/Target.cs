ac3f63b59c143aff1e9a74ada2fec4ba9d8eff5f
Paul Chote <paul@chote.net>
2013-07-21 10:03:48 +1200

    Remove IHasLocation lookup from Target.

--- OpenRA.Game/Traits/Target.cs
+++ OpenRA.Game/Traits/Target.cs
@@ -45,8 +45,7 @@ namespace OpenRA.Traits
 		}
 
 		public bool IsValid { get { return valid && (actor == null || (actor.IsInWorld && !actor.IsDead() && actor.Generation == generation)); } }
-		public PPos PxPosition { get { return IsActor ? actor.Trait<IHasLocation>().PxPosition : PPos.FromWPos(pos); } }
-		public PPos CenterLocation { get { return PxPosition; } }
+		public PPos CenterLocation { get { return IsActor ? actor.CenterLocation : PPos.FromWPos(pos); } }
 		public Actor Actor { get { return IsActor ? actor : null; } }
 
 		// TODO: This should return true even if the actor is destroyed
