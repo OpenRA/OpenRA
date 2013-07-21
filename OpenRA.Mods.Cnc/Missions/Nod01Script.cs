cff5b8bfb0f58a9afa55dea9edcd5539f068fbb6
Paul Chote <paul@chote.net>
2013-07-21 20:41:04 +1200

    Remove rebase cruft.

--- OpenRA.Mods.Cnc/Missions/Nod01Script.cs
+++ OpenRA.Mods.Cnc/Missions/Nod01Script.cs
@@ -5,7 +5,6 @@
  * available to you under the terms of the GNU General Public License
  * as published by the Free Software Foundation. For more information,
  * see COPYING.
-<<<<<<< HEAD
  */
 #endregion
 
