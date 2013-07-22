bf9c737a11abf90a4336c853dbbd62733489a2bd
Paul Chote <paul@chote.net>
2013-07-22 20:21:23 +1200

    Remove obsolete Gauss1D/Gauss2D functions.

--- OpenRA.Mods.RA/TeslaZapRenderable.cs
+++ OpenRA.Mods.RA/TeslaZapRenderable.cs
@@ -101,8 +101,8 @@ namespace OpenRA.Mods.RA
 			var renderables = new List<IRenderable>();
 			if (Game.CosmeticRandom.Next(2) != 0)
 			{
-				var p1 = from + (1 / 3f) * dist + Game.CosmeticRandom.Gauss1D(1) * .2f * dist.Length * norm;
-				var p2 = from + (2 / 3f) * dist + Game.CosmeticRandom.Gauss1D(1) * .2f * dist.Length * norm;
+				var p1 = from + (1 / 3f) * dist + WRange.FromPDF(Game.CosmeticRandom, 2).Range * dist.Length / 4096 * norm;
+				var p2 = from + (2 / 3f) * dist + WRange.FromPDF(Game.CosmeticRandom, 2).Range * dist.Length / 4096 * norm;
 
 				renderables.AddRange(DrawZap(wr, from, p1, s, out p1));
 				renderables.AddRange(DrawZap(wr, p1, p2, s, out p2));
@@ -110,7 +110,7 @@ namespace OpenRA.Mods.RA
 			}
 			else
 			{
-				var p1 = from + (1 / 2f) * dist + Game.CosmeticRandom.Gauss1D(1) * .2f * dist.Length * norm;
+				var p1 = from + (1 / 2f) * dist + WRange.FromPDF(Game.CosmeticRandom, 2).Range * dist.Length / 4096 * norm;
 
 				renderables.AddRange(DrawZap(wr, from, p1, s, out p1));
 				renderables.AddRange(DrawZap(wr, p1, to, s, out z));
