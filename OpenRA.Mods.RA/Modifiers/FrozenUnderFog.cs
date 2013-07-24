126fc79eed924f98e488099d7038e7a31f219ab3
Paul Chote <paul@chote.net>
2013-07-24 22:36:57 +1200

    Fix FrozenUnderFog.

--- OpenRA.Mods.RA/Modifiers/FrozenUnderFog.cs
+++ OpenRA.Mods.RA/Modifiers/FrozenUnderFog.cs
@@ -1,6 +1,6 @@
 ﻿#region Copyright & License Information
 /*
- * Copyright 2007-2011 The OpenRA Developers (see AUTHORS)
+ * Copyright 2007-2013 The OpenRA Developers (see AUTHORS)
  * This file is part of OpenRA, which is free software. It is made
  * available to you under the terms of the GNU General Public License
  * as published by the Free Software Foundation. For more information,
@@ -11,25 +11,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using OpenRA.Graphics;
+using OpenRA.Mods.RA.Buildings;
+using OpenRA.Mods.RA.Effects;
 using OpenRA.Traits;
 
 namespace OpenRA.Mods.RA
 {
-	class FrozenUnderFogInfo : TraitInfo<FrozenUnderFog> {}
-
-	class FrozenUnderFog : IRenderModifier, IVisibilityModifier
+	public class FrozenUnderFogInfo : ITraitInfo, Requires<BuildingInfo>, Requires<RenderSpritesInfo>
 	{
-		public bool IsVisible(Actor self, Player byPlayer)
+		public object Create(ActorInitializer init) { return new FrozenUnderFog(init.self); }
+	}
+
+	public class FrozenUnderFog : IRenderModifier, IVisibilityModifier, ITickRender
+	{
+		FrozenActorProxy proxy;
+		IEnumerable<CPos> footprint;
+		bool visible;
+
+		public FrozenUnderFog(Actor self)
 		{
-			return byPlayer == null || Shroud.GetVisOrigins(self).Any(o => byPlayer.Shroud.IsVisible(o));
+			footprint = FootprintUtils.Tiles(self);
+			proxy = new FrozenActorProxy(self, footprint);
+			self.World.AddFrameEndTask(w => w.Add(proxy));
 		}
 
-		IRenderable[] cache = { };
+		public bool IsVisible(Actor self, Player byPlayer)
+		{
+			return byPlayer == null || footprint.Any(c => byPlayer.Shroud.IsVisible(c));
+		}
+
+		public void TickRender(WorldRenderer wr, Actor self)
+		{
+			if (self.Destroyed)
+				return;
+
+			visible = IsVisible(self, self.World.RenderPlayer);
+			if (visible)
+				proxy.SetRenderables(self.Render(wr));
+		}
+
 		public IEnumerable<IRenderable> ModifyRender(Actor self, WorldRenderer wr, IEnumerable<IRenderable> r)
 		{
-			if (IsVisible(self, self.World.RenderPlayer))
-				cache = r.ToArray();
-			return cache;
+			return visible ? r : SpriteRenderable.None;
 		}
 	}
 }
