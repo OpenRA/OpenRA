f95c19e5b295db6da591657866659d4a624f0542
Paul Chote <paul@chote.net>
2013-07-24 17:21:55 +1200

    Fix BibLayer style nits.

--- OpenRA.Mods.RA/Buildings/BibLayer.cs
+++ OpenRA.Mods.RA/Buildings/BibLayer.cs
@@ -1,6 +1,6 @@
 #region Copyright & License Information
 /*
- * Copyright 2007-2011 The OpenRA Developers (see AUTHORS)
+ * Copyright 2007-2013 The OpenRA Developers (see AUTHORS)
  * This file is part of OpenRA, which is free software. It is made
  * available to you under the terms of the GNU General Public License
  * as published by the Free Software Foundation. For more information,
@@ -19,12 +19,12 @@ namespace OpenRA.Mods.RA.Buildings
 {
 	class BibLayerInfo : ITraitInfo
 	{
-		public readonly string[] BibTypes = {"bib3", "bib2", "bib1"};
-		public readonly int[] BibWidths = {2,3,4};
+		public readonly string[] BibTypes = { "bib3", "bib2", "bib1" };
+		public readonly int[] BibWidths = { 2, 3, 4 };
 		public object Create(ActorInitializer init) { return new BibLayer(init.self, this); }
 	}
 
-	class BibLayer: IRenderOverlay, IWorldLoaded
+	class BibLayer : IRenderOverlay, IWorldLoaded
 	{
 		World world;
 		BibLayerInfo info;
@@ -36,10 +36,8 @@ namespace OpenRA.Mods.RA.Buildings
 			this.info = info;
 			bibSprites = info.BibTypes.Select(x => Game.modData.SpriteLoader.LoadAllSprites(x)).ToArray();
 
-			self.World.ActorAdded +=
-				a => { if (a.HasTrait<Bib>()) DoBib(a,true); };
-			self.World.ActorRemoved +=
-				a => { if (a.HasTrait<Bib>()) DoBib(a,false); };
+			self.World.ActorAdded += a => DoBib(a, true);
+			self.World.ActorRemoved += a => DoBib(a, false);
 		}
 
 		public void WorldLoaded(World w)
@@ -50,18 +48,21 @@ namespace OpenRA.Mods.RA.Buildings
 
 		public void DoBib(Actor b, bool isAdd)
 		{
+			if (!b.HasTrait<Bib>())
+				return;
+
 			var buildingInfo = b.Info.Traits.Get<BuildingInfo>();
 			var size = buildingInfo.Dimensions.X;
 			var bibOffset = buildingInfo.Dimensions.Y - 1;
 
-			int bib = Array.IndexOf(info.BibWidths,size);
+			int bib = Array.IndexOf(info.BibWidths, size);
 			if (bib < 0)
 			{
 				Log.Write("debug", "Cannot bib {0}-wide building {1}", size, b.Info.Name);
 				return;
 			}
 
-			for (int i = 0; i < 2 * size; i++)
+			for (var i = 0; i < 2 * size; i++)
 			{
 				var p = b.Location + new CVec(i % size, i / size + bibOffset);
 				if (isAdd)
@@ -71,10 +72,11 @@ namespace OpenRA.Mods.RA.Buildings
 			}
 		}
 
-		public void Render( WorldRenderer wr )
+		public void Render(WorldRenderer wr)
 		{
 			var pal = wr.Palette("terrain");
 			var cliprect = Game.viewport.WorldBounds(world);
+
 			foreach (var kv in tiles)
 			{
 				if (!cliprect.Contains(kv.Key.X, kv.Key.Y))
