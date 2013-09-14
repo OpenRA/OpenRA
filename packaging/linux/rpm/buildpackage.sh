c880e47adde5db41d78d2986c086b3608c1fea0b
Matthias Mailänder <matthias@mailaender.name>
2013-09-14 20:48:06 +0200

    list files again to avoid owning standard dirs in RPM
    reverts commit 9d256d93a59302c06d26f36c88125ce574d7e360
    
    fixes file /usr/bin from install of openra-release.20130915-1.noarch
    conflicts with file from package filesystem-3.2-13.fc19.x86_64

--- packaging/linux/rpm/buildpackage.sh
+++ packaging/linux/rpm/buildpackage.sh
@@ -13,7 +13,7 @@ sed -i "s/{VERSION_FIELD}/$PKGVERSION/" openra.spec
 rootdir=`readlink -f $2`
 sed -i "s|{ROOT_DIR}|$rootdir|" openra.spec
 
-for x in `find $rootdir -type d`
+for x in `find $rootdir -type f`
 do
     y="${x#$rootdir}"
     sed -i "/%files/ a ${y}" openra.spec
