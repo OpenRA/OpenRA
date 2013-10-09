2993c11d2219f2ba80db8354b27f517f1890cbb3
Phrohdoh <Phrohdoh@gmail.com>
2013-10-09 13:55:53 -0400

    Update EjectOnDeath.cs
    System now works for ground units as well as aircraft!

--- OpenRA.Mods.RA/Air/EjectOnDeath.cs
+++ OpenRA.Mods.RA/Air/EjectOnDeath.cs
@@ -8,6 +8,7 @@
  */
 #endregion
 
+using System.Linq;
 using OpenRA.FileFormats;
 using OpenRA.Mods.RA.Effects;
 using OpenRA.Traits;
@@ -17,12 +18,11 @@ namespace OpenRA.Mods.RA
 	public class EjectOnDeathInfo : TraitInfo<EjectOnDeath>
 	{
 		[ActorReference]
-		public readonly string PilotActor = "MEDI"; // E1
-		public readonly int SuccessRate = 100;		//	50
+		public readonly string PilotActor = "E1";
+		public readonly int SuccessRate = 50;
 		public readonly string ChuteSound = "chute1.aud";
-		public readonly bool PilotEjectInAir = true;
-		public readonly bool PilotEjectOnGround = true;
-		//public readonly WRange MinimumEjectHeight = new WRange(427);
+		public readonly bool PilotEjectInAir = false;
+		public readonly bool PilotEjectOnGround = false;
 	}
 
 	public class EjectOnDeath : INotifyKilled
@@ -30,53 +30,30 @@ namespace OpenRA.Mods.RA
 		public void Killed(Actor self, AttackInfo e)
 		{
 			var info = self.Info.Traits.Get<EjectOnDeathInfo>();
-
-			var pilot = self.World.CreateActor
-			            (
-				            false, info.PilotActor.ToLowerInvariant(),
-							new TypeDictionary
-							{
-								new OwnerInit(self.Owner),
-								new LocationInit(self.Location)
-							}
-				         );
-
+			var pilot = self.World.CreateActor(false, info.PilotActor.ToLowerInvariant(),new TypeDictionary{new OwnerInit(self.Owner),new LocationInit(self.Location)});
 			var r = self.World.SharedRandom.Next(1, 100);
 			var cp = self.CenterPosition;
 
-			if (IsSuitableCell(pilot, self.Location)
-			    && r > 100 - info.SuccessRate
-			    && self.Owner.WinState != WinState.Lost)
+			if (IsSuitableCell(self, pilot, self.Location) && r > 100 - info.SuccessRate && self.Owner.WinState != WinState.Lost)
 			{
-				//Game.Debug("Suitable. Success Rate: " + 100 - info.SuccessRate + ". Not Lost.");
 				if (cp.Z > 0 && info.PilotEjectInAir == true)
 				{
-					Game.Debug("Z > 0, EjectInAir: " + info.PilotEjectInAir.ToString());
 					self.World.AddFrameEndTask(w => w.Add(new Parachute(pilot, cp)));
 					Sound.Play(info.ChuteSound, cp);
 				}
 				else if (cp.Z == 0 && info.PilotEjectOnGround == true)
-				{
-					Game.Debug("Z == 0, EjectOnGround: " + info.PilotEjectOnGround.ToString());
 					self.World.AddFrameEndTask(w => w.Add(pilot));
-				}
-				else
-				{
-					Game.Debug("Something went wrong.");
-				}
 			}
 			else
-			{
-				Game.Debug("pilot.Destroy()");
-				Game.Debug("Suitable: " + IsSuitableCell(pilot, self.Location));    //  this returns false
-				Game.Debug("SuccessRate: 100 - " + info.SuccessRate);
 				pilot.Destroy();
-			}
 		}
 
-		bool IsSuitableCell(Actor actorToDrop, CPos p)
+		bool IsSuitableCell(Actor self, Actor actorToDrop, CPos p)
 		{
-			return actorToDrop.Trait<IPositionable>().CanEnterCell(p);
+			if (self.World.ActorMap.GetUnitsAt(self.Location).Count() > 1)
+				return false;
+			else
+				return true;
 		}
 	}
 }
