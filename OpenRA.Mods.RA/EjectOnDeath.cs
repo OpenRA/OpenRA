602978b1824970cc3dd707537f6b28386c0828d9
Phrohdoh <Phrohdoh@gmail.com>
2013-10-10 01:37:38 -0400

    Include another safety check for empty cells.
    Some renaming and cleaning up.
    Added another check for EjectInAir attempts.

--- OpenRA.Mods.RA/EjectOnDeath.cs
+++ OpenRA.Mods.RA/EjectOnDeath.cs
@@ -21,8 +21,8 @@ namespace OpenRA.Mods.RA
 		public readonly string PilotActor = "E1";
 		public readonly int SuccessRate = 50;
 		public readonly string ChuteSound = "chute1.aud";
-		public readonly bool PilotEjectInAir = false;
-		public readonly bool PilotEjectOnGround = false;
+		public readonly bool EjectInAir = false;
+		public readonly bool EjectOnGround = false;
 	}
 
 	public class EjectOnDeath : INotifyKilled
@@ -30,18 +30,22 @@ namespace OpenRA.Mods.RA
 		public void Killed(Actor self, AttackInfo e)
 		{
 			var info = self.Info.Traits.Get<EjectOnDeathInfo>();
-			var pilot = self.World.CreateActor(false, info.PilotActor.ToLowerInvariant(),new TypeDictionary{new OwnerInit(self.Owner),new LocationInit(self.Location)});
 			var r = self.World.SharedRandom.Next(1, 100);
+
+			if (r <= 100 - info.SuccessRate || self.Owner.WinState != WinState.Lost)
+				return;
+
+			var pilot = self.World.CreateActor(false, info.PilotActor.ToLowerInvariant(),new TypeDictionary{new OwnerInit(self.Owner),new LocationInit(self.Location)});
 			var cp = self.CenterPosition;
 
-			if (IsSuitableCell(self, pilot, self.Location) && r > 100 - info.SuccessRate && self.Owner.WinState != WinState.Lost)
+			if (IsSuitableCell(self, pilot, self.Location)/* && r > 100 - info.SuccessRate && self.Owner.WinState != WinState.Lost*/)
 			{
-				if (cp.Z > 0 && info.PilotEjectInAir == true)
+				if (cp.Z > 0 && info.EjectInAir)
 				{
 					self.World.AddFrameEndTask(w => w.Add(new Parachute(pilot, cp)));
 					Sound.Play(info.ChuteSound, cp);
 				}
-				else if (cp.Z == 0 && info.PilotEjectOnGround == true)
+				else if (cp.Z == 0 && info.EjectOnGround)
 					self.World.AddFrameEndTask(w => w.Add(pilot));
 			}
 			else
@@ -50,10 +54,20 @@ namespace OpenRA.Mods.RA
 
 		bool IsSuitableCell(Actor self, Actor actorToDrop, CPos p)
 		{
-			if (self.World.ActorMap.GetUnitsAt(self.Location).Count() > 1)
-				return false;
-			else
-				return true;
+			var info = self.Info.Traits.Get<EjectOnDeathInfo>();
+			var cp = self.CenterPosition;
+
+			if (cp.Z == 0 && info.EjectOnGround)
+			{
+				if (self.World.ActorMap.GetUnitsAt(self.Location).Count() >= 1)
+					return false;
+				else
+					return true;
+			}
+			if (cp.Z > 0 && info.EjectInAir)
+				return actorToDrop.Trait<IPositionable>().CanEnterCell(p);
+
+			return false;
 		}
 	}
 }
