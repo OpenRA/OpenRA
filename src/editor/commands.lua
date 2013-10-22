799ee9061c2e917918ab36b42ee2077b24fb821f
Paul Kulchenko <paul@kulchenko.com>
2013-10-21 22:09:22 -0700

    Fixed format of the file name reported after compilation errors.

--- src/editor/commands.lua
+++ src/editor/commands.lua
@@ -527,28 +527,25 @@ function CompileProgram(editor, params)
   local editorText = editor:GetText():gsub("^#!.-\n", "\n")
   local id = editor:GetId()
   local filePath = DebuggerMakeFileName(editor, openDocuments[id].filePath)
-  local func, line_num, errMsg, _ = loadstring(editorText, filePath), -1
-  if not func then
-    _, errMsg, line_num = wxlua.CompileLuaScript(editorText, filePath)
-  end
+  local func, err = loadstring(editorText, '@'..filePath)
+  local line = not func and tonumber(err:match(":(%d+)%s*:")) or nil
 
   if ide.frame.menuBar:IsChecked(ID_CLEAROUTPUT) then ClearOutput() end
 
   compileTotal = compileTotal + 1
-  if line_num > -1 then
-    DisplayOutput(TR("Compilation error")
-      .." "..TR("on line %d"):format(line_num)
-      ..":\n"..errMsg:gsub("Lua:.-\n", ""))
-    if params.jumponerror then editor:GotoLine(line_num-1) end
-  else
+  if func then
     compileOk = compileOk + 1
     if params.reportstats then
       DisplayOutputLn(TR("Compilation successful; %.0f%% success rate (%d/%d).")
         :format(compileOk/compileTotal*100, compileOk, compileTotal))
     end
+  else
+    DisplayOutputLn(TR("Compilation error").." "..TR("on line %d"):format(line)..":")
+    DisplayOutputLn((err:gsub("\n$", "")))
+    if line and params.jumponerror then editor:GotoLine(line-1) end
   end
 
-  return line_num == -1, editorText -- return true if it compiled ok
+  return func ~= nil, editorText -- return true if it compiled ok
 end
 
 ------------------
