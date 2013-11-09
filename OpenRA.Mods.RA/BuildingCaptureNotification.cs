fb6bb63b29aba30d8f2ed8e6c3b17c6884fdf6b8
Matthias Mailänder <matthias@mailaender.name>
2013-11-09 10:26:09 +0100

    removed BuildingCapture redundancy

--- OpenRA.Mods.RA/BuildingCaptureNotification.cs
+++ OpenRA.Mods.RA/BuildingCaptureNotification.cs
@@ -1,6 +1,6 @@
 #region Copyright & License Information
 /*
- * Copyright 2007-2011 The OpenRA Developers (see AUTHORS)
+ * Copyright 2007-2013 The OpenRA Developers (see AUTHORS)
  * This file is part of OpenRA, which is free software. It is made
  * available to you under the terms of the GNU General Public License
  * as published by the Free Software Foundation. For more information,
@@ -15,25 +15,26 @@ namespace OpenRA.Mods.RA
 	class CaptureNotificationInfo : ITraitInfo
 	{
 		public readonly string Notification = "BuildingCaptured";
+		public readonly bool NewOwnerVoice = true;
 
 		public object Create(ActorInitializer init) { return new CaptureNotification(this); }
 	}
 
 	class CaptureNotification : INotifyCapture
 	{
-		CaptureNotificationInfo Info;
+		CaptureNotificationInfo info;
 		public CaptureNotification(CaptureNotificationInfo info)
 		{
-			Info = info;
+			this.info = info;
 		}
 
-		public void OnCapture (Actor self, Actor captor, Player oldOwner, Player newOwner)
+		public void OnCapture(Actor self, Actor captor, Player oldOwner, Player newOwner)
 		{
 			if (captor.World.LocalPlayer != captor.Owner)
 				return;
 
-			Sound.PlayNotification(captor.World.LocalPlayer, "Speech", Info.Notification, oldOwner.Country.Race);
+			var race = info.NewOwnerVoice ? newOwner.Country.Race : oldOwner.Country.Race;
+			Sound.PlayNotification(captor.World.LocalPlayer, "Speech", info.Notification, race);
 		}
 	}
 }
-
