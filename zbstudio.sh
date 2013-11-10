292f7b176b8961f7f0feeafe46cbcb3ce29574e0
Paul Kulchenko <paul@kulchenko.com>
2013-11-09 23:47:45 -0800

    Added changing directory when launching on Linux (closes #157).

--- zbstudio.sh
+++ zbstudio.sh
@@ -4,5 +4,7 @@ if [[ $(uname) == 'Darwin' ]]; then
   open zbstudio/ZeroBraneStudio.app --args "$@"
 else
   if [[ "$(uname -m)" == "x86_64" ]]; then ARCH="x64"; else ARCH="x86"; fi
-  bin/linux/$ARCH/lua src/main.lua zbstudio "$@" &
+  # Special thanks to StackOverflow wiki (http://stackoverflow.com/a/246128)
+  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  (cd $DIR; bin/linux/$ARCH/lua src/main.lua zbstudio "$@") &
 fi
