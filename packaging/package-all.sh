3e5255d61952fdecf8952c6a3b0112c909e423b3
Matthias Mailänder <matthias@mailaender.name>
2013-11-16 11:21:33 +0100

    remove old and misleading note

--- packaging/package-all.sh
+++ packaging/package-all.sh
@@ -26,8 +26,6 @@ markdown CONTRIBUTING.md > CONTRIBUTING.html
 markdown DOCUMENTATION.md > DOCUMENTATION.html
 
 # List of files that are packaged on all platforms
-# Note that the Tao dlls are shipped on all platforms except osx and that
-# they are now installed to the game directory instead of placed in the gac
 FILES=('OpenRA.Game.exe' 'OpenRA.Editor.exe' 'OpenRA.Utility.exe' \
 'OpenRA.FileFormats.dll' 'OpenRA.Renderer.SdlCommon.dll' 'OpenRA.Renderer.Sdl2.dll' 'OpenRA.Renderer.Cg.dll' 'OpenRA.Renderer.Gl.dll' 'OpenRA.Renderer.Null.dll' 'OpenRA.Irc.dll' \
 'FreeSans.ttf' 'FreeSansBold.ttf' \
