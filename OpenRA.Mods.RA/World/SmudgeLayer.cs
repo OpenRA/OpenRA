473f8dfbb8701682978bd93da3c72d32f590f193
Paul Chote <paul@chote.net>
2013-12-03 18:16:40 +1300

    Fix an off-by-one error in SmudgeLayer. Fixes #4139.

--- OpenRA.Mods.RA/World/SmudgeLayer.cs
+++ OpenRA.Mods.RA/World/SmudgeLayer.cs
@@ -50,7 +50,7 @@ namespace OpenRA.Mods.RA
 
 			// Add map smudges
 			foreach (var s in w.Map.Smudges.Value.Where(s => Info.Types.Contains(s.Type)))
-				tiles.Add((CPos)s.Location, new TileReference<byte, byte>((byte)Array.IndexOf(Info.Types, s.Type), (byte)s.Depth));
+				tiles.Add((CPos)s.Location, new TileReference<byte, byte>((byte)(Array.IndexOf(Info.Types, s.Type) + 1), (byte)s.Depth));
 		}
 
 		public void AddSmudge(CPos loc)
