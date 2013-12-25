991edfdac3bcab765411144f979aec40cd50cee3
Christoph Kubisch <crazybutcher@luxinia.de>
2013-12-25 15:42:11 +0100

    updated luxinia2 interpreter to reflect new luxinia2 structure

--- interpreters/luxinia2.lua
+++ interpreters/luxinia2.lua
@@ -2,94 +2,60 @@
 return {
   name = "Luxinia2",
   description = "Luxinia2",
-  api = {"baselib","glfw","glewgl","assimp20","luxmath","luxgfx","luxscene","luajit2",},
-
-  finitclient = function(self)
-    if (not CommandLineRunning(self:fuid(wfilename))) then return end
-    if not ide.config.path.luxinia2 then wx.wxMessageBox("Please define 'path.luxinia2' in your cfg/user.lua (see estrela.lua for examples)"); return end
-    local init = dofile(ide.config.path.luxinia2.."/../comserver/client.lua")
-    local fenv = {}
-    setmetatable(fenv,{__index = _G})
-    fenv.print = function(...) DisplayOutput(...); DisplayOutput("\n"); end
-
-    setfenv(init,fenv)
-    local client = init()
-
-    self.fclient = client
-    return client
-  end,
+  api = {"baselib","glfw","glewgl","assimp20","luxmath","luxscene","luajit2",},
 
   frun = function(self,wfilename,rundebug)
-    local luxdir = ide.config.path.luxinia2
-    local projdir = ide.config.path.projectdir
-    assert(projdir and projdir:len()>0,"no project directory")
-    local basedir = luxdir
-    local startfile = projdir.."/main.lua"
-    local startargs = " -e "..startfile
-
-    if (CommandLineRunning(self:fuid(wfilename))) then
-      if (not self.fclient) then
-        self:finitclient()
-      end
-      -- try to communicate with server
-      self.fclient("dofile([["..wfilename:GetFullPath().."]])")
+    local editorDir = string.gsub(ide.editorFilename:gsub("[^/\\]+$",""),"\\","/")
+    local luxDir = ide.config.path.luxinia2 or os.getenv("LUXINIA2")
+    if (wx.wxFileName(luxDir):IsRelative()) then
+      luxDir = editorDir..luxDir
+    end
+    
+    if (not luxDir) then
+      DisplayOutputLn("Error: path.luxinia2 not set in config or LUXINIA2 environment missing")
       return
     end
-
-    self.fclient = nil
-    local fname = wfilename:GetFullName()
-    local args = (fname and (" -f "..fname) or "")
-
-    if rundebug then
-      DebuggerAttachDefault({
-          basedir=basedir,
-          startfile=startfile,
-          run=true, noshell=true,}
-      )
-      local editorDir = string.gsub(ide.editorFilename:gsub("[^/\\]+$",""),"\\","/")
-      script = ""..
-      "package.path=package.path..';"..editorDir.."lualibs/?/?.lua';"..
-      "io.stdout:setvbuf('no'); require('mobdebug').start('" .. ide.debugger.hostname.."',"..ide.debugger.portnumber..")"
-
-      args = args..' -es "'..script..'"'..startargs
-    else
-      args = " -s "..args..startargs
-    end
-
-    local jitargs = ide.config.luxinia2jitargs
-    jitargs = jitargs or ""
-    local cmd = 'luajit.exe '..jitargs..' ../main.lua '..args
-
-
-    local pid = CommandLineRun(cmd,ide.config.path.luxinia2,true,true,nil,self:fuid(wfilename),
-      function()
-        ShellSupportRemote(nil)
-        if (rundebug) then
-          DebuggerStop()
-        end
-      end)
+    local exe = luxDir.."/luajit.exe"
     
-    if(not pid) then return end
-
-    if not rundebug then
-      local client = self:finitclient()
-      ShellSupportRemote(client,self:fuid(wfilename))
-      pid = nil
+    local wdir = self:fworkdir(wfilename)
+    if (wx.wxFileExists(wdir.."/main.lua")) then
+      wfilename = wx.wxFileName(wdir.."/main.lua")
+      DisplayOutputLn("luxinia2: using project main.lua")
     end
+
+    local pid, proc
+    if (CommandLineRunning(self:fuid(wfilename))) then
+      -- kill process
+      wx.wxProcess.Kill(pid)
+    end
+    
+
+    local filepath = wfilename:GetFullName()
+    local args = ide.config.luxinia2jitargs or ""
+    if rundebug then
+      DebuggerAttachDefault({runstart = ide.config.debugger.runonstart == true})
+
+      local script = "package.path=package.path..';"..editorDir.."lualibs/?/?.lua';"
+      args = args..' -e "'..script..'" '
+    end
+    args = args..(rundebug 
+      and ([[ -e "io.stdout:setvbuf('no'); %s"]]):format(rundebug)
+       or ([[ -e "io.stdout:setvbuf('no')" "%s"]]):format(filepath))
+    local cmd = '"'..exe..'" '..args
+
+    local pid = CommandLineRun(cmd,wdir,true,true,nil,self:fuid(wfilename),
+      function() ide.debugger.pid = nil end)
     
     return pid
   end,
-  fuid = function(self,wfilename) return "luxinia2 "..(ide.config.path.projectdir or "") end,
+  fuid = function(self,wfilename) return "luxinia2 "..wfilename:GetFullName() end,
   fprojdir = function(self,wfilename)
-    local path = GetPathWithSep(wfilename)
-    filepath = wx.wxFileName(path)
-
-    while ((not wx.wxFileExists(path.."main.lua")) and (filepath:GetDirCount() > 0)) do
-      filepath:RemoveDir(filepath:GetDirCount()-1)
-      path = GetPathWithSep(filepath)
-    end
-
-    return path:sub(0,-2)
+    return wfilename:GetPath(wx.wxPATH_GET_VOLUME)
+  end,
+  fworkdir = function (self,wfilename)
+    return ide.config.path.projectdir or wfilename:GetPath(wx.wxPATH_GET_VOLUME)
   end,
   hasdebugger = true,
+  fattachdebug = function(self) DebuggerAttachDefault() end,
+  --scratchextloop = false, -- not supported yet
 }
