0e8513a3d2406822beae1bad3427a67a5196ac46
Matthias Mailänder <matthias@mailaender.name>
2014-02-10 17:45:44 +0100

    backfall to master.ihptru.net
    closes #2574

--- OpenRA.Game/GameRules/Settings.cs
+++ OpenRA.Game/GameRules/Settings.cs
@@ -25,7 +25,7 @@ namespace OpenRA.GameRules
 		public int ExternalPort = 1234;
 		public bool AdvertiseOnline = true;
 		public string Password = "";
-		public string[] MasterServers = { "http://master.open-ra.org/" };
+		public string[] MasterServers = { "http://master.open-ra.org/", "http://master.ihptru.net/" };
 		public bool DiscoverNatDevices = false; // Allow users to disable NAT discovery if problems occur
 		public bool AllowPortForward = true; // let the user disable it even if compatible devices are found
 		public bool NatDeviceAvailable = false; // internal check if discovery succeeded
