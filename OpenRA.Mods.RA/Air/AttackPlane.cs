116e4acda33ea4961a03cdd0b11edacf272a7ec0
Paul Chote <paul@chote.net>
2014-03-08 12:11:23 +1300

    Fix style nits in other Attack traits (no logic changes).

--- OpenRA.Mods.RA/Air/AttackPlane.cs
+++ OpenRA.Mods.RA/Air/AttackPlane.cs
@@ -1,6 +1,6 @@
 ﻿#region Copyright & License Information
 /*
- * Copyright 2007-2013 The OpenRA Developers (see AUTHORS)
+ * Copyright 2007-2014 The OpenRA Developers (see AUTHORS)
  * This file is part of OpenRA, which is free software. It is made
  * available to you under the terms of the GNU General Public License
  * as published by the Free Software Foundation. For more information,
@@ -19,7 +19,8 @@ namespace OpenRA.Mods.RA.Air
 
 	class AttackPlane : AttackFrontal
 	{
-		public AttackPlane(Actor self, AttackPlaneInfo info) : base(self, info) { }
+		public AttackPlane(Actor self, AttackPlaneInfo info)
+			: base(self, info) { }
 
 		public override Activity GetAttackActivity(Actor self, Target newTarget, bool allowMove)
 		{
--- OpenRA.Mods.RA/Attack/AttackLoyalty.cs
+++ OpenRA.Mods.RA/Attack/AttackLoyalty.cs
@@ -1,6 +1,6 @@
 ﻿#region Copyright & License Information
 /*
- * Copyright 2007-2011 The OpenRA Developers (see AUTHORS)
+ * Copyright 2007-2014 The OpenRA Developers (see AUTHORS)
  * This file is part of OpenRA, which is free software. It is made
  * available to you under the terms of the GNU General Public License
  * as published by the Free Software Foundation. For more information,
@@ -21,7 +21,7 @@ namespace OpenRA.Mods.RA
 	public class AttackLoyalty : AttackFrontal
 	{
 		public AttackLoyalty(Actor self, AttackLoyaltyInfo info)
-			: base( self, info ) {}
+			: base(self, info) { }
 
 		public override void DoAttack(Actor self, Target target)
 		{
