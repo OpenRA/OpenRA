c0ddad642f0a760e8fcc69427621fe577c36d156
Paul Kulchenko <paul@kulchenko.com>
2014-03-06 18:21:55 -0800

    Fixed lunching zbstudio from folders with spaces; thanks to @Asmageddon.

--- zbstudio.sh
+++ zbstudio.sh
@@ -6,5 +6,5 @@ else
   if [[ "$(uname -m)" == "x86_64" ]]; then ARCH="x64"; else ARCH="x86"; fi
   # Special thanks to StackOverflow wiki (http://stackoverflow.com/a/246128)
   DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-  (cd $DIR; bin/linux/$ARCH/lua src/main.lua zbstudio "$@") &
+  (cd "$DIR"; bin/linux/$ARCH/lua src/main.lua zbstudio "$@") &
 fi
