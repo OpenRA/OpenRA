9c3b35d8fe27b8ffb335d1f91944829dc1dfc4aa
Paul Kulchenko <paul@kulchenko.com>
2014-03-15 17:44:22 -0700

    Added support for running zbstudio script from any folder on OSX.

--- zbstudio.sh
+++ zbstudio.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+# Thanks to StackOverflow wiki (http://stackoverflow.com/a/246128)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 if [[ $(uname) == 'Darwin' ]]; then
-  open zbstudio/ZeroBraneStudio.app --args "$@"
+  (cd "$DIR"; open zbstudio/ZeroBraneStudio.app --args "$@")
 else
   if [[ "$(uname -m)" == "x86_64" ]]; then ARCH="x64"; else ARCH="x86"; fi
-  # Special thanks to StackOverflow wiki (http://stackoverflow.com/a/246128)
-  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
   (cd "$DIR"; bin/linux/$ARCH/lua src/main.lua zbstudio "$@") &
 fi
