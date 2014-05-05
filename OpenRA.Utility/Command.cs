b1045dfce6933b2740bbfb9de16acd825105b856
Matthias Mailänder <Mailaender@users.noreply.github.com>
2014-05-05 12:03:01 +0200

    Update Command.cs
    WRange is actually a 1D world distance with 1024 units = 1 cell.

--- OpenRA.Utility/Command.cs
+++ OpenRA.Utility/Command.cs
@@ -1,6 +1,6 @@
 #region Copyright & License Information
 /*
- * Copyright 2007-2012 The OpenRA Developers (see AUTHORS)
+ * Copyright 2007-2014 The OpenRA Developers (see AUTHORS)
  * This file is part of OpenRA, which is free software. It is made
  * available to you under the terms of the GNU General Public License
  * as published by the Free Software Foundation. For more information,
@@ -284,7 +284,7 @@ namespace OpenRA.Utility
 				return "3D World Position";
 
 			if (t == typeof(WRange))
-				return "3D World Range";
+				return "1D World Range";
 
 			if (t == typeof(WVec))
 				return "3D World Vector";
