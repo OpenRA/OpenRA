98e48ae991acb6b2e4c51246d4928c0e7093a5aa
reaperrr <thereaperrr@aol.com>
2014-05-09 01:14:42 +0200

    Converts InfDeath from integer to string.
    Wire up updated DeathSounds for all mods.

--- OpenRA.Mods.RA/TeslaInstantKills.cs
+++ OpenRA.Mods.RA/TeslaInstantKills.cs
@@ -1,6 +1,6 @@
 ﻿#region Copyright & License Information
 /*
- * Copyright 2007-2011 The OpenRA Developers (see AUTHORS)
+ * Copyright 2007-2014 The OpenRA Developers (see AUTHORS)
  * This file is part of OpenRA, which is free software. It is made
  * available to you under the terms of the GNU General Public License
  * as published by the Free Software Foundation. For more information,
@@ -13,13 +13,23 @@ using OpenRA.Traits;
 
 namespace OpenRA.Mods.RA
 {
-	class TeslaInstantKillsInfo : TraitInfo<TeslaInstantKills> { }
+	public class TeslaInstantKillsInfo : ITraitInfo
+	{ 
+		[Desc("InfDeath that leads to instant kill.")]
+		public readonly string InfDeath = "6";
 
-	class TeslaInstantKills : IDamageModifier
+		public object Create(ActorInitializer init) { return new TeslaInstantKills(this); }
+	}
+
+	public class TeslaInstantKills : IDamageModifier
 	{
-		public float GetDamageModifier(Actor attacker, WarheadInfo warhead )
+		TeslaInstantKillsInfo info;
+
+		public TeslaInstantKills(TeslaInstantKillsInfo info) { this.info = info; }
+
+		public float GetDamageModifier(Actor attacker, WarheadInfo warhead)
 		{
-			if( warhead != null && warhead.InfDeath == 6 )
+			if( warhead != null && warhead.InfDeath == info.InfDeath )
 				return 1000f;
 			return 1f;
 		}
