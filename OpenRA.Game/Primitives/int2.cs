bd55ffc10d086b21ec1c92a458c074fd8799bb74
Matthias Mailänder <matthias@mailaender.name>
2014-05-09 08:44:50 +0200

    StyleCop

--- OpenRA.Game/Primitives/int2.cs
+++ OpenRA.Game/Primitives/int2.cs
@@ -15,11 +15,11 @@ namespace OpenRA
 {
 	public struct int2
 	{
-		public int X,Y;
+		public int X, Y;
 
-		public int2( int x, int y ) { this.X = x; this.Y = y; }
-		public int2( Point p ) { X = p.X; Y = p.Y; }
-		public int2( Size p ) { X = p.Width; Y = p.Height; }
+		public int2(int x, int y) { this.X = x; this.Y = y; }
+		public int2(Point p) { X = p.X; Y = p.Y; }
+		public int2(Size p) { X = p.Width; Y = p.Height; }
 
 		public static int2 operator +(int2 a, int2 b) { return new int2(a.X + b.X, a.Y + b.Y); }
 		public static int2 operator -(int2 a, int2 b) { return new int2(a.X - b.X, a.Y - b.Y); }
@@ -29,11 +29,11 @@ namespace OpenRA
 
 		public static int2 operator -(int2 a) { return new int2(-a.X, -a.Y); }
 
-		public static bool operator ==(int2 me, int2 other) { return (me.X == other.X && me.Y == other.Y); }
+		public static bool operator ==(int2 me, int2 other) { return me.X == other.X && me.Y == other.Y; }
 		public static bool operator !=(int2 me, int2 other) { return !(me == other); }
 
 		public int2 Sign() { return new int2(Math.Sign(X), Math.Sign(Y)); }
-		public int2 Abs() { return new int2( Math.Abs( X ), Math.Abs( Y ) ); }
+		public int2 Abs() { return new int2(Math.Abs(X), Math.Abs(Y)); }
 		public int LengthSquared { get { return X * X + Y * Y; } }
 		public int Length { get { return (int)Math.Sqrt(LengthSquared); } }
 		public override int GetHashCode() { return X.GetHashCode() ^ Y.GetHashCode(); }
@@ -77,6 +77,5 @@ namespace OpenRA
 		}
 
 		public static int Dot(int2 a, int2 b) { return a.X * b.X + a.Y * b.Y; }
-
 	}
 }
