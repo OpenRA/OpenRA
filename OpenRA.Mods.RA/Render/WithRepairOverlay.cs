5274776d5507c3ed2714f359e4f6c209d3013958
Matthias Mailänder <matthias@mailaender.name>
2014-05-20 10:52:04 +0200

    add the Dune 2000 repair depot active overlay

---
+++ OpenRA.Mods.RA/Render/WithRepairOverlay.cs
@@ -0,0 +1,77 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2014 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation. For more information,
+ * see COPYING.
+ */
+#endregion
+
+using System.Linq;
+using OpenRA.FileFormats;
+using OpenRA.Graphics;
+using OpenRA.Traits;
+using OpenRA.Mods.RA.Buildings;
+using OpenRA.Effects;
+
+namespace OpenRA.Mods.RA.Render
+{
+	public class WithRepairOverlayInfo : ITraitInfo, Requires<RenderSpritesInfo>, Requires<IBodyOrientationInfo>
+	{
+		[Desc("Sequence name to use")]
+		public readonly string Sequence = "active";
+
+		[Desc("Position relative to body")]
+		public readonly WVec Offset = WVec.Zero;
+
+		public readonly bool PauseOnLowPower = false;
+
+		public object Create(ActorInitializer init) { return new WithRepairOverlay(init.self, this); }
+	}
+
+	public class WithRepairOverlay : INotifyDamageStateChanged, INotifyBuildComplete, INotifySold, INotifyRepair
+	{
+		Animation overlay;
+		bool buildComplete;
+
+		public WithRepairOverlay(Actor self, WithRepairOverlayInfo info)
+		{
+			var rs = self.Trait<RenderSprites>();
+			var body = self.Trait<IBodyOrientation>();
+			var disabled = self.TraitsImplementing<IDisable>();
+
+			buildComplete = !self.HasTrait<Building>(); // always render instantly for units
+			overlay = new Animation(self.World, rs.GetImage(self));
+			overlay.Play(info.Sequence);
+			rs.anims.Add("repair_{0}".F(info.Sequence),
+				new AnimationWithOffset(overlay,
+					() => body.LocalToWorld(info.Offset.Rotate(body.QuantizeOrientation(self, self.Orientation))),
+					() => !buildComplete,
+					() => info.PauseOnLowPower && disabled.Any(d => d.Disabled),
+					p => WithTurret.ZOffsetFromCenter(self, p, 1)));
+		}
+
+		public void BuildingComplete(Actor self)
+		{
+			self.World.AddFrameEndTask(w => w.Add(new DelayedAction(120, () =>
+				buildComplete = true)));
+		}
+
+		public void Sold(Actor self) { }
+		public void Selling(Actor self)
+		{
+			buildComplete = false;
+		}
+
+		public void DamageStateChanged(Actor self, AttackInfo e)
+		{
+			overlay.ReplaceAnim(RenderSprites.NormalizeSequence(overlay, e.DamageState, overlay.CurrentSequence.Name));
+		}
+
+		public void Repairing(Actor self, Actor host)
+		{
+			overlay.Play(overlay.CurrentSequence.Name);
+		}
+	}
+}
