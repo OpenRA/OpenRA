521ca70254c7df3ddece0f5415f5f15995ab8c33
Matthias Mailänder <matthias@mailaender.name>
2014-06-16 10:30:14 +0200

    set the git user and email to avoid problems

--- packaging/update-wiki.sh
+++ packaging/update-wiki.sh
@@ -1,3 +1,6 @@
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+
 echo "Updating https://github.com/OpenRA/OpenRA/wiki/Traits"
 rm -rf openra-wiki
 git clone git@github.com:OpenRA/OpenRA.wiki.git openra-wiki
