ef2e02d00863ce35ac4cd3c56d40c4968475975c
Matthias Mailänder <matthias@mailaender.name>
2014-06-18 12:30:40 +0200

    document ScaredyCat

--- OpenRA.Mods.RA/ScaredyCat.cs
+++ OpenRA.Mods.RA/ScaredyCat.cs
@@ -1,6 +1,6 @@
 #region Copyright & License Information
 /*
- * Copyright 2007-2011 The OpenRA Developers (see AUTHORS)
+ * Copyright 2007-2014 The OpenRA Developers (see AUTHORS)
  * This file is part of OpenRA, which is free software. It is made
  * available to you under the terms of the GNU General Public License
  * as published by the Free Software Foundation. For more information,
@@ -13,6 +13,7 @@ using OpenRA.Traits;
 
 namespace OpenRA.Mods.RA
 {
+	[Desc("Makes the unit automatically run around when taking damage.")]
 	class ScaredyCatInfo : ITraitInfo
 	{
 		public readonly int PanicLength = 25 * 10;
