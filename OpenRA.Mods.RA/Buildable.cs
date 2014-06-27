362c86764f7a0cb69bdb732b468e40273c83d8ca
Matthias Mailänder <matthias@mailaender.name>
2014-06-27 17:31:47 +0200

    document the buildable and country trait

--- OpenRA.Mods.RA/Buildable.cs
+++ OpenRA.Mods.RA/Buildable.cs
@@ -1,6 +1,6 @@
 #region Copyright & License Information
 /*
- * Copyright 2007-2011 The OpenRA Developers (see AUTHORS)
+ * Copyright 2007-2014 The OpenRA Developers (see AUTHORS)
  * This file is part of OpenRA, which is free software. It is made
  * available to you under the terms of the GNU General Public License
  * as published by the Free Software Foundation. For more information,
@@ -14,10 +14,20 @@ namespace OpenRA.Mods.RA
 {
 	public class BuildableInfo : TraitInfo<Buildable>
 	{
+		[Desc("The prerequisite names that must be available before this can be built.",
+			"This can be prefixed with ! to invert the prerequisite (disabling production if the prerequisite is available)",
+			"and/or ~ to hide the actor from the production palette if the prerequisite is not available.",
+			"Prerequisites are granted by actors with the Building trait (with a prerequisite string given by the lower case actor name)",
+			"and by the ProvidesCustomPrerequisite trait.")]
 		public readonly string[] Prerequisites = { };
+
+		[Desc("Restrict production to a specific race(s). **Deprecated**: Use race-specific prerequisites instead.")]
 		public readonly string[] Owner = { };
 
+		[Desc("Production queue(s) that can produce this.")]
 		public readonly string[] Queue = { };
+
+		[Desc("Disable production when there are more than this many of this actor on the battlefield. Set to 0 to disable.")]
 		public readonly int BuildLimit = 0;
 
 		// TODO: UI fluff; doesn't belong here
