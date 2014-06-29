ec1e0e86121744d3938def394f792b9e820038ed
Oliver Brakmann <ob-web2013+github@posteo.net>
2014-06-30 00:08:48 +0200

    More Lua API enhancements

--- OpenRA.Mods.RA/Scripting/Global/DateTimeGlobal.cs
+++ OpenRA.Mods.RA/Scripting/Global/DateTimeGlobal.cs
@@ -17,12 +17,26 @@ namespace OpenRA.Mods.RA.Scripting
 	[ScriptGlobal("Date")]
 	public class DateGlobal : ScriptGlobal
 	{
-		public DateGlobal(ScriptContext context) : base(context) { }
+		public DateGlobal(ScriptContext context)
+			: base(context) { }
 
 		[Desc("True on the 31st of October.")]
 		public bool IsHalloween
 		{
 			get { return DateTime.Today.Month == 10 && DateTime.Today.Day == 31; }
+		}
+	}
+
+	[ScriptGlobal("Time")]
+	public class TimeGlobal : ScriptGlobal
+	{
+		public TimeGlobal(ScriptContext context)
+			: base(context) { }
+
+		[Desc("Get the current game time (in ticks)")]
+		public int GameTime
+		{
+			get { return context.World.WorldTick; }
 		}
 	}
 }
