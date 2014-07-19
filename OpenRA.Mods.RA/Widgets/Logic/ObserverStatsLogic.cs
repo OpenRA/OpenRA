d88b27745b6232130d652dc23424cedd4d326a27
Paul Chote <paul@chote.net>
2014-07-19 17:57:01 +1200

    Fix observer buttons.

--- OpenRA.Mods.RA/Widgets/Logic/ObserverStatsLogic.cs
+++ OpenRA.Mods.RA/Widgets/Logic/ObserverStatsLogic.cs
@@ -40,7 +40,7 @@ namespace OpenRA.Mods.RA.Widgets.Logic
 		WorldRenderer worldRenderer;
 
 		[ObjectCreator.UseCtor]
-		public ObserverStatsLogic(World world, WorldRenderer worldRenderer, Widget widget)
+		public ObserverStatsLogic(World world, WorldRenderer worldRenderer, Widget widget, Action onExit, bool transient)
 		{
 			this.world = world;
 			this.worldRenderer = worldRenderer;
@@ -136,6 +136,21 @@ namespace OpenRA.Mods.RA.Widgets.Logic
 
 			ClearStats();
 			DisplayStats(BasicStats);
+
+			var close = widget.GetOrNull<ButtonWidget>("CLOSE");
+			if (close != null)
+			{
+				close.OnClick = () =>
+				{
+					if (transient)
+					{
+						Ui.CloseWindow();
+						Ui.Root.RemoveChild(widget);
+					}
+
+					onExit();
+				};
+			}
 		}
 
 		void ClearStats()
