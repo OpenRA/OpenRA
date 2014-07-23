778931f65014b81afe5b0d0a7efa8dcaf179af2a
Matthias Mailänder <matthias@mailaender.name>
2014-07-23 07:07:16 +0200

    document ReturnOnIdle

--- OpenRA.Mods.RA/Air/ReturnOnIdle.cs
+++ OpenRA.Mods.RA/Air/ReturnOnIdle.cs
@@ -1,6 +1,6 @@
 ﻿#region Copyright & License Information
 /*
- * Copyright 2007-2013 The OpenRA Developers (see AUTHORS)
+ * Copyright 2007-2014 The OpenRA Developers (see AUTHORS)
  * This file is part of OpenRA, which is free software. It is made
  * available to you under the terms of the GNU General Public License
  * as published by the Free Software Foundation. For more information,
@@ -15,6 +15,7 @@ using OpenRA.Traits;
 
 namespace OpenRA.Mods.RA.Air
 {
+	[Desc("Return to a player owned RearmBuildings. If none available, head back to base and circle over it.")]
 	class ReturnOnIdleInfo : TraitInfo<ReturnOnIdle> { }
 
 	class ReturnOnIdle : INotifyIdle
