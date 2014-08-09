aa41638a80784aae3599233002ae085e937f6250
Phoib <emiel.suilen@gmail.com>
2014-08-09 18:22:07 +0200

    Added Trait GivePlayerExperience

--- OpenRA.Mods.RA/GivesPlayerExperience.cs
+++ OpenRA.Mods.RA/GivesPlayerExperience.cs
@@ -15,23 +15,22 @@ using OpenRA.Traits;
 
 namespace OpenRA.Mods.RA
 {
-	[Desc("You get money for playing this actor.")]
-	class GivesBountyInfo : TraitInfo<GivesBounty>
+	[Desc("You get player experience for playing this actor.")]
+	class GivesPlayerExperienceInfo : TraitInfo<GivesPlayerExperience>
 	{
-		[Desc("Calculated by Cost or CustomSellValue so they have to be set to avoid crashes.")]
-		public readonly int Percentage = 10;
+		public readonly int Percentage = 100;
 		[Desc("Higher ranked units give higher bounties.")]
 		public readonly int LevelMod = 125;
 		[Desc("Destroying creeps and enemies is rewarded.")]
 		public readonly Stance[] Stances = {Stance.Neutral, Stance.Enemy};
 	}
 
-	class GivesBounty : INotifyKilled
+	class GivesPlayerExperience : INotifyKilled
 	{
 		static int GetMultiplier(Actor self)
 		{
 			// returns 100's as 1, so as to keep accuracy for longer.
-			var info = self.Info.Traits.Get<GivesBountyInfo>();
+			var info = self.Info.Traits.Get<GivesPlayerExperienceInfo>();
 			var gainsExp = self.TraitOrDefault<GainsExperience>();
 			if (gainsExp == null)
 				return 100;
@@ -42,7 +41,7 @@ namespace OpenRA.Mods.RA
 
 		public void Killed(Actor self, AttackInfo e)
 		{
-			var info = self.Info.Traits.Get<GivesBountyInfo>();
+			var info = self.Info.Traits.Get<GivesPlayerExperienceInfo>();
 
 			if (e.Attacker == null || e.Attacker.Destroyed)	return;
 
@@ -52,10 +51,7 @@ namespace OpenRA.Mods.RA
 			// 2 hundreds because of GetMultiplier and info.Percentage.
 			var bounty = cost * GetMultiplier(self) * info.Percentage / 10000;
 
-			if (bounty > 0 && e.Attacker.Owner.IsAlliedWith(self.World.RenderPlayer))
-				e.Attacker.World.AddFrameEndTask(w => w.Add(new CashTick(self.CenterPosition, e.Attacker.Owner.Color.RGB, bounty)));
-
-			e.Attacker.Owner.PlayerActor.Trait<PlayerResources>().GiveCash(bounty);
+			e.Attacker.Owner.PlayerActor.Trait<PlayerExperience>().GiveExperience(bounty);
 		}
 	}
 }
