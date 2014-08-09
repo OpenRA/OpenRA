b61aaed498e5d64d9ea84dec60cfdaae3c9a6e64
Phoib <emiel.suilen@gmail.com>
2014-08-09 17:36:01 +0200

    Experience added, ranks added

--- OpenRA.Game/Traits/Player/PlayerExperience.cs
+++ OpenRA.Game/Traits/Player/PlayerExperience.cs
@@ -10,49 +10,89 @@
 
 using System;
 using System.Linq;
+using System.Collections.Generic;
+using OpenRA.GameRules;
+using OpenRA.Primitives;
+using OpenRA.Traits;
 
 namespace OpenRA.Traits
 {
 	public class PlayerExperienceInfo : ITraitInfo
 	{
+		[FieldLoader.LoadUsing("LoadExperience")]
+		[Desc("At which level should # sciencepoints be bestowed")]
+		public readonly Dictionary<int, int> ExperienceLevels = null;
+
 		public object Create(ActorInitializer init) { return new PlayerExperience(init.self, this); }
+		static object LoadExperience(MiniYaml y)
+		{
+			MiniYaml experienceLevel;
+
+			if (!y.ToDictionary().TryGetValue("Experience", out experienceLevel))
+			{
+				return new Dictionary<int, int>()
+				{
+					{ 0, 1 },
+					{ 800, 1 },
+					{ 1500, 1 },
+					{ 2500, 1 },
+					{ 3500, 1 },
+					{ 5000, 1 },
+				};
+			}
+
+			return experienceLevel.Nodes.ToDictionary(
+				kv => FieldLoader.GetValue<int>("(key)", kv.Key),
+				kv => FieldLoader.GetValue<int>("(value)", kv.Value.Value));
+		}
 	}
 
 	public class PlayerExperience : ISync
 	{
-		//readonly Player Owner;
+		readonly List<Pair<int, int>> nextLevel = new List<Pair<int, int>>();
 
 		public PlayerExperience(Actor self, PlayerExperienceInfo info)
 		{
-			//Owner = self.Owner;
-
 			Experience = 0;
 			Rank = 0;
 			SciencePoints = 0;
+
+			MaxRank = info.ExperienceLevels.Count;
+
+			foreach (var kv in info.ExperienceLevels)
+				nextLevel.Add(Pair.New(kv.Key, kv.Value));
+			/* Init with 0, to advance the first rank, if so defined in player.yaml */
+			GiveExperience(0);
 		}
 
 		[Sync] public int Experience;
 		[Sync] public int Rank;
-
 		[Sync] public int SciencePoints;
+
+		public readonly int MaxRank;
 
 
 		public void GiveExperience(int num)
 		{
 			Experience += num;
+			while (Rank < MaxRank && Experience >= nextLevel[Rank].First)
+			{
+				LevelUp(nextLevel[Rank].Second);
+			}
 		}
 
-		public void LevelUp() 
+		public void LevelUp(int num) 
 		{
 			Rank++;
+			GiveSciencePoints(num);
 		}
 
-		public void GiveSciencePoint(int num)
+		public void GiveSciencePoints(int num)
 		{
 			SciencePoints += num;
 		}
 
-		public bool UseSciencePoint(int num)
+		public bool UseSciencePoints(int num)
 		{
 			if(num > SciencePoints) return false;
 			SciencePoints -= num;
@@ -60,4 +100,12 @@ namespace OpenRA.Traits
 		}
 
 	}
+
+/*	class PlayerExperienceInit : IActorInit<int>
+	{
+		[FieldFromYamlKey] public readonly int value = 0;
+		public PlayerExperienceInit() { }
+		public PlayerExperienceInit(int init) { value = init; }
+		public int Value(World world) { return value; }
+	}*/
 }
