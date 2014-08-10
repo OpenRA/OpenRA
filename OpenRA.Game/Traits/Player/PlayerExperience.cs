fa926e324fafaac83d306ff1dbee4a42763c97a7
Phoib <emiel.suilen@gmail.com>
2014-08-10 13:02:50 +0200

    Moved GivesPlayerExperience to GivesExperience, preparing for sciences as upgrades

--- OpenRA.Game/Traits/Player/PlayerExperience.cs
+++ OpenRA.Game/Traits/Player/PlayerExperience.cs
@@ -21,7 +21,7 @@ namespace OpenRA.Traits
 	{
 		[FieldLoader.LoadUsing("LoadExperience")]
 		[Desc("At which level should # sciencepoints be bestowed")]
-		public readonly Dictionary<int, int> ExperienceLevels = null;
+		public readonly Dictionary<int, string[]> ExperienceLevels = null;
 
 		public object Create(ActorInitializer init) { return new PlayerExperience(init.self, this); }
 		static object LoadExperience(MiniYaml y)
@@ -30,26 +30,26 @@ namespace OpenRA.Traits
 
 			if (!y.ToDictionary().TryGetValue("Experience", out experienceLevel))
 			{
-				return new Dictionary<int, int>()
+				return new Dictionary<int, string[]>()
 				{
-					{ 0, 1 },
-					{ 800, 1 },
-					{ 1500, 1 },
-					{ 2500, 1 },
-					{ 3500, 1 },
-					{ 5000, 1 },
+					{ 0, new[] { "sciencerank1", "sciencepoint" } },
+					{ 800, new[] { "sciencerank2", "sciencepoint" } },
+					{ 1500, new[] { "sciencerank3", "sciencepoint" } },
+					{ 2500, new[] { "sciencerank4", "sciencepoint" } },
+					{ 3500, new[] { "sciencerank5", "sciencepoint" } },
+					{ 5000, new[] { "sciencerank6", "sciencepoint" } },
 				};
 			}
 
 			return experienceLevel.Nodes.ToDictionary(
 				kv => FieldLoader.GetValue<int>("(key)", kv.Key),
-				kv => FieldLoader.GetValue<int>("(value)", kv.Value.Value));
+				kv => FieldLoader.GetValue<string[]>("(value)", kv.Value.Value));
 		}
 	}
 
 	public class PlayerExperience : ISync
 	{
-		readonly List<Pair<int, int>> nextLevel = new List<Pair<int, int>>();
+		readonly List<Pair<int, string[]>> nextLevel = new List<Pair<int, string[]>>();
 
 		public PlayerExperience(Actor self, PlayerExperienceInfo info)
 		{
@@ -77,7 +77,8 @@ namespace OpenRA.Traits
 			Experience += num;
 			while (Rank < MaxRank && Experience >= nextLevel[Rank].First)
 			{
-				LevelUp(nextLevel[Rank].Second);
+				//Temporary fix until it is implemented properly as upgrades.
+				LevelUp(1);//nextLevel[Rank].Second);
 			}
 		}
 
