86d64d153d57d9de464c5ace4e8dca2babfe5e3c
ImagoTrigger <imagotrigger@gmail.com>
2014-08-29 13:17:31 -0500

    Revert "Paste (and working n Copy)"
    This reverts commit fb4c180cc8518ff1bb9ec42ec7a677f6ae9c8283.

--- OpenRA.Renderer.Sdl2/Sdl2Input.cs
+++ OpenRA.Renderer.Sdl2/Sdl2Input.cs
@@ -8,8 +8,7 @@
 */
 #endregion
 
-using System;
-using System.Runtime.InteropServices;
+using System.Text;
 using SDL2;
 
 namespace OpenRA.Renderer.Sdl2
@@ -17,8 +16,6 @@ namespace OpenRA.Renderer.Sdl2
 	public class Sdl2Input
 	{
 		MouseButton lastButtonBits = (MouseButton)0;
-
-		public string GetClipboard() { return SDL.SDL_GetClipboardText(); }
 
 		static MouseButton MakeButton(byte b)
 		{
@@ -127,6 +124,29 @@ namespace OpenRA.Renderer.Sdl2
 						break;
 					}
 
+					case SDL.SDL_EventType.SDL_TEXTINPUT:
+					{
+						string input;
+						unsafe
+						{
+							var data = new byte[SDL.SDL_TEXTINPUTEVENT_TEXT_SIZE];
+							var i = 0;
+							for (; i < SDL.SDL_TEXTINPUTEVENT_TEXT_SIZE; i++)
+							{
+								var b = e.text.text[i];
+								if (b == '\0')
+									break;
+
+								data[i] = b;
+							}
+
+							input = Encoding.UTF8.GetString(data, 0, i);
+						}
+
+						inputHandler.OnTextInput(input);
+						break;
+					}
+
 					case SDL.SDL_EventType.SDL_KEYDOWN:
 					case SDL.SDL_EventType.SDL_KEYUP:
 					{
@@ -154,16 +174,8 @@ namespace OpenRA.Renderer.Sdl2
 						else
 							inputHandler.OnKeyInput(keyEvent);
 
-                        break;
+						break;
 					}
-
-                    case SDL.SDL_EventType.SDL_TEXTINPUT:
-                    {
-                        byte[] rawBytes = new byte[SDL.SDL_TEXTINPUTEVENT_TEXT_SIZE];
-                        unsafe { Marshal.Copy((IntPtr)e.text.text, rawBytes, 0, SDL.SDL_TEXTINPUTEVENT_TEXT_SIZE); }
-                        inputHandler.OnTextInput(System.Text.Encoding.UTF8.GetString(rawBytes, 0, Array.IndexOf(rawBytes, (byte)0)));
-                        break;
-                    }
 				}
 			}
 
