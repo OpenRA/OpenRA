803b610141df0d33698b29c515bc88fdc802be43
partouf <partouf@gmail.com>
2014-09-03 01:35:23 +0200

    bracket style undofix

--- OpenRA.Mods.RA/Widgets/Logic/Ingame/LoadIngamePlayerOrObserverUILogic.cs
+++ OpenRA.Mods.RA/Widgets/Logic/Ingame/LoadIngamePlayerOrObserverUILogic.cs
@@ -31,10 +31,12 @@ namespace OpenRA.Mods.RA.Widgets.Logic
 				var playerWidgets = Game.LoadWidget(world, "PLAYER_WIDGETS", playerRoot, new WidgetArgs());
 				var sidebarTicker = playerWidgets.Get<LogicTickerWidget>("SIDEBAR_TICKER");
 
-				sidebarTicker.OnTick = () => {
+				sidebarTicker.OnTick = () =>
+				{
 					// Switch to observer mode after win/loss
 					if (world.ObserveAfterWinOrLose && world.LocalPlayer.WinState != WinState.Undefined)
-						Game.RunAfterTick(() => {
+						Game.RunAfterTick(() =>
+						{
 							playerRoot.RemoveChildren();
 							Game.LoadWidget(world, "OBSERVER_WIDGETS", playerRoot, new WidgetArgs());
 							Game.LoadWidget(world, "OBSERVER_STATS", playerRoot, new WidgetArgs());
