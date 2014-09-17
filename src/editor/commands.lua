OpenRA/OpenRA

commit df6b9fe9b3c6af4047dd5dbbf084c31a18ab03cc

Updated search/replace to always use the current editor/output/console tab.

src/editor/findreplace.lua
@@ -43,12 +43,8 @@ ide.findReplace = {
 }
 local findReplace = ide.findReplace
 
-local lastEditor
 function findReplace:GetEditor()
-  lastEditor = findReplace.oveditor or GetEditorWithFocus() or lastEditor
-  -- last editor may already be "userdata" instead of a Scintilla object,
-  -- so check if this is still a valid wxSTC object
-  return pcall(function() lastEditor:GetId() end) and lastEditor or GetEditor()
+  return findReplace.oveditor or GetEditorWithFocus() or GetEditor()
 end
 
 -------------------- Find replace dialog

commit 4e055d0d4ad1e2b4a8470fad129fa276565d9d80

Fixed an issue with searching in Output and Console windows.
This was broken by an earlier commit (df6b9fe9), which enabled search in
the current editor window. This change initiates the search in the last
editor component with a focus.

src/editor/commands.lua
@@ -955,7 +955,6 @@ frame:Connect(wx.wxEVT_TIMER, function() saveAutoRecovery() end)
 -- tickets: http://trac.wxwidgets.org/ticket/14142
 -- and http://trac.wxwidgets.org/ticket/14269)
 
-local infocus
 ide.editorApp:Connect(wx.wxEVT_SET_FOCUS, function(event)
   if ide.exitingProgram then return end
 
@@ -963,8 +962,8 @@ ide.editorApp:Connect(wx.wxEVT_SET_FOCUS, function(event)
   if win then
     local class = win:GetClassInfo():GetClassName()
     -- don't set focus on the main frame or toolbar
-    if infocus and (class == 'wxAuiToolBar' or class == 'wxFrame') then
-      pcall(function() infocus:SetFocus() end)
+    if ide.infocus and (class == 'wxAuiToolBar' or class == 'wxFrame') then
+      pcall(function() ide.infocus:SetFocus() end)
       return
     end
 
@@ -984,14 +983,14 @@ ide.editorApp:Connect(wx.wxEVT_SET_FOCUS, function(event)
       parent = parent:GetParent()
     end
     if mainwin then
-      if infocus and infocus ~= win and ide.osname == 'Macintosh' then
+      if ide.infocus and ide.infocus ~= win and ide.osname == 'Macintosh' then
         -- kill focus on the control that had the focus as wxwidgets on OSX
         -- doesn't do it: http://trac.wxwidgets.org/ticket/14142;
         -- wrap into pcall in case the window is already deleted
         local ev = wx.wxFocusEvent(wx.wxEVT_KILL_FOCUS)
-        pcall(function() infocus:GetEventHandler():ProcessEvent(ev) end)
+        pcall(function() ide.infocus:GetEventHandler():ProcessEvent(ev) end)
       end
-      infocus = win
+      ide.infocus = win
     end
   end
 
@@ -1001,11 +1000,11 @@ end)
 ide.editorApp:Connect(wx.wxEVT_ACTIVATE_APP,
   function(event)
     if not ide.exitingProgram then
-      if ide.osname == 'Macintosh' and infocus and event:GetActive() then
+      if ide.osname == 'Macintosh' and ide.infocus and event:GetActive() then
         -- restore focus to the last element that received it;
         -- wrap into pcall in case the element has disappeared
         -- while the application was out of focus
-        pcall(function() infocus:SetFocus() end)
+        pcall(function() ide.infocus:SetFocus() end)
       end
 
       -- save auto-recovery record when making the app inactive

src/editor/findreplace.lua
@@ -46,7 +46,7 @@ local findReplace = ide.findReplace
 local NOTFOUND = -1
 
 function findReplace:GetEditor()
-  return findReplace.oveditor or GetEditorWithFocus() or GetEditor()
+  return findReplace.oveditor or ide:GetEditorWithLastFocus() or GetEditor()
 end
 
 -------------------- Find replace dialog
@@ -128,8 +128,8 @@ local function shake(window, shakes, duration, vigour)
 end
 
 function findReplace:FindString(reverse)
-  if findReplace:HasText() then
-    local editor = findReplace:GetEditor()
+  local editor = findReplace:GetEditor()
+  if editor and findReplace:HasText() then
     local fDown = iff(reverse, not findReplace.fDown, findReplace.fDown)
     setSearchFlags(editor)
     setTarget(editor, fDown)
@@ -161,9 +161,9 @@ end
 
 function findReplace:FindStringAll(inFileRegister)
   local found = false
-  if findReplace:HasText() then
+  local editor = findReplace:GetEditor()
+  if editor and findReplace:HasText() then
     local findLen = string.len(findReplace.findText)
-    local editor = findReplace:GetEditor()
     local e = setTargetAll(editor)
 
     setSearchFlags(editor)
@@ -189,9 +189,8 @@ end
 
 function findReplace:ReplaceString(fReplaceAll, inFileRegister)
   local replaced = false
-
-  if findReplace:HasText() then
-    local editor = findReplace:GetEditor()
+  local editor = findReplace:GetEditor()
+  if editor and findReplace:HasText() then
     -- don't replace in read-only editors
     if editor:GetReadOnly() then return false end
 

src/editor/package.lua
@@ -74,6 +74,12 @@ end
 function ide:GetApp() return self.editorApp end
 function ide:GetEditor(index) return GetEditor(index) end
 function ide:GetEditorWithFocus(ed) return GetEditorWithFocus(ed) end
+function ide:GetEditorWithLastFocus()
+  -- make sure ide.infocus is still a valid component and not "some" userdata
+  return (pcall(function() ide.infocus:GetId() end)
+    and ide.infocus:GetClassInfo():GetClassName() == "wxStyledTextCtrl"
+    and ide.infocus:DynamicCast("wxStyledTextCtrl") or nil)
+end
 function ide:GetMenuBar() return self.frame.menuBar end
 function ide:GetStatusBar() return self.frame.statusBar end
 function ide:GetToolBar() return self.frame.toolBar end

src/main.lua
@@ -144,6 +144,7 @@ ide = {
 
   -- misc
   exitingProgram = false, -- are we currently exiting, ID_EXIT
+  infocus = nil, -- last component with a focus
   editorApp = wx.wxGetApp(),
   editorFilename = nil,
   openDocuments = {},-- open notebook editor documents[winId] = {