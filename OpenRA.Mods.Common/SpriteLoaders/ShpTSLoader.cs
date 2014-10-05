c798b306c4a4ab7f988c95bdcff2bbae502a90e3
Paul Chote <paul@chote.net>
2014-10-05 15:34:56 +1300

    Move ShpTS sprite loading into Mods.Common.

--- OpenRA.Mods.Common/SpriteLoaders/ShpTSLoader.cs
+++ OpenRA.Mods.Common/SpriteLoaders/ShpTSLoader.cs
@@ -8,15 +8,19 @@
  */
 #endregion
 
+using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
+using System.Linq;
+using OpenRA.FileFormats;
 using OpenRA.Graphics;
 
-namespace OpenRA.FileFormats
+namespace OpenRA.Mods.Common.SpriteLoaders
 {
-	public class ShpTSReader : ISpriteSource
+	public class ShpTSLoader : ISpriteLoader
 	{
-		class FrameHeader : ISpriteFrame
+		class ShpTSFrame : ISpriteFrame
 		{
 			public Size Size { get; private set; }
 			public Size FrameSize { get; private set; }
@@ -27,7 +31,7 @@ namespace OpenRA.FileFormats
 			public readonly uint FileOffset;
 			public readonly byte Format;
 
-			public FrameHeader(Stream stream, Size frameSize)
+			public ShpTSFrame(Stream stream, Size frameSize)
 			{
 				var x = stream.ReadUInt16();
 				var y = stream.ReadUInt16();
@@ -45,20 +49,56 @@ namespace OpenRA.FileFormats
 			}
 		}
 
-		public IReadOnlyList<ISpriteFrame> Frames { get; private set; }
-
-		public ShpTSReader(Stream stream)
+		bool IsShpTS(Stream s)
 		{
-			stream.ReadUInt16();
-			var width = stream.ReadUInt16();
-			var height = stream.ReadUInt16();
-			var size = new Size(width, height);
-			var frameCount = stream.ReadUInt16();
+			var start = s.Position;
 
-			var frames = new FrameHeader[frameCount];
-			Frames = frames.AsReadOnly();
+			// First word is zero
+			if (s.ReadUInt16() != 0)
+			{
+				s.Position = start;
+				return false;
+			}
+
+			// Sanity Check the image count
+			s.Position += 4;
+			var imageCount = s.ReadUInt16();
+			if (s.Position + 24 * imageCount > s.Length)
+			{
+				s.Position = start;
+				return false;
+			}
+
+			// Check the size and format flag
+			// Some files define bogus frames, so loop until we find a valid one
+			s.Position += 4;
+			ushort w, h, f = 0;
+			byte type;
+			do
+			{
+				w = s.ReadUInt16();
+				h = s.ReadUInt16();
+				type = s.ReadUInt8();
+			}
+			while (w == 0 && h == 0 && f++ < imageCount);
+
+			s.Position = start;
+			return type < 4;
+		}
+
+		ShpTSFrame[] ParseFrames(Stream s)
+		{
+			var start = s.Position;
+
+			s.ReadUInt16();
+			var width = s.ReadUInt16();
+			var height = s.ReadUInt16();
+			var size = new Size(width, height);
+			var frameCount = s.ReadUInt16();
+
+			var frames = new ShpTSFrame[frameCount];
 			for (var i = 0; i < frames.Length; i++)
-				frames[i] = new FrameHeader(stream, size);
+				frames[i] = new ShpTSFrame(s, size);
 
 			for (var i = 0; i < frameCount; i++)
 			{
@@ -66,13 +106,13 @@ namespace OpenRA.FileFormats
 				if (f.FileOffset == 0)
 					continue;
 
-				stream.Position = f.FileOffset;
+				s.Position = f.FileOffset;
 
 				var frameSize = f.Size.Width * f.Size.Height;
 
 				// Uncompressed
 				if (f.Format == 1 || f.Format == 0)
-					f.Data = stream.ReadBytes(frameSize);
+					f.Data = s.ReadBytes(frameSize);
 
 				// Uncompressed scanlines
 				else if (f.Format == 2)
@@ -80,9 +120,9 @@ namespace OpenRA.FileFormats
 					f.Data = new byte[frameSize];
 					for (var j = 0; j < f.Size.Height; j++)
 					{
-						var length = stream.ReadUInt16() - 2;
+						var length = s.ReadUInt16() - 2;
 						var offset = f.Size.Width * j;
-						stream.ReadBytes(f.Data, offset, length);
+						s.ReadBytes(f.Data, offset, length);
 					}
 				}
 
@@ -92,12 +132,27 @@ namespace OpenRA.FileFormats
 					f.Data = new byte[frameSize];
 					for (var j = 0; j < f.Size.Height; j++)
 					{
-						var length = stream.ReadUInt16() - 2;
+						var length = s.ReadUInt16() - 2;
 						var offset = f.Size.Width * j;
-						Format2.DecodeInto(stream.ReadBytes(length), f.Data, offset);
+						Format2.DecodeInto(s.ReadBytes(length), f.Data, offset);
 					}
 				}
 			}
+
+			s.Position = start;
+			return frames;
+		}
+
+		public bool TryParseSprite(Stream s, out ISpriteFrame[] frames)
+		{
+			if (!IsShpTS(s))
+			{
+				frames = null;
+				return false;
+			}
+
+			frames = ParseFrames(s);
+			return true;
 		}
 	}
 }
