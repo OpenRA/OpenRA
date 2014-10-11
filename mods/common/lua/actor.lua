6640fa2539cf37aca63553447a71d80266fc85b6
atlimit8 <atlimit8-vcs@gmx.com>
2014-10-11 06:27:49 -0500

    Fixes old Lua Move Activity

--- mods/common/lua/actor.lua
+++ mods/common/lua/actor.lua
@@ -24,14 +24,14 @@ Actor.Move = function(actor, location)
 end
 
 Actor.MoveNear = function(actor, location, nearEnough)
-	actor:QueueActivity(OpenRA.New("Move", { location, WRange.FromCells(nearEnough) }))
+	actor:QueueActivity(OpenRA.New("Move", { actor, location, WRange.FromCells(nearEnough) }))
 end
 
 Actor.ScriptedMove = function(actor, location)
 	if Actor.HasTrait(actor, "Helicopter") then
 		Internal.HeliFlyToPos(actor, Map.CenterOfCell(location))
 	else
-		actor:QueueActivity(OpenRA.New("Move", { location }))
+		actor:QueueActivity(OpenRA.New("Move", { actor, location }))
 	end
 end
 
