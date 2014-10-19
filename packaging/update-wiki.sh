680370a590831eb6dacb79b26fc90cf281497322
Matthias Mailänder <matthias@mailaender.name>
2014-10-19 14:13:10 +0200

    not just Traits anymore

--- packaging/update-wiki.sh
+++ packaging/update-wiki.sh
@@ -3,7 +3,7 @@
 git config --global user.email "orabot@users.noreply.github.com"
 git config --global user.name "orabot"
 
-echo "Updating https://github.com/OpenRA/OpenRA/wiki/Traits"
+echo "Updating https://github.com/OpenRA/OpenRA/wiki/"
 rm -rf $HOME/openra-wiki
 git clone git@github.com:OpenRA/OpenRA.wiki.git $HOME/openra-wiki
 cp -fr ../DOCUMENTATION.md $HOME/openra-wiki/Traits.md
