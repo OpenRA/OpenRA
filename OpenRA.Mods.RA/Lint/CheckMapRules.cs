7a8948b7d2bb8f1088d882eaa4f90515363f3dbd
Matthias Mailänder <matthias@mailaender.name>
2014-11-01 12:39:23 +0100

    add a new lint check for map rule loading

---
+++ OpenRA.Mods.RA/Lint/CheckMapRules.cs
@@ -0,0 +1,31 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2014 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation. For more information,
+ * see COPYING.
+ */
+#endregion
+
+using System;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.RA
+{
+	public class CheckMapRules : ILintPass
+	{
+		public void Run(Action<string> emitError, Action<string> emitWarning, Map map)
+		{
+			try
+			{
+				Game.modData.RulesetCache.LoadMapRules(map);
+			}
+			catch (Exception e)
+			{
+				emitError(e.Message);
+			}
+		}
+	}
+}
+
