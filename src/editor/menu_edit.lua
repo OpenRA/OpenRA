d2a12c55e74ca96b3ec884f50677970aab329cdb
Paul Kulchenko <paul@kulchenko.com>
2014-11-17 13:37:33 -0800

    Moved commandbar/Navigate menu to `Search` (#31).

--- src/editor/menu_edit.lua
+++ src/editor/menu_edit.lua
@@ -5,7 +5,6 @@
 
 local ide = ide
 local unpack = table.unpack or unpack
-local q = EscapeMagic
 
 -- ---------------------------------------------------------------------------
 -- Create the Edit menu and attach the callback functions
@@ -28,10 +27,6 @@ local editMenu = wx.wxMenu {
   { },
 }
 
-editMenu:Append(ID_NAVIGATE, TR("Navigate"), wx.wxMenu {
-  { ID_NAVIGATETOFILE, TR("To File...")..KSC(ID_NAVIGATETOFILE), TR("Navigate to file") },
-  { ID_NAVIGATETOLINE, TR("To Line...")..KSC(ID_NAVIGATETOLINE), TR("Navigate to line") },
-})
 editMenu:Append(ID_SOURCE, TR("Source"), wx.wxMenu {
   { ID_COMMENT, TR("C&omment/Uncomment")..KSC(ID_COMMENT), TR("Comment or uncomment current or selected lines") },
   { ID_REINDENT, TR("Correct &Indentation")..KSC(ID_REINDENT), TR("Re-indent selected lines") },
@@ -366,137 +361,3 @@ end
 frame:Connect(ID_BOOKMARKTOGGLE, wx.wxEVT_COMMAND_MENU_SELECTED, bookmarkToggle)
 frame:Connect(ID_BOOKMARKNEXT, wx.wxEVT_COMMAND_MENU_SELECTED, bookmarkNext)
 frame:Connect(ID_BOOKMARKPREV, wx.wxEVT_COMMAND_MENU_SELECTED, bookmarkPrev)
-
-local markername = "commandbar.background"
-local function navigateTo(default)
-  local styles = ide.config.styles
-  local marker = ide:AddMarker(markername,
-    wxstc.wxSTC_MARK_BACKGROUND, styles.text.fg, styles.caretlinebg.bg)
-
-  local nb = ide:GetEditorNotebook()
-  local selection = nb:GetSelection()
-  local projectFiles, preview, origline
-
-  CommandBarShow(
-    function(t, enter, text) -- onDone
-      local mac = ide.osname == 'Macintosh'
-      if not mac then nb:Freeze() end
-
-      -- delete all current line markers if any; restore line position
-      local ed = ide:GetEditor()
-      if ed and origline then
-        ed:MarkerDeleteAll(marker)
-        ed:EnsureVisibleEnforcePolicy(origline-1)
-      end
-
-      if enter then
-        local _, file, tabindex = unpack(t or {})
-        if tabindex or preview then -- switch to existing tab
-          SetEditorSelection(tabindex or nb:GetPageIndex(preview))
-        elseif file then -- load a new file
-          LoadFile(MergeFullPath(ide:GetProject(), file), nil, true)
-        end
-
-        -- set line position in the new editor if requested
-        if text and text:find(':') then
-          local toline = tonumber(text:match(':(%d+)'))
-          local ed = ide:GetEditor()
-          if toline and ed then
-            ed:GotoLine(toline-1)
-            ed:EnsureVisibleEnforcePolicy(toline-1)
-          end
-        end
-      else
-        -- close preview
-        if preview then
-          ClosePage(nb:GetPageIndex(preview))
-          preview = nil
-        end
-        -- restore original selection if canceled
-        if nb:GetSelection() ~= selection then nb:SetSelection(selection) end
-      end
-      if not mac then nb:Thaw() end
-    end,
-    function(text) -- onUpdate
-      local lines = {}
-      local projdir = ide:GetProject()
-
-      -- delete all current line markers if any
-      -- restore the original position in case "goto line" is removed from bar
-      local ed = ide:GetEditor()
-      if ed and origline then
-        ed:MarkerDeleteAll(marker)
-        ed:EnsureVisibleEnforcePolicy(origline-1)
-      end
-
-      if text and text:find(':') then
-        local toline = tonumber(text:match(':(%d+)'))
-        if toline and ed then
-          ed:MarkerDefine(ide:GetMarker(markername))
-          ed:MarkerAdd(toline-1, marker)
-          local curline = ed:GetCurrentLine() -- TODO find the middle line
-          origline = origline or (curline+1)
-          ed:EnsureVisibleEnforcePolicy(toline-1)
-        end
-      elseif text and #text > 0 and projdir and #projdir > 0 then
-        -- populate the list of files
-        if not projectFiles then
-          projectFiles = FileSysGetRecursive(projdir, true)
-          for k = #projectFiles, 1, -1 do
-            if IsDirectory(projectFiles[k]) then
-              table.remove(projectFiles, k)
-            else
-              projectFiles[k] = projectFiles[k]:gsub("^"..q(projdir), "")
-            end
-          end
-        end
-        local topscore
-        for _, item in ipairs(CommandBarScoreFiles(projectFiles, text)) do
-          local file, score = unpack(item)
-          topscore = topscore or score
-          if score > topscore / 2 and score > 1 then
-            table.insert(lines, {
-                ("%2d %s"):format(score, wx.wxFileName(file):GetFullName()),
-                file,
-            })
-          end
-        end
-      else
-        for _, doc in pairs(ide:GetDocuments()) do
-          lines[doc:GetTabIndex()+1] = {doc:GetFileName(), doc:GetFilePath(), doc:GetTabIndex()}
-        end
-      end
-      return lines
-    end,
-    function(t) return unpack(t) end, -- onItem
-    function(t) -- onSelection
-      local _, file, tabindex = unpack(t)
-      if file then file = MergeFullPath(ide:GetProject(), file) end
-      -- disabling event handlers for the notebook and the editor
-      -- to minimize changes in the UI when editors are switched
-      -- or files in the preview are updated.
-      nb:SetEvtHandlerEnabled(false)
-      local doc = file and ide:FindDocument(file)
-      if doc and not tabindex then tabindex = doc:GetTabIndex() end
-      if tabindex then
-        local ed = nb:GetPage(tabindex)
-        ed:SetEvtHandlerEnabled(false)
-        nb:SetSelection(tabindex)
-        ed:SetEvtHandlerEnabled(true)
-      elseif file then
-        preview = preview or NewFile()
-        preview:SetEvtHandlerEnabled(false)
-        LoadFile(file, preview, true, true)
-        preview:SetFocus()
-        preview:SetEvtHandlerEnabled(true)
-      end
-      nb:SetEvtHandlerEnabled(true)
-    end,
-    default or ""
-  )
-end
-
-frame:Connect(ID_NAVIGATETOFILE, wx.wxEVT_COMMAND_MENU_SELECTED,
-  function() navigateTo("") end)
-frame:Connect(ID_NAVIGATETOLINE, wx.wxEVT_COMMAND_MENU_SELECTED,
-  function() navigateTo(":") end)
