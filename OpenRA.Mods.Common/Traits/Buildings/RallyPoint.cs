aadceb085be21dce679d45b5bc3d7d13ae4d839c
reaperrr <thereaperrr@aol.com>
2014-12-10 19:55:56 +0100

    Renames RallyPoint effect to RallyPointIndicator

--- OpenRA.Mods.Common/Traits/Buildings/RallyPoint.cs
+++ OpenRA.Mods.Common/Traits/Buildings/RallyPoint.cs
@@ -9,6 +9,7 @@
 #endregion
 
 using System.Collections.Generic;
+using OpenRA.Mods.Common.Effects;
 using OpenRA.Traits;
 
 namespace OpenRA.Mods.Common.Traits
@@ -29,7 +30,7 @@ namespace OpenRA.Mods.Common.Traits
 		public RallyPoint(Actor self, RallyPointInfo info)
 		{
 			Location = self.Location + info.RallyPoint;
-			self.World.AddFrameEndTask(w => w.Add(new Effects.RallyPoint(self, info.IndicatorPalettePrefix)));
+			self.World.AddFrameEndTask(w => w.Add(new RallyPointIndicator(self, info.IndicatorPalettePrefix)));
 		}
 
 		public IEnumerable<IOrderTargeter> Orders
