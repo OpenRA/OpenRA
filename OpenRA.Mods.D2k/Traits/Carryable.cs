0868558e9484e275582fb047debb28444ca727c2
Matthijs Benschop <matthijs.benschop@gmail.com>
2014-12-21 23:03:29 +0100

    Allow a carryable to change his destination after being reserved

--- OpenRA.Mods.D2k/Traits/Carryable.cs
+++ OpenRA.Mods.D2k/Traits/Carryable.cs
@@ -54,17 +54,18 @@ namespace OpenRA.Mods.D2k.Traits
 
 		void RequestTransport(CPos destination, Activity afterLandActivity)
 		{
-			if (locked || Reserved)
+			if (destination == CPos.Zero || (self.Location - destination).Length < info.MinDistance)
+			{
+				WantsTransport = false; // Be sure to cancel any pending transports
 				return;
-
-			if (destination == CPos.Zero)
-				return;
-
-			if ((self.Location - destination).Length < info.MinDistance)
-				return;
+			}
 
 			Destination = destination;
 			this.afterLandActivity = afterLandActivity;
+
+			if (locked || Reserved)
+				return;
+
 			WantsTransport = true;
 
 			// Inform all idle carriers
