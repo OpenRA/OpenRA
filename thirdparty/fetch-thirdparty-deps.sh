13785ddabd3f894e1048030e903c245a0c146ba7
Matthias Mailänder <matthias@mailaender.name>
2014-12-26 14:51:17 +0100

    fetch MaxMind.GeoIP2 from nuget

--- thirdparty/fetch-thirdparty-deps.sh
+++ thirdparty/fetch-thirdparty-deps.sh
@@ -12,3 +12,16 @@ if [ ! -f ICSharpCode.SharpZipLib.dll ]; then
 	cp ./SharpZipLib.0.86.0/lib/20/ICSharpCode.SharpZipLib.dll .
 	rm -rf SharpZipLib.0.86.0
 fi
+
+if [ ! -f MaxMind.GeoIP2.dll ]; then
+	echo "Fetching MaxMind.GeoIP2 from nuget"
+	nuget install MaxMind.GeoIP2 -Version 2.1.0
+	cp ./MaxMind.Db.1.0.0.0/lib/net40/MaxMind.Db.* .
+	rm -rf MaxMind.Db.1.0.0.0
+	cp ./MaxMind.GeoIP2.2.1.0.0/lib/net40/MaxMind.GeoIP2* .
+	rm -rf MaxMind.GeoIP2.2.1.0.0
+	cp ./Newtonsoft.Json.6.0.5/lib/net40/Newtonsoft.Json* .
+	rm -rf Newtonsoft.Json.6.0.5
+	cp ./RestSharp.105.0.0/lib/net4-client/RestSharp* .
+	rm -rf RestSharp.105.0.0
+fi
