fa72e040423d23786f4f536d6912f7b7ac6ff9fa
Hellhake <Hellhake@users.noreply.github.com>
2015-01-01 16:12:11 +0100

    Remove BOM

--- OpenRA.TilesetBuilder/FormBuilder.cs
+++ OpenRA.TilesetBuilder/FormBuilder.cs
@@ -1,4 +1,4 @@
-﻿#region Copyright & License Information
+#region Copyright & License Information
 /*
  * Copyright 2007-2014 The OpenRA Developers (see AUTHORS)
  * This file is part of OpenRA, which is free software. It is made 
