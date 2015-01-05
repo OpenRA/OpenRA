18ac3953cc57c52c0706e11c09ee2639799c0301
penev92 <penev92@gmail.com>
2015-01-06 01:26:23 +0200

    Move everything ExternalCapture-related to Mods.Common

--- OpenRA.Mods.Common/Traits/ExternalCapturable.cs
+++ OpenRA.Mods.Common/Traits/ExternalCapturable.cs
@@ -9,10 +9,9 @@
 #endregion
 
 using System.Linq;
-using OpenRA.Mods.Common.Traits;
 using OpenRA.Traits;
 
-namespace OpenRA.Mods.RA.Traits
+namespace OpenRA.Mods.Common.Traits
 {
 	[Desc("This actor can be captured by a unit with ExternalCaptures: trait.")]
 	public class ExternalCapturableInfo : ITraitInfo
--- OpenRA.Mods.Common/Traits/ExternalCaptures.cs
+++ OpenRA.Mods.Common/Traits/ExternalCaptures.cs
@@ -10,12 +10,11 @@
 
 using System.Collections.Generic;
 using System.Drawing;
-using OpenRA.Mods.Common;
+using OpenRA.Mods.Common.Activities;
 using OpenRA.Mods.Common.Orders;
-using OpenRA.Mods.RA.Activities;
 using OpenRA.Traits;
 
-namespace OpenRA.Mods.RA.Traits
+namespace OpenRA.Mods.Common.Traits
 {
 	[Desc("This actor can capture other actors which have the ExternalCapturable: trait.")]
 	class ExternalCapturesInfo : ITraitInfo
