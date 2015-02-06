OpenRA/OpenRA

commit 2ee9599863383833b26c3f498b33f475429680c1

Improved command line file check for relative files on Windows.
Loading `file:10` was interpreted as network share for non-existing files.

src/editor/commands.lua
@@ -201,7 +201,7 @@ end
 
 function ActivateFile(filename)
   local name, suffix, value = filename:match('(.+):([lLpP]?)(%d+)$')
-  if name and not wx.wxFileExists(filename) and wx.wxFileExists(name) then
+  if name and not wx.wxFileExists(filename) and not wx.wxIsAbsolutePath(filename) then
     filename = name
   end
 

commit 71667d2912d015c4169145414f3808afa23a72e1

Fixed loading file with absolute name and line number (fixes #408).
This was broken by the change in 2ee9599 that was made to avoid delay when
relative filename with a line number looked like a network path (Windows).

src/editor/commands.lua
@@ -205,9 +205,7 @@ end
 
 function ActivateFile(filename)
   local name, suffix, value = filename:match('(.+):([lLpP]?)(%d+)$')
-  if name and not wx.wxFileExists(filename) and not wx.wxIsAbsolutePath(filename) then
-    filename = name
-  end
+  if name and not wx.wxFileExists(filename) then filename = name end
 
   -- check if non-existing file can be loaded from the project folder;
   -- this is to handle: "project file" used on the command line

t/8-file.lua
@@ -2,3 +2,9 @@ ok(not LoadFile(''), "Don't load file with an empty name.")
 ok(not LoadFile("\r\n "), "Don't load file with name that only has whitespaces.")
 ok(not LoadFile('t'), "Don't load file with directory as the name (1/2).")
 ok(not LoadFile('./'), "Don't load file with directory as the name (2/2).")
+
+local fullpath = MergeFullPath(wx.wxFileName.GetCwd(), 't/test.lua')
+ok(ActivateFile('t/test.lua:10'), "Load file:line.")
+ok(not ActivateFile('t/foo.bar:10'), "Doesn't load non-existent file:line.")
+ok(ActivateFile(fullpath..':10'), "Load fullpath/file:line.")
+ok(not ActivateFile(fullpath..'/foo.bar:10'), "Doesn't load non-existent fullpath/file:line.")