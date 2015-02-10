5e25fb06bf7110f57d8cbc4f8f99b34cd04d9464
Paul Chote <pchote@users.noreply.github.com>
2015-02-10 14:26:44 +0000

    Introduce IFinalizedRenderable.ScreenBounds.

--- OpenRA.Mods.Common/Graphics/BeamRenderable.cs
+++ OpenRA.Mods.Common/Graphics/BeamRenderable.cs
@@ -54,5 +54,6 @@ namespace OpenRA.Mods.Common.Graphics
 		}
 
 		public void RenderDebugGeometry(WorldRenderer wr) { }
+		public Rectangle ScreenBounds(WorldRenderer wr) { return Rectangle.Empty; }
 	}
 }
