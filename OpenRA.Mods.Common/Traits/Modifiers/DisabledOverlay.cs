6baa35a763aea067ecbfa82b93582cd25ca45c0d
RoosterDragon <roosterdragon@outlook.com>
2015-02-23 20:26:54 +0000

    Adjust DisabledOverlay.ModifyRender to be more efficient.
    When not disabled, ModifyRender doesn't actually alter the sequence, so we can just return it directly and avoid adding another layer of enumerable. If it is disabled, then the modifying enumerable has one less condition to check inside the loop.

--- OpenRA.Mods.Common/Traits/Modifiers/DisabledOverlay.cs
+++ OpenRA.Mods.Common/Traits/Modifiers/DisabledOverlay.cs
@@ -21,11 +21,19 @@ namespace OpenRA.Mods.Common.Traits
 	{
 		public IEnumerable<IRenderable> ModifyRender(Actor self, WorldRenderer wr, IEnumerable<IRenderable> r)
 		{
-			var disabled = self.IsDisabled();
+			if (!self.IsDisabled())
+				return r;
+
+			return ModifiedRender(self, wr, r);
+		}
+
+		IEnumerable<IRenderable> ModifiedRender(Actor self, WorldRenderer wr, IEnumerable<IRenderable> r)
+		{
 			foreach (var a in r)
 			{
 				yield return a;
-				if (disabled && !a.IsDecoration)
+
+				if (!a.IsDecoration)
 					yield return a.WithPalette(wr.Palette("disabled"))
 						.WithZOffset(a.ZOffset + 1)
 						.AsDecoration();
