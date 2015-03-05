OpenRA/OpenRA

commit 89751b430d723153ccbc0b7532449c2d2135d714

Added reporting of location in more cases when debugging is suspended.

src/editor/debugger.lua
@@ -626,6 +626,9 @@ debugger.listen = function(start)
         and stoppedAtBreakpoint(file, line) then
           activateDocument(file, line)
           options.runstart = false
+        elseif file and line then
+          DisplayOutputLn(TR("Debugging suspended at %s:%s (couldn't activate the file).")
+            :format(file, line))
         end
       elseif not (options.run or debugger.scratchpad) then
         local file, line, err = debugger.loadfile(startfile)
@@ -681,7 +684,11 @@ debugger.listen = function(start)
           debugger.scratchable = ide.interpreter.scratchextloop ~= nil
         else
           debugger.scratchable = true
-          activateDocument(startfile, 0) -- find the appropriate line
+          local activated = activateDocument(startfile, 0) -- find the appropriate line
+          if not activated then
+            DisplayOutputLn(TR("Debugging suspended at %s:%s (couldn't activate the file).")
+              :format(startfile, '?'))
+          end
         end
       end
 

commit 2b4c5eafc8a4019952688d10eff8b3c2a0c8752d

Updated `debugging suspended` message to not trigger with `runonstart`.
This is to address the changes caused by 89751b43.

src/editor/debugger.lua
@@ -637,10 +637,11 @@ debugger.listen = function(start)
             .." "..TR("Compilation error")
             ..":\n"..err)
           return debugger.terminate()
-        elseif options.runstart and not debugger.scratchpad
-        and stoppedAtBreakpoint(file, line) then
-          activateDocument(file, line)
-          options.runstart = false
+        elseif options.runstart and not debugger.scratchpad then
+          if stoppedAtBreakpoint(file, line) then
+            activateDocument(file, line)
+            options.runstart = false
+          end
         elseif file and line then
           DisplayOutputLn(TR("Debugging suspended at '%s:%s' (couldn't activate the file).")
             :format(file, line))