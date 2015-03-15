dc9d19f521e5d744a51db9500a1e268a9b01c18e
Paul Kulchenko <paul@kulchenko.com>
2015-03-15 16:43:10 -0700

    Fixed restoring pane size after hide/show operations (fixes #424).

--- src/editor/menu_view.lua
+++ src/editor/menu_view.lua
@@ -61,8 +61,10 @@ local panels = {
 
 local function togglePanel(event)
   local panel = panels[event:GetId()]
-  local shown = not uimgr:GetPane(panel):IsShown()
-  uimgr:GetPane(panel):Show(shown)
+  local pane = uimgr:GetPane(panel)
+  local shown = not pane:IsShown()
+  if not shown then pane:BestSize(pane.window:GetSize()) end
+  pane:Show(shown)
   uimgr:Update()
 
   return shown
