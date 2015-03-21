0a6e21d90861a1a5c70b1fd22d4fe6f4463320c1
Paul Chote <pchote@users.noreply.github.com>
2015-03-21 16:54:01 +0000

    Support tileset-specific art in TS.

--- OpenRA.Mods.Common/Graphics/TilesetSpecificSpriteSequence.cs
+++ OpenRA.Mods.Common/Graphics/TilesetSpecificSpriteSequence.cs
@@ -20,6 +20,7 @@ namespace OpenRA.Mods.Common.Graphics
 	{
 		public readonly string DefaultSpriteExtension = ".shp";
 		public readonly Dictionary<string, string> TilesetExtensions = new Dictionary<string, string>();
+		public readonly Dictionary<string, string> TilesetCodes = new Dictionary<string, string>();
 
 		public TilesetSpecificSpriteSequenceLoader(ModData modData)
 			: base(modData)
@@ -31,6 +32,9 @@ namespace OpenRA.Mods.Common.Graphics
 
 			if (metadata.TryGetValue("TilesetExtensions", out yaml))
 				TilesetExtensions = yaml.ToDictionary(kv => kv.Value);
+
+			if (metadata.TryGetValue("TilesetCodes", out yaml))
+				TilesetCodes = yaml.ToDictionary(kv => kv.Value);
 		}
 
 		public override ISpriteSequence CreateSequence(ModData modData, TileSet tileSet, SpriteCache cache, string sequence, string animation, MiniYaml info)
@@ -44,26 +48,40 @@ namespace OpenRA.Mods.Common.Graphics
 		public TilesetSpecificSpriteSequence(ModData modData, TileSet tileSet, SpriteCache cache, ISpriteSequenceLoader loader, string sequence, string animation, MiniYaml info)
 			: base(modData, tileSet, cache, loader, sequence, animation, info) { }
 
+		string ResolveTilesetId(TileSet tileSet, Dictionary<string, MiniYaml> d)
+		{
+			var tsId = tileSet.Id;
+
+			MiniYaml yaml;
+			if (d.TryGetValue("TilesetOverrides", out yaml))
+			{
+				var tsNode = yaml.Nodes.FirstOrDefault(n => n.Key == tsId);
+				if (tsNode != null)
+					tsId = tsNode.Value.Value;
+			}
+
+			return tsId;
+		}
+
 		protected override string GetSpriteSrc(ModData modData, TileSet tileSet, string sequence, string animation, string sprite, Dictionary<string, MiniYaml> d)
 		{
 			var loader = (TilesetSpecificSpriteSequenceLoader)Loader;
 
 			var spriteName = sprite ?? sequence;
+
+			if (LoadField<bool>(d, "UseTilesetCode", false))
+			{
+				string code;
+				if (loader.TilesetCodes.TryGetValue(ResolveTilesetId(tileSet, d), out code))
+					spriteName = spriteName.Substring(0, 1) + code + spriteName.Substring(2, spriteName.Length - 2);
+			}
+
 			if (LoadField<bool>(d, "AddExtension", true))
 			{
 				var useTilesetExtension = LoadField<bool>(d, "UseTilesetExtension", false);
-				var tsId = tileSet.Id;
-
-				MiniYaml yaml;
-				if (d.TryGetValue("TilesetOverrides", out yaml))
-				{
-					var tsNode = yaml.Nodes.FirstOrDefault(n => n.Key == tsId);
-					if (tsNode != null)
-						tsId = tsNode.Value.Value;
-				}
 
 				string tilesetExtension;
-				if (useTilesetExtension && loader.TilesetExtensions.TryGetValue(tsId, out tilesetExtension))
+				if (useTilesetExtension && loader.TilesetExtensions.TryGetValue(ResolveTilesetId(tileSet, d), out tilesetExtension))
 					return spriteName + tilesetExtension;
 
 				return spriteName + loader.DefaultSpriteExtension;
