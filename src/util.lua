OpenRA/OpenRA

commit 4f3b8596f21bc1526ac25a70f5a760675bd78b42

Improved file traversal to speed up search by pre-filtering masks (#416).

src/util.lua
@@ -163,7 +163,13 @@ function FileSysGetRecursive(path, recursive, spec, skip)
 
   -- recursion is done in all folders but only those folders that match
   -- the spec are returned. This is the pattern that matches the spec.
-  local specmask = spec:gsub("%.", "%%."):gsub("%*", ".*").."$"
+  -- Mask could be a list, so generate a table with matching patterns
+  -- accept "*.lua; .txt,.wlua" combinations
+  local masks = {}
+  for m in spec:gmatch("[^%s;,]+") do
+    -- escape all special characters and replace (escaped) * with .*
+    table.insert(masks, EscapeMagic(m):gsub("%%%*", ".*").."$")
+  end
 
   local function getDir(path, spec)
     local dir = wx.wxDir(path)
@@ -174,7 +180,13 @@ function FileSysGetRecursive(path, recursive, spec, skip)
     while found do
       if not skip or not file:find(skip) then
         local fname = wx.wxFileName(path, file):GetFullPath()
-        if fname:find(specmask) then table.insert(content, fname..sep) end
+        for _, mask in ipairs(masks) do
+          if file:find(mask) then
+            table.insert(content, fname..sep)
+            break
+          end
+        end
+
         -- check if this name already appears in the path earlier;
         -- Skip the processing if it does as it could lead to infinite
         -- recursion with circular references created by symlinks.

src/editor/findreplace.lua
@@ -341,42 +341,31 @@ local function onFileRegister(pos, length)
 end
 
 local function ProcInFiles(startdir,mask,subdirs)
-  local files = FileSysGetRecursive(startdir,subdirs,"*")
+  local files = FileSysGetRecursive(startdir, subdirs, mask)
   local start = TimeGet()
 
-  -- mask could be a list, so generate a table with matching patterns
-  -- accept "*.lua; .txt,.wlua" combinations
-  local masks = {}
-  for m in mask:gmatch("[^%s;,]+") do
-    -- escape all special characters and replace (escaped) * with .*
-    table.insert(masks, q(m):gsub("%%%*", ".*").."$")
-  end
   for _,file in ipairs(files) do
     -- skip folders as these are included in the list as well
     if not IsDirectory(file) then
-      local match = false
-      for _, mask in ipairs(masks) do match = match or file:find(mask) end
-      if match then
-        findReplace.curfilename = file
+      findReplace.curfilename = file
 
-        local filetext = FileRead(file)
-        if filetext and not isBinary(filetext:sub(1, 2048)) then
-          findReplace.oveditor:SetText(filetext)
+      local filetext = FileRead(file)
+      if filetext and not isBinary(filetext:sub(1, 2048)) then
+        findReplace.oveditor:SetText(filetext)
 
-          if findReplace:FindStringAll(onFileRegister) then
-            findReplace.files = findReplace.files + 1
-          end
+        if findReplace:FindStringAll(onFileRegister) then
+          findReplace.files = findReplace.files + 1
+        end
 
-          -- give time to the UI to refresh
-          if TimeGet() - start > 0.25 then wx.wxSafeYield() end
-          -- the IDE may be quitting after Yield or the tab may be closed,
-          local ok, mgr = pcall(function() return ide:GetUIManager() end)
-          -- so check to make sure the manager is still active
-          if not (ok and mgr:GetPane(searchpanel):IsShown())
-          -- and check that the search results tab is still open
-          or not pcall(function() findReplace.reseditor:GetId() end) then
-            return false
-          end
+        -- give time to the UI to refresh
+        if TimeGet() - start > 0.25 then wx.wxSafeYield() end
+        -- the IDE may be quitting after Yield or the tab may be closed,
+        local ok, mgr = pcall(function() return ide:GetUIManager() end)
+        -- so check to make sure the manager is still active
+        if not (ok and mgr:GetPane(searchpanel):IsShown())
+        -- and check that the search results tab is still open
+        or not pcall(function() findReplace.reseditor:GetId() end) then
+          return false
         end
       end
     end

commit 74de23cf5d788211318e58290be0f285d39affba

Fixed file traversal for multiple masks broken by 4f3b8596 (#416).

src/util.lua
@@ -161,21 +161,21 @@ function FileDirHasContent(dir)
 end
 
 function FileSysGetRecursive(path, recursive, spec, skip)
-  spec = spec or "*"
   local content = {}
   local sep = GetPathSeparator()
 
   -- recursion is done in all folders but only those folders that match
   -- the spec are returned. This is the pattern that matches the spec.
   -- Mask could be a list, so generate a table with matching patterns
-  -- accept "*.lua; .txt,.wlua" combinations
+  -- accept "*.lua" and "*.txt,*.wlua" combinations
   local masks = {}
-  for m in spec:gmatch("[^%s;,]+") do
+  for m in (spec or "*"):gmatch("[^%s;,]+") do
     -- escape all special characters and replace (escaped) * with .*
     table.insert(masks, EscapeMagic(m):gsub("%%%*", ".*").."$")
   end
+  if #masks >= 2 then spec = nil end
 
-  local function getDir(path, spec)
+  local function getDir(path)
     local dir = wx.wxDir(path)
     if not dir:IsOpened() then return end
 
@@ -195,21 +195,30 @@ function FileSysGetRecursive(path, recursive, spec, skip)
         -- Skip the processing if it does as it could lead to infinite
         -- recursion with circular references created by symlinks.
         if recursive and select(2, fname:gsub(EscapeMagic(file..sep),'')) <= 2 then
-          getDir(fname, spec)
+          getDir(fname)
         end
       end
       found, file = dir:GetNext()
     end
-    found, file = dir:GetFirst(spec, wx.wxDIR_FILES)
+    found, file = dir:GetFirst(spec or "*", wx.wxDIR_FILES)
     while found do
       if not skip or not file:find(skip) then
         local fname = wx.wxFileName(path, file):GetFullPath()
-        table.insert(content, fname)
+        if #masks < 2 then -- files already filtered by spec
+          table.insert(content, fname)
+        else -- need to filter by mask as spec includes multiple extensions
+          for _, mask in ipairs(masks) do
+            if file:find(mask) then
+              table.insert(content, fname)
+              break
+            end
+          end
+        end
       end
       found, file = dir:GetNext()
     end
   end
-  getDir(path, spec)
+  getDir(path)
 
   local prefix = '\001' -- prefix to sort directories first
   local shadow = {}

t/8-file.lua
@@ -8,3 +8,8 @@ ok(ActivateFile('t/test.lua:10'), "Load file:line.")
 ok(not ActivateFile('t/foo.bar:10'), "Doesn't load non-existent file:line.")
 ok(ActivateFile(fullpath..':10'), "Load fullpath/file:line.")
 ok(not ActivateFile(fullpath..'/foo.bar:10'), "Doesn't load non-existent fullpath/file:line.")
+
+local luas = FileSysGetRecursive('.', true, '*.lua')
+local more = FileSysGetRecursive('.', true, '*.lua; *.more')
+cmp_ok(#luas, '>', 0, "List of files is returned for '.lua' extension")
+is(#luas, #more, "List of files is returned for '.lua' and '.lua; .more' is the same")