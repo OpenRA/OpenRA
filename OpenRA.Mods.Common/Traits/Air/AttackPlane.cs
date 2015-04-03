b1d348656102d8e494b694d3d764500bf37439f9
Oliver Brakmann <ob-web2013+github@posteo.net>
2015-04-03 23:56:59 +0200

    Expose Plane turn-to-attack delay to yaml

--- OpenRA.Mods.Common/Traits/Air/AttackPlane.cs
+++ OpenRA.Mods.Common/Traits/Air/AttackPlane.cs
@@ -16,13 +16,21 @@ namespace OpenRA.Mods.Common.Traits
 {
 	public class AttackPlaneInfo : AttackFrontalInfo
 	{
+		[Desc("Delay, in game ticks, before turning to attack.")]
+		public readonly int AttackTurnDelay = 50;
+
 		public override object Create(ActorInitializer init) { return new AttackPlane(init.Self, this); }
 	}
 
 	public class AttackPlane : AttackFrontal
 	{
+		public readonly AttackPlaneInfo AttackPlaneInfo;
+
 		public AttackPlane(Actor self, AttackPlaneInfo info)
-			: base(self, info) { }
+			: base(self, info)
+		{
+			AttackPlaneInfo = info;
+		}
 
 		public override Activity GetAttackActivity(Actor self, Target newTarget, bool allowMove)
 		{
