adb5119b896f34402b01bcb2bd56abaa2ebb6d38
Robin Neatherway <robin@semmle.com>
2015-04-09 16:30:38 +0100

    Remove version information from NuGet folder names
    This makes the names consistent and prevents changes in versions of
    dependencies from breaking the build.

--- thirdparty/fetch-thirdparty-deps.sh
+++ thirdparty/fetch-thirdparty-deps.sh
@@ -5,70 +5,70 @@ set -e
 
 if [ ! -f StyleCopPlus.dll ]; then
 	echo "Fetching StyleCopPlus from nuget"
-	nuget install StyleCopPlus.MSBuild -Version 4.7.49.5
-	cp ./StyleCopPlus.MSBuild.4.7.49.5/tools/StyleCopPlus.dll .
-	rm -rf StyleCopPlus.MSBuild.4.7.49.5
+	nuget install StyleCopPlus.MSBuild -Version 4.7.49.5 -ExcludeVersion
+	cp ./StyleCopPlus.MSBuild/tools/StyleCopPlus.dll .
+	rm -rf StyleCopPlus.MSBuild
 fi
 
 if [ ! -f StyleCop.dll ]; then
 	echo "Fetching StyleCop files from nuget"
-	nuget install StyleCop.MSBuild -Version 4.7.49.0
-	cp ./StyleCop.MSBuild.4.7.49.0/tools/StyleCop*.dll .
-	rm -rf StyleCop.MSBuild.4.7.49.0
+	nuget install StyleCop.MSBuild -Version 4.7.49.0 -ExcludeVersion
+	cp ./StyleCop.MSBuild/tools/StyleCop*.dll .
+	rm -rf StyleCop.MSBuild
 fi
 
 if [ ! -f ICSharpCode.SharpZipLib.dll ]; then
 	echo "Fetching ICSharpCode.SharpZipLib from nuget"
-	nuget install SharpZipLib -Version 0.86.0
-	cp ./SharpZipLib.0.86.0/lib/20/ICSharpCode.SharpZipLib.dll .
-	rm -rf SharpZipLib.0.86.0
+	nuget install SharpZipLib -Version 0.86.0 -ExcludeVersion
+	cp ./SharpZipLib/lib/20/ICSharpCode.SharpZipLib.dll .
+	rm -rf SharpZipLib
 fi
 
 if [ ! -f MaxMind.GeoIP2.dll ]; then
 	echo "Fetching MaxMind.GeoIP2 from nuget"
-	nuget install MaxMind.GeoIP2 -Version 2.1.0
-	cp ./MaxMind.Db.1.0.0.0/lib/net40/MaxMind.Db.* .
-	rm -rf MaxMind.Db.1.0.0.0
-	cp ./MaxMind.GeoIP2.2.1.0.0/lib/net40/MaxMind.GeoIP2* .
-	rm -rf MaxMind.GeoIP2.2.1.0.0
-	cp ./Newtonsoft.Json.6.0.5/lib/net40/Newtonsoft.Json* .
-	rm -rf Newtonsoft.Json.6.0.5
-	cp ./RestSharp.105.0.0/lib/net4-client/RestSharp* .
-	rm -rf RestSharp.105.0.0
+	nuget install MaxMind.GeoIP2 -Version 2.1.0 -ExcludeVersion
+	cp ./MaxMind.Db/lib/net40/MaxMind.Db.* .
+	rm -rf MaxMind.Db
+	cp ./MaxMind.GeoIP2/lib/net40/MaxMind.GeoIP2* .
+	rm -rf MaxMind.GeoIP2
+	cp ./Newtonsoft.Json/lib/net40/Newtonsoft.Json* .
+	rm -rf Newtonsoft.Json
+	cp ./RestSharp/lib/net4-client/RestSharp* .
+	rm -rf RestSharp
 fi
 
 if [ ! -f SharpFont.dll ]; then
 	echo "Fetching SharpFont from nuget"
-	nuget install SharpFont -Version 2.5.0.1
-	cp ./SharpFont.2.5.0.1/lib/net20/SharpFont* .
-	sed '/osx/s@\(dll="\)[^"]*\(" />\)@\1/Library/Frameworks/Mono.framework/Libraries/libfreetype.6.dylib\2@' ./SharpFont.2.5.0.1/Content/SharpFont.dll.config > SharpFont.dll.config
-	rm -rf SharpFont.2.5.0.1
+	nuget install SharpFont -Version 2.5.0.1 -ExcludeVersion
+	cp ./SharpFont/lib/net20/SharpFont* .
+	sed '/osx/s@\(dll="\)[^"]*\(" />\)@\1/Library/Frameworks/Mono.framework/Libraries/libfreetype.6.dylib\2@' ./SharpFont/Content/SharpFont.dll.config > SharpFont.dll.config
+	rm -rf SharpFont
 fi
 
 if [ ! -f nunit.framework.dll ]; then
 	echo "Fetching NUnit from nuget"
-	nuget install NUnit -Version 2.6.4
-	cp ./NUnit.2.6.4/lib/nunit.framework* .
-	rm -rf NUnit.2.6.4
+	nuget install NUnit -Version 2.6.4 -ExcludeVersion
+	cp ./NUnit/lib/nunit.framework* .
+	rm -rf NUnit
 fi
 
 if [ ! -f Mono.Nat.dll ]; then
 	echo "Fetching Mono.Nat from nuget"
-	nuget install Mono.Nat -Version 1.2.21
-	cp ./Mono.Nat.1.2.21.0/lib/net40/Mono.Nat.dll .
-	rm -rf Mono.Nat.1.2.21.0
+	nuget install Mono.Nat -Version 1.2.21 -ExcludeVersion
+	cp ./Mono.Nat/lib/net40/Mono.Nat.dll .
+	rm -rf Mono.Nat
 fi
 
 if [ ! -f Moq.dll ]; then
 	echo "Fetching Moq from NuGet."
-	nuget install Moq -Version 4.2.1502.0911
-	cp ./Moq.4.2.1502.0911/lib/net40/Moq.dll .
-	rm -rf Moq.4.2.1502.0911
+	nuget install Moq -Version 4.2.1502.0911 -ExcludeVersion
+	cp ./Moq/lib/net40/Moq.dll .
+	rm -rf Moq
 fi
 
 if [ ! -f FuzzyLogicLibrary.dll ]; then
 	echo "Fetching FuzzyLogicLibrary from NuGet."
-	nuget install FuzzyLogicLibrary -Version 1.2.0
-	cp ./FuzzyLogicLibrary.1.2.0/bin/Release/FuzzyLogicLibrary.dll .
-	rm -rf FuzzyLogicLibrary.1.2.0
+	nuget install FuzzyLogicLibrary -Version 1.2.0 -ExcludeVersion
+	cp ./FuzzyLogicLibrary/bin/Release/FuzzyLogicLibrary.dll .
+	rm -rf FuzzyLogicLibrary
 fi
