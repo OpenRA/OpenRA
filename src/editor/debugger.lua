OpenRA/OpenRA

commit 460fb617662d8b7add837fdcfd7efe0a8fac1014

Added copying of watch values to use when the value is too long to be shown.

src/editor/debugger.lua
@@ -1152,7 +1152,7 @@ local function debuggerCreateWatchWindow()
       and (watchCtrl:IsWatch(item) or watchCtrl:GetItemName(item) ~= nil))
   end
 
-  function watchCtrl:UpdateItemValue(item, value)
+  function watchCtrl:GetItemFullExpression(item)
     local expr = ''
     local origitem = item
     while true do
@@ -1165,6 +1165,25 @@ local function debuggerCreateWatchWindow()
       item = watchCtrl:GetItemParent(item)
       if not item:IsOk() then break end
     end
+    return expr
+  end
+
+  function watchCtrl:CopyItemValue(item)
+    local expr = self:GetItemFullExpression(item)
+
+    if debugger.running then debugger.update() end
+    if debugger.server and not debugger.running
+    and (not debugger.scratchpad or debugger.scratchpad.paused) then
+      copas.addthread(function ()
+        local _, values, error = debugger.evaluate(expr)
+        ide:CopyToClipboard(error and error:gsub("%[.-%]:%d+:%s+","")
+          or (#values == 0 and 'nil' or values[1]))
+      end)
+    end
+  end
+
+  function watchCtrl:UpdateItemValue(item, value)
+    local expr = self:GetItemFullExpression(item)
 
     if debugger.running then debugger.update() end
     if debugger.server and not debugger.running
@@ -1223,6 +1242,7 @@ local function debuggerCreateWatchWindow()
         { ID_ADDWATCH, TR("&Add Watch")..KSC(ID_ADDWATCH) },
         { ID_EDITWATCH, editlabel..KSC(ID_EDITWATCH) },
         { ID_DELETEWATCH, TR("&Delete Watch")..KSC(ID_DELETEWATCH) },
+        { ID_COPYWATCHVALUE, TR("&Copy Value")..KSC(ID_COPYWATCHVALUE) },
       })
       item = nil
     end)
@@ -1239,6 +1259,14 @@ local function debuggerCreateWatchWindow()
     function (event) watchCtrl:Delete(item or watchCtrl:GetSelection()) end)
   watchCtrl:Connect(ID_DELETEWATCH, wx.wxEVT_UPDATE_UI,
     function (event) event:Enable(watchCtrl:IsWatch(item or watchCtrl:GetSelection())) end)
+
+  watchCtrl:Connect(ID_COPYWATCHVALUE, wx.wxEVT_COMMAND_MENU_SELECTED,
+    function (event) watchCtrl:CopyItemValue(item or watchCtrl:GetSelection()) end)
+  watchCtrl:Connect(ID_COPYWATCHVALUE, wx.wxEVT_UPDATE_UI, function (event)
+    -- allow copying only when the debugger is available
+    event:Enable(item:IsOk() and debugger.server and not debugger.running
+     and (not debugger.scratchpad or debugger.scratchpad.paused))
+  end)
 
   local label
   watchCtrl:Connect(wx.wxEVT_COMMAND_TREE_BEGIN_LABEL_EDIT,

src/editor/ids.lua
@@ -135,6 +135,7 @@ ID_HELPCOMMUNITY    = NewID()
 ID_ADDWATCH         = NewID()
 ID_EDITWATCH        = NewID()
 ID_DELETEWATCH      = NewID()
+ID_COPYWATCHVALUE   = NewID()
 -- Editor popup menu items
 ID_GOTODEFINITION   = NewID()
 ID_RENAMEALLINSTANCES = NewID()

commit 06d63407e557cb5e98f180fd343d0fa5de4e8e5b

Fixed watch update and error reporting broken by 460fb617.

src/editor/debugger.lua
@@ -1154,7 +1154,6 @@ local function debuggerCreateWatchWindow()
 
   function watchCtrl:GetItemFullExpression(item)
     local expr = ''
-    local origitem = item
     while true do
       local name = watchCtrl:GetItemName(item)
       expr = (watchCtrl:IsWatch(item)
@@ -1165,7 +1164,7 @@ local function debuggerCreateWatchWindow()
       item = watchCtrl:GetItemParent(item)
       if not item:IsOk() then break end
     end
-    return expr
+    return expr, item:IsOk() and item or nil
   end
 
   function watchCtrl:CopyItemValue(item)
@@ -1183,7 +1182,7 @@ local function debuggerCreateWatchWindow()
   end
 
   function watchCtrl:UpdateItemValue(item, value)
-    local expr = self:GetItemFullExpression(item)
+    local expr, itemupd = self:GetItemFullExpression(item)
 
     if debugger.running then debugger.update() end
     if debugger.server and not debugger.running
@@ -1191,9 +1190,9 @@ local function debuggerCreateWatchWindow()
       copas.addthread(function ()
         local _, _, err = debugger.execute(expr..'='..value)
         if err then
-          watchCtrl:SetItemText(origitem, 'error: '..err:gsub("%[.-%]:%d+:%s+",""))
-        else
-          updateWatchesSync(item)
+          watchCtrl:SetItemText(item, 'error: '..err:gsub("%[.-%]:%d+:%s+",""))
+        elseif itemupd then
+          updateWatchesSync(itemupd)
         end
         updateStackSync()
       end)