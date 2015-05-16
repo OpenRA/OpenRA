db38f52721246a16f8b857abc4460623c1ff5272
deniz1a <deniz1a@deniz1a>
2015-05-16 15:20:07 +0300

    Fixes location of phase transport being revealed by attack line.

--- OpenRA.Game/Traits/World/Shroud.cs
+++ OpenRA.Game/Traits/World/Shroud.cs
@@ -383,11 +383,11 @@ namespace OpenRA.Traits
 
 		public bool IsTargetable(Actor a)
 		{
-			if (HasFogVisibility())
-				return true;
-
 			if (a.TraitsImplementing<IVisibilityModifier>().Any(t => !t.IsVisible(a, self.Owner)))
 				return false;
+
+			if (HasFogVisibility())
+				return true;
 
 			return GetVisOrigins(a).Any(IsVisible);
 		}
