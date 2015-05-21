615c22f158e0ad67275c2b9d2c3b6053dc58df48
Glenn Martin Jensen <glenn_jensen2@hotmail.com>
2015-05-21 09:49:04 +0200

    Added possibility to use spaces in Server.Name
    This way it's easier for others to set a servername with spaces in it by
    default

--- launch-dedicated.sh
+++ launch-dedicated.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # example launch script, see https://github.com/OpenRA/OpenRA/wiki/Dedicated for details
-Name="Dedicated-Server"
+Name="Dedicated Server"
 Mod="ra"
 Dedicated="True"
 DedicatedLoop="True"
@@ -10,6 +10,6 @@ AdvertiseOnline="False"
 
 while true; do
      mono --debug OpenRA.Game.exe Game.Mod=$Mod Server.Dedicated=$Dedicated Server.DedicatedLoop=$DedicatedLoop \
-     Server.Name=$Name Server.ListenPort=$ListenPort Server.ExternalPort=$ExternalPort \
+     Server.Name="$Name" Server.ListenPort=$ListenPort Server.ExternalPort=$ExternalPort \
      Server.AdvertiseOnline=$AdvertiseOnline
 done
