68cc00447c460da09a49f5ba6786aa84d893497e
Moviuro <moviuro@gmail.com>
2015-06-06 22:03:54 +0200

    launch-dedicated.sh: Support passing args from CLI

--- launch-dedicated.sh
+++ launch-dedicated.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # example launch script, see https://github.com/OpenRA/OpenRA/wiki/Dedicated for details
-Name="Dedicated Server"
-Mod="ra"
+Name="${Name:-"Dedicated Server"}"
+Mod="${Mod:-"ra"}"
 Dedicated="True"
 DedicatedLoop="True"
-ListenPort=1234
-ExternalPort=1234
-AdvertiseOnline="False"
+ListenPort="${ListenPort:-"1234"}"
+ExternalPort="${ExternalPort:-"1234"}"
+AdvertiseOnline="${AdvertiseOnline:-"False"}"
 
 while true; do
      mono --debug OpenRA.Game.exe Game.Mod=$Mod Server.Dedicated=$Dedicated Server.DedicatedLoop=$DedicatedLoop \
