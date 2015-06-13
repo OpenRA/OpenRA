9df6b3c15fac1390660517818c89406e496c5225
Moviuro <moviuro@gmail.com>
2015-06-13 10:23:24 +0200

    launch-dedicated.sh: add a tiny comment/usage

--- launch-dedicated.sh
+++ launch-dedicated.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 # example launch script, see https://github.com/OpenRA/OpenRA/wiki/Dedicated for details
+
+# Usage:
+#  $ ./launch-dedicated.sh # Launch a dedicated server with default settings
+#  $ Mod="d2k" ./launch-dedicated.sh # Launch a dedicated server with default settings but override the Mod
+#  Read the file to see which settings you can override
+
 Name="${Name:-"Dedicated Server"}"
 Mod="${Mod:-"ra"}"
 Dedicated="True"
