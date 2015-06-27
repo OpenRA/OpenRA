daec860192ccd1b5522ddf1f3809de2ef57ed64b
atlimit8 <atlimit8-vcs@gmx.com>
2015-06-27 11:40:52 -0500

    Added multiplier modifier traits, removing GainsStatUpgrades, InvulnerabilityUpgrade, & Invulnerable.

---
+++ OpenRA.Mods.Common/Traits/Multipliers/InaccuracyMultiplier.cs
@@ -0,0 +1,28 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2015 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation. For more information,
+ * see COPYING.
+ */
+#endregion
+
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Common.Traits
+{
+	[Desc("The inaccuracy of this actor is multipled based on upgrade level if specified.")]
+	public class InaccuracyMultiplierInfo : UpgradeMultiplierTraitInfo, ITraitInfo
+	{
+		public object Create(ActorInitializer init) { return new InaccuracyMultiplier(this, init.Self.Info.Name); }
+	}
+
+	public class InaccuracyMultiplier : UpgradeMultiplierTrait, IInaccuracyModifier
+	{
+		public InaccuracyMultiplier(InaccuracyMultiplierInfo info, string actorType)
+			: base(info, "InaccuracyMultiplier", actorType) { }
+
+		public int GetInaccuracyModifier() { return GetModifier(); }
+	}
+}
---
+++ OpenRA.Mods.Common/Traits/Multipliers/SpeedMultiplier.cs
@@ -0,0 +1,28 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2015 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation. For more information,
+ * see COPYING.
+ */
+#endregion
+
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Common.Traits
+{
+	[Desc("The speed of this actor is multiplied based on upgrade level if specified.")]
+	public class SpeedMultiplierInfo : UpgradeMultiplierTraitInfo, ITraitInfo
+	{
+		public object Create(ActorInitializer init) { return new SpeedMultiplier(this, init.Self.Info.Name); }
+	}
+
+	public class SpeedMultiplier : UpgradeMultiplierTrait, ISpeedModifier
+	{
+		public SpeedMultiplier(SpeedMultiplierInfo info, string actorType)
+			: base(info, "SpeedMultiplier", actorType) { }
+
+		public int GetSpeedModifier() { return GetModifier(); }
+	}
+}
