baa798d8313733c566f9f265330aeca354f1d5b2
Matthias Mailänder <matthias@mailaender.name>
2015-07-04 21:13:00 +0200

    autogenerate and install a UNIX man page

---
+++ OpenRA.Mods.Common/UtilityCommands/CreateManPage.cs
@@ -0,0 +1,68 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2015 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation. For more information,
+ * see COPYING.
+ */
+#endregion
+
+using System;
+using System.Linq;
+
+namespace OpenRA.Mods.Common.UtilityCommands
+{
+	class CreateManPage : IUtilityCommand
+	{
+		public string Name { get { return "--man-page"; } }
+
+		[Desc("Create a man page in troff format.")]
+		public void Run(ModData modData, string[] args)
+		{
+			Console.WriteLine(".TH OPENRA 6");
+			Console.WriteLine(".SH NAME");
+			Console.WriteLine("openra \\- An Open Source modernization of the early 2D Command & Conquer games.");
+			Console.WriteLine(".SH SYNOPSIS");
+			Console.WriteLine(".B openra");
+			Console.WriteLine("[\\fB\\Game.Mod=\\fR\\fImodchooser\\fR]");
+			Console.WriteLine(".SH DESCRIPTION");
+			Console.WriteLine(".B openra");
+			Console.WriteLine("starts the game.");
+			Console.WriteLine(".SH OPTIONS");
+
+			var sections = Game.Settings.Sections;
+			sections.Add("Launch", new OpenRA.Mods.Common.LoadScreens.BlankLoadScreen.LaunchArgs());
+			foreach (var section in sections.OrderBy(s => s.Key))
+			{
+				var fields = section.Value.GetType().GetFields();
+				foreach (var field in fields)
+				{
+					if (!field.HasAttribute<DescAttribute>())
+						continue;
+
+					Console.WriteLine(".TP");
+
+					Console.Write(".BR {0}.{1}=".F(section.Key, field.Name));
+					var value = field.GetValue(section.Value);
+					if (value != null && !value.ToString().StartsWith("System."))
+						Console.WriteLine("\\fI{0}\\fR".F(value));
+					else
+						Console.WriteLine();
+
+					var lines = field.GetCustomAttributes<DescAttribute>(false).SelectMany(d => d.Lines);
+					foreach (var line in lines)
+						Console.WriteLine(line);
+				}
+			}
+
+			Console.WriteLine(".SH FILES");
+			Console.WriteLine("Settings are stored in the ~/.openra user folder.");
+			Console.WriteLine(".SH BUGS");
+			Console.WriteLine("Known issues are tracked at http://bugs.openra.net");
+			Console.WriteLine(".SH COPYRIGHT");
+			Console.WriteLine("Copyright 2007-2015 The OpenRA Developers (see AUTHORS)");
+			Console.WriteLine("This manual is part of OpenRA, which is free software. It is GNU GPL v3 licensed. See COPYING for details.");
+		}
+	}
+}
