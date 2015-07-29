a534290dc5bd1b08a19edb71a0cf6d080e97f1be
deniz1a <deniz1a@deniz1a>
2015-07-29 23:47:30 +0300

    Removes spacing between dropdown menu items.

--- OpenRA.Mods.Common/Widgets/ListLayout.cs
+++ OpenRA.Mods.Common/Widgets/ListLayout.cs
@@ -21,22 +21,25 @@ namespace OpenRA.Mods.Common.Widgets
 		public void AdjustChild(Widget w)
 		{
 			if (widget.Children.Count == 0)
-				widget.ContentHeight = widget.ItemSpacing;
+				widget.ContentHeight = 2 * widget.TopBottomSpacing - widget.ItemSpacing;
 
-			w.Bounds.Y = widget.ContentHeight;
+			w.Bounds.Y = widget.ContentHeight - widget.TopBottomSpacing + widget.ItemSpacing;
 			if (!widget.CollapseHiddenChildren || w.IsVisible())
 				widget.ContentHeight += w.Bounds.Height + widget.ItemSpacing;
 		}
 
 		public void AdjustChildren()
 		{
-			widget.ContentHeight = widget.ItemSpacing;
+			widget.ContentHeight = widget.TopBottomSpacing;
 			foreach (var w in widget.Children)
 			{
 				w.Bounds.Y = widget.ContentHeight;
 				if (!widget.CollapseHiddenChildren || w.IsVisible())
 					widget.ContentHeight += w.Bounds.Height + widget.ItemSpacing;
 			}
+
+			// The loop above appended an extra widget.ItemSpacing after the last item. Replace it with proper bottom spacing.
+			widget.ContentHeight += widget.TopBottomSpacing - widget.ItemSpacing;
 		}
 	}
 }
