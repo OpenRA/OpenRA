519be4374c2fbd8710669387be57be66a8ee1f9c
RoosterDragon <RoosterDragon@outlook.com>
2015-08-13 23:38:23 +0100

    Fixed pooling of layers used for pathfinding.
    The previous implementation:
    - Was failing to dispose of pooled layers.
    - Was using a finalizer to allow undisposed layers to be reused.
    
    This means all pooled layers are kept alive indefinitely until the map changes. If the finalizer is slow for any reason then the pathfiinder will allocate new layers when the pool runs out. Since these new layers are eventually stuffed back into the pool when the finalizer does run, this can theoretically leak unbounded memory until the pool goes out of scope. In practice it would leak tens of megabytes.
    
    The new implementation ensures layers are disposed and pooled correctly to allow proper memory reuse. It also introduces some safeguards against memory leaks:
    - A cap is set on the number of pooled layers. If more concurrent layers are needed than this, then the excess layers will not be pooled but instead be allowed to be garbage collected.
    - No finalizer. An implementation that fails to call dispose simply allows the layer to be garbage collected instead.

---
+++ OpenRA.Mods.Common/Pathfinder/CellInfoLayerPool.cs
@@ -0,0 +1,78 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2015 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation. For more information,
+ * see COPYING.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace OpenRA.Mods.Common.Pathfinder
+{
+	sealed class CellInfoLayerPool
+	{
+		const int MaxPoolSize = 4;
+		readonly Stack<CellLayer<CellInfo>> pool = new Stack<CellLayer<CellInfo>>(MaxPoolSize);
+		readonly CellLayer<CellInfo> defaultLayer;
+
+		public CellInfoLayerPool(Map map)
+		{
+			defaultLayer =
+				CellLayer<CellInfo>.CreateInstance(
+					mpos => new CellInfo(int.MaxValue, int.MaxValue, mpos.ToCPos(map), CellStatus.Unvisited),
+					new Size(map.MapSize.X, map.MapSize.Y),
+					map.TileShape);
+		}
+
+		public PooledCellInfoLayer Get()
+		{
+			return new PooledCellInfoLayer(this);
+		}
+
+		CellLayer<CellInfo> GetLayer()
+		{
+			CellLayer<CellInfo> layer = null;
+			lock (pool)
+				if (pool.Count > 0)
+					layer = pool.Pop();
+
+			if (layer == null)
+				layer = new CellLayer<CellInfo>(defaultLayer.Shape, defaultLayer.Size);
+			layer.CopyValuesFrom(defaultLayer);
+			return layer;
+		}
+
+		void ReturnLayer(CellLayer<CellInfo> layer)
+		{
+			lock (pool)
+			   if (pool.Count < MaxPoolSize)
+					pool.Push(layer);
+		}
+
+		public class PooledCellInfoLayer : IDisposable
+		{
+			public CellLayer<CellInfo> Layer { get; private set; }
+			CellInfoLayerPool layerPool;
+
+			public PooledCellInfoLayer(CellInfoLayerPool layerPool)
+			{
+				this.layerPool = layerPool;
+				Layer = layerPool.GetLayer();
+			}
+
+			public void Dispose()
+			{
+				if (Layer == null)
+					return;
+				layerPool.ReturnLayer(Layer);
+				Layer = null;
+				layerPool = null;
+			}
+		}
+	}
+}
--- OpenRA.Mods.Common/Traits/Harvester.cs
+++ OpenRA.Mods.Common/Traits/Harvester.cs
@@ -165,23 +165,24 @@ namespace OpenRA.Mods.Common.Traits
 				select new { Location = r.Actor.Location + r.Trait.DeliveryOffset, Actor = r.Actor, Occupancy = linkedHarvs }).ToDictionary(r => r.Location);
 
 			// Start a search from each refinery's delivery location:
+			List<CPos> path;
 			var mi = self.Info.Traits.Get<MobileInfo>();
-			var path = self.World.WorldActor.Trait<IPathFinder>().FindPath(
-				PathSearch.FromPoints(self.World, mi, self, refs.Values.Select(r => r.Location), self.Location, false)
-					.WithCustomCost(loc =>
-					{
-						if (!refs.ContainsKey(loc))
-							return 0;
+			using (var search = PathSearch.FromPoints(self.World, mi, self, refs.Values.Select(r => r.Location), self.Location, false)
+				.WithCustomCost(loc =>
+				{
+					if (!refs.ContainsKey(loc))
+						return 0;
 
-						var occupancy = refs[loc].Occupancy;
+					var occupancy = refs[loc].Occupancy;
 
-						// 4 harvesters clogs up the refinery's delivery location:
-						if (occupancy >= 3)
-							return Constants.InvalidNode;
+					// 4 harvesters clogs up the refinery's delivery location:
+					if (occupancy >= 3)
+						return Constants.InvalidNode;
 
-						// Prefer refineries with less occupancy (multiplier is to offset distance cost):
-						return occupancy * 12;
-					}));
+					// Prefer refineries with less occupancy (multiplier is to offset distance cost):
+					return occupancy * 12;
+				}))
+				path = self.World.WorldActor.Trait<IPathFinder>().FindPath(search);
 
 			if (path.Count != 0)
 				return refs[path.Last()].Actor;
