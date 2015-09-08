e80118de1e0c9a69578b71d15ced41beba52d02d
Paul Kulchenko <paul@kulchenko.com>
2015-09-07 21:20:15 -0700

    Updated package data serialization to use simpler dumper for faster processing.

--- src/editor/settings.lua
+++ src/editor/settings.lua
@@ -268,17 +268,41 @@ function SettingsRestorePackage(package)
   return outtab
 end
 
+local function plaindump(val, opts, done)
+  local keyignore = opts and opts.keyignore or {}
+  local final = done == nil
+  opts, done = opts or {}, done or {}
+  local t = type(val)
+  if t == "table" then
+    done[#done+1] = '{'
+    done[#done+1] = ''
+    for key, value in pairs (val) do
+      if not keyignore[key] then
+        done[#done+1] = '['
+        plaindump(key, opts, done)
+        done[#done+1] = ']='
+        plaindump(value, opts, done)
+        done[#done+1] = ","
+      end
+    end
+    done[#done] = '}'
+  elseif t == "string" then
+    done[#done+1] = ("%q"):format(val):gsub("\010","n"):gsub("\026","\\026")
+  elseif t == "number" then
+    done[#done+1] = ("%.17g"):format(val)
+  else
+    done[#done+1] = tostring(val)
+  end
+  return final and table.concat(done, '')
+end
+
 function SettingsSavePackage(package, values, opts)
   local packagename = "/package/"..package
   local path = settings:GetPath()
-  local mdb = require('mobdebug')
-
-  local options = {comment = false, nocode = true, sortkeys = false, compact = true}
-  for k,v in pairs(opts or {}) do options[k] = v end
 
   settings:DeleteGroup(packagename)
   settings:SetPath(packagename)
-  for k,v in pairs(values or {}) do settings:Write(k, mdb.line(v, options)) end
+  for k,v in pairs(values or {}) do settings:Write(k, plaindump(v, opts)) end
   settings:SetPath(path)
 end
 
