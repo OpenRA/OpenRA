OpenRA/OpenRA

commit 5e4e1962bd315059feb19eb28e9a397055cdc226

Updated Run/Debug to not force saving new tabs when `Start` file is set.

src/editor/menu_project.lua
@@ -184,6 +184,7 @@ frame:Connect(ID_PROJECTDIRFROMFILE, wx.wxEVT_UPDATE_UI,
 
 local function getNameToRun(skipcheck)
   local editor = GetEditor()
+  if not editor then return end
 
   -- test compile it before we run it, if successful then ask to save
   -- only compile if lua api
@@ -196,11 +197,12 @@ local function getNameToRun(skipcheck)
   end
 
   local doc = ide:GetDocument(editor)
-  if not doc:GetFilePath() then doc:SetModified(true) end
+  local name = ide:GetProjectStartFile() or doc:GetFilePath()
+  if not name then doc:SetModified(true) end
   if not SaveIfModified(editor) then return end
   if ide.config.editor.saveallonrun then SaveAll(true) end
 
-  return wx.wxFileName(ide:GetProjectStartFile() or doc:GetFilePath())
+  return wx.wxFileName(name)
 end
 
 function ActivateOutput()

commit 45c38ab3ecbc822a0df858b56c2065343f3c6cf2

Fixed running `Run`/`Debug` commands on unnamed files; regression from 5e4e1962.

src/editor/menu_project.lua
@@ -202,7 +202,7 @@ local function getNameToRun(skipcheck)
   if not SaveIfModified(editor) then return end
   if ide.config.editor.saveallonrun then SaveAll(true) end
 
-  return wx.wxFileName(name)
+  return wx.wxFileName(name or doc:GetFilePath())
 end
 
 function ActivateOutput()