d2a5d74a1e2ee7e85d58bec1181c1a530b99f109
Matthias Mailänder <matthias@mailaender.name>
2015-09-27 18:37:17 +0200

    expose pips to trait documentation and lint testing

--- OpenRA.Mods.RA/Traits/Buildings/Fake.cs
+++ OpenRA.Mods.RA/Traits/Buildings/Fake.cs
@@ -9,15 +9,45 @@
 #endregion
 
 using System.Collections.Generic;
+using OpenRA.Graphics;
 using OpenRA.Traits;
 
 namespace OpenRA.Mods.RA.Traits
 {
-	[Desc("Display a sprite tag \"fake\" when selected.")]
-	class FakeInfo : TraitInfo<Fake> { }
-
-	class Fake : ITags
+	[Desc("Display a sprite tag when selected.")]
+	class FakeInfo : ITraitInfo
 	{
-		public IEnumerable<TagType> GetTags() { yield return TagType.Fake; }
+		public readonly string Image = "pips";
+
+		[SequenceReference("Image")] public readonly string TagSequence = "tag-fake";
+
+		[PaletteReference] public readonly string Palette = "chrome";
+
+		public object Create(ActorInitializer init) { return new Fake(init.Self, this); }
+	}
+
+	class Fake : IPostRenderSelection
+	{
+		readonly FakeInfo info;
+		readonly Actor self;
+
+		public Fake(Actor self, FakeInfo info)
+		{
+			this.self = self;
+			this.info = info;
+		}
+
+		public IEnumerable<IRenderable> RenderAfterWorld(WorldRenderer wr)
+		{
+			var tagImages = new Animation(wr.World, info.Image);
+			var pal = wr.Palette(info.Palette);
+			var tagxyOffset = new int2(0, 6);
+			tagImages.PlayRepeating(info.TagSequence);
+			var b = self.VisualBounds;
+			var center = wr.ScreenPxPosition(self.CenterPosition);
+			var tm = wr.Viewport.WorldToViewPx(center + new int2((b.Left + b.Right) / 2, b.Top));
+			var pos = tm + tagxyOffset - (0.5f * tagImages.Image.Size).ToInt2();
+			yield return new UISpriteRenderable(tagImages.Image, pos, 0, pal, 1f);
+		}
 	}
 }
