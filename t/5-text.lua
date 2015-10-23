c3134f85b133710efc4f49c7f67c00ae86882777
Paul Kulchenko <paul@kulchenko.com>
2015-10-23 14:27:23 -0700

    Fixed text validation tests to avoid skipping results.

--- t/5-text.lua
+++ t/5-text.lua
@@ -2,13 +2,15 @@ local output = ide:GetOutput()
 local shell = ide:GetConsole()
 
 local text = output:GetText()
+local ro = output:GetReadOnly()
 output:SetReadOnly(false)
-output:SetText("")
 DisplayOutputLn(string.char(0x80, 0x81, 0x82)) -- three invalid UTF-8 chars
-is(output:GetText(), '\022\022\022\n',
-  "Output with invalid UTF-8 characters is displayed.")
-output:SetText(text)
-output:SetReadOnly(true)
+local cmptext1, cmptext2 = output:GetText(), "\022\022\022\n"
+output:SetTargetStart(output:GetLength()-1-#cmptext2)
+output:SetTargetEnd(output:GetLength()-1)
+output:ReplaceTarget("")
+output:SetReadOnly(ro)
+is(cmptext1:sub(-#cmptext2), cmptext2, "Output with invalid UTF-8 characters is displayed.")
 
 local text = shell:GetText()
 shell:SetText("")
