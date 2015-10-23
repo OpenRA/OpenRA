1f8b2a746f47f03c1f60197764f8204a750b111d
Paul Kulchenko <paul@kulchenko.com>
2015-10-23 16:17:20 -0700

    Fixed texts that use Console.

--- t/5-text.lua
+++ t/5-text.lua
@@ -1,21 +1,21 @@
 local output = ide:GetOutput()
-local shell = ide:GetConsole()
+DisplayOutputLn(string.char(0x80, 0x81, 0x82)) -- three invalid UTF-8 chars
+local cmptext, text = "\022\022\022\n", output:GetText()
 
-local text = output:GetText()
 local ro = output:GetReadOnly()
 output:SetReadOnly(false)
-DisplayOutputLn(string.char(0x80, 0x81, 0x82)) -- three invalid UTF-8 chars
-local cmptext1, cmptext2 = output:GetText(), "\022\022\022\n"
-output:SetTargetStart(output:GetLength()-1-#cmptext2)
+output:SetTargetStart(output:GetLength()-1-#cmptext)
 output:SetTargetEnd(output:GetLength()-1)
 output:ReplaceTarget("")
 output:SetReadOnly(ro)
-is(cmptext1:sub(-#cmptext2), cmptext2, "Output with invalid UTF-8 characters is displayed.")
 
-local text = shell:GetText()
-shell:SetText("")
+is(text:sub(-#cmptext), cmptext, "Output with invalid UTF-8 characters is displayed.")
+
+local shell = ide:GetConsole()
 DisplayShell(string.char(0x80, 0x81, 0x82)) -- three invalid UTF-8 chars
-is(shell:GetText(), [[\128\129\130]].."\n",
+local cmptext = [[\128\129\130]].."\n"
+is(shell:GetText():sub(-#cmptext), cmptext,
   "Console output with invalid UTF-8 characters is displayed.")
-shell:SetText(text)
-
+shell:SetTargetStart(shell:GetLength()-1-#cmptext)
+shell:SetTargetEnd(shell:GetLength()-1)
+shell:ReplaceTarget("")
