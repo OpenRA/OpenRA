8072b866c37e708bb00c03f61b3ad66c69537a94
Zimmermann Gyula <graiondilach@hotmail.com>
2015-11-03 17:39:53 +0100

    Throw a semi-basic cluster warhead implementation.

---
+++ Traits/Warheads/FireClusterWarhead.cs
@@ -0,0 +1,75 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2007-2015 OpenRA.Mods.AS Developers (see AUTHORS)
+ * This file is a part of a third-party plugin for OpenRA, which is
+ * free software. It is made available to you under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation. For more information, see COPYING.
+ */
+#endregion
+
+using System.Collections.Generic;
+using System.Linq;
+using OpenRA.Mods.Common.Warheads;
+using OpenRA.Traits;
+using OpenRA.GameRules;
+
+namespace OpenRA.Mods.AS.Warheads
+{
+	public class FireClusterWarhead : Warhead
+	{
+		[WeaponReference, FieldLoader.Require]
+		[Desc("Has to be defined in weapons.yaml as well.")]
+		public readonly string Weapon = null;
+
+		[Desc("The range of the cells where the weapon should be fired.")]
+		public readonly int Range = 0;
+
+		public override void DoImpact(Target target, Actor firedBy, IEnumerable<int> damageModifiers)
+		{
+			var map = firedBy.World.Map;
+			var targetCells = map.FindTilesInCircle(map.CellContaining(target.CenterPosition), Range);
+			var weapon = map.Rules.Weapons[Weapon.ToLowerInvariant()];
+
+			foreach (var cell in targetCells) {
+
+				var args = new ProjectileArgs
+				{
+					Weapon = weapon,
+					Facing = Util.GetFacing(map.CenterOfCell(cell)-target.CenterPosition, 0),
+
+					DamageModifiers = firedBy.TraitsImplementing<IFirepowerModifier>()
+						.Select(a => a.GetFirepowerModifier()).ToArray(),
+
+					InaccuracyModifiers = firedBy.TraitsImplementing<IInaccuracyModifier>()
+						.Select(a => a.GetInaccuracyModifier()).ToArray(),
+
+					RangeModifiers = firedBy.TraitsImplementing<IRangeModifier>()
+						.Select(a => a.GetRangeModifier()).ToArray(),
+
+					Source = target.CenterPosition,
+					SourceActor = firedBy,
+					PassiveTarget = map.CenterOfCell(cell),
+					GuidedTarget = Target.FromCell(firedBy.World, cell)
+				};
+
+				if (args.Weapon.Projectile != null)
+				{
+					var projectile = args.Weapon.Projectile.Create(args);
+					if (projectile != null)
+						firedBy.World.Add(projectile);
+
+					if (args.Weapon.Report != null && args.Weapon.Report.Any())
+						Game.Sound.Play(args.Weapon.Report.Random(firedBy.World.SharedRandom), target.CenterPosition);
+				}
+			}
+
+		}
+
+		public bool IsValidImpact(WPos pos, Actor firedBy)
+		{
+			return true;
+		}
+	}
+}
+
