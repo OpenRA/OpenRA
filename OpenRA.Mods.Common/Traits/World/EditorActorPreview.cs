f28b37323f4e3e9be29b1652c401ddcf4db3002c
atlimit8 <atlimit8-vcs@gmx.com>
2015-11-07 14:37:50 -0600

    Made editor actor preview tooltip easier to read

--- OpenRA.Mods.Common/Traits/World/EditorActorPreview.cs
+++ OpenRA.Mods.Common/Traits/World/EditorActorPreview.cs
@@ -70,7 +70,8 @@ namespace OpenRA.Mods.Common.Traits
 			}
 
 			var tooltip = Info.TraitInfoOrDefault<TooltipInfo>();
-			Tooltip = tooltip == null ? ID + ": " + Info.Name : ID + ": " + tooltip.Name + " (" + Info.Name + ")" + "\n" + owner.Name + " (" + owner.Faction + ")";
+			Tooltip = (tooltip == null ? " < " + Info.Name + " >" : tooltip.Name) + "\n" + owner.Name + " (" + owner.Faction + ")"
+				+ "\nID: " + ID + "\nType: " + Info.Name;
 
 			GeneratePreviews();
 
