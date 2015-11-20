6c539495dca4280a2150881087c5be077883c586
Zimmermann Gyula <graiondilach@hotmail.com>
2015-11-20 11:52:00 +0100

    Implement RA2 Chrono Miner.
    Based on https://github.com/Phrohdoh/OpenRA/pull/53.

---
+++ Effects/SimpleAnim.cs
@@ -0,0 +1,43 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2015- OpenRA.Mods.AS Developers (see AUTHORS)
+ * This file is a part of a third-party plugin for OpenRA, which is
+ * free software. It is made available to you under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation. For more information, see COPYING.
+ */
+#endregion
+
+using System.Collections.Generic;
+using OpenRA.Effects;
+using OpenRA.Graphics;
+
+namespace OpenRA.Mods.AS.Effects
+{
+	public class SimpleAnim : IEffect
+	{
+		readonly World world;
+		readonly string palette;
+		readonly Animation anim;
+		WPos pos;
+
+		public SimpleAnim(World world, WPos pos, string name, string sequence, string palette)
+		{
+			this.world = world;
+			this.pos = pos;
+			this.palette = palette;
+			anim = new Animation(world, name);
+			anim.PlayThen(sequence, () => world.AddFrameEndTask(w => w.Remove(this)));
+		}
+
+		public void Tick(World world) { anim.Tick(); }
+
+		public IEnumerable<IRenderable> Render(WorldRenderer wr)
+		{
+			if (world.FogObscures(pos))
+				return SpriteRenderable.None;
+
+			return anim.Render(pos, wr.Palette(palette));
+		}
+	}
+}
