5c846496a4f673a9ba14f1f7e5952426e6279f51
Zimmermann Gyula <graiondilach@hotmail.com>
2015-11-20 11:44:30 +0100

    Fix copyright notice.

--- Traits/Multipliers/HealthPercentageFirepowerMultiplier.cs
+++ Traits/Multipliers/HealthPercentageFirepowerMultiplier.cs
@@ -1,6 +1,6 @@
 ﻿#region Copyright & License Information
 /*
- * Copyright 2007-2015 OpenRA.Mods.AS Developers (see AUTHORS)
+ * Copyright 2015- OpenRA.Mods.AS Developers (see AUTHORS)
  * This file is a part of a third-party plugin for OpenRA, which is
  * free software. It is made available to you under the terms of the
  * GNU General Public License as published by the Free Software
