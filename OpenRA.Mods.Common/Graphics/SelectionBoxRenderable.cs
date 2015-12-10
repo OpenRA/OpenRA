b0806fca927d7f16ffd86c57e7142836244baafc
Paul Chote <pchote@users.noreply.github.com>
2015-12-10 17:48:52 +0000

    Use RgbaColorRenderer in SelectionBoxRenderable.

--- OpenRA.Mods.Common/Graphics/SelectionBoxRenderable.cs
+++ OpenRA.Mods.Common/Graphics/SelectionBoxRenderable.cs
@@ -17,18 +17,16 @@ namespace OpenRA.Mods.Common.Graphics
 	public struct SelectionBoxRenderable : IRenderable, IFinalizedRenderable
 	{
 		readonly WPos pos;
-		readonly float scale;
 		readonly Rectangle visualBounds;
 		readonly Color color;
 
 		public SelectionBoxRenderable(Actor actor, Color color)
-			: this(actor.CenterPosition, actor.VisualBounds, 1f, color) { }
+			: this(actor.CenterPosition, actor.VisualBounds, color) { }
 
-		public SelectionBoxRenderable(WPos pos, Rectangle visualBounds, float scale, Color color)
+		public SelectionBoxRenderable(WPos pos, Rectangle visualBounds, Color color)
 		{
 			this.pos = pos;
 			this.visualBounds = visualBounds;
-			this.scale = scale;
 			this.color = color;
 		}
 
@@ -40,30 +38,26 @@ namespace OpenRA.Mods.Common.Graphics
 
 		public IRenderable WithPalette(PaletteReference newPalette) { return this; }
 		public IRenderable WithZOffset(int newOffset) { return this; }
-		public IRenderable OffsetBy(WVec vec) { return new SelectionBoxRenderable(pos + vec, visualBounds, scale, color); }
+		public IRenderable OffsetBy(WVec vec) { return new SelectionBoxRenderable(pos + vec, visualBounds, color); }
 		public IRenderable AsDecoration() { return this; }
 
 		public IFinalizedRenderable PrepareRender(WorldRenderer wr) { return this; }
 		public void Render(WorldRenderer wr)
 		{
+			var iz = 1 / wr.Viewport.Zoom;
 			var screenPos = wr.ScreenPxPosition(pos);
-			var tl = screenPos + scale * new float2(visualBounds.Left, visualBounds.Top);
-			var br = screenPos + scale * new float2(visualBounds.Right, visualBounds.Bottom);
+			var tl = screenPos + new float2(visualBounds.Left, visualBounds.Top);
+			var br = screenPos + new float2(visualBounds.Right, visualBounds.Bottom);
 			var tr = new float2(br.X, tl.Y);
 			var bl = new float2(tl.X, br.Y);
-			var u = new float2(4f / wr.Viewport.Zoom, 0);
-			var v = new float2(0, 4f / wr.Viewport.Zoom);
+			var u = new float2(4 * iz, 0);
+			var v = new float2(0, 4 * iz);
 
-			var wlr = Game.Renderer.WorldLineRenderer;
-			wlr.DrawLine(tl + u, tl, color);
-			wlr.DrawLine(tl, tl + v, color);
-			wlr.DrawLine(tr, tr - u, color);
-			wlr.DrawLine(tr, tr + v, color);
-
-			wlr.DrawLine(bl, bl + u, color);
-			wlr.DrawLine(bl, bl - v, color);
-			wlr.DrawLine(br, br - u, color);
-			wlr.DrawLine(br, br - v, color);
+			var wcr = Game.Renderer.WorldRgbaColorRenderer;
+			wcr.DrawLine(new[] { tl + u, tl, tl + v }, iz, color, true);
+			wcr.DrawLine(new[] { tr - u, tr, tr + v }, iz, color, true);
+			wcr.DrawLine(new[] { br - u, br, br - v }, iz, color, true);
+			wcr.DrawLine(new[] { bl + u, bl, bl - v }, iz, color, true);
 		}
 
 		public void RenderDebugGeometry(WorldRenderer wr) { }
