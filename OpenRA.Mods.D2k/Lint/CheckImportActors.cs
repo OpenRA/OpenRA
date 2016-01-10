4012433e1007c99908c83c77cf00c300f8224726
Matthias Mailänder <matthias@mailaender.name>
2016-01-10 07:12:06 +0100

    test and correct the ActorDataByActorCode

---
+++ OpenRA.Mods.D2k/Lint/CheckImportActors.cs
@@ -0,0 +1,28 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2015 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation. For more information,
+ * see COPYING.
+ */
+#endregion
+
+using System;
+using OpenRA.Mods.D2k.UtilityCommands;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.D2k.Lint
+{
+	public class CheckImportActors : ILintRulesPass
+	{
+		public void Run(Action<string> emitError, Action<string> emitWarning, Ruleset rules)
+		{
+			foreach (var actorData in D2kMapImporter.ActorDataByActorCode.Values)
+			{
+				if (!rules.Actors.ContainsKey(actorData.First))
+					emitError("Undefined actor {0} in map import code.".F(actorData.First));
+			}
+		}
+	}
+}
