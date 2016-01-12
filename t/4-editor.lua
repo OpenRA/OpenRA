05d2cc8f31e875ea5f3beff352188a40724defa2
Paul Kulchenko <paul@kulchenko.com>
2016-01-12 09:49:29 -0800

    Added breakpoint/bookmark toggling tests.

--- t/4-editor.lua
+++ t/4-editor.lua
@@ -49,6 +49,22 @@ text = editor:GetText()
 ok(text:find(comment.."  "..comment..line) and text:find(comment.."1") and not text:find(comment.."3"),
   "Selection that ends at the beginning of the line doesn't comment line.")
 
+editor:SetText("1\n--2\n3\n")
+is(#editor:MarkerGetAll(), 0, "No marker is set in a new editor.")
+editor:BookmarkToggle(0)
+is(#editor:MarkerGetAll(), 1, "Bookmark is set after toggling.")
+is(#editor:MarkerGetAll(2^StylesGetMarker("bookmark")), 1, "Bookmark is the only marker set after toggling.")
+editor:BookmarkToggle(0)
+is(#editor:MarkerGetAll(), 0, "Bookmark is removed after toggling on a line with a bookmark.")
+
+editor:BreakpointToggle(0)
+is(#editor:MarkerGetAll(), 1, "Breakpoint is set after toggling.")
+is(#editor:MarkerGetAll(2^StylesGetMarker("breakpoint")), 1, "Breakpoint is the only marker set after toggling.")
+editor:BreakpointToggle(0)
+is(#editor:MarkerGetAll(), 0, "Breakpoint is removed after toggling on a line with a breakpoint.")
+editor:BreakpointToggle(1)
+is(#editor:MarkerGetAll(), 0, "Breakpoint is not set on a comment line.")
+
 -- cleanup
-ide:GetDocument(editor).isModified = false
+ide:GetDocument(editor):SetModified(false)
 ClosePage()
