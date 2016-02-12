f1fcd44add20f720f1c9ce1e0cf18062c9a4b6cc
Zimmermann Gyula <graiondilach@hotmail.com>
2016-02-12 18:53:04 +0100

    Add an expanded JASC palette parser based on input from Apollo.

---
+++ Traits/World/PaletteFromJasc32File.cs
@@ -0,0 +1,86 @@
+#region Copyright & License Information
+/*
+ * Copyright 2015- OpenRA.Mods.AS Developers (see AUTHORS)
+ * This file is a part of a third-party plugin for OpenRA, which is
+ * free software. It is made available to you under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation. For more information, see COPYING.
+ */
+#endregion
+
+using OpenRA.Graphics;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace OpenRA.Mods.AS.Traits
+{
+	[Desc("This is an expanded JASC palette format aiming to have 32bit colors in a palette."
+		+ "It also supports loading proper (legacy) JASC palettes.")]
+	class PaletteFromJasc32FileInfo : ITraitInfo
+	{
+		[FieldLoader.Require, PaletteDefinition]
+		[Desc("Internal palette name.")]
+		public readonly string Name = null;
+
+		[Desc("If defined, load the palette only for this tileset.")]
+		public readonly string Tileset = null;
+
+		[FieldLoader.Require]
+		[Desc("Name of the file to load.")]
+		public readonly string Filename = null;
+
+		[Desc("Map listed indices to shadow. Ignores previous color.")]
+		public readonly int[] ShadowIndex = { };
+
+		public readonly bool AllowModifiers = true;
+
+		public object Create(ActorInitializer init) { return new PaletteFromJasc32File(init.World, this); }
+	}
+
+	class PaletteFromJasc32File : ILoadsPalettes, IProvidesAssetBrowserPalettes
+	{
+		readonly World world;
+		readonly PaletteFromJasc32FileInfo info;
+		public PaletteFromJasc32File(World world, PaletteFromJasc32FileInfo info)
+		{
+			this.world = world;
+			this.info = info;
+		}
+
+		public void LoadPalettes(WorldRenderer wr)
+		{
+			var colors = new uint[Palette.Size];
+			using (var s = Game.ModData.ModFiles.Open(info.Filename))
+			{
+				var lines = s.ReadAllLines().ToArray();
+				if (lines[0] != "JASC-PAL")
+					throw new InvalidDataException("File {0} is not a valid JASC palatte!".F(info.Filename));
+
+				colors[0] = 0;
+				for (var i = 1; i < Palette.Size; i++)
+				{
+					var split = lines[i + 3].Split(' ');
+					colors[i] = (split.Count() == 4)
+						? (uint)Color.FromArgb(int.Parse(split[3]), int.Parse(split[0]), int.Parse(split[1]), int.Parse(split[2])).ToArgb()
+						: (uint)Color.FromArgb(int.Parse(split[0]), int.Parse(split[1]), int.Parse(split[2])).ToArgb();
+				}
+			}
+
+			wr.AddPalette(info.Name, new ImmutablePalette(colors), info.AllowModifiers);
+		}
+
+		public IEnumerable<string> PaletteNames
+		{
+			get
+			{
+				// Only expose the palette if it is available for the shellmap's tileset (which is a requirement for its use).
+				if (info.Tileset == null || info.Tileset == world.TileSet.Id)
+					yield return info.Name;
+			}
+		}
+	}
+}
