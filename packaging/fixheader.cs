e71225496b21222c328ab81206b74ba50650a0a4
Paul Chote <pchote@users.noreply.github.com>
2016-02-21 16:30:48 +0000

    Clarify GPL version.

--- packaging/fixheader.cs
+++ packaging/fixheader.cs
@@ -3,8 +3,9 @@
  * Copyright 2007-2011 The OpenRA Developers (see AUTHORS)
  * This file is part of OpenRA, which is free software. It is made
  * available to you under the terms of the GNU General Public License
- * as published by the Free Software Foundation. For more information,
- * see COPYING.
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
  */
 #endregion
 
