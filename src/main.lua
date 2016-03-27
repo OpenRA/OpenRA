OpenRA/OpenRA

commit 97c7bb96e762e8df6ad4fc17638ae05f6396e6f1

Reorganized restoring settings to make current file better visible in the project tree.

src/main.lua
@@ -621,6 +621,7 @@ ProjectUpdateInterpreters()
 -- load rest of settings
 SettingsRestoreEditorSettings()
 SettingsRestoreFramePosition(ide.frame, "MainFrame")
+SettingsRestoreView()
 SettingsRestoreFileHistory(SetFileHistory)
 SettingsRestoreProjectSession(FileTreeSetProjects)
 SettingsRestoreFileSession(function(tabs, params)
@@ -628,7 +629,6 @@ SettingsRestoreFileSession(function(tabs, params)
   then return SetOpenTabs(params)
   else return SetOpenFiles(tabs, params) end
 end)
-SettingsRestoreView()
 
 -- ---------------------------------------------------------------------------
 -- Load the filenames

commit cb45bcd5035cacb626fc10772f32ba2d8a84e269

Fixed restoring split tab configuration that was broken by 97c7bb96.

src/main.lua
@@ -652,7 +652,6 @@ ProjectUpdateInterpreters()
 
 -- load rest of settings
 SettingsRestoreFramePosition(ide.frame, "MainFrame")
-SettingsRestoreView()
 SettingsRestoreFileHistory(SetFileHistory)
 SettingsRestoreEditorSettings()
 SettingsRestoreProjectSession(FileTreeSetProjects)
@@ -661,6 +660,7 @@ SettingsRestoreFileSession(function(tabs, params)
   then return SetOpenTabs(params)
   else return SetOpenFiles(tabs, params) end
 end)
+SettingsRestoreView()
 
 -- ---------------------------------------------------------------------------
 -- Load the filenames