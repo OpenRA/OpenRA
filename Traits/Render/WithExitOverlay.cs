c7efe5e293ee40eb85a9ad6a6bf3e21e3f229c06
Zimmermann Gyula <graiondilach@hotmail.com>
2016-04-30 15:37:34 +0200

    Add the RA2 WithExitOverlay for fixing Soviet War Factory.
    Thanks, Mailaender!

---
+++ Traits/Render/WithExitOverlay.cs
@@ -0,0 +1,89 @@
+#region Copyright & License Information
+/*
+ * Copyright 2015- OpenRA.Mods.AS Developers (see AUTHORS)
+ * This file is a part of a third-party plugin for OpenRA, which is
+ * free software. It is made available to you under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation. For more information, see COPYING.
+ */
+#endregion
+
+using System;
+using System.Linq;
+using OpenRA.Graphics;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Mods.Common.Traits.Render;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.AS.Traits.Render
+{
+	[Desc("Renders an animation when the actor is leaving from a production building.")]
+	public class WithExitOverlayInfo : ITraitInfo, Requires<RenderSpritesInfo>, Requires<BodyOrientationInfo>
+	{
+		[Desc("Sequence name to use")]
+		[SequenceReference] public readonly string Sequence = "exit-overlay";
+
+		[Desc("Position relative to body")]
+		public readonly WVec Offset = WVec.Zero;
+
+		[Desc("Custom palette name")]
+		[PaletteReference("IsPlayerPalette")] public readonly string Palette = null;
+
+		[Desc("Custom palette is a player palette BaseName")]
+		public readonly bool IsPlayerPalette = false;
+
+		public object Create(ActorInitializer init) { return new WithExitOverlay(init.Self, this); }
+	}
+
+	public class WithExitOverlay : INotifyDamageStateChanged, INotifyBuildComplete, INotifySold, INotifyProduction
+	{
+		readonly Actor self;
+		readonly Animation overlay;
+		bool buildComplete;
+		CPos exit;
+
+		bool IsExitBlocked
+		{
+			get { return self.World.ActorMap.GetActorsAt(exit).Any(a => a != self); }
+		}
+
+		public WithExitOverlay(Actor self, WithExitOverlayInfo info)
+		{
+			this.self = self;
+			var rs = self.Trait<RenderSprites>();
+			var body = self.Trait<BodyOrientation>();
+
+			buildComplete = !self.Info.HasTraitInfo<BuildingInfo>(); // always render instantly for units
+
+			overlay = new Animation(self.World, rs.GetImage(self));
+			overlay.PlayRepeating(info.Sequence);
+
+			var anim = new AnimationWithOffset(overlay,
+				() => body.LocalToWorld(info.Offset.Rotate(body.QuantizeOrientation(self, self.Orientation))),
+				() => !buildComplete || !IsExitBlocked);
+
+			rs.Add(anim, info.Palette, info.IsPlayerPalette);
+		}
+
+		public void BuildingComplete(Actor self)
+		{
+			buildComplete = true;
+		}
+
+		public void Sold(Actor self) { }
+		public void Selling(Actor self)
+		{
+			buildComplete = false;
+		}
+
+		public void DamageStateChanged(Actor self, AttackInfo e)
+		{
+			overlay.ReplaceAnim(RenderSprites.NormalizeSequence(overlay, e.DamageState, overlay.CurrentSequence.Name));
+		}
+
+		public void UnitProduced(Actor self, Actor other, CPos exit)
+		{
+			this.exit = exit;
+		}
+	}
+}
