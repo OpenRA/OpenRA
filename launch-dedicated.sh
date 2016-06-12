b85f1e95550c8986922267ac8d5b9358b3d829c1
Curtis Shmyr <cjshmyr@gmail.com>
2016-06-12 13:10:14 -0600

    Make launch-dedicated scripts consistent between platforms

--- launch-dedicated.sh
+++ launch-dedicated.sh
@@ -12,9 +12,12 @@ ListenPort="${ListenPort:-"1234"}"
 ExternalPort="${ExternalPort:-"1234"}"
 AdvertiseOnline="${AdvertiseOnline:-"True"}"
 AllowPortForward="${AllowPortForward:-"False"}"
+DisableSinglePlayer="${DisableSinglePlayer:-"True"}"
+Password="${Password:-""}"
 
 while true; do
      mono --debug OpenRA.Server.exe Game.Mod=$Mod \
      Server.Name="$Name" Server.ListenPort=$ListenPort Server.ExternalPort=$ExternalPort \
-     Server.AdvertiseOnline=$AdvertiseOnline Server.AllowPortForward=$AllowPortForward
+     Server.AdvertiseOnline=$AdvertiseOnline Server.AllowPortForward=$AllowPortForward \
+     Server.DisableSinglePlayer=$DisableSinglePlayer Server.Password=$Password
 done
