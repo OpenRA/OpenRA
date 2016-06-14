054b3a43a4610500a58c6c8a06f73d3b40cc40f1
Paul Chote <pchote@users.noreply.github.com>
2016-06-14 01:45:51 +0100

    Add a lightweight MSCab parser.

---
+++ OpenRA.Mods.Common/UtilityCommands/ListMSCabContentsCommand.cs
@@ -0,0 +1,35 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2016 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System;
+using System.IO;
+using OpenRA.Mods.Common.FileFormats;
+
+namespace OpenRA.Mods.Common.UtilityCommands
+{
+	class ListMSCabContentsCommand : IUtilityCommand
+	{
+		public string Name { get { return "--list-mscab"; } }
+
+		public bool ValidateArguments(string[] args)
+		{
+			return args.Length == 2;
+		}
+
+		[Desc("ARCHIVE.CAB", "Lists the filenames contained within a MSCAB file")]
+		public void Run(ModData modData, string[] args)
+		{
+			var package = new MSCabCompression(File.OpenRead(args[1]));
+			foreach (var file in package.Contents)
+				Console.WriteLine("{0}", file);
+		}
+	}
+}
