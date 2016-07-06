2fc77a315eeeefde30cb9b556a0cb458327cc073
Curtis Shmyr <cjshmyr@gmail.com>
2016-07-06 13:49:08 -0600

    Add timestamp to exception.log

--- OpenRA.Game/Support/Program.cs
+++ OpenRA.Game/Support/Program.cs
@@ -62,6 +62,7 @@ namespace OpenRA
 				Log.Write("exception", "on map {0} ({1} by {2}).", map.Uid, map.Title, map.Author);
 			}
 
+			Log.Write("exception", "Date: {0:u}", DateTime.UtcNow);
 			Log.Write("exception", "Operating System: {0} ({1})", Platform.CurrentPlatform, Environment.OSVersion);
 			Log.Write("exception", "Runtime Version: {0}", Platform.RuntimeVersion);
 			var rpt = BuildExceptionReport(e).ToString();
