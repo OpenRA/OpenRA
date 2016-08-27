c2f21a21ef276c02d2c61daed7a452fe1a14fc04
abc013 <abc013@alpenjodel.de>
2016-08-27 16:53:05 +0200

    Replaced the AlliedMissiles boolean on JamsMissiles by a Stances field (DeflectionStances).

--- OpenRA.Mods.Common/Projectiles/Missile.cs
+++ OpenRA.Mods.Common/Projectiles/Missile.cs
@@ -396,7 +396,7 @@ namespace OpenRA.Mods.Common.Projectiles
 			if ((tp.Actor.CenterPosition - pos).HorizontalLengthSquared > tp.Trait.Range.LengthSquared)
 				return false;
 
-			if (tp.Actor.Owner.Stances[args.SourceActor.Owner] == Stance.Ally && !tp.Trait.AlliedMissiles)
+			if (!tp.Actor.Owner.Stances[args.SourceActor.Owner].HasStance(tp.Trait.DeflectionStances))
 				return false;
 
 			return tp.Actor.World.SharedRandom.Next(100 / tp.Trait.Chance) == 0;
