cb553fbe1506fb12c1be4449c834d32fcc659562
Paul Chote <pchote@users.noreply.github.com>
2016-09-21 14:51:53 +0100

    Converting dates to string using the InvariantCulture.

--- OpenRA.Game/Network/SyncReport.cs
+++ OpenRA.Game/Network/SyncReport.cs
@@ -18,6 +18,7 @@ using OpenRA.Primitives;
 
 namespace OpenRA.Network
 {
+	using System.Globalization;
 	using NamesValuesPair = Pair<string[], object[]>;
 
 	class SyncReport
@@ -92,7 +93,7 @@ namespace OpenRA.Network
 
 		internal void DumpSyncReport(int frame, IEnumerable<FrameData.ClientOrder> orders)
 		{
-			var reportName = "syncreport-" + DateTime.UtcNow.ToString("yyyy-MM-ddTHHmmssZ") + ".log";
+			var reportName = "syncreport-" + DateTime.UtcNow.ToString("yyyy-MM-ddTHHmmssZ", CultureInfo.InvariantCulture) + ".log";
 			Log.AddChannel("sync", reportName);
 
 			foreach (var r in syncReports)
