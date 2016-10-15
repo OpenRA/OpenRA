64e0ed514c2410295862a00d83529d115a9acb0a
Zimmermann Gyula <graiondilach@hotmail.com>
2016-10-15 16:46:14 +0200

    Implement a WithVoxelHelicopterBody trait.

---
+++ Traits/Render/WithVoxelHelicopterBody.cs
@@ -0,0 +1,106 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2015- OpenRA.Mods.AS Developers (see AUTHORS)
+ * This file is a part of a third-party plugin for OpenRA, which is
+ * free software. It is made available to you under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation. For more information, see COPYING.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenRA.Graphics;
+using OpenRA.Mods.Common.Graphics;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Mods.Common.Traits.Render;
+using OpenRA.Primitives;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.AS.Traits
+{
+	[Desc("Render an animated voxel based upon the voxel being inair.")]
+	public class WithVoxelHelicopterBodyInfo : UpgradableTraitInfo, IRenderActorPreviewVoxelsInfo,  Requires<RenderVoxelsInfo>
+	{
+		public readonly string Sequence = "idle";
+
+		[Desc("The rate of the voxel animation.")]
+		public readonly int TickRate = 5;
+
+		[Desc("Defines if the Voxel should have a shadow.")]
+		public readonly bool ShowShadow = true;
+
+		public override object Create(ActorInitializer init) { return new WithVoxelHelicopterBody(init.Self, this); }
+
+		public IEnumerable<VoxelAnimation> RenderPreviewVoxels(
+			ActorPreviewInitializer init, RenderVoxelsInfo rv, string image, Func<WRot> orientation, int facings, PaletteReference p)
+		{
+			var voxel = VoxelProvider.GetVoxel(image, Sequence);
+			var body = init.Actor.TraitInfo<BodyOrientationInfo>();
+			var frame = init.Contains<BodyAnimationFrameASInit>() ? init.Get<BodyAnimationFrameASInit, uint>() : 0;
+
+			yield return new VoxelAnimation(voxel, () => WVec.Zero,
+				() => new[] { body.QuantizeOrientation(orientation(), facings) },
+				() => false, () => frame, ShowShadow);
+		}
+	}
+
+	public class WithVoxelHelicopterBody : UpgradableTrait<WithVoxelHelicopterBodyInfo>, IAutoSelectionSize, ITick, IActorPreviewInitModifier
+	{
+		WithVoxelHelicopterBodyInfo info;
+		int2 size;
+		uint tick, frame, frames;
+
+		public WithVoxelHelicopterBody(Actor self, WithVoxelHelicopterBodyInfo info)
+			: base(info)
+		{
+			this.info = info;
+
+			var body = self.Trait<BodyOrientation>();
+			var rv = self.Trait<RenderVoxels>();
+
+			var voxel = VoxelProvider.GetVoxel(rv.Image, info.Sequence);
+			frames = voxel.Frames;
+			rv.Add(new VoxelAnimation(voxel, () => WVec.Zero,
+				() => new[] { body.QuantizeOrientation(self, self.Orientation) },
+				() => IsTraitDisabled, () => frame, info.ShowShadow));
+
+			// Selection size
+			var rvi = self.Info.TraitInfo<RenderVoxelsInfo>();
+			var s = (int)(rvi.Scale * voxel.Size.Aggregate(Math.Max));
+			size = new int2(s, s);
+		}
+
+		public int2 SelectionSize(Actor self) { return size; }
+
+		public void Tick(Actor self)
+		{
+			if (IsTraitDisabled)
+				return;
+
+			if (self.World.Map.DistanceAboveTerrain(self.CenterPosition) > WDist.Zero)
+				tick++;
+
+			if (tick < info.TickRate)
+				return;
+
+			tick = 0;
+			if (++frame == frames)
+				frame = 0;
+		}
+
+		void IActorPreviewInitModifier.ModifyActorPreviewInit(Actor self, TypeDictionary inits)
+		{
+			inits.Add(new BodyAnimationFrameASInit(frame));
+		}
+	}
+
+	public class BodyAnimationFrameASInit : IActorInit<uint>
+	{
+		[FieldFromYamlKey] readonly uint value = 0;
+		public BodyAnimationFrameASInit() { }
+		public BodyAnimationFrameASInit(uint init) { value = init; }
+		public uint Value(World world) { return value; }
+	}
+}
