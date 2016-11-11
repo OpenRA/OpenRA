988778fe1f3b91c4de709d0fb933ec9baf6fa5ad
Paul Chote <pchote@users.noreply.github.com>
2016-11-11 14:39:38 +0000

    Add an introduction dialog for non-{DEV_VERSION} TS builds.

---
+++ OpenRA.Mods.TS/Widgets/Logic/PreReleaseWarningPrompt.cs
@@ -0,0 +1,37 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2016 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using OpenRA.Mods.Common.Widgets;
+using OpenRA.Widgets;
+
+namespace OpenRA.Mods.TS.Widgets.Logic
+{
+	public class PreReleaseWarningPrompt : ChromeLogic
+	{
+		static bool promptAccepted;
+
+		[ObjectCreator.UseCtor]
+		public PreReleaseWarningPrompt(Widget widget, World world, ModData modData)
+		{
+			if (!promptAccepted && modData.Manifest.Metadata.Version != "{DEV_VERSION}")
+				widget.Get<ButtonWidget>("CONTINUE_BUTTON").OnClick = () => ShowMainMenu(world);
+			else
+				ShowMainMenu(world);
+		}
+
+		void ShowMainMenu(World world)
+		{
+			promptAccepted = true;
+			Ui.ResetAll();
+			Game.LoadWidget(world, "MAINMENU", Ui.Root, new WidgetArgs());
+		}
+	}
+}
