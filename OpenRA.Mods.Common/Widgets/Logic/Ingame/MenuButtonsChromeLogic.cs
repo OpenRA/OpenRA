55f76c02d6572a387c84a343f6aa3105ed58aa8a
Paul Chote <pchote@users.noreply.github.com>
2016-12-10 18:41:31 +0000

    Disable world sounds when the ingame menu is open.

--- OpenRA.Mods.Common/Widgets/Logic/Ingame/MenuButtonsChromeLogic.cs
+++ OpenRA.Mods.Common/Widgets/Logic/Ingame/MenuButtonsChromeLogic.cs
@@ -105,11 +105,18 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 			if (button.Pause && world.LobbyInfo.IsSinglePlayer)
 				world.SetPauseState(true);
 
+			var cachedDisableWorldSounds = Game.Sound.DisableWorldSounds;
+			if (button.DisableWorldSounds)
+				Game.Sound.DisableWorldSounds = true;
+
 			widgetArgs = widgetArgs ?? new WidgetArgs();
 			widgetArgs.Add("onExit", () =>
 			{
 				if (button.HideIngameUI)
 					worldRoot.IsVisible = () => true;
+
+				if (button.DisableWorldSounds)
+					Game.Sound.DisableWorldSounds = cachedDisableWorldSounds;
 
 				if (button.Pause && world.LobbyInfo.IsSinglePlayer)
 					world.SetPauseState(cachedPause);
