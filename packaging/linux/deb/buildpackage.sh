2dd6376a39641f663f51315928a4dc3ad9173105
Paul van Tilburg <paul@luon.net>
2016-12-30 15:56:57 +0100

    Leave the openra-server man page out for now; add an lintian override
    See also issue #12525.

--- packaging/linux/deb/buildpackage.sh
+++ packaging/linux/deb/buildpackage.sh
@@ -32,9 +32,8 @@ sed "s|/usr/bin|/usr/games|g" "${DEB_BUILD_ROOT}/usr/games/openra" > temp
 mv -f temp "${DEB_BUILD_ROOT}/usr/games/openra"
 chmod 0755 "${DEB_BUILD_ROOT}/usr/games/openra"*
 
-# Link and compress the man pages
+# Compress the man page
 gzip -9n "${DEB_BUILD_ROOT}/usr/share/man/man6/openra.6"
-ln -s openra.6.gz "${DEB_BUILD_ROOT}/usr/share/man/man6/openra-server.6.gz"
 
 # Put the copyright and changelog in /usr/share/doc/openra/
 mkdir -p "${DEB_BUILD_ROOT}/${DOCDIR}"
