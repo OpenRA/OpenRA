61659d0d3293d15335c3d8f7938250cd13fcdc97
abcdefg30 <alphabet@streber24.de>
2017-04-13 16:27:02 +0200

    Add support for using launch-game.cmd with parameters

--- launch-game.cmd
+++ launch-game.cmd
@@ -1,18 +1,28 @@
 @echo off
 title OpenRA
+for %%x in (%*) do (
+  if "%%~x" EQU "Game.Mod" (goto launch)
+)
+
 :choosemod
 set /P mod=Select mod (ra, cnc, d2k, ts) or --exit: 
 if /I "%mod%" EQU "--exit" (exit)
-if /I "%mod%" EQU "ra" (goto launch)
-if /I "%mod%" EQU "cnc" (goto launch)
-if /I "%mod%" EQU "ts" (goto launch)
-if /I "%mod%" EQU "d2k" (goto launch)
+if /I "%mod%" EQU "ra" (goto launchmod)
+if /I "%mod%" EQU "cnc" (goto launchmod)
+if /I "%mod%" EQU "ts" (goto launchmod)
+if /I "%mod%" EQU "d2k" (goto launchmod)
 echo.
 echo Unknown mod: %mod%
 echo.
 goto choosemod
+
+:launchmod
+OpenRA.Game.exe Game.Mod=%mod% %*
+goto end
 :launch
-OpenRA.Game.exe Game.Mod=%mod%
+OpenRA.Game.exe %*
+
+:end
 if %errorlevel% neq 0 goto crashdialog
 exit
 :crashdialog
