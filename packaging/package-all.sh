696b48b7bf5133d0b5f625559c076ab541d913c0
Paul Chote <pchote@users.noreply.github.com>
2017-04-30 12:24:14 +0000

    Overhaul the packaging scripts:
    - Ingame mod versions now always match the package version
    - Adds macOS compatibility
    - Removes trait and lua api docs pending future overhaul
    - Individual platform packages can be compiled directly
    - Improved error checking
    - Removes unnecessary redundancy and indirection

--- packaging/package-all.sh
+++ packaging/package-all.sh
@@ -6,95 +6,31 @@ if [ $# -ne "2" ]; then
     exit 1
 fi
 
-# Resolve the absolute source path from the location of this script
-SRCDIR=$(readlink -f $(dirname $0)/../)
-BUILTDIR="${SRCDIR}/packaging/built"
-TAG=$1
-OUTPUTDIR=$(readlink -f $2)
-
-# Build the code and push the files into a clean dir
-cd "$SRCDIR"
-mkdir packaging/built
-mkdir packaging/built/mods
-make package
-
-# Remove the mdb files that are created during `make`
-find . -path "*.mdb" -delete
-
-test -e Changelog.md && rm Changelog.md
-curl -s -L -O https://raw.githubusercontent.com/wiki/OpenRA/OpenRA/Changelog.md
-
-markdown Changelog.md > CHANGELOG.html
-markdown README.md > README.html
-markdown CONTRIBUTING.md > CONTRIBUTING.html
-markdown DOCUMENTATION.md > DOCUMENTATION.html
-markdown Lua-API.md > Lua-API.html
-
-# List of files that are packaged on all platforms
-FILES=('OpenRA.Game.exe' 'OpenRA.Game.exe.config' 'OpenRA.Utility.exe' 'OpenRA.Server.exe' 
-'OpenRA.Platforms.Default.dll' \
-'lua' 'glsl' 'mods/common' 'mods/ra' 'mods/cnc' 'mods/d2k' 'mods/modcontent' \
-'AUTHORS' 'COPYING' 'README.html' 'CONTRIBUTING.html' 'DOCUMENTATION.html' 'CHANGELOG.html' \
-'global mix database.dat' 'GeoLite2-Country.mmdb.gz')
-
-echo "Copying files..."
-for i in "${FILES[@]}"; do
-    cp -R "${i}" "packaging/built/${i}" || exit 3
-done
-
-# SharpZipLib for zip file support
-cp thirdparty/download/ICSharpCode.SharpZipLib.dll packaging/built
-
-# FuzzyLogicLibrary for improved AI
-cp thirdparty/download/FuzzyLogicLibrary.dll packaging/built
-
-# SharpFont for FreeType support
-cp thirdparty/download/SharpFont* packaging/built
-
-# SDL2-CS
-cp thirdparty/download/SDL2-CS* packaging/built
-
-# OpenAL-CS
-cp thirdparty/download/OpenAL-CS* packaging/built
-
-# Open.NAT for UPnP support
-cp thirdparty/download/Open.Nat.dll packaging/built
-
-# Eluant (Lua integration)
-cp thirdparty/download/Eluant* packaging/built
-
-# GeoIP database access
-cp thirdparty/download/MaxMind.Db.dll packaging/built
-
-# global chat
-cp thirdparty/download/SmarIrc4net.dll packaging/built
-
-cd packaging
-echo "Creating packages..."
+# Set the working dir to the location of this script
+cd $(dirname $0)
 
 pushd windows >/dev/null
-./buildpackage.sh "$TAG" "$BUILTDIR" "$SRCDIR" "$OUTPUTDIR"
+echo "Building Windows package"
+./buildpackage.sh "$1" "$2"
 if [ $? -ne 0 ]; then
     echo "Windows package build failed."
 fi
 popd >/dev/null
 
 pushd osx >/dev/null
-echo "Building OS X package"
-./buildpackage.sh "$TAG" "$BUILTDIR" "$OUTPUTDIR"
+echo "Building macOS package"
+./buildpackage.sh "$1" "$2"
 if [ $? -ne 0 ]; then
-    echo "OS X package build failed."
+    echo "macOS package build failed."
 fi
 popd >/dev/null
 
 pushd linux >/dev/null
 echo "Building Linux packages"
-./buildpackage.sh "$TAG" "$BUILTDIR" "$OUTPUTDIR"
+./buildpackage.sh "$1" "$2"
 if [ $? -ne 0 ]; then
     echo "Linux package build failed."
 fi
 popd >/dev/null
 
 echo "Package build done."
-
-rm -rf $BUILTDIR
