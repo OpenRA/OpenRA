c5befd536d85fcf57593c5f5c75ba2f2f92bb1f9
forcecore <ssanzing@gmail.com>
2017-05-14 16:20:16 -0500

    Updated diff.sh

--- OpenRA.Mods.yupgi_alert/diff.sh
+++ OpenRA.Mods.yupgi_alert/diff.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-meld ../OpenRA.Mods.Common/Traits/Air/FallsToEarth.cs Traits/Air/BlimpFallsToEarth.cs
-meld ../OpenRA.Mods.Common/Activities/Air/FallToEarth.cs Activities/Air/BlimpFallToEarth.cs
+#meld ../OpenRA.Mods.Common/Traits/DamagedByTerrain.cs Traits/DamagedByRadioactivity.cs
+#meld ../OpenRA.Mods.Common/Traits/Air/BlimpFallsToEarth.cs Traits/Air/BlimpFallsToEarth.cs
+#meld ../OpenRA.Mods.Common/Traits/Air/FallsToEarth.cs Traits/Air/BlimpFallsToEarth.cs
+#meld ../OpenRA.Mods.Common/Activities/Air/FallToEarth.cs Activities/Air/BlimpFallToEarth.cs
+meld ../OpenRA.Mods.Common/Traits/Explodes.cs Traits/SpawnedExplodes.cs
