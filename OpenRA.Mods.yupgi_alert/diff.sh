f15d448c37908b510634511a7506fee154171cc9
forcecore <ssanzing@gmail.com>
2017-05-14 15:08:02 -0500

    Add diff.sh to fetch "almost the same" module's changes to yupgi_alert

---
+++ OpenRA.Mods.yupgi_alert/diff.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+meld ../OpenRA.Mods.Common/Traits/Air/FallsToEarth.cs Traits/Air/BlimpFallsToEarth.cs
+meld ../OpenRA.Mods.Common/Activities/Air/FallToEarth.cs Activities/Air/BlimpFallToEarth.cs
