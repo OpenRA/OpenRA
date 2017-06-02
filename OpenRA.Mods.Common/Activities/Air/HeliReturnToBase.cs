1d1802a163becdead89dc7bb248f451231fb916e
Jean-Rémy Buchs <jrb0001@692b8c32.de>
2017-06-03 00:48:28 +0200

    Add LandOnCondition to the trait Aircraft which triggers a landing and prevents takeoffs while the condition is met

--- OpenRA.Mods.Common/Activities/Air/HeliReturnToBase.cs
+++ OpenRA.Mods.Common/Activities/Air/HeliReturnToBase.cs
@@ -42,6 +42,11 @@ namespace OpenRA.Mods.Common.Activities
 
 		public override Activity Tick(Actor self)
 		{
+			// Refuse to take off if it would land immediately again.
+			// Special case: Don't kill other deploy hotkey activities.
+			if (heli.ForceLanding)
+				return NextActivity;
+
 			if (IsCanceled)
 				return NextActivity;
 
--- OpenRA.Mods.Common/Activities/Air/TakeOff.cs
+++ OpenRA.Mods.Common/Activities/Air/TakeOff.cs
@@ -28,6 +28,13 @@ namespace OpenRA.Mods.Common.Activities
 
 		public override Activity Tick(Actor self)
 		{
+			// Refuse to take off if it would land immediately again.
+			if (aircraft.ForceLanding)
+			{
+				Cancel(self);
+				return NextActivity;
+			}
+
 			aircraft.UnReserve();
 
 			var host = aircraft.GetActorBelow();
