cb41aed7a43a467e40639965db6a7a07b8063f7a
vapre <vapre@hotmail.com>
2017-06-25 13:04:31 +0200

    Use array to lookup trait types in the trait dictionary. To avoid more expensive hash map lookups. In a small test program 100 million hash table look ups with a table size of 512 take ~1.558 seconds where array index lookup takes ~0.125s.

---
+++ OpenRA.Game/TraitTypeIndex.cs
@@ -0,0 +1,72 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2017 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+
+namespace OpenRA
+{
+	
+	public class TraitTypeIndexMap
+	{
+		private static Dictionary<Type, int> map = new Dictionary<Type, int>();
+
+		public static int RegisterType(Type t) {
+			int index;
+			if (map.TryGetValue(t, out index)) {
+				return index;
+			}
+			index = map.Count+1;
+			map.Add(t, index);
+			return index;
+		}
+
+		public static int FindType(Type t) {
+			int index;
+			if (map.TryGetValue(t, out index)) {
+				return index;
+			}
+			return 0;
+		}
+
+		public static int GetCount() {
+			return map.Count;
+		}
+
+		public static Type GetType(int index) {
+			if (index > 0 && index < map.Count) {
+				foreach (var t in map) {
+					if (t.Value == index) {
+						return t.Key;
+					}
+				}
+			}
+			return null;
+		}
+	};
+
+	// Index of type in array
+	public class TraitTypeIndex<T>
+	{
+		private static int TypeIndex = 0;
+		public static int GetTypeIndex() {
+			if (TypeIndex == 0) {
+				TypeIndex = TraitTypeIndexMap.RegisterType(typeof(T));
+			}
+			return TypeIndex;
+		}
+
+		public static bool IsRegistered() {
+			return TypeIndex != 0;
+		}
+	};
+}
+
