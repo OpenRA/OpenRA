e5071488ca7ee539806d61f16d6976d27ab1c588
forcecore <ssanzing@gmail.com>
2017-06-26 20:03:54 -0500

    Packaging script updated to include lua folder.

--- packaging/package-opmod.sh
+++ packaging/package-opmod.sh
@@ -5,6 +5,7 @@ WD=$TRAVIS_BUILD_DIR/packaging
 cp $WD/../OpenRA.Game.exe $WD/OpenRA.Game.exe
 cp $WD/../mods/common/OpenRA.Mods.Common.dll $WD/OpenRA.Mods.Uncommon.dll
 cp $WD/../mods/yupgi_alert/OpenRA.Mods.yupgi_alert.dll $WD
+cp -r $WD/../lua $WD
 
 cd $WD
 zip yupgi-$TRAVIS_COMMIT.zip OpenRA.Game.exe OpenRA.Mods.Uncommon.dll OpenRA.Mods.yupgi_alert.dll
