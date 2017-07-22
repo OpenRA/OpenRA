1adf7eb007e6b20a1501eac0e62650d88d1ace45
forcecore <ssanzing@gmail.com>
2017-07-22 15:46:43 -0500

    No sound bug fix for DeliversCash.cs

--- OpenRA.Mods.Common/Traits/DeliversCash.cs
+++ OpenRA.Mods.Common/Traits/DeliversCash.cs
@@ -15,6 +15,11 @@ using OpenRA.Mods.Common.Activities;
 using OpenRA.Mods.Common.Orders;
 using OpenRA.Traits;
 
+/*
+// Modifications for Mod.yupgi_alert:
+// Add sound effect parameter and INotifyCashTransfer.
+*/
+
 namespace OpenRA.Mods.Common.Traits
 {
 	[Desc("Donate money to actors with the `AcceptsDeliveredCash` trait.")]
@@ -29,12 +34,15 @@ namespace OpenRA.Mods.Common.Traits
 		[Desc("Identifier checked against AcceptsDeliveredCash.ValidTypes. Only needed if the latter is not empty.")]
 		public readonly string Type = null;
 
+		[Desc("Sound to play when delivering cash")]
+		public readonly string[] Sounds = { };
+
 		[VoiceReference] public readonly string Voice = "Action";
 
 		public object Create(ActorInitializer init) { return new DeliversCash(this); }
 	}
 
-	public class DeliversCash : IIssueOrder, IResolveOrder, IOrderVoice
+	public class DeliversCash : IIssueOrder, IResolveOrder, IOrderVoice, INotifyCashTransfer
 	{
 		readonly DeliversCashInfo info;
 
@@ -78,6 +86,12 @@ namespace OpenRA.Mods.Common.Traits
 
 			self.SetTargetLine(target, Color.Yellow);
 			self.QueueActivity(new DonateCash(self, target.Actor, info.Payload, info.PlayerExperience));
+		}
+
+		void INotifyCashTransfer.OnCashTransfer(Actor self, Actor donor)
+		{
+			if (info.Sounds.Length > 0)
+				Game.Sound.Play(SoundType.World, info.Sounds.Random(self.World.SharedRandom), self.CenterPosition);
 		}
 
 		public class DeliversCashOrderTargeter : UnitOrderTargeter
