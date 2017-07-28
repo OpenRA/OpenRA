c7c726da9f3bd211b91d9d171311676f06017692
reaperrr <thereaperrr@aol.com>
2017-07-28 14:18:57 +0200

    Remove redundant Disposed checks
    IsDead returns true if Disposed is true.

--- OpenRA.Mods.Common/AI/States/GroundStates.cs
+++ OpenRA.Mods.Common/AI/States/GroundStates.cs
@@ -95,7 +95,7 @@ namespace OpenRA.Mods.Common.AI
 			else
 			{
 				var enemies = owner.World.FindActorsInCircle(leader.CenterPosition, WDist.FromCells(12))
-					.Where(a1 => !a1.Disposed && !a1.IsDead);
+					.Where(a1 => !a1.IsDead);
 				var enemynearby = enemies.Where(a1 => a1.Info.HasTraitInfo<ITargetableInfo>() && leader.Owner.Stances[a1.Owner] == Stance.Enemy);
 				var target = enemynearby.ClosestTo(leader.CenterPosition);
 				if (target != null)
