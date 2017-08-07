ffef1d3689a47694dad6e781e43a6e2312fa4a9b
forcecore <ssanzing@gmail.com>
2017-08-07 13:12:30 -0500

    Merged upstream AttackFrontal fix by me
    bleed PR drifted away from my mod's fix.

--- OpenRA.Mods.Common/Traits/Attack/AttackFrontal.cs
+++ OpenRA.Mods.Common/Traits/Attack/AttackFrontal.cs
@@ -39,27 +39,19 @@ namespace OpenRA.Mods.Common.Traits
 			this.info = info;
 		}
 
-		public static bool WithinFacingTolerance(int facing, int desiredFacing, int facingTolerance)
-		{
-			if (facingTolerance == 0 && facing == desiredFacing)
-				return true;
-
-			var delta = Util.NormalizeFacing(desiredFacing - facing);
-			if (delta <= facingTolerance || delta >= 256 - facingTolerance)
-				return true;
-
-			return false;
-		}
-
 		protected override bool CanAttack(Actor self, Target target)
 		{
 			if (!base.CanAttack(self, target))
 				return false;
 
-			var delta = target.CenterPosition - self.CenterPosition;
-			var facingToTarget = delta.HorizontalLengthSquared != 0 ? delta.Yaw.Facing : facing.Facing;
+			var pos = self.CenterPosition;
+			var targetedPosition = GetTargetPosition(pos, target);
+			var delta = targetedPosition - pos;
 
-			return WithinFacingTolerance(facing.Facing, facingToTarget, info.FacingTolerance);
+			if (delta.HorizontalLengthSquared == 0)
+				return true;
+
+			return Util.FacingWithinTolerance(facing.Facing, delta.Yaw.Facing, info.FacingTolerance);
 		}
 
 		public override Activity GetAttackActivity(Actor self, Target newTarget, bool allowMove, bool forceAttack)
