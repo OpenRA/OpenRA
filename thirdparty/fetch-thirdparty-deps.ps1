cfddcd8297f8227b282afbaa4e7520cd0b553bba
Paul Chote <pchote@users.noreply.github.com>
2017-08-10 20:12:00 +0100

    Update BeaconLib to 1.0.1.

--- thirdparty/fetch-thirdparty-deps.ps1
+++ thirdparty/fetch-thirdparty-deps.ps1
@@ -161,7 +161,7 @@ if (!(Test-Path "SmarIrc4net.dll"))
 if (!(Test-Path "rix0rrr.BeaconLib.dll"))
 {
 	echo "Fetching rix0rrr.BeaconLib from NuGet."
-	./nuget.exe install rix0rrr.BeaconLib -Version 1.0.0 -ExcludeVersion -Verbosity quiet
+	./nuget.exe install rix0rrr.BeaconLib -Version 1.0.1 -ExcludeVersion -Verbosity quiet
 	cp rix0rrr.BeaconLib/lib/net40/rix0rrr.BeaconLib.dll .
 	rmdir rix0rrr.BeaconLib -Recurse
 }
