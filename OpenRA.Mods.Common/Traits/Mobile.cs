ecb0a0a05d6a900ee27e3b8c4f048d5c8ae183b8
Paul Chote <pchote@users.noreply.github.com>
2017-08-11 21:08:25 +0100

    Revert TicksBeforePathing move from Mobile to Move.
    This partially reverts commit 11c8cda0c38ad0ac71c58557d34480925307cebd.

--- OpenRA.Mods.Common/Traits/Mobile.cs
+++ OpenRA.Mods.Common/Traits/Mobile.cs
@@ -390,6 +390,10 @@ namespace OpenRA.Mods.Common.Traits
 	public class Mobile : ConditionalTrait<MobileInfo>, INotifyCreated, IIssueOrder, IResolveOrder, IOrderVoice, IPositionable, IMove,
 		IFacing, IDeathActorInitModifier, INotifyAddedToWorld, INotifyRemovedFromWorld, INotifyBlockingMove, IActorPreviewInitModifier, INotifyBecomingIdle
 	{
+		const int AverageTicksBeforePathing = 5;
+		const int SpreadTicksBeforePathing = 5;
+		internal int TicksBeforePathing = 0;
+
 		readonly Actor self;
 		readonly Lazy<IEnumerable<int>> speedModifiers;
 		public bool IsMoving { get; set; }
@@ -627,6 +631,8 @@ namespace OpenRA.Mods.Common.Traits
 
 				if (!order.Queued)
 					self.CancelActivity();
+
+				TicksBeforePathing = AverageTicksBeforePathing + self.World.SharedRandom.Next(-SpreadTicksBeforePathing, SpreadTicksBeforePathing);
 
 				self.SetTargetLine(Target.FromCell(self.World, order.TargetLocation), Color.Green);
 				self.QueueActivity(order.Queued, new Move(self, order.TargetLocation, WDist.FromCells(8), null, true));
