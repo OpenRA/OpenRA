6553ba5990d7b1fe189a6e3cdccc2fb93d6ef93f
forcecore <ssanzing@gmail.com>
2017-08-17 23:48:44 -0500

    Landing midair code changed.

--- OpenRA.Mods.Common/Activities/Air/Land.cs
+++ OpenRA.Mods.Common/Activities/Air/Land.cs
@@ -44,7 +44,7 @@ namespace OpenRA.Mods.Common.Activities
 				return NextActivity;
 			}
 
-			Fly.FlyToward(self, plane, d.Yaw.Facing, plane.Info.LandAltitude);
+			Fly.FlyToward(self, plane, d.Yaw.Facing, new WDist(target.CenterPosition.Z));
 
 			return this;
 		}
