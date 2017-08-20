420b596a3a1492281a8d83c4af93778463a03cdc
Matthias Mailänder <matthias@mailaender.name>
2017-08-20 14:45:56 +0200

    Wiki publish the auto-generated settings documentation.

--- packaging/update-wiki.sh
+++ packaging/update-wiki.sh
@@ -29,12 +29,14 @@ git clone git@github.com:OpenRA/OpenRA.wiki.git $HOME/openra-wiki
 
 mono --debug ../OpenRA.Utility.exe all --docs > "${HOME}/openra-wiki/Traits${TAG}.md"
 mono --debug ../OpenRA.Utility.exe all --lua-docs > "${HOME}/openra-wiki/Lua API${TAG}.md"
+mono --debug ../OpenRA.Utility.exe all --settings-docs > "${HOME}/openra-wiki/Settings${TAG}.md"
 
 pushd $HOME/openra-wiki
 git config --local user.email "orabot@users.noreply.github.com"
 git config --local user.name "orabot"
 git add "Traits${TAG}.md"
 git add "Lua API${TAG}.md"
+git add "Settings${TAG}.md"
 git commit -m "Update trait and scripting documentation for branch $1" &&
 git push origin master
 popd
