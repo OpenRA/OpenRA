dbd48831b7843cc99ae32c801996ec489be9fb86
Matthias Mailänder <matthias@mailaender.name>
2017-08-29 11:30:23 +0200

    Fix indentions of weapon documentation table of contents.

--- OpenRA.Mods.Common/UtilityCommands/ExtractWeaponDocsCommand.cs
+++ OpenRA.Mods.Common/UtilityCommands/ExtractWeaponDocsCommand.cs
@@ -66,7 +66,7 @@ namespace OpenRA.Mods.Common.UtilityCommands
 				}
 
 				var traitName = t.Name.EndsWith("Info") ? t.Name.Substring(0, t.Name.Length - 4) : t.Name;
-				toc.AppendLine(" * [{0}](#{1})".F(traitName, traitName.ToLowerInvariant()));
+				toc.AppendLine("  * [{0}](#{1})".F(traitName, traitName.ToLowerInvariant()));
 				doc.AppendLine();
 				doc.AppendLine("### {0}".F(traitName));
 
