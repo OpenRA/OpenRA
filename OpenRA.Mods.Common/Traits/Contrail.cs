024887b268d6bc3ba1020917330274be5f3c18a5
reaperrr <thereaperrr@aol.com>
2017-09-04 19:38:29 +0200

    Move Contrail to Traits
    And make interface implementations explicit while we're at it.

--- OpenRA.Mods.Common/Traits/Contrail.cs
+++ OpenRA.Mods.Common/Traits/Contrail.cs
@@ -13,10 +13,9 @@ using System.Collections.Generic;
 using System.Drawing;
 using OpenRA.Graphics;
 using OpenRA.Mods.Common.Graphics;
-using OpenRA.Mods.Common.Traits;
 using OpenRA.Traits;
 
-namespace OpenRA.Mods.Common.Effects
+namespace OpenRA.Mods.Common.Traits
 {
 	[Desc("Draw a colored contrail behind this actor when they move.")]
 	class ContrailInfo : ITraitInfo, Requires<BodyOrientationInfo>
@@ -61,18 +60,18 @@ namespace OpenRA.Mods.Common.Effects
 			body = self.Trait<BodyOrientation>();
 		}
 
-		public void Tick(Actor self)
+		void ITick.Tick(Actor self)
 		{
 			var local = info.Offset.Rotate(body.QuantizeOrientation(self, self.Orientation));
 			trail.Update(self.CenterPosition + body.LocalToWorld(local));
 		}
 
-		public IEnumerable<IRenderable> Render(Actor self, WorldRenderer wr)
+		IEnumerable<IRenderable> IRender.Render(Actor self, WorldRenderer wr)
 		{
 			return new IRenderable[] { trail };
 		}
 
-		public void AddedToWorld(Actor self)
+		void INotifyAddedToWorld.AddedToWorld(Actor self)
 		{
 			trail = new ContrailRenderable(self.World, color, info.TrailWidth, info.TrailLength, 0, info.ZOffset);
 		}
