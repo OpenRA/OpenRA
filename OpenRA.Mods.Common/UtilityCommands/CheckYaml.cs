e54cf9acc3afa6e3be2ac274658750b07b3847ea
Paul Chote <pchote@users.noreply.github.com>
2017-09-07 19:47:18 +0000

    Move linter interfaces to Mods.Common.

--- OpenRA.Mods.Common/UtilityCommands/CheckYaml.cs
+++ OpenRA.Mods.Common/UtilityCommands/CheckYaml.cs
@@ -13,7 +13,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using OpenRA.FileSystem;
-using OpenRA.Traits;
+using OpenRA.Mods.Common.Lint;
 
 namespace OpenRA.Mods.Common.UtilityCommands
 {
