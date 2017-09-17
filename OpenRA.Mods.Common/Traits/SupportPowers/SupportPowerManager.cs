48a018d994ea8a2752cc98a89203782773a778e4
Paul Chote <pchote@users.noreply.github.com>
2017-09-17 13:07:53 +0100

    Fix cheats reenabling OneShot support powers.

--- OpenRA.Mods.Common/Traits/SupportPowers/SupportPowerManager.cs
+++ OpenRA.Mods.Common/Traits/SupportPowers/SupportPowerManager.cs
@@ -160,13 +160,14 @@ namespace OpenRA.Mods.Common.Traits
 		public int RemainingTime;
 		public int TotalTime;
 		public bool Active { get; private set; }
-		public bool Disabled { get { return (!prereqsAvailable && !manager.DevMode.AllTech) || !instancesEnabled; } }
+		public bool Disabled { get { return (!prereqsAvailable && !manager.DevMode.AllTech) || !instancesEnabled || oneShotFired; } }
 
 		public SupportPowerInfo Info { get { return Instances.Select(i => i.Info).FirstOrDefault(); } }
 		public bool Ready { get { return Active && RemainingTime == 0; } }
 
 		bool instancesEnabled;
 		bool prereqsAvailable = true;
+		bool oneShotFired;
 
 		public SupportPowerInstance(string key, SupportPowerManager manager)
 		{
@@ -249,7 +250,10 @@ namespace OpenRA.Mods.Common.Traits
 			notifiedCharging = notifiedReady = false;
 
 			if (Info.OneShot)
+			{
 				PrerequisitesAvailable(false);
+				oneShotFired = true;
+			}
 		}
 	}
 
