fe8341a3aaa6e3700ea13928c64463079d8ca441
Paul Chote <pchote@users.noreply.github.com>
2017-10-12 20:05:00 +0100

    Add missing StringComparison to IngameChatLogic.

--- OpenRA.Mods.Common/Widgets/Logic/Ingame/IngameChatLogic.cs
+++ OpenRA.Mods.Common/Widgets/Logic/Ingame/IngameChatLogic.cs
@@ -9,6 +9,7 @@
  */
 #endregion
 
+using System;
 using System.Drawing;
 using System.Linq;
 using OpenRA.Mods.Common.Commands;
@@ -73,7 +74,7 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 				var team = teamChat && !disableTeamChat;
 				if (chatText.Text != "")
 				{
-					if (!chatText.Text.StartsWith("/"))
+					if (!chatText.Text.StartsWith("/", StringComparison.Ordinal))
 						orderManager.IssueOrder(Order.Chat(team, chatText.Text.Trim()));
 					else if (chatTraits != null)
 					{
