0e3de710367492171762aaecb228cd5a9e934e43
Paul Chote <pchote@users.noreply.github.com>
2017-10-12 20:05:13 +0100

    Add line limit to global chat.

--- OpenRA.Mods.Common/Widgets/Logic/GlobalChatLogic.cs
+++ OpenRA.Mods.Common/Widgets/Logic/GlobalChatLogic.cs
@@ -42,6 +42,9 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 			inputBox.IsDisabled = () => Game.GlobalChat.ConnectionStatus != ChatConnectionStatus.Joined;
 			inputBox.OnEnterKey = EnterPressed;
 
+			// IRC protocol limits messages to 510 characters + CRLF
+			inputBox.MaxLength = 510;
+
 			var nickName = Game.GlobalChat.SanitizedName(Game.Settings.Player.Name);
 			var nicknameBox = widget.Get<TextFieldWidget>("NICKNAME_TEXTFIELD");
 			nicknameBox.Text = nickName;
