24850294528918bede53df2fba71d885fb1b38f7
reaperrr <thereaperrr@aol.com>
2017-11-14 15:41:24 +0100

    Add Rearmable trait

---
+++ OpenRA.Mods.Common/Traits/Rearmable.cs
@@ -0,0 +1,46 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2018 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System.Collections.Generic;
+using System.Linq;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Common.Traits
+{
+	public class RearmableInfo : ITraitInfo
+	{
+		[Desc("Actors that this actor can dock to and get rearmed by.")]
+		[FieldLoader.Require]
+		[ActorReference] public readonly HashSet<string> RearmActors = new HashSet<string> { };
+
+		[Desc("Name(s) of AmmoPool(s) that use this trait to rearm.")]
+		public readonly HashSet<string> AmmoPools = new HashSet<string> { "primary" };
+
+		public object Create(ActorInitializer init) { return new Rearmable(this); }
+	}
+
+	public class Rearmable : INotifyCreated
+	{
+		public readonly RearmableInfo Info;
+
+		public Rearmable(RearmableInfo info)
+		{
+			Info = info;
+		}
+
+		public AmmoPool[] RearmableAmmoPools { get; private set; }
+
+		void INotifyCreated.Created(Actor self)
+		{
+			RearmableAmmoPools = self.TraitsImplementing<AmmoPool>().Where(p => Info.AmmoPools.Contains(p.Info.Name)).ToArray();
+		}
+	}
+}
