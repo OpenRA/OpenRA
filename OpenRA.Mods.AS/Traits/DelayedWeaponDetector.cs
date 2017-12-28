8caf6e128da9ad51e42fa6981019b58c8ebd90e8
Zimmermann Gyula <graiondilach@hotmail.com>
2017-12-28 19:28:39 +0100

    Headers + authors inclusion.

--- OpenRA.Mods.AS/Traits/DelayedWeaponDetector.cs
+++ OpenRA.Mods.AS/Traits/DelayedWeaponDetector.cs
@@ -1,4 +1,14 @@
-﻿using System.Collections.Generic;
+﻿#region Copyright & License Information
+/*
+ * Copyright 2015- OpenRA.Mods.AS Developers (see AUTHORS)
+ * This file is a part of a third-party plugin for OpenRA, which is
+ * free software. It is made available to you under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation. For more information, see COPYING.
+ */
+#endregion
+
+using System.Collections.Generic;
 using System.Linq;
 using OpenRA.Mods.Common.Traits;
 using OpenRA.Traits;
