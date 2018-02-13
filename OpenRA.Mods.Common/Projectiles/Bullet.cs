733055007a4d24d44643cc9388b9657db8799934
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2018-02-13 09:55:58 +0300

    Add Player Palette support for Bullet, Missile and GravityBomb.

--- OpenRA.Mods.Common/Projectiles/Bullet.cs
+++ OpenRA.Mods.Common/Projectiles/Bullet.cs
@@ -40,6 +40,9 @@ namespace OpenRA.Mods.Common.Projectiles
 		[Desc("The palette used to draw this projectile.")]
 		[PaletteReference] public readonly string Palette = "effect";
 
+		[Desc("Palette is a player palette BaseName")]
+		public readonly bool IsPlayerPalette = false;
+
 		[Desc("Does this projectile have a shadow?")]
 		public readonly bool Shadow = false;
 
@@ -277,7 +280,7 @@ namespace OpenRA.Mods.Common.Projectiles
 						yield return r;
 				}
 
-				var palette = wr.Palette(info.Palette);
+				var palette = wr.Palette(info.Palette + (info.IsPlayerPalette ? args.SourceActor.Owner.InternalName : ""));
 				foreach (var r in anim.Render(pos, palette))
 					yield return r;
 			}
