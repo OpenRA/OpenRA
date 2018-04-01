c17e3aff8a731ee55846971d4583c7262c0015a3
forcecore <ssanzing@gmail.com>
2018-04-01 18:17:07 +0200

    Implement mindcontrol functionality.

---
+++ OpenRA.Mods.AS/Traits/MindControllable.cs
@@ -0,0 +1,139 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2015- OpenRA.Mods.AS Developers (see AUTHORS)
+ * This file is a part of a third-party plugin for OpenRA, which is
+ * free software. It is made available to you under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation. For more information, see COPYING.
+ */
+#endregion
+
+using System.Collections.Generic;
+using System.Linq;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.AS.Traits
+{
+	[Desc("This actor can be mind controlled by other actors.")]
+	public class MindControllableInfo : PausableConditionalTraitInfo
+	{
+		[Desc("Condition to grant when under mindcontrol.")]
+		[GrantedConditionReference]
+		public readonly string Condition = null;
+
+		[Desc("The sound played when the mindcontrol is revoked.")]
+		public readonly string[] RevokeControlSounds = { };
+
+		public override object Create(ActorInitializer init) { return new MindControllable(init.Self, this); }
+	}
+
+	public class MindControllable : PausableConditionalTrait<MindControllableInfo>, INotifyKilled, INotifyActorDisposing, INotifyCreated, INotifyOwnerChanged
+	{
+		readonly MindControllableInfo info;
+
+		Actor master;
+		Player creatorOwner;
+		bool controlChanging;
+
+		ConditionManager conditionManager;
+		int token = ConditionManager.InvalidConditionToken;
+
+		public Actor Master { get { return master; } }
+
+		public MindControllable(Actor self, MindControllableInfo info)
+			: base(info)
+		{
+			this.info = info;
+		}
+
+		protected override void Created(Actor self)
+		{
+			conditionManager = self.TraitOrDefault<ConditionManager>();
+		}
+
+		public void LinkMaster(Actor self, Actor master)
+		{
+			self.CancelActivity();
+
+			if (this.master == null)
+				creatorOwner = self.Owner;
+
+			controlChanging = true;
+
+			var oldOwner = self.Owner;
+			self.ChangeOwner(master.Owner);
+
+			UnlinkMaster(self, this.master);
+			this.master = master;
+
+			if (conditionManager != null && token == ConditionManager.InvalidConditionToken && !string.IsNullOrEmpty(Info.Condition))
+				token = conditionManager.GrantCondition(self, Info.Condition);
+
+			if (master.Owner == creatorOwner)
+				UnlinkMaster(self, master);
+
+			self.World.AddFrameEndTask(_ => controlChanging = false);
+		}
+
+		public void UnlinkMaster(Actor self, Actor master)
+		{
+			if (master == null)
+				return;
+
+			self.World.AddFrameEndTask(_ =>
+				{
+					if (master.IsDead || master.Disposed)
+						return;
+
+					master.Trait<MindController>().UnlinkSlave(master, self);
+				});
+
+			this.master = null;
+
+			if (conditionManager != null && token != ConditionManager.InvalidConditionToken)
+				token = conditionManager.RevokeCondition(self, token);
+		}
+
+		public void RevokeMindControl(Actor self)
+		{
+			self.CancelActivity();
+
+			controlChanging = true;
+
+			if (creatorOwner.WinState == WinState.Lost)
+				self.ChangeOwner(self.World.WorldActor.Owner);
+			else
+				self.ChangeOwner(creatorOwner);
+
+			UnlinkMaster(self, master);
+
+			if (info.RevokeControlSounds.Any())
+				Game.Sound.Play(SoundType.World, info.RevokeControlSounds.Random(self.World.SharedRandom), self.CenterPosition);
+
+			self.World.AddFrameEndTask(_ => controlChanging = false);
+		}
+
+		void INotifyKilled.Killed(Actor self, AttackInfo e)
+		{
+			UnlinkMaster(self, master);
+		}
+
+		void INotifyActorDisposing.Disposing(Actor self)
+		{
+			UnlinkMaster(self, master);
+		}
+
+		void INotifyOwnerChanged.OnOwnerChanged(Actor self, Player oldOwner, Player newOwner)
+		{
+			if (!controlChanging)
+				UnlinkMaster(self, master);
+		}
+
+		protected override void TraitDisabled(Actor self)
+		{
+			if (master != null)
+				RevokeMindControl(self);
+		}
+	}
+}
---
+++ OpenRA.Mods.AS/Traits/Render/WithMindControlArc.cs
@@ -0,0 +1,91 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2015- OpenRA.Mods.AS Developers (see AUTHORS)
+ * This file is a part of a third-party plugin for OpenRA, which is
+ * free software. It is made available to you under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation. For more information, see COPYING.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using OpenRA.Graphics;
+using OpenRA.Mods.AS.Graphics;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.AS.Traits
+{
+	public class WithMindControlArcInfo : ITraitInfo
+	{
+		[Desc("Color of the arc")]
+		public readonly Color Color = Color.Red;
+
+		public readonly bool UsePlayerColor = false;
+
+		public readonly int Transparency = 255;
+
+		[Desc("Drawing from self.CenterPosition draws the curve from the foot. Add this much for better looks.")]
+		public readonly WVec Offset = new WVec(0, 0, 0);
+
+		[Desc("The angle of the arc of the beam.")]
+		public readonly WAngle Angle = new WAngle(64);
+
+		[Desc("Controls how fine-grained the resulting arc should be.")]
+		public readonly int QuantizedSegments = 16;
+
+		[Desc("Equivalent to sequence ZOffset. Controls Z sorting.")]
+		public readonly int ZOffset = 0;
+
+		[Desc("The width of the zap.")]
+		public readonly WDist Width = new WDist(43);
+
+		public virtual object Create(ActorInitializer init) { return new WithMindControlArc(init.Self, this); }
+	}
+
+	public class WithMindControlArc : IRenderAboveShroudWhenSelected, INotifySelected, INotifyCreated
+	{
+		readonly WithMindControlArcInfo info;
+		MindController mindController;
+		MindControllable mindControllable;
+
+		public WithMindControlArc(Actor self, WithMindControlArcInfo info)
+		{
+			this.info = info;
+		}
+
+		public void Created(Actor self)
+		{
+			mindController = self.TraitOrDefault<MindController>();
+			mindControllable = self.TraitOrDefault<MindControllable>();
+		}
+
+		void INotifySelected.Selected(Actor a) { }
+
+		IEnumerable<IRenderable> IRenderAboveShroudWhenSelected.RenderAboveShroud(Actor self, WorldRenderer wr)
+		{
+			var color = Color.FromArgb(info.Transparency, info.UsePlayerColor ? self.Owner.Color.RGB : info.Color);
+
+			if (mindController != null)
+			{
+				foreach (var s in mindController.Slaves)
+					yield return new ArcRenderable(
+						self.CenterPosition + info.Offset,
+						s.CenterPosition + info.Offset,
+						info.ZOffset, info.Angle, color, info.Width, info.QuantizedSegments);
+				yield break;
+			}
+
+			if (mindControllable == null || mindControllable.Master == null || !mindControllable.Master.IsInWorld)
+				yield break;
+
+			yield return new ArcRenderable(
+				mindControllable.Master.CenterPosition + info.Offset,
+				self.CenterPosition + info.Offset,
+				info.ZOffset, info.Angle, color, info.Width, info.QuantizedSegments);
+		}
+
+		bool IRenderAboveShroudWhenSelected.SpatiallyPartitionable { get { return false; } }
+	}
+}
