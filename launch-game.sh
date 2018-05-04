9804e7fc6918c17b2a06ddf75a6c5ff3416186c4
Paul Chote <pchote@users.noreply.github.com>
2018-05-04 23:53:24 +0100

    Add kdialog support for KDE.

--- launch-game.sh
+++ launch-game.sh
@@ -28,6 +28,8 @@ if [ $? != 0 ] && [ $? != 1 ]; then
 	ERROR_MESSAGE="OpenRA has encountered a fatal error.\nPlease refer to the crash logs and FAQ for more information.\n\nLog files are located in ~/.openra/Logs\nThe FAQ is available at http://wiki.openra.net/FAQ"
 	if command -v zenity > /dev/null; then
 		zenity --no-wrap --error --title "{MODNAME}" --text "${ERROR_MESSAGE}" 2> /dev/null
+	elif command -v kdialog > /dev/null; then
+		kdialog --title "{MODNAME}" --error "${ERROR_MESSAGE}"
 	else
 		printf "${ERROR_MESSAGE}\n"
 	fi
