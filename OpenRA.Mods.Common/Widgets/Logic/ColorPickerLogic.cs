a7a33ce3eaccf025f2e0ecfd2596f8f83bf98cb3
Paul Chote <pchote@users.noreply.github.com>
2018-06-01 18:17:52 +0100

    Remove unused usings and add end of file newline.

--- OpenRA.Mods.Common/Widgets/Logic/ColorPickerLogic.cs
+++ OpenRA.Mods.Common/Widgets/Logic/ColorPickerLogic.cs
@@ -11,10 +11,8 @@
 
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using OpenRA.Graphics;
-using OpenRA.Mods.Common.Lint;
 using OpenRA.Mods.Common.Traits;
 using OpenRA.Primitives;
 using OpenRA.Widgets;
