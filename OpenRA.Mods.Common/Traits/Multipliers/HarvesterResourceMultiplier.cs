8ce4e9746efed58a8f7da177fd47590421c135d2
Zimmermann Gyula <graiondilach@hotmail.com>
2018-06-20 11:36:06 +0200

    Implement HarvesterResourceMultiplier.

---
+++ OpenRA.Mods.Common/Traits/Multipliers/HarvesterResourceMultiplier.cs
@@ -0,0 +1,33 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2007-2019 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Common.Traits
+{
+	[Desc("Modifies the bale values of this harvester.")]
+	public class HarvesterResourceMultiplierInfo : ConditionalTraitInfo
+	{
+		[FieldLoader.Require]
+		[Desc("Percentage modifier to apply.")]
+		public readonly int Modifier = 100;
+
+		public override object Create(ActorInitializer init) { return new HarvesterResourceMultiplier(this); }
+	}
+
+	public class HarvesterResourceMultiplier : ConditionalTrait<HarvesterResourceMultiplierInfo>, Requires<HarvesterInfo>
+	{
+		public HarvesterResourceMultiplier(HarvesterResourceMultiplierInfo info)
+			: base(info) { }
+
+		public int GetModifier() { return IsTraitDisabled ? 100 : Info.Modifier; }
+	}
+}
