74b55c858f3832662c28c87f8358a0b95c1b527c
Dylan Manitta <dylan@plugnpay.com>
2018-07-25 10:19:56 -0400

    Refactored Tick function, made tab indentation uniform throught lua file

--- mods/ra/maps/ant-01/ant-01.lua
+++ mods/ra/maps/ant-01/ant-01.lua
@@ -27,35 +27,31 @@ AtEndGame = false
 TimerTicks = DateTime.Minutes(30)
 ticks = TimerTicks
 
---[[ 
-	We set up some basics, then we can define our specialized functions.
-    Finally, we'll call our WorldLoaded function and our Higher level functions 
-]]
-
 WorldLoaded = function()
-        allies = Player.GetPlayer("Spain")
-        ussr = Player.GetPlayer("USSR")
-        creeps = Player.GetPlayer("Creeps")
-        InitObjectives()
+	allies = Player.GetPlayer("Spain")
+	ussr = Player.GetPlayer("USSR")
+	creeps = Player.GetPlayer("Creeps")
+	InitObjectives()
 end
-
 
 InitObjectives = function() 
 	Trigger.OnObjectiveAdded(allies, function(p, id)
 		Media.DisplayMessage(p.GetObjectiveDescription(id), "New " .. string.lower(p.GetObjectiveType(id)) .. " objective")
 	end)
-  DiscoverObjective = allies.AddPrimaryObjective("Find and repair Allied Outpost")
 
-  Utils.Do(AlliedBase, function(actor)
-    Trigger.OnEnteredProximityTrigger(actor.CenterPosition, WDist.FromCells(8), function(discoverer, id)
-      DiscoveredAlliedBase(actor, discoverer)
-    end)
-  end)
-  Trigger.AfterDelay(DateTime.Seconds(1), function()
-    creeps.GetActorsByType("harv")[1].Stop()
-  end)
+	DiscoverObjective = allies.AddPrimaryObjective("Find Allied Outpost")
+
+	Utils.Do(AlliedBase, function(actor)
+		Trigger.OnEnteredProximityTrigger(actor.CenterPosition, WDist.FromCells(8), function(discoverer, id)
+			DiscoveredAlliedBase(actor, discoverer)
+		end)
+	end)
+	Trigger.AfterDelay(DateTime.Seconds(1), function()
+		creeps.GetActorsByType("harv")[1].Stop()
+	end)
+
 	Trigger.OnObjectiveCompleted(allies, function(p, id)
-    Media.DisplayMessage(p.GetObjectiveDescription(id), "Objective completed")
+		Media.DisplayMessage(p.GetObjectiveDescription(id), "Objective completed")
 	end)
 	Trigger.OnObjectiveFailed(allies, function(p, id)
 		Media.DisplayMessage(p.GetObjectiveDescription(id), "Objective failed")
@@ -68,7 +64,7 @@ InitObjectives = function()
 		Trigger.AfterDelay(DateTime.Seconds(1), function() Media.PlaySpeechNotification(allies, "MissionAccomplished")  end)
 	end)
   
-  Camera.Position = Actor143.CenterPosition
+	Camera.Position = Actor143.CenterPosition
 end
 
 SendAnts = function(direction, amount)
@@ -80,45 +76,46 @@ SendFireAnts = function(direction, amount)
 end
 
 AntSendFunc = function(direction, amount, antType)
-  local index = 0
-  local path = AntPathN
-  local AntActors = ants
-  if antType == "fireant" then
-	AntActors = fireAnts
-  end
-  
-  if direction == "east" then
-	path = AntPathE
-  elseif direction == "west" then
-	path = AntPathW
-  elseif direction == "south" then
-	path = AntPathS
-  else
-	path = AntPathN
-  end
-  
-  while index < amount do
-	Reinforcements.Reinforce(ussr,AntActors,path,DateTime.Seconds(2))
-	index = index + 1
-  end
+	local index = 0
+	local path = AntPathN
+	local AntActors = ants
 
-  
-  Trigger.AfterDelay(DateTime.Seconds(4), function()
-      for i,actor in pairs(ussr.GetActorsByType(antType)) do
-        actor.AttackMove(CPos.New(65,65))
-        actor.Hunt()
-      end
-  end)
+	if antType == "fireant" then
+		AntActors = fireAnts
+	end
+	
+	if direction == "east" then
+		path = AntPathE
+	elseif direction == "west" then
+		path = AntPathW
+	elseif direction == "south" then
+		path = AntPathS
+	else
+		path = AntPathN
+	end
+	
+	while index < amount do
+		Reinforcements.Reinforce(ussr,AntActors,path,DateTime.Seconds(2))
+		index = index + 1
+	end
+
+	
+	Trigger.AfterDelay(DateTime.Seconds(4), function()
+		for i,actor in pairs(ussr.GetActorsByType(antType)) do
+	  		actor.AttackMove(CPos.New(65,65))
+	  		actor.Hunt()
+		end
+	end)
 end
 
 SendTanks = function() 
-  Media.PlaySpeechNotification(allies, "ReinforcementsArrived")
-  Reinforcements.Reinforce(allies, AlliedForces, TankPath, DateTime.Seconds(1))
+	Media.PlaySpeechNotification(allies, "ReinforcementsArrived")
+	Reinforcements.Reinforce(allies, AlliedForces, TankPath, DateTime.Seconds(1))
 end
 
 SendInsertionHelicopter = function()
-        Media.PlaySpeechNotification(allies, "AlliedReinforcementsSouth")
-        Reinforcements.ReinforceWithTransport(allies, InsertionHelicopterType, ChooperTeam, InsertionPath, { waypoint4.Location })
+	Media.PlaySpeechNotification(allies, "AlliedReinforcementsSouth")
+	Reinforcements.ReinforceWithTransport(allies, InsertionHelicopterType, ChooperTeam, InsertionPath, { waypoint4.Location })
 end
 
 FinishTimer = function()
@@ -134,35 +131,30 @@ FinishTimer = function()
 end
 
 TimerExpired = function()
-    if not (CheckBase()) then
-        allies.MarkCompletedObjective(SurviveObjective)
-    else 
-        allies.MarkFailedObjective(SurviveObjective)
-    end
-    expireSeconds = 0
+	if not (CheckBase()) then
+		allies.MarkCompletedObjective(SurviveObjective)
+	else 
+		allies.MarkFailedObjective(SurviveObjective)
+	end
+	expireSeconds = 0
 end
 
 DiscoveredAlliedBase = function(actor, discoverer)
-  if (not baseDiscovered and discoverer.Owner == allies) then
-    baseDiscovered = true  
-    Media.PlaySpeechNotification(allies,"ObjectiveReached")
-    Utils.Do(AlliedBase, function(building)
-      building.Owner = allies
-    end)
-    UserInterface.SetMissionText("") 
-    Media.PlaySoundNotification(allies,"ChatLine")
-      
-    Trigger.AfterDelay(DateTime.Seconds(4), function()  
-      SurviveObjective = allies.AddPrimaryObjective("Defend outpost until reinforcements arrive")
-      Media.PlaySpeechNotification(allies, "TimerStarted")
-      Trigger.AfterDelay(DateTime.Seconds(1), function() allies.MarkCompletedObjective(DiscoverObjective) end)
-    end)
+	if (not baseDiscovered and discoverer.Owner == allies) then
+		baseDiscovered = true  
     
-    creeps.GetActorsByType("harv")[1].FindResources()
-    creeps.GetActorsByType("harv")[1].Owner = allies
-  else
-    return
-  end
+		Media.PlaySpeechNotification(allies,"ObjectiveReached")
+		Utils.Do(AlliedBase, function(building)
+			building.Owner = allies
+		end)
+		SurviveObjective = allies.AddPrimaryObjective("Defend outpost until reinforcements arrive")
+		Media.PlaySpeechNotification(allies, "TimerStarted")
+		Trigger.AfterDelay(DateTime.Seconds(1), function() allies.MarkCompletedObjective(DiscoverObjective) end)
+		creeps.GetActorsByType("harv")[1].FindResources()
+		creeps.GetActorsByType("harv")[1].Owner = allies
+	else
+		return
+	end
 end
 
 CheckBase = function()
@@ -174,112 +166,147 @@ CheckBase = function()
 	return validBuildings == 0 
 end
 
---[[ I am not proud of this function ]]
-Tick = function() 
-  if SurviveObjective ~= nil then
-	if ticks % DateTime.Seconds(1) == 0 then 
-		if CheckBase() then 
-			allies.MarkFailedObjective(SurviveObjective)
-		end  
-	end
-    if ticks > 0 then
-	  if DateTime.Minutes(29) == ticks then
-		SendAnts("north",1)
-	  elseif DateTime.Minutes(28) == ticks then 
+ThirtyMinuteTriggerInit = function()
+	Trigger.AfterDelay(DateTime.Minutes(1),function()
+		 SendAnts("north",1)
+ 	end)
+
+        Trigger.AfterDelay(DateTime.Minutes(2),function() 
 		SendAnts("east",1)
 		SendAnts("west",1)
-      elseif DateTime.Minutes(27) == ticks then 
-        SendAnts("west",1)
-        SendAnts("north",2)
-      elseif DateTime.Minutes(26) == ticks then
-        SendAnts("south",2)
-		SendAnts("west",2)
-        Trigger.AfterDelay(DateTime.Seconds(1), function() SendInsertionHelicopter() end)
+        end)
 
-      elseif DateTime.Minutes(25) == ticks then
-        SendFireAnts("north",1)
+        Trigger.AfterDelay(DateTime.Minutes(3),function() 
+		 SendAnts("west",1)
+		 SendAnts("north",2)
+        end)
+
+        Trigger.AfterDelay(DateTime.Minutes(4),function() 
+		SendAnts("south",2)
+		SendAnts("west",2)
+		SendInsertionHelicopter() 
+        end)
+
+        Trigger.AfterDelay(DateTime.Minutes(5),function() 
+		SendFireAnts("north",1)
 		SendAnts("west",1)
-      elseif DateTime.Minutes(23) == ticks then
+        end)
+
+        Trigger.AfterDelay(DateTime.Minutes(7),function() 
 		SendAnts("east",2)
 		Trigger.AfterDelay(DateTime.Seconds(30),function() SendAnts("west",3) end)
-      elseif DateTime.Minutes(20) == ticks then
-        SendAnts("west",3)
-		SendAnts("east",3)
-		SendAnts("north",2)
-        Media.PlaySpeechNotification(allies, "TwentyMinutesRemaining")
-      elseif DateTime.Minutes(18) == ticks then
-        SendAnts("west",2)
+        end)
+end
+
+TwentyMinuteTriggerInit = function()
+	Trigger.AfterDelay(DateTime.Minutes(2),function()
+		SendAnts("west",2)
 		SendAnts("south",2)
-		Trigger.AfterDelay(DateTime.Minutes(2), function()
-			SendAnts("west",1)
-			SendAnts("south", 2)
-		end)
-      elseif DateTime.Minutes(15) == ticks then
-			SendAnts("west",2) 
-			SendFireAnts("south",2)
-		Trigger.AfterDelay(DateTime.Minutes(1),function() SendAnts("east",2) end)
-		Trigger.AfterDelay(DateTime.Minutes(2),function() 
-			SendAnts("east",2) 
-			SendAnts("north",2) 
-			SendAnts("west",3) 
-		end)
-      elseif DateTime.Minutes(11) == ticks then
-        SendFireAnts("west",1)
+	end)
+
+	Trigger.AfterDelay(DateTime.Minutes(3), function()
+		SendAnts("west",1)
+		SendAnts("south", 2)
+	end)
+
+	Trigger.AfterDelay(DateTime.Minutes(5),function()
+		SendAnts("west",2)
+		SendFireAnts("south",2)
+	end)
+
+	Trigger.AfterDelay(DateTime.Minutes(6),function()
+		SendAnts("east",2)
+	end)
+
+        Trigger.AfterDelay(DateTime.Minutes(7),function()
+		SendAnts("east",2)
+		SendAnts("north",2)
+		SendAnts("west",3)
+	end)
+
+	Trigger.AfterDelay(DateTime.Minutes(9), function()
+		SendFireAnts("west",1)
 		SendAnts("north",1)
 		SendAnts("east",1)
-      elseif DateTime.Minutes(10) == ticks then
-        Media.PlaySpeechNotification(allies, "TenMinutesRemaining")
-		Trigger.AfterDelay(DateTime.Minutes(1), function()
-			SendAnts("west",2)
-			SendAnts("north",2)
-			SendAnts("east",2)
-		end)
-		Trigger.AfterDelay(DateTime.Minutes(2), function()
-			SendAnts("west",3)
-		end)
-      elseif DateTime.Minutes(7) == ticks then
-        SendAnts("south",2)
+	end)
+end
+
+TenMinuteTriggerInit = function()
+	Trigger.AfterDelay(DateTime.Minutes(1), function()
+		SendAnts("west",2)
+		SendAnts("north",2)
+		SendAnts("east",2)
+	end)
+
+	Trigger.AfterDelay(DateTime.Minutes(2), function()
+		SendAnts("west",3)
+	end)
+
+	Trigger.AfterDelay(DateTime.Minutes(3), function()
+		SendAnts("south",2)
 		SendAnts("west",3)
 		SendFireAnts("north",1)
-      elseif DateTime.Minutes(5) == ticks then
-        Media.PlaySpeechNotification(allies, "WarningFiveMinutesRemaining")
-        SendAnts("south",5)
-		SendFireAnts("west",1)
-      elseif DateTime.Minutes(4) == ticks then
-        Media.PlaySpeechNotification(allies, "WarningFourMinutesRemaining")
-		SendAnts("west",1)
-		SendAnts("north",3)
-      elseif DateTime.Minutes(3) == ticks then
-        Media.PlaySpeechNotification(allies, "WarningThreeMinutesRemaining")
-        SendAnts("north",2)
-		SendFireAnts("east",2)
-        SendAnts("south",4)
-        SendAnts("west",4)
-      elseif DateTime.Minutes(2) == ticks then
-        Media.PlaySpeechNotification(allies, "WarningTwoMinutesRemaining")
-      elseif DateTime.Minutes(1) == ticks then
-        Media.PlaySpeechNotification(allies, "WarningOneMinuteRemaining")
-		SendAnts("east",2)
-        SendAnts("west",3)
-        SendAnts("north",3)
-        SendAnts("south",3)
-		SendFireAnts("east", 2)
-      elseif DateTime.Seconds(5) == ticks then
-        Media.PlaySpeechNotification(allies, "AlliedForcesApproaching")
-      end
+	end)
+end
 
-      ticks = ticks - 1;
-      UserInterface.SetMissionText("Reinforcements arrive in " .. Utils.FormatTime(ticks), TimerColor)
-    else
-		if not AtEndGame then
-			Media.PlaySpeechNotification(allies, "SecondObjectiveMet")
-			FinishTimer()
-			AtEndGame = true
-			Camera.Position = waypoint13.CenterPosition
-			SendTanks()
-			Trigger.AfterDelay(DateTime.Seconds(2), function() TimerExpired() end)
+Tick = function() 
+	if SurviveObjective ~= nil then
+		if ticks % DateTime.Seconds(1) == 0 then 
+			if CheckBase() then 
+				allies.MarkFailedObjective(SurviveObjective)
+			end
 		end
-		ticks = ticks - 1
-    end
-  end
+
+		if ticks > 0 then
+			if DateTime.Minutes(30) == ticks then
+				ThirtyMinuteTriggerInit()
+			elseif DateTime.Minutes(20) == ticks then
+				SendAnts("west",3)
+				SendAnts("east",3)
+				SendAnts("north",2)
+				Media.PlaySpeechNotification(allies, "TwentyMinutesRemaining")
+			elseif DateTime.Minutes(10) == ticks then
+				Media.PlaySpeechNotification(allies, "TenMinutesRemaining")
+				TenMinuteTriggerInit()
+			elseif DateTime.Minutes(5) == ticks then
+				Media.PlaySpeechNotification(allies, "WarningFiveMinutesRemaining")
+				SendAnts("south",5)
+				SendFireAnts("west",1)
+			elseif DateTime.Minutes(4) == ticks then
+				Media.PlaySpeechNotification(allies, "WarningFourMinutesRemaining")
+				SendAnts("west",1)
+				SendAnts("north",3)
+			elseif DateTime.Minutes(3) == ticks then
+				Media.PlaySpeechNotification(allies, "WarningThreeMinutesRemaining")
+				SendAnts("north",2)
+				SendFireAnts("east",2)
+				SendAnts("south",4)
+				SendAnts("west",4)
+			elseif DateTime.Minutes(2) == ticks then
+				Media.PlaySpeechNotification(allies, "WarningTwoMinutesRemaining")
+			elseif DateTime.Minutes(1) == ticks then
+			  	Media.PlaySpeechNotification(allies, "WarningOneMinuteRemaining")
+				SendAnts("east",2)
+			  	SendAnts("west",3)
+				SendAnts("north",3)
+				SendAnts("south",3)
+				SendFireAnts("east", 2)
+			elseif DateTime.Seconds(5) == ticks then
+				Media.PlaySpeechNotification(allies, "AlliedForcesApproaching")
+			end
+		
+			ticks = ticks - 1;
+			UserInterface.SetMissionText("Reinforcements arrive in " .. Utils.FormatTime(ticks), TimerColor)
+		else
+			if not AtEndGame then
+				Media.PlaySpeechNotification(allies, "SecondObjectiveMet")
+				FinishTimer()
+				AtEndGame = true
+				Camera.Position = waypoint13.CenterPosition
+				SendTanks()
+				Trigger.AfterDelay(DateTime.Seconds(2), function() TimerExpired() end)
+			end
+			ticks = ticks - 1
+		end
+	end
 end
