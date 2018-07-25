21f591a915e3f5cce825547fc44f5dc63496eb37
Dylan Manitta <dylan@plugnpay.com>
2018-07-25 17:05:58 -0400

    Creating mission 2

---
+++ mods/ra/maps/ant-02/ant-02.lua
@@ -0,0 +1,11 @@
+WorldLoaded = function()
+        allies = Player.GetPlayer("Spain")
+	creeps = Player.GetPlayer("Creeps")
+	Germany = Player.GetPlayer("Germany")
+	GoodGuy = Player.GetPlayer("GoodGuy")
+	Turkey = Player.GetPlayer("Turkey")
+	Ukraine = Player.GetPlayer("Ukraine")
+	Special = Player.GetPlayer("Special")
+	BadGuy = Player.GetPlayer("BadGuy")
+	Camera.Position = DefaultCameraPosition.CenterPosition
+end
