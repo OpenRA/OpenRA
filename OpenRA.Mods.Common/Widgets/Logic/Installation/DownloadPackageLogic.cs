68a10b7009e6c3e2eb7265d752429cc5e86e5063
Emmalyn Renato <emmalynrenato@gmail.com>
2018-08-09 14:16:52 -0700

    Fix display of progress text in Advanced Install when total file size is unknown.

--- OpenRA.Mods.Common/Widgets/Logic/Installation/DownloadPackageLogic.cs
+++ OpenRA.Mods.Common/Widgets/Logic/Installation/DownloadPackageLogic.cs
@@ -85,6 +85,9 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 					dataTotal = float.NaN;
 					dataReceived = i.BytesReceived;
 					dataSuffix = SizeSuffixes[0];
+					getStatusText = () => "Downloading from {2} {0:0.00} {1}".F(dataReceived, dataSuffix,
+						downloadHost ?? "unknown host");
+					progressBar.Indeterminate = true;
 				}
 				else
 				{
@@ -92,14 +95,13 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 					dataTotal = i.TotalBytesToReceive / (float)(1L << (mag * 10));
 					dataReceived = i.BytesReceived / (float)(1L << (mag * 10));
 					dataSuffix = SizeSuffixes[mag];
+					getStatusText = () => "Downloading from {4} {1:0.00}/{2:0.00} {3} ({0}%)".F(i.ProgressPercentage,
+						dataReceived, dataTotal, dataSuffix,
+						downloadHost ?? "unknown host");
+					progressBar.Indeterminate = false;
 				}
 
-				progressBar.Indeterminate = false;
 				progressBar.Percentage = i.ProgressPercentage;
-
-				getStatusText = () => "Downloading from {4} {1:0.00}/{2:0.00} {3} ({0}%)".F(i.ProgressPercentage,
-					dataReceived, dataTotal, dataSuffix,
-					downloadHost ?? "unknown host");
 			};
 
 			Action<string> onExtractProgress = s => Game.RunAfterTick(() => getStatusText = () => s);
