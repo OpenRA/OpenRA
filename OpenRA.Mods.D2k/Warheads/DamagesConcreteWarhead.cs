dce8f0a3242ead2f58c8c3e130070237f8dcc3fe
Matthias Mailänder <matthias@mailaender.name>
2018-09-13 21:17:32 +0200

    Add a DamagesConcreteWarhead to remove buildable concrete.

---
+++ OpenRA.Mods.D2k/Warheads/DamagesConcreteWarhead.cs
@@ -0,0 +1,34 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2007-2018 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System.Collections.Generic;
+using OpenRA.Mods.Common.Warheads;
+using OpenRA.Mods.D2k.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.D2k.Warheads
+{
+	[Desc("Interacts with the BuildableTerrainLayer trait.")]
+	public class DamagesConcreteWarhead : Warhead
+	{
+		[Desc("How much damage to deal.")]
+		[FieldLoader.Require]
+		public readonly int Damage = 0;
+
+		public override void DoImpact(Target target, Actor firedBy, IEnumerable<int> damageModifiers)
+		{
+			var world = firedBy.World;
+			var layer = world.WorldActor.Trait<BuildableTerrainLayer>();
+			var cell = world.Map.CellContaining(target.CenterPosition);
+			layer.HitTile(cell, Damage);
+		}
+	}
+}
