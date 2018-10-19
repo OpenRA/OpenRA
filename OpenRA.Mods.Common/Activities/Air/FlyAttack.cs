c775c1e99ac57a2cf53487841e08dff398819d7a
reaperrr <thereaperrr@aol.com>
2018-10-19 23:22:33 +0200

    Merge HeliAttack into FlyAttack

--- OpenRA.Mods.Common/Activities/Air/FlyAttack.cs
+++ OpenRA.Mods.Common/Activities/Air/FlyAttack.cs
@@ -9,6 +9,7 @@
  */
 #endregion
 
+using System.Drawing;
 using System.Linq;
 using OpenRA.Activities;
 using OpenRA.Mods.Common.Traits;
@@ -18,19 +19,36 @@ namespace OpenRA.Mods.Common.Activities
 {
 	public class FlyAttack : Activity
 	{
-		readonly Target target;
 		readonly Aircraft aircraft;
 		readonly AttackAircraft attackAircraft;
+		readonly bool attackOnlyVisibleTargets;
 		readonly Rearmable rearmable;
 
 		int ticksUntilTurn;
-
-		public FlyAttack(Actor self, Target target)
+		Target target;
+		bool canHideUnderFog;
+		protected Target Target
 		{
-			this.target = target;
+			get
+			{
+				return target;
+			}
+
+			private set
+			{
+				target = value;
+				if (target.Type == TargetType.Actor)
+					canHideUnderFog = target.Actor.Info.HasTraitInfo<HiddenUnderFogInfo>();
+			}
+		}
+
+		public FlyAttack(Actor self, Target target, bool attackOnlyVisibleTargets = true)
+		{
+			Target = target;
 			aircraft = self.Trait<Aircraft>();
 			attackAircraft = self.Trait<AttackAircraft>();
 			rearmable = self.TraitOrDefault<Rearmable>();
+			this.attackOnlyVisibleTargets = attackOnlyVisibleTargets;
 			ticksUntilTurn = attackAircraft.AttackAircraftInfo.AttackTurnDelay;
 		}
 
@@ -46,29 +64,80 @@ namespace OpenRA.Mods.Common.Activities
 			if (!target.IsValidFor(self))
 				return NextActivity;
 
+			var pos = self.CenterPosition;
+			var targetPos = attackAircraft.GetTargetPosition(pos, target);
+			if (attackOnlyVisibleTargets && target.Type == TargetType.Actor && canHideUnderFog
+				&& !target.Actor.CanBeViewedByPlayer(self.Owner))
+			{
+				var newTarget = Target.FromCell(self.World, self.World.Map.CellContaining(targetPos));
+
+				Cancel(self);
+				self.SetTargetLine(newTarget, Color.Green);
+				return new Fly(self, newTarget);
+			}
+
 			// If all valid weapons have depleted their ammo and Rearmable trait exists, return to RearmActor to reload and then resume the activity
 			if (rearmable != null && attackAircraft.Armaments.All(x => x.IsTraitPaused || !x.Weapon.IsValidAgainst(target, self.World, self)))
 				return ActivityUtils.SequenceActivities(new ReturnToBase(self, aircraft.Info.AbortOnResupply), this);
 
-			attackAircraft.DoAttack(self, target);
-
-			if (ChildActivity == null)
+			if (aircraft.Info.CanHover)
 			{
 				if (IsCanceled)
 					return NextActivity;
 
-				// TODO: This should fire each weapon at its maximum range
-				if (attackAircraft != null && target.IsInRange(self.CenterPosition, attackAircraft.Armaments.Where(Exts.IsTraitEnabled).Select(a => a.Weapon.MinRange).Min()))
-					ChildActivity = ActivityUtils.SequenceActivities(new FlyTimed(ticksUntilTurn, self), new Fly(self, target), new FlyTimed(ticksUntilTurn, self));
-				else
-					ChildActivity = ActivityUtils.SequenceActivities(new Fly(self, target), new FlyTimed(ticksUntilTurn, self));
+				var dist = targetPos - pos;
 
-				// HACK: This needs to be done in this round-about way because TakeOff doesn't behave as expected when it doesn't have a NextActivity.
-				if (self.World.Map.DistanceAboveTerrain(self.CenterPosition).Length < aircraft.Info.MinAirborneAltitude)
-					ChildActivity = ActivityUtils.SequenceActivities(new TakeOff(self), ChildActivity);
+				// Can rotate facing while ascending
+				var desiredFacing = dist.HorizontalLengthSquared != 0 ? dist.Yaw.Facing : aircraft.Facing;
+				aircraft.Facing = Util.TickFacing(aircraft.Facing, desiredFacing, aircraft.TurnSpeed);
+
+				if (Fly.AdjustAltitude(self, aircraft, aircraft.Info.CruiseAltitude))
+					return this;
+
+				// Fly towards the target
+				if (!target.IsInRange(pos, attackAircraft.GetMaximumRangeVersusTarget(target)))
+					aircraft.SetPosition(self, aircraft.CenterPosition + aircraft.FlyStep(desiredFacing));
+
+				// Fly backwards from the target
+				if (target.IsInRange(pos, attackAircraft.GetMinimumRangeVersusTarget(target)))
+				{
+					// Facing 0 doesn't work with the following position change
+					var facing = 1;
+					if (desiredFacing != 0)
+						facing = desiredFacing;
+					else if (aircraft.Facing != 0)
+						facing = aircraft.Facing;
+
+					Fly.FlyToward(self, aircraft, facing, aircraft.Info.CruiseAltitude, moveVerticalOnly: false, flyBackward: true);
+					return this;
+				}
+				else if (withinMaxRange && !withinMinRange && desiredFacing != aircraft.Facing)
+					aircraft.Facing = Util.TickFacing(aircraft.Facing, desiredFacing, aircraft.TurnSpeed);
+
+				attackAircraft.DoAttack(self, target);
 			}
+			else
+			{
+				attackAircraft.DoAttack(self, target);
 
-			ActivityUtils.RunActivity(self, ChildActivity);
+				if (ChildActivity == null)
+				{
+					if (IsCanceled)
+						return NextActivity;
+
+					// TODO: This should fire each weapon at its maximum range
+					if (target.IsInRange(self.CenterPosition, attackAircraft.Armaments.Where(Exts.IsTraitEnabled).Select(a => a.Weapon.MinRange).Min()))
+						ChildActivity = ActivityUtils.SequenceActivities(new FlyTimed(ticksUntilTurn, self), new Fly(self, target), new FlyTimed(ticksUntilTurn, self));
+					else
+						ChildActivity = ActivityUtils.SequenceActivities(new Fly(self, target), new FlyTimed(ticksUntilTurn, self));
+
+					// HACK: This needs to be done in this round-about way because TakeOff doesn't behave as expected when it doesn't have a NextActivity.
+					if (self.World.Map.DistanceAboveTerrain(self.CenterPosition).Length < aircraft.Info.MinAirborneAltitude)
+						ChildActivity = ActivityUtils.SequenceActivities(new TakeOff(self), ChildActivity);
+				}
+
+				ActivityUtils.RunActivity(self, ChildActivity);
+			}
 
 			return this;
 		}
