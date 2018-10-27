94088d37a63891246abe62b7fe6c75a09ad12464
Paul Chote <pchote@users.noreply.github.com>
2018-10-27 16:56:24 +0000

    Remove building lock from Sellable.

--- OpenRA.Mods.Common/UpdateRules/Rules/20180923/RemovedNotifyBuildComplete.cs
+++ OpenRA.Mods.Common/UpdateRules/Rules/20180923/RemovedNotifyBuildComplete.cs
@@ -22,7 +22,7 @@ namespace OpenRA.Mods.Common.UpdateRules.Rules
 			get
 			{
 				return "Traits are no longer force-disabled while the WithMakeAnimation trait is active.\n" +
-					"This affects the With*Animation, With*Overlay, Transforms, Gate, and ConyardChronoReturn traits.\n" +
+					"This affects the With*Animation, With*Overlay, Transforms, Sellable, Gate, and ConyardChronoReturn traits.\n" +
 				    "The AnnounceOnBuild trait has been replaced with a new VoiceAnnouncement trait.\n" +
 					"Affected actors are listed so that conditions may be manually defined.";
 			}
@@ -53,6 +53,7 @@ namespace OpenRA.Mods.Common.UpdateRules.Rules
 			"WithCrumbleOverlay",
 			"WithDeliveryOverlay",
 			"Transforms",
+			"Sellable",
 			"Gate",
 			"ConyardChronoReturn",
 			"VoiceAnnouncement"
