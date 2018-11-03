737069d0e3e6bfcaa0363fcb434132b89a6e9123
reaperrr <thereaperrr@aol.com>
2018-11-03 04:31:19 +0100

    Convert AIHarvesterManager into *Module

---
+++ OpenRA.Mods.Common/Traits/BotModules/HarvesterBotModule.cs
@@ -0,0 +1,133 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2018 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenRA.Mods.Common.Activities;
+using OpenRA.Mods.Common.AI;
+using OpenRA.Mods.Common.Pathfinder;
+using OpenRA.Support;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Common.Traits
+{
+	[Desc("Put this on the Player actor. Manages bot harvesters to ensure they always continue harvesting as long as there are resources on the map.")]
+	public class HarvesterBotModuleInfo : ConditionalTraitInfo, Requires<BotOrderManagerInfo>
+	{
+		[Desc("Interval (in ticks) between giving out orders to idle harvesters.")]
+		public readonly int ScanForIdleHarvestersInterval = 20;
+
+		[Desc("Avoid enemy actors nearby when searching for a new resource patch. Should be somewhere near the max weapon range.")]
+		public readonly WDist HarvesterEnemyAvoidanceRadius = WDist.FromCells(8);
+
+		public override object Create(ActorInitializer init) { return new HarvesterBotModule(init.Self, this); }
+	}
+
+	public class HarvesterBotModule : ConditionalTrait<HarvesterBotModuleInfo>, ITick
+	{
+		readonly World world;
+		readonly Player player;
+		readonly Predicate<Actor> unitCannotBeOrdered;
+		IPathFinder pathfinder;
+		DomainIndex domainIndex;
+		ResourceLayer resLayer;
+		ResourceClaimLayer claimLayer;
+		BotOrderManager botOrderManager;
+		List<Actor> harvesters = new List<Actor>();
+		int scanForIdleHarvestersTicks;
+
+		public HarvesterBotModule(Actor self, HarvesterBotModuleInfo info)
+			: base(info)
+		{
+			world = self.World;
+			player = self.Owner;
+			unitCannotBeOrdered = a => a.Owner != self.Owner || a.IsDead || !a.IsInWorld;
+		}
+
+		protected override void TraitEnabled(Actor self)
+		{
+			pathfinder = world.WorldActor.Trait<IPathFinder>();
+			domainIndex = world.WorldActor.Trait<DomainIndex>();
+			resLayer = world.WorldActor.TraitOrDefault<ResourceLayer>();
+			claimLayer = world.WorldActor.TraitOrDefault<ResourceClaimLayer>();
+			botOrderManager = self.Owner.PlayerActor.Trait<BotOrderManager>();
+			scanForIdleHarvestersTicks = Info.ScanForIdleHarvestersInterval;
+		}
+
+		void ITick.Tick(Actor self)
+		{
+			if (IsTraitDisabled)
+				return;
+
+			if (resLayer == null || resLayer.IsResourceLayerEmpty)
+				return;
+
+			if (--scanForIdleHarvestersTicks > 0)
+				return;
+
+			harvesters.RemoveAll(unitCannotBeOrdered);
+			scanForIdleHarvestersTicks = Info.ScanForIdleHarvestersInterval;
+
+			// Find new harvesters
+			// TODO: Look for a more performance-friendly way to update this list
+			var newHarvesters = world.ActorsHavingTrait<Harvester>().Where(a => a.Owner == player && !harvesters.Contains(a));
+			foreach (var a in newHarvesters)
+				harvesters.Add(a);
+
+			// Find idle harvesters and give them orders:
+			foreach (var harvester in harvesters)
+			{
+				var harv = harvester.Trait<Harvester>();
+				if (!harv.IsEmpty)
+					continue;
+
+				if (!harvester.IsIdle)
+				{
+					var act = harvester.CurrentActivity;
+					if (!harv.LastSearchFailed || act.NextActivity == null || act.NextActivity.GetType() != typeof(FindResources))
+						continue;
+				}
+
+				var para = harvester.TraitOrDefault<Parachutable>();
+				if (para != null && para.IsInAir)
+					continue;
+
+				// Tell the idle harvester to quit slacking:
+				var newSafeResourcePatch = FindNextResource(harvester, harv);
+				AIUtils.BotDebug("AI: Harvester {0} is idle. Ordering to {1} in search for new resources.".F(harvester, newSafeResourcePatch));
+				botOrderManager.QueueOrder(new Order("Harvest", harvester, Target.FromCell(world, newSafeResourcePatch), false));
+			}
+		}
+
+		CPos FindNextResource(Actor actor, Harvester harv)
+		{
+			var locomotorInfo = actor.Info.TraitInfo<MobileInfo>().LocomotorInfo;
+
+			Func<CPos, bool> isValidResource = cell =>
+				domainIndex.IsPassable(actor.Location, cell, locomotorInfo) &&
+				harv.CanHarvestCell(actor, cell) &&
+				claimLayer.CanClaimCell(actor, cell);
+
+			var path = pathfinder.FindPath(
+				PathSearch.Search(world, locomotorInfo, actor, true, isValidResource)
+					.WithCustomCost(loc => world.FindActorsInCircle(world.Map.CenterOfCell(loc), Info.HarvesterEnemyAvoidanceRadius)
+						.Where(u => !u.IsDead && actor.Owner.Stances[u.Owner] == Stance.Enemy)
+						.Sum(u => Math.Max(WDist.Zero.Length, Info.HarvesterEnemyAvoidanceRadius.Length - (world.Map.CenterOfCell(loc) - u.CenterPosition).Length)))
+					.FromPoint(actor.Location));
+
+			if (path.Count == 0)
+				return CPos.Zero;
+
+			return path[0];
+		}
+	}
+}
---
+++ OpenRA.Mods.Common/UpdateRules/Rules/20180923/AddHarvesterBotModule.cs
@@ -0,0 +1,122 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2018 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OpenRA.Mods.Common.UpdateRules.Rules
+{
+	public class AddHarvesterBotModule : UpdateRule
+	{
+		public override string Name { get { return "Split HackyAI harvester handling to HarvesterBotModule"; } }
+		public override string Description
+		{
+			get
+			{
+				return "Some properties and all harvester handling have been moved from HackyAI\n" +
+					"to the new HarvesterBotModule.";
+			}
+		}
+
+		bool messageShown;
+
+		readonly string[] harvesterFields =
+		{
+			"HarvesterEnemyAvoidanceRadius", "AssignRolesInterval"
+		};
+
+		public override IEnumerable<string> AfterUpdate(ModData modData)
+		{
+			var message = "You may want to check your AI yamls for possible redundant module entries.\n" +
+				"Additionally, make sure the Player actor has the ConditionManager trait and add it manually if it doesn't.";
+
+			if (!messageShown)
+				yield return message;
+
+			messageShown = true;
+		}
+
+		public override IEnumerable<string> UpdateActorNode(ModData modData, MiniYamlNode actorNode)
+		{
+			if (actorNode.Key != "Player")
+				yield break;
+
+			var hackyAIs = actorNode.ChildrenMatching("HackyAI");
+			if (!hackyAIs.Any())
+				yield break;
+
+			var addNodes = new List<MiniYamlNode>();
+
+			// We add a 'default' HarvesterBotModule in any case,
+			// and only add more for AIs that define custom values for one of its fields.
+			var defaultHarvNode = new MiniYamlNode("HarvesterBotModule", "");
+
+			foreach (var hackyAINode in hackyAIs)
+			{
+				// HackyAIInfo.Name might contain spaces, so Type is better suited to be used as condition name
+				var aiType = hackyAINode.LastChildMatching("Type").NodeValue<string>();
+				var conditionString = "enable-" + aiType + "-ai";
+				var requiresCondition = new MiniYamlNode("RequiresCondition", conditionString);
+				var conditionNode = hackyAINode.LastChildMatching("Condition");
+				if (conditionNode == null)
+				{
+					var enableModule = new MiniYamlNode("Condition", conditionString);
+					hackyAINode.AddNode(enableModule);
+				}
+
+				if (harvesterFields.Any(f => hackyAINode.ChildrenMatching(f).Any()))
+				{
+					var harvNode = new MiniYamlNode("HarvesterBotModule@" + aiType, "");
+					harvNode.AddNode(requiresCondition);
+
+					foreach (var hf in harvesterFields)
+					{
+						var fieldNode = hackyAINode.LastChildMatching(hf);
+						if (fieldNode != null)
+						{
+							if (hf == "AssignRolesInterval")
+								fieldNode.MoveAndRenameNode(hackyAINode, harvNode, "ScanForIdleHarvestersInterval");
+							else
+								fieldNode.MoveNode(hackyAINode, harvNode);
+						}
+					}
+
+					addNodes.Add(harvNode);
+				}
+				else
+				{
+					// We want the default module to be enabled for every AI that didn't customise one of its fields,
+					// so we need to update RequiresCondition to be enabled on any of the conditions granted by these AIs,
+					// but only if the condition hasn't been added yet.
+					var requiresConditionNode = defaultHarvNode.LastChildMatching("RequiresCondition");
+					if (requiresConditionNode == null)
+						defaultHarvNode.AddNode(requiresCondition);
+					else
+					{
+						var oldValue = requiresConditionNode.NodeValue<string>();
+						if (oldValue.Contains(conditionString))
+							continue;
+
+						requiresConditionNode.ReplaceValue(oldValue + " || " + conditionString);
+					}
+				}
+			}
+
+			addNodes.Add(defaultHarvNode);
+
+			foreach (var node in addNodes)
+				actorNode.AddNode(node);
+
+			yield break;
+		}
+	}
+}
