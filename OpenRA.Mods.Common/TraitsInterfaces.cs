c2cf8ba5998ab23ce7943bdd090808970e16331a
Oliver Brakmann <ob-web2013+github@posteo.net>
2018-12-26 13:38:26 +0000

    Add an interface to prevent actors from being spawned by SpawnMapActors

--- OpenRA.Mods.Common/TraitsInterfaces.cs
+++ OpenRA.Mods.Common/TraitsInterfaces.cs
@@ -538,4 +538,10 @@ namespace OpenRA.Mods.Common.Traits
 			OnChange = onChange;
 		}
 	}
+
+	[RequireExplicitImplementation]
+	public interface IPreventMapSpawn
+	{
+		bool PreventMapSpawn(World world, ActorReference actorReference);
+	}
 }
