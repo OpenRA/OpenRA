8b9603168a4d7987fb52d78bbe222ed59eab87fc
abcdefg30 <alphabet@streber24.de>
2019-01-08 15:29:28 +0100

    Change the year number in all lua headers from 2018 to 2019

--- mods/cnc/maps/nod10a/nod10a.lua
+++ mods/cnc/maps/nod10a/nod10a.lua
@@ -1,10 +1,10 @@
 --[[
-	Copyright 2007-2018 The OpenRA Developers (see AUTHORS)
-	This file is part of OpenRA, which is free software. It is made
-	available to you under the terms of the GNU General Public License
-	as published by the Free Software Foundation, either version 3 of
-	the License, or (at your option) any later version. For more
-	information, see COPYING.
+   Copyright 2007-2019 The OpenRA Developers (see AUTHORS)
+   This file is part of OpenRA, which is free software. It is made
+   available to you under the terms of the GNU General Public License
+   as published by the Free Software Foundation, either version 3 of
+   the License, or (at your option) any later version. For more
+   information, see COPYING.
 ]]
 if Map.LobbyOption("difficulty") == "easy" then
 	Rambo = "rmbo.easy"
