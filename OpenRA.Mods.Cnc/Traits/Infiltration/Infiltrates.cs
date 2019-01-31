f4465a6f1e1901d28fa1e67d9aff0f96d74c198c
Paul Chote <pchote@users.noreply.github.com>
2019-01-31 22:18:01 +0000

    Port Infiltrate to the new Enter activity.

--- OpenRA.Mods.Cnc/Traits/Infiltration/Infiltrates.cs
+++ OpenRA.Mods.Cnc/Traits/Infiltration/Infiltrates.cs
@@ -92,21 +92,34 @@ namespace OpenRA.Mods.Cnc.Traits
 				? Info.Voice : null;
 		}
 
+		public bool CanInfiltrateTarget(Actor self, Target target)
+		{
+			switch (target.Type)
+			{
+				case TargetType.Actor:
+					return Info.Types.Overlaps(target.Actor.GetEnabledTargetTypes()) &&
+					       Info.ValidStances.HasStance(self.Owner.Stances[target.Actor.Owner]);
+				case TargetType.FrozenActor:
+					return target.FrozenActor.IsValid && Info.Types.Overlaps(target.FrozenActor.TargetTypes) &&
+					       Info.ValidStances.HasStance(self.Owner.Stances[target.FrozenActor.Owner]);
+				default:
+					return false;
+			}
+		}
+
 		public void ResolveOrder(Actor self, Order order)
 		{
 			if (order.OrderString != "Infiltrate" || !IsValidOrder(self, order) || IsTraitDisabled)
 				return;
 
-			var target = self.ResolveFrozenActorOrder(order, Color.Red);
-			if (target.Type != TargetType.Actor
-				|| !Info.Types.Overlaps(target.Actor.GetAllTargetTypes()))
+			if (!CanInfiltrateTarget(self, order.Target))
 				return;
 
 			if (!order.Queued)
 				self.CancelActivity();
 
-			self.SetTargetLine(target, Color.Red);
-			self.QueueActivity(new Infiltrate(self, target.Actor, this));
+			self.SetTargetLine(order.Target, Color.Red);
+			self.QueueActivity(new Infiltrate(self, order.Target, this));
 		}
 	}
 
