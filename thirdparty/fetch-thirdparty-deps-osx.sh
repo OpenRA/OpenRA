54d5afed573ad7d6bc7a50b5c4ad26145154bf53
Paul Chote <pchote@users.noreply.github.com>
2019-03-17 13:38:49 +0000

    Package mono inside the macOS app bundles.
    This also removes the explicit dependency on mono's
    packaged FreeType, which should allow development
    builds to be used with homebrew-packaged mono.

--- thirdparty/fetch-thirdparty-deps-osx.sh
+++ thirdparty/fetch-thirdparty-deps-osx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LAUNCHER_TAG="osx-launcher-20171118"
+LAUNCHER_TAG="osx-launcher-20190317"
 
 download_dir="${0%/*}/download/osx"
 mkdir -p "$download_dir"
@@ -18,5 +18,10 @@ fi
 
 if [ ! -f Eluant.dll.config ]; then
 	echo "Fetching OS X Lua configuration file from GitHub."
-	curl -LOs https://raw.githubusercontent.com/OpenRA/OpenRALauncherOSX/${LAUNCHER_TAG}/dependencies/Eluant.dll.config
+	curl -LOs https://raw.githubusercontent.com/OpenRA/OpenRALauncherOSX/${LAUNCHER_TAG}/Eluant.dll.config
+fi
+
+if [ ! -f libfreetype.6.dylib ]; then
+	echo "Fetching OS X FreeType library from GitHub."
+	curl -LOs https://github.com/OpenRA/OpenRALauncherOSX/releases/download/${LAUNCHER_TAG}/libfreetype.6.dylib
 fi
