f537d42ff7ba49ec8aa932dc903dcfe04263ecb9
Paul Chote <pchote@users.noreply.github.com>
2019-03-22 08:19:43 +0000

    Display the correct version on generated wiki pages.

--- OpenRA.Mods.Common/UtilityCommands/ExtractWeaponDocsCommand.cs
+++ OpenRA.Mods.Common/UtilityCommands/ExtractWeaponDocsCommand.cs
@@ -26,17 +26,21 @@ namespace OpenRA.Mods.Common.UtilityCommands
 			return true;
 		}
 
-		[Desc("Generate weaponry documentation in MarkDown format.")]
+		[Desc("[VERSION]", "Generate weaponry documentation in MarkDown format.")]
 		void IUtilityCommand.Run(Utility utility, string[] args)
 		{
 			// HACK: The engine code assumes that Game.modData is set.
 			Game.ModData = utility.ModData;
 
+			var version = utility.ModData.Manifest.Metadata.Version;
+			if (args.Length > 1)
+				version = args[1];
+
 			Console.WriteLine(
 				"This documentation is aimed at modders. It displays a template for weapon definitions " +
 				"as well as its contained types (warheads and projectiles) with default values and developer commentary. " +
 				"Please do not edit it directly, but add new `[Desc(\"String\")]` tags to the source code. This file has been " +
-				"automatically generated for version {0} of OpenRA.", utility.ModData.Manifest.Metadata.Version);
+				"automatically generated for version {0} of OpenRA.", version);
 			Console.WriteLine();
 
 			var toc = new StringBuilder();
