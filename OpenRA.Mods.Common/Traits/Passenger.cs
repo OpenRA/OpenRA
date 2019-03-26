cadcbdf638266d3c785e969e49801c79e8fb7f53
Oliver Brakmann <ob-web2013+github@posteo.net>
2019-03-27 00:49:33 +0100

    Fix crash due to conflicting access to collection in killed Cargo

--- OpenRA.Mods.Common/Traits/Passenger.cs
+++ OpenRA.Mods.Common/Traits/Passenger.cs
@@ -189,7 +189,9 @@ namespace OpenRA.Mods.Common.Traits
 			if (Transport == null)
 				return;
 
-			Transport.Trait<Cargo>().Unload(Transport, self);
+			// Something killed us, but it wasn't our transport blowing up. Remove us from the cargo.
+			if (!Transport.IsDead)
+				Transport.Trait<Cargo>().Unload(Transport, self);
 		}
 	}
 }
