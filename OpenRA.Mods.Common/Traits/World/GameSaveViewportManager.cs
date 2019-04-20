c8f2ee5270d5c8ca195b5b1b1c8a499bf9e1de91
Paul Chote <pchote@users.noreply.github.com>
2019-04-20 11:01:55 +0000

    Save and load skirmish spectator viewport.

--- OpenRA.Mods.Common/Traits/World/GameSaveViewportManager.cs
+++ OpenRA.Mods.Common/Traits/World/GameSaveViewportManager.cs
@@ -29,25 +29,44 @@ namespace OpenRA.Mods.Common.Traits
 
 		List<MiniYamlNode> IGameSaveTraitData.IssueTraitData(Actor self)
 		{
-			if (worldRenderer.World.LocalPlayer == null || self != worldRenderer.World.LocalPlayer.PlayerActor)
+			// HACK: Store the viewport state for the skirmish observer on the first bot's trait
+			// TODO: This won't make sense for MP saves
+			var localPlayer = worldRenderer.World.LocalPlayer;
+			if ((localPlayer != null && localPlayer.PlayerActor != self) ||
+			    self.Owner != self.World.Players.FirstOrDefault(p => p.IsBot))
 				return null;
 
-			return new List<MiniYamlNode>()
+			var nodes = new List<MiniYamlNode>()
 			{
-				new MiniYamlNode("Viewport", FieldSaver.FormatValue(worldRenderer.Viewport.CenterPosition)),
-				new MiniYamlNode("Selection", "", self.World.Selection.Serialize())
+				new MiniYamlNode("Viewport", FieldSaver.FormatValue(worldRenderer.Viewport.CenterPosition))
 			};
+
+			var renderPlayer = worldRenderer.World.RenderPlayer;
+			if (localPlayer == null && renderPlayer != null)
+				nodes.Add(new MiniYamlNode("RenderPlayer", FieldSaver.FormatValue(renderPlayer.PlayerActor.ActorID)));
+
+			if (localPlayer != null && localPlayer.PlayerActor == self)
+				nodes.Add(new MiniYamlNode("Selection", "", self.World.Selection.Serialize()));
+
+			return nodes;
 		}
 
 		void IGameSaveTraitData.ResolveTraitData(Actor self, List<MiniYamlNode> data)
 		{
 			var viewportNode = data.FirstOrDefault(n => n.Key == "Viewport");
 			if (viewportNode != null)
-				worldRenderer.Viewport.Center(FieldLoader.GetValue<WPos>("data", viewportNode.Value.Value));
+				worldRenderer.Viewport.Center(FieldLoader.GetValue<WPos>("Viewport", viewportNode.Value.Value));
 
 			var selectionNode = data.FirstOrDefault(n => n.Key == "Selection");
 			if (selectionNode != null)
 				self.World.Selection.Deserialize(self.World, selectionNode.Value.Nodes);
+
+			var renderPlayerNode = data.FirstOrDefault(n => n.Key == "RenderPlayer");
+			if (renderPlayerNode != null)
+			{
+				var renderPlayerActorID = FieldLoader.GetValue<uint>("RenderPlayer", renderPlayerNode.Value.Value);
+				worldRenderer.World.RenderPlayer = worldRenderer.World.GetActorById(renderPlayerActorID).Owner;
+			}
 		}
 	}
 }
