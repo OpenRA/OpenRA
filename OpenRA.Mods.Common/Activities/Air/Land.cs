ac08f24828b43228031e9a76782179cccb037268
reaperrr <thereaperrr@aol.com>
2019-05-25 16:12:20 +0200

    Make aircraft move to CruiseAltitude on new order

--- OpenRA.Mods.Common/Activities/Air/Land.cs
+++ OpenRA.Mods.Common/Activities/Air/Land.cs
@@ -48,8 +48,23 @@ namespace OpenRA.Mods.Common.Activities
 
 			if (IsCanceling || target.Type == TargetType.Invalid)
 			{
-				aircraft.RemoveInfluence();
-				return NextActivity;
+				// We must return the actor to a sensible height before continuing.
+				// If the aircraft lands when idle and is idle, continue landing,
+				// otherwise climb back to CruiseAltitude.
+				// TODO: Remove this after fixing all activities to work properly with arbitrary starting altitudes.
+				var continueLanding = aircraft.Info.LandWhenIdle && self.CurrentActivity.IsCanceling && self.CurrentActivity.NextActivity == null;
+				if (!continueLanding)
+				{
+					var dat = self.World.Map.DistanceAboveTerrain(aircraft.CenterPosition);
+					if (dat > aircraft.LandAltitude && dat < aircraft.Info.CruiseAltitude)
+					{
+						QueueChild(self, new TakeOff(self), true);
+						return this;
+					}
+
+					aircraft.RemoveInfluence();
+					return NextActivity;
+				}
 			}
 
 			if (!landingInitiated)
