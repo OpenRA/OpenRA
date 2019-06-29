f56f17e261790377dff8d4248d1dd44446d90d69
Christopher Birmingham <chris.birmingham@hotmail.co.uk>
2019-06-29 11:18:57 +0100

    Updated help comment
    Updated the help comment at the top of the script with the new script name

--- launch-dedicated-appimage.sh
+++ launch-dedicated-appimage.sh
@@ -2,8 +2,8 @@
 # example launch script, see https://github.com/OpenRA/OpenRA/wiki/Dedicated for details
 
 # Usage:
-#  $ ./launch-dedicated.sh # Launch a dedicated server with default settings
-#  $ Mod="d2k" ./launch-dedicated.sh # Launch a dedicated server with default settings but override the Mod
+#  $ ./launch-dedicated-appimage.sh # Launch a dedicated server with default settings
+#  $ Mod="d2k" ./launch-dedicated-appimage.sh # Launch a dedicated server with default settings but override the Mod
 #  Read the file to see which settings you can override
 
 Name="${Name:-"Dedicated Server"}"
