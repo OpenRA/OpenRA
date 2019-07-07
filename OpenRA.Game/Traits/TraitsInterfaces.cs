599eb89b4257ac743ca029e4a65ea6e3a7d02168
teinarss <einarsson.tomas@gmail.com>
2019-07-07 19:46:33 +0200

    Added CellUpdated event to ActorMap

--- OpenRA.Game/Traits/TraitsInterfaces.cs
+++ OpenRA.Game/Traits/TraitsInterfaces.cs
@@ -259,6 +259,8 @@ namespace OpenRA.Traits
 
 		WDist LargestActorRadius { get; }
 		WDist LargestBlockingActorRadius { get; }
+
+		event Action<IEnumerable<CPos>> CellsUpdated;
 	}
 
 	[RequireExplicitImplementation]
