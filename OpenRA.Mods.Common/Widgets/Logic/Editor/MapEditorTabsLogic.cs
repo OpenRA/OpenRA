0e451e5f5b0245386ee3d4c3feee6a9987fb935e
teinarss <einarsson.tomas@gmail.com>
2019-07-13 16:39:39 +0200

    Cleanup in MapEditorTabsLogic

--- OpenRA.Mods.Common/Widgets/Logic/Editor/MapEditorTabsLogic.cs
+++ OpenRA.Mods.Common/Widgets/Logic/Editor/MapEditorTabsLogic.cs
@@ -16,34 +16,31 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 {
 	public class MapEditorTabsLogic : ChromeLogic
 	{
+		readonly Widget widget;
+
 		protected enum MenuType { Tiles, Layers, Actors }
 		protected MenuType menuType = MenuType.Tiles;
+		readonly Widget tabContainer;
 
 		[ObjectCreator.UseCtor]
 		public MapEditorTabsLogic(Widget widget, WorldRenderer worldRenderer)
 		{
-			var tabContainer = widget.Get("MAP_EDITOR_TAB_CONTAINER");
+			this.widget = widget;
+			tabContainer = widget.Get("MAP_EDITOR_TAB_CONTAINER");
 
-			var tilesTab = tabContainer.Get<ButtonWidget>("TILES_TAB");
-			tilesTab.IsHighlighted = () => menuType == MenuType.Tiles;
-			tilesTab.OnClick = () => { menuType = MenuType.Tiles; };
+			SetupTab("TILES_TAB", "TILE_WIDGETS", MenuType.Tiles);
+			SetupTab("OVERLAYS_TAB", "LAYER_WIDGETS", MenuType.Layers);
+			SetupTab("ACTORS_TAB", "ACTOR_WIDGETS", MenuType.Actors);
+		}
 
-			var overlaysTab = tabContainer.Get<ButtonWidget>("OVERLAYS_TAB");
-			overlaysTab.IsHighlighted = () => menuType == MenuType.Layers;
-			overlaysTab.OnClick = () => { menuType = MenuType.Layers; };
+		void SetupTab(string buttonId, string tabId, MenuType tabType)
+		{
+			var tab = tabContainer.Get<ButtonWidget>(buttonId);
+			tab.IsHighlighted = () => menuType == tabType;
+			tab.OnClick = () => { menuType = tabType; };
 
-			var actorsTab = tabContainer.Get<ButtonWidget>("ACTORS_TAB");
-			actorsTab.IsHighlighted = () => menuType == MenuType.Actors;
-			actorsTab.OnClick = () => { menuType = MenuType.Actors; };
-
-			var tileContainer = widget.Parent.Get<ContainerWidget>("TILE_WIDGETS");
-			tileContainer.IsVisible = () => menuType == MenuType.Tiles;
-
-			var layerContainer = widget.Parent.Get<ContainerWidget>("LAYER_WIDGETS");
-			layerContainer.IsVisible = () => menuType == MenuType.Layers;
-
-			var actorContainer = widget.Parent.Get<ContainerWidget>("ACTOR_WIDGETS");
-			actorContainer.IsVisible = () => menuType == MenuType.Actors;
+			var container = widget.Parent.Get<ContainerWidget>(tabId);
+			container.IsVisible = () => menuType == tabType;
 		}
 	}
 }
