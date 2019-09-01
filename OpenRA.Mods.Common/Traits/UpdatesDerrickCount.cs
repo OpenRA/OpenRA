db0c3b75b616d8d39c267b2b4514ec5e100beb1c
abcdefg30 <alphabet@streber24.de>
2019-09-01 12:43:35 +0200

    Add an "UpdatesDerrickCount" trait

---
+++ OpenRA.Mods.Common/Traits/UpdatesDerrickCount.cs
@@ -0,0 +1,20 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2007-2019 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Common.Traits
+{
+	[Desc("Tag trait for updating the 'Oil Derrick' count economy statistic.")]
+	public class UpdatesDerrickCountInfo : TraitInfo<UpdatesDerrickCount> { }
+
+	public class UpdatesDerrickCount { }
+}
