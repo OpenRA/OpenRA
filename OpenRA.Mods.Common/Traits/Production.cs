fa1ca981ac3fb38b8ef9e85aca373964bf8331da
teinarss <einarsson.tomas@gmail.com>
2019-09-18 18:27:48 +0200

    Production should set SubCellInit

--- OpenRA.Mods.Common/Traits/Production.cs
+++ OpenRA.Mods.Common/Traits/Production.cs
@@ -73,6 +73,7 @@ namespace OpenRA.Mods.Common.Traits
 				td.Add(new LocationInit(exit));
 				td.Add(new CenterPositionInit(spawn));
 				td.Add(new FacingInit(initialFacing));
+				td.Add(new SubCellInit(SubCell.Any));
 				if (exitinfo != null)
 					td.Add(new MoveIntoWorldDelayInit(exitinfo.ExitDelay));
 			}
