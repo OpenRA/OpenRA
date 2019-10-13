e445746f8afbeac1db9c5605f6d42d9e8256fc0a
abcdefg30 <alphabet@streber24.de>
2019-10-13 19:31:17 +0200

    Fix the IgnoreActor check in Parachutable

--- OpenRA.Mods.Common/Traits/Parachutable.cs
+++ OpenRA.Mods.Common/Traits/Parachutable.cs
@@ -102,7 +102,7 @@ namespace OpenRA.Mods.Common.Traits
 			if (positionable.CanEnterCell(cell, self))
 				return;
 
-			if (IgnoreActor != null && self.World.ActorMap.GetActorsAt(cell).Any(a => a != IgnoreActor))
+			if (IgnoreActor != null && !self.World.ActorMap.GetActorsAt(cell).Any(a => a != IgnoreActor))
 				return;
 
 			var onWater = info.WaterTerrainTypes.Contains(self.World.Map.GetTerrainInfo(cell).Type);
