d20182f158bdf2baa91945b437e628a610c71cc3
blackhand1001 <blackhand1001@gmail.com>
2019-10-25 19:46:29 -0400

    Simplify for loop structure now that it only has one check
    Simplify for loop structure now that it only has one check

--- OpenRA.Mods.Common/Traits/BotModules/McvManagerBotModule.cs
+++ OpenRA.Mods.Common/Traits/BotModules/McvManagerBotModule.cs
@@ -202,12 +202,8 @@ namespace OpenRA.Mods.Common.Traits
 					cells = cells.Shuffle(world.LocalRandom);
 
 				foreach (var cell in cells)
-				{
-					if (!world.CanPlaceBuilding(cell + offset, actorInfo, bi, null))
-						continue;
-
-					return cell;
-				}
+					if (world.CanPlaceBuilding(cell + offset, actorInfo, bi, null))
+						return cell;
 
 				return null;
 			};
