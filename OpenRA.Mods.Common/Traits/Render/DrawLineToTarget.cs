OpenRA/OpenRA

commit 96263d47c51d19ed0f03ad3ea88c3d1f8e8da671

Restore target line display for allied players and spectators.
Change behaviour to require the force-display modifier to reduce
visual noise.

OpenRA.Mods.Common/Traits/Render/DrawLineToTarget.cs
@@ -42,23 +42,25 @@ namespace OpenRA.Mods.Common.Traits
 			this.info = info;
 		}
 
-		public void ShowTargetLines(Actor a)
+		public void ShowTargetLines(Actor self)
 		{
-			if (a.IsIdle)
+			// Target lines are only automatically shown for the owning player
+			// Spectators and allies must use the force-display modifier
+			if (self.IsIdle || self.Owner != self.World.LocalPlayer)
 				return;
 
 			// Reset the order line timeout.
 			lifetime = info.Delay;
 		}
 
-		void INotifySelected.Selected(Actor a)
+		void INotifySelected.Selected(Actor self)
 		{
-			ShowTargetLines(a);
+			ShowTargetLines(self);
 		}
 
 		IEnumerable<IRenderable> IRenderAboveShroudWhenSelected.RenderAboveShroud(Actor self, WorldRenderer wr)
 		{
-			if (self.Owner != self.World.LocalPlayer)
+			if (!self.Owner.IsAlliedWith(self.World.LocalPlayer))
 				yield break;
 
 			// Players want to see the lines when in waypoint mode.

commit dbead299fc6231493aed2a3b19dd1d9efd5d57a4

Restore automatic target line display for spectators
This fixes a (voluntary) regression introduced in
https://github.com/OpenRA/OpenRA/pull/16876

In the current release (20190314) observer mode does automatically show
target lines for the selected units when a new order is issued by the
player. This is very convenient as an observer interested in
monitoring the player actions.

The behaviour changed for allied players but also for spectators in
b0e89cf1. While this makes sense in-game in order to not pollute allied
player visions, requiring spectators to hold the shift modifier
constantly to watch out for new actions is counter-productive.

This commit restores the old behaviour.

OpenRA.Mods.Common/Traits/Render/DrawLineToTarget.cs
@@ -130,9 +130,9 @@ namespace OpenRA.Mods.Common.Traits
 	{
 		public static void ShowTargetLines(this Actor self)
 		{
-			// Target lines are only automatically shown for the owning player
-			// Spectators and allies must use the force-display modifier
-			if (self.Owner != self.World.LocalPlayer)
+			// Target lines are only automatically shown for the owning player and spectators.
+			// Allies must use the force-display modifier.
+			if (self.Owner != self.World.LocalPlayer && self.World.LocalPlayer != null)
 				return;
 
 			// Draw after frame end so that all the queueing of activities are done before drawing.