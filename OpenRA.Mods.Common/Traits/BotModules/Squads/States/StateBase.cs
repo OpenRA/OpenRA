02fdbefaa0262254a48ecf904fd06e14981715c3
blackhand1001 <blackhand1001@gmail.com>
2019-11-15 16:44:22 -0500

    Queue the goto Random Building orders
    Queue the goto Random Building orders instead of doing them instantly in case we are finishing an action.

--- OpenRA.Mods.Common/Traits/BotModules/Squads/States/StateBase.cs
+++ OpenRA.Mods.Common/Traits/BotModules/Squads/States/StateBase.cs
@@ -23,7 +23,7 @@ namespace OpenRA.Mods.Common.Traits.BotModules.Squads
 		{
 			var loc = RandomBuildingLocation(squad);
 			foreach (var a in squad.Units)
-				squad.Bot.QueueOrder(new Order("Move", a, Target.FromCell(squad.World, loc), false));
+				squad.Bot.QueueOrder(new Order("Move", a, Target.FromCell(squad.World, loc), true));
 		}
 
 		protected static CPos RandomBuildingLocation(Squad squad)
