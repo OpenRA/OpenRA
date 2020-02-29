2aff0a2266e047d807edfce67ffc2936bb43e4b5
Ivaylo Draganov <dragunoff@gmail.com>
2020-02-29 22:39:14 +0200

    Add transcription support to speech notifications

--- OpenRA.Game/Sound/Sound.cs
+++ OpenRA.Game/Sound/Sound.cs
@@ -408,6 +408,9 @@ namespace OpenRA
 
 					currentSounds[id] = sound;
 				}
+
+				if (!string.IsNullOrEmpty(pool.Transcription))
+					Game.AddTranscribedChatLine(pool.Transcription);
 			}
 
 			return true;
