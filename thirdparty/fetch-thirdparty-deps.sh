70fa14a33347c763c10008302bac091b69f4b9cb
Matthias Mailänder <matthias@mailaender.name>
2020-03-08 12:20:58 +0100

    Fixed a case sensitive related error on Mac.

--- thirdparty/fetch-thirdparty-deps.sh
+++ thirdparty/fetch-thirdparty-deps.sh
@@ -45,7 +45,6 @@ fi
 if [ ! -f Open.Nat.dll ]; then
 	echo "Fetching Open.Nat from NuGet"
 	../noget.sh Open.Nat 2.1.0
-	if [ -d ./Open.NAT ]; then mv Open.NAT Open.Nat; fi
 	cp ./Open.Nat/lib/net45/Open.Nat.dll .
 	rm -rf Open.Nat
 fi
