ba7e1319ac8dbeb650906f480d6324f13293a087
abcdefg30 <alphabet@streber24.de>
2020-03-23 20:23:45 +0100

    Remove DisableWindowsRenderThread and use threaded renderering for non windowed mode

--- OpenRA.Platforms.Default/Sdl2PlatformWindow.cs
+++ OpenRA.Platforms.Default/Sdl2PlatformWindow.cs
@@ -288,9 +288,8 @@ namespace OpenRA.Platforms.Default
 			// Run graphics rendering on a dedicated thread.
 			// The calling thread will then have more time to process other tasks, since rendering happens in parallel.
 			// If the calling thread is the main game thread, this means it can run more logic and render ticks.
-			// This is disabled on Windows because it breaks the ability to minimize/restore the window from the taskbar for reasons that we dont understand.
-			var threadedRenderer = Platform.CurrentPlatform != PlatformType.Windows || !Game.Settings.Graphics.DisableWindowsRenderThread;
-			if (!threadedRenderer)
+			// This is disabled when running in windowed mode on Windows because it breaks the ability to minimize/restore the window.
+			if (Platform.CurrentPlatform == PlatformType.Windows && windowMode == WindowMode.Windowed)
 			{
 				var ctx = new Sdl2GraphicsContext(this);
 				ctx.InitializeOpenGL();
