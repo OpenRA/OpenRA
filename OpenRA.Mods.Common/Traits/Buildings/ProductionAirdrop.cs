90316d2ac4dad660f7258199ba9c34e6cbf1da58
Pavel Penev <penev92@gmail.com>
2020-03-23 02:12:50 +0200

    Added configurable delivery actor exits to ProductionAirdrop
    # Conflicts:
    #	OpenRA.Mods.Common/Traits/Buildings/ProductionAirdrop.cs

--- OpenRA.Mods.Common/Traits/Buildings/ProductionAirdrop.cs
+++ OpenRA.Mods.Common/Traits/Buildings/ProductionAirdrop.cs
@@ -20,6 +20,8 @@ namespace OpenRA.Mods.Common.Traits
 {
 	public enum EntryType { Fixed, PlayerSpawnClosestEdge, DropSiteClosestEdge }
 
+	public enum ExitType { OppositeToEntry, StraightAhead, SameAsEntry, Closest }
+
 	[Desc("Deliver the unit in production via skylift.")]
 	public class ProductionAirdropInfo : ProductionInfo
 	{
@@ -34,8 +36,14 @@ namespace OpenRA.Mods.Common.Traits
 		[Desc("The delivery aircraft spawn and entry behaviour.")]
 		public readonly EntryType EntryType = EntryType.Fixed;
 
+		[Desc("The delivery aircraft map exit behaviour.")]
+		public readonly ExitType ExitType = ExitType.OppositeToEntry;
+
 		[Desc("Offset relative to the automatically calculated spawn point.")]
 		public readonly CVec SpawnOffset = CVec.Zero;
+
+		[Desc("Offset relative to the automatically calculated exit point.")]
+		public readonly CVec ExitOffset = CVec.Zero;
 
 		[Desc("Direction the aircraft should face to land. A value of -1 would mean to ignore facing and just land.")]
 		public readonly int LandingFacing = -1;
@@ -95,8 +103,12 @@ namespace OpenRA.Mods.Common.Traits
 					Game.Sound.PlayNotification(self.World.Map.Rules, self.Owner, "Speech", info.ReadyAudio, self.Owner.Faction.InternalName);
 				}));
 
-				actor.QueueActivity(new FlyOffMap(actor, Target.FromCell(w, deliveryPath.ExitPosition)));
-				actor.QueueActivity(new RemoveSelf());
+				actor.QueueActivity(new CallFunc(() =>
+				{
+					var exitPosition = deliveryPath.CalculateExitPosition(actor);
+					actor.QueueActivity(new FlyOffMap(actor, Target.FromCell(w, exitPosition)));
+					actor.QueueActivity(new RemoveSelf());
+				}));
 			});
 
 			return true;
@@ -109,9 +121,8 @@ namespace OpenRA.Mods.Common.Traits
 			var bounds = map.Bounds;
 			var mpStart = owner.World.WorldActor.TraitOrDefault<MPStartLocations>();
 
-			CPos startPos;
-			CPos endPos;
-			int spawnFacing;
+			var startPos = CPos.Zero;
+			var spawnFacing = 0;
 
 			switch (info.EntryType)
 			{
@@ -120,11 +131,9 @@ namespace OpenRA.Mods.Common.Traits
 					// This makes the production timing independent of spawnpoint
 					var loc = self.Location.ToMPos(map);
 					startPos = new MPos(loc.U + map.Bounds.Width, loc.V).ToCPos(map);
-					endPos = new MPos(map.Bounds.Left, loc.V).ToCPos(map);
 
 					spawnFacing = GetSpawnFacing(self.Location, startPos);
-
-					return new DeliveryActorPathInfo(startPos, endPos, spawnFacing, info.LandingFacing);
+					break;
 
 				case EntryType.PlayerSpawnClosestEdge:
 					if (mpStart == null)
@@ -134,11 +143,9 @@ namespace OpenRA.Mods.Common.Traits
 					var center = new MPos(bounds.Left + bounds.Width / 2, bounds.Top + bounds.Height / 2).ToCPos(map);
 					var spawnVec = spawn - center;
 					startPos = spawn + spawnVec * (Exts.ISqrt((bounds.Height * bounds.Height + bounds.Width * bounds.Width) / (4 * spawnVec.LengthSquared)));
-					endPos = startPos;
 
 					spawnFacing = GetSpawnFacing(self.Location, startPos);
-
-					return new DeliveryActorPathInfo(startPos, endPos, spawnFacing, info.LandingFacing);
+					break;
 
 				case EntryType.DropSiteClosestEdge:
 					startPos = self.World.Map.ChooseClosestEdgeCell(self.Location);
@@ -146,14 +153,17 @@ namespace OpenRA.Mods.Common.Traits
 					spawnFacing = GetSpawnFacing(self.Location, startPos);
 
 					bounds = self.World.Map.Bounds;
-					if ((info.SpawnOffset.X != 0 && startPos.X != bounds.X && startPos.X != bounds.X + bounds.Width)
-						|| (info.SpawnOffset.Y != 0 && startPos.Y != bounds.Y && startPos.Y != bounds.Y + bounds.Height))
-						startPos += info.SpawnOffset;
+					if (info.SpawnOffset.X != 0 && startPos.X != bounds.X && startPos.X != bounds.X + bounds.Width)
+						startPos += new CVec(info.SpawnOffset.X, 0);
 
-					return new DeliveryActorPathInfo(startPos, startPos, spawnFacing, info.LandingFacing);
+					if (info.SpawnOffset.Y != 0 && startPos.Y != bounds.Y && startPos.Y != bounds.Y + bounds.Height)
+						startPos += new CVec(0, info.SpawnOffset.Y);
+					break;
 			}
 
-			throw new ArgumentOutOfRangeException("info.EntryType", info.EntryType, "Unsupported value for delivery actor entry type!");
+			var exitPositionFunc = GetExitPositionFunc(self, startPos, spawnFacing);
+
+			return new DeliveryActorPathInfo(startPos, spawnFacing, info.LandingFacing, exitPositionFunc);
 		}
 
 		static int GetSpawnFacing(CPos targetLocation, CPos spawnLocation)
@@ -163,19 +173,71 @@ namespace OpenRA.Mods.Common.Traits
 			return spawnDirection.Yaw.Facing;
 		}
 
+		Func<Actor, CPos> GetExitPositionFunc(Actor self, CPos startPos, int spawnFacing)
+		{
+			Func<Actor, CPos> exitPositionFunc = actor => CPos.Zero;
+			switch (info.ExitType)
+			{
+				case ExitType.OppositeToEntry:
+					exitPositionFunc = actor =>
+					{
+						var rotation = WRot.FromFacing(spawnFacing);
+						var delta = new WVec(0, -1024, 0).Rotate(rotation);
+						var finishEdge = actor.CenterPosition + self.World.Map.DistanceToEdge(actor.CenterPosition, delta).Length * delta / 1024;
+						return new CPos(finishEdge.X / 1024, finishEdge.Y / 1024, 0);
+					};
+
+					break;
+
+				case ExitType.StraightAhead:
+					exitPositionFunc = actor =>
+					{
+						var rotation = WRot.FromFacing(actor.Orientation.Yaw.Facing);
+						var delta = new WVec(0, -1024, 0).Rotate(rotation);
+						var finishEdge = actor.CenterPosition + self.World.Map.DistanceToEdge(actor.CenterPosition, delta).Length * delta / 1024;
+						return new CPos(finishEdge.X / 1024, finishEdge.Y / 1024, 0);
+					};
+
+					break;
+
+				case ExitType.SameAsEntry:
+					exitPositionFunc = actor => startPos;
+					break;
+
+				case ExitType.Closest:
+					exitPositionFunc = actor =>
+					{
+						// By the time the func gets executed the map bounds could have changed, so fetching them fresh.
+						var mapBounds = self.World.Map.Bounds;
+						var exitCell = self.World.Map.ChooseClosestEdgeCell(actor.Location);
+						if (info.ExitOffset.X != 0 && exitCell.X != mapBounds.X && exitCell.X != mapBounds.X + mapBounds.Width)
+							exitCell += new CVec(info.ExitOffset.X, 0);
+
+						if (info.ExitOffset.Y != 0 && exitCell.Y != mapBounds.Y && exitCell.Y != mapBounds.Y + mapBounds.Height)
+							exitCell += new CVec(0, info.ExitOffset.Y);
+
+						return exitCell;
+					};
+
+					break;
+			}
+
+			return exitPositionFunc;
+		}
+
 		class DeliveryActorPathInfo
 		{
 			public readonly CPos EntryPosition;
-			public readonly CPos ExitPosition;
 			public readonly int SpawnFacing;
 			public readonly int LandingFacing;
+			public readonly Func<Actor, CPos> CalculateExitPosition;
 
-			public DeliveryActorPathInfo(CPos entryPosition, CPos exitPosition, int spawnFacing, int landingFacing)
+			public DeliveryActorPathInfo(CPos entryPosition, int spawnFacing, int landingFacing, Func<Actor, CPos> calculateExitPosition)
 			{
 				EntryPosition = entryPosition;
-				ExitPosition = exitPosition;
 				SpawnFacing = spawnFacing;
 				LandingFacing = landingFacing;
+				CalculateExitPosition = calculateExitPosition;
 			}
 		}
 	}
