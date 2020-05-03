eb9f67049184aac0a8455ff64799521035dddacc
Pavel Penev <penev92@gmail.com>
2020-05-03 04:05:31 +0300

    Added back the DescAttributes on projectiles' Inaccuracy fields

--- OpenRA.Mods.Common/Projectiles/Missile.cs
+++ OpenRA.Mods.Common/Projectiles/Missile.cs
@@ -71,6 +71,7 @@ namespace OpenRA.Mods.Common.Projectiles
 		[Desc("Width of projectile (used for finding blocking actors).")]
 		public readonly WDist Width = new WDist(1);
 
+		[Desc("The maximum/constant/incremental inaccuracy used in conjunction with the InaccuracyType property.")]
 		public readonly WDist Inaccuracy = WDist.Zero;
 
 		[Desc("Controls the way inaccuracy is calculated. Possible values are 'Maximum' - scale from 0 to max with range, 'PerCellIncrement' - scale from 0 with range and 'Absolute' - use set value regardless of range.")]
--- OpenRA.Mods.Common/Projectiles/Railgun.cs
+++ OpenRA.Mods.Common/Projectiles/Railgun.cs
@@ -25,6 +25,7 @@ namespace OpenRA.Mods.Common.Projectiles
 		[Desc("Damage all units hit by the beam instead of just the target?")]
 		public readonly bool DamageActorsInLine = false;
 
+		[Desc("The maximum/constant/incremental inaccuracy used in conjunction with the InaccuracyType property.")]
 		public readonly WDist Inaccuracy = WDist.Zero;
 
 		[Desc("Controls the way inaccuracy is calculated. Possible values are 'Maximum' - scale from 0 to max with range, 'PerCellIncrement' - scale from 0 with range and 'Absolute' - use set value regardless of range.")]
