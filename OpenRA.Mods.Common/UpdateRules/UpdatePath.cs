10f23bc24e5f71957526b4e275f486edf1e614f4
Matthias Mailänder <matthias@mailaender.name>
2020-05-11 20:30:44 +0200

    Add upgrade rule to convert ranges to footprints.

--- OpenRA.Mods.Common/UpdateRules/UpdatePath.cs
+++ OpenRA.Mods.Common/UpdateRules/UpdatePath.cs
@@ -63,6 +63,7 @@ namespace OpenRA.Mods.Common.UpdateRules
 				new RenameWithNukeLaunch(),
 				new SpawnActorPowerDefaultEffect(),
 				new RemoveConditionManager(),
+				new ConvertSupportPowerRangesToFootprint(),
 			})
 		};
 
