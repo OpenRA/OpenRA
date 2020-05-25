7a0e55a02a30e5e587e7e10c5b32a12db8abfe0d
abcdefg30 <alphabet@streber24.de>
2020-05-25 12:55:44 +0200

    Restore trailing whitespaces to windows batch scripts

--- launch-game.cmd
+++ launch-game.cmd
@@ -5,7 +5,7 @@ for %%x in (%*) do (
 )
 
 :choosemod
-set /P mod=Select mod (ra, cnc, d2k, ts) or --exit:
+set /P mod="Select mod (ra, cnc, d2k, ts) or --exit: "
 if /I "%mod%" EQU "--exit" (exit /b)
 if "%mod%" EQU "ra" (goto launchmod)
 if "%mod%" EQU "cnc" (goto launchmod)
