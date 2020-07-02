bd7c9c3557bf1ad19b28455d5431c58b0a78beed
abcdefg30 <alphabet@streber24.de>
2020-07-02 23:54:57 +0200

    Use tabs as indentation consistently

--- packaging/package-all.sh
+++ packaging/package-all.sh
@@ -2,8 +2,8 @@
 # OpenRA master packaging script
 
 if [ $# -ne "2" ]; then
-    echo "Usage: ${0##*/} version outputdir."
-    exit 1
+	echo "Usage: ${0##*/} version outputdir."
+	exit 1
 fi
 
 export GIT_TAG="$1"
@@ -14,15 +14,15 @@ cd "${0%/*}" || exit 1
 
 #build packages using a subshell so directory changes do not persist beyond the function
 function build_package() (
-    function on_build() {
-        echo "$1 package build failed." 1>&2
-    }
-    #trap function executes on any error in the following commands
-    trap "on_build $1" ERR
-    set -e
-    echo "Building $1 package(s)."
-    cd "$1"
-    ./buildpackage.sh "${GIT_TAG}" "${BUILD_OUTPUT_DIR}"
+	function on_build() {
+		echo "$1 package build failed." 1>&2
+	}
+	#trap function executes on any error in the following commands
+	trap "on_build $1" ERR
+	set -e
+	echo "Building $1 package(s)."
+	cd "$1"
+	./buildpackage.sh "${GIT_TAG}" "${BUILD_OUTPUT_DIR}"
 )
 
 #exit on any non-zero exited (failed) command
