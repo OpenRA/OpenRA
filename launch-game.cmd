1f6669e360de059bb9e76953a0e1ac9127a57a84
abcdefg30 <alphabet@streber24.de>
2020-07-02 23:56:28 +0200

    Correct the support folder location in the crash dialog

--- launch-game.cmd
+++ launch-game.cmd
@@ -25,10 +25,15 @@ OpenRA.Game.exe %*
 :end
 if %errorlevel% neq 0 goto crashdialog
 exit /b
+
 :crashdialog
+set logs=%AppData%\OpenRA\Logs
+if exist %USERPROFILE%\Documents\OpenRA\Logs (set logs=%USERPROFILE%\Documents\OpenRA\Logs)
+if exist Support\Logs (set logs=%cd%\Support\Logs)
+
 echo ----------------------------------------
 echo OpenRA has encountered a fatal error.
-echo   * Log Files are available in Documents\OpenRA\Logs
+echo   * Log Files are available in %logs%
 echo   * FAQ is available at https://github.com/OpenRA/OpenRA/wiki/FAQ
 echo ----------------------------------------
 pause
