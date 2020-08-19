444077799b4bd31a41efd956c2d119b6290678aa
teinarss <einarsson.tomas@gmail.com>
2020-08-19 20:47:35 +0200

    Make Target readonly

--- OpenRA.Game/Traits/Target.cs
+++ OpenRA.Game/Traits/Target.cs
@@ -16,7 +16,7 @@ using System.Linq;
 namespace OpenRA.Traits
 {
 	public enum TargetType : byte { Invalid, Actor, Terrain, FrozenActor }
-	public struct Target
+	public readonly struct Target
 	{
 		public static readonly Target[] None = { };
 		public static readonly Target Invalid = default(Target);
