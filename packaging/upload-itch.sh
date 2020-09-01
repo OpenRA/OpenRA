86a714a588c0c0b0db6a854aeea6996f694e1361
Matthias Mailänder <matthias@mailaender.name>
2020-09-01 20:16:08 +0200

    Enable itch app auto updates on Windows.

--- packaging/upload-itch.sh
+++ packaging/upload-itch.sh
@@ -29,10 +29,12 @@ chmod +x butler
 
 ./butler -V
 ./butler login
-./butler push "${BUILD_OUTPUT_DIR}/OpenRA-${GIT_TAG}-x64.exe" "openra/openra:win" --userversion-file ../VERSION
+cp ${BUILD_OUTPUT_DIR}/OpenRA-${GIT_TAG}-x64-winportable.zip OpenRA-${GIT_TAG}-x64-win-itch-portable.zip
+zip -u OpenRA-${GIT_TAG}-x64-win-itch-portable.zip .itch.toml
+./butler push "OpenRA-${GIT_TAG}-x64-win-itch-portable.zip" "openra/openra:win" --userversion-file ../VERSION
 ./butler push --fix-permissions "${BUILD_OUTPUT_DIR}/OpenRA-${GIT_TAG}.dmg" "openra/openra:osx" --userversion-file ../VERSION
 ./butler push --fix-permissions "${BUILD_OUTPUT_DIR}/OpenRA-Dune-2000-x86_64.AppImage" "openra/openra:linux-d2k" --userversion-file ../VERSION
 ./butler push --fix-permissions "${BUILD_OUTPUT_DIR}/OpenRA-Red-Alert-x86_64.AppImage" "openra/openra:linux-ra" --userversion-file ../VERSION
 ./butler push --fix-permissions "${BUILD_OUTPUT_DIR}/OpenRA-Tiberian-Dawn-x86_64.AppImage" "openra/openra:linux-td" --userversion-file ../VERSION
 
-rm butler butler-linux-amd64.zip
+rm butler butler-linux-amd64.zip 7z.so libc7zip.so
