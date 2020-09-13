1073a7124f54c4a38d9eb731a58d6cdc4fc55802
Matthias Mailänder <matthias@mailaender.name>
2020-09-13 10:43:54 +0200

    Remove an unused field from TakeOff.cs

--- OpenRA.Mods.Common/Activities/Air/TakeOff.cs
+++ OpenRA.Mods.Common/Activities/Air/TakeOff.cs
@@ -17,12 +17,10 @@ namespace OpenRA.Mods.Common.Activities
 	public class TakeOff : Activity
 	{
 		readonly Aircraft aircraft;
-		readonly IMove move;
 
 		public TakeOff(Actor self)
 		{
 			aircraft = self.Trait<Aircraft>();
-			move = self.Trait<IMove>();
 		}
 
 		protected override void OnFirstRun(Actor self)
