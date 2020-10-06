6bb7ab0f97d643be726703ec35131eb1b39420b1
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2020-10-06 10:35:04 +0300

    Add GrantConditionOnCombatantOwner.

---
+++ OpenRA.Mods.Common/Traits/Conditions/GrantConditionOnCombatantOwner.cs
@@ -0,0 +1,53 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2007-2020 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Common.Traits
+{
+	[Desc("Grants a condition if the owner is a combatant.")]
+	public class GrantConditionOnCombatantOwnerInfo : TraitInfo
+	{
+		[FieldLoader.Require]
+		[GrantedConditionReference]
+		[Desc("The condition to grant.")]
+		public readonly string Condition = null;
+
+		public override object Create(ActorInitializer init) { return new GrantConditionOnCombatantOwner(init.Self, this); }
+	}
+
+	public class GrantConditionOnCombatantOwner : INotifyCreated, INotifyOwnerChanged
+	{
+		readonly GrantConditionOnCombatantOwnerInfo info;
+
+		int conditionToken = Actor.InvalidConditionToken;
+
+		public GrantConditionOnCombatantOwner(Actor self, GrantConditionOnCombatantOwnerInfo info)
+		{
+			this.info = info;
+		}
+
+		void INotifyCreated.Created(Actor self)
+		{
+			if (!self.Owner.NonCombatant)
+				conditionToken = self.GrantCondition(info.Condition);
+		}
+
+		void INotifyOwnerChanged.OnOwnerChanged(Actor self, Player oldOwner, Player newOwner)
+		{
+			if (conditionToken != Actor.InvalidConditionToken)
+				conditionToken = self.RevokeCondition(conditionToken);
+
+			if (!newOwner.NonCombatant)
+				conditionToken = self.GrantCondition(info.Condition);
+		}
+	}
+}
