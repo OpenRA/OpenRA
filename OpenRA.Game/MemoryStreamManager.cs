f4210cdffaf664e1e53165ba6e625d3b068f25e2
Leonardo-Ferreira <index.xp@gmail.com>
2020-10-14 17:36:32 -0300

    New singleton class to handle MemoryStream distrib Added package ref

---
+++ OpenRA.Game/MemoryStreamManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Microsoft.IO;
+
+namespace OpenRA
+{
+	public class MemoryStreamManager
+	{
+		static RecyclableMemoryStreamManager singletonManager;
+		MemoryStreamManager()
+		{
+		}
+
+		public static MemoryStream GetMemoryStream()
+		{
+			if (singletonManager == null)
+			{
+				singletonManager = new RecyclableMemoryStreamManager();
+			}
+
+			return singletonManager.GetStream();
+		}
+
+		public static MemoryStream GetMemoryStream(byte[] buffer)
+		{
+			if (singletonManager == null)
+			{
+				singletonManager = new RecyclableMemoryStreamManager();
+			}
+
+			return singletonManager.GetStream(buffer);
+		}
+	}
+}
