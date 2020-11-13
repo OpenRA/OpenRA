bf14a4ce80a1421f537bbadf466a6339a2d83c78
abcdefg30 <alphabet@streber24.de>
2020-11-13 19:45:21 +0100

    Remove an unused variable from LoadComplete

--- OpenRA.Game/World.cs
+++ OpenRA.Game/World.cs
@@ -280,7 +280,6 @@ namespace OpenRA
 					using (new PerfTimer(iwl.GetType().Name + ".WorldLoaded"))
 						iwl.WorldLoaded(this, wr);
 
-			var assignSpawnLocations = WorldActor.TraitOrDefault<IAssignSpawnPoints>();
 			gameInfo.StartTimeUtc = DateTime.UtcNow;
 			foreach (var player in Players)
 				gameInfo.AddPlayer(player, OrderManager.LobbyInfo);
