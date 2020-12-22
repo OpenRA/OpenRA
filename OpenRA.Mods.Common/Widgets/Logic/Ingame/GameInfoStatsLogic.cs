1852980c6785ba4e3a930d8a0e9693573698461b
Ivaylo Draganov <dragunoff@gmail.com>
2020-12-22 21:56:19 +0200

    Add player random faction info in the objectives panel

--- OpenRA.Mods.Common/Widgets/Logic/Ingame/GameInfoStatsLogic.cs
+++ OpenRA.Mods.Common/Widgets/Logic/Ingame/GameInfoStatsLogic.cs
@@ -101,7 +101,8 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 					if (player == null || player.RelationshipWith(pp) == PlayerRelationship.Ally || player.WinState != WinState.Undefined)
 					{
 						flag.GetImageName = () => pp.Faction.InternalName;
-						item.Get<LabelWidget>("FACTION").GetText = () => pp.Faction.Name;
+						var factionName = pp.Faction.Name != pp.DisplayFaction.Name ? "{0} ({1})".F(pp.DisplayFaction.Name, pp.Faction.Name) : pp.Faction.Name;
+						item.Get<LabelWidget>("FACTION").GetText = () => factionName;
 					}
 					else
 					{
