dde3d32ce20359561b0e26a50e0ef555dce3e14c
Paul Chote <pchote@users.noreply.github.com>
2021-01-01 23:49:45 +0000

    Fix map editor inverting preview color channels.

--- OpenRA.Game/Map/Map.cs
+++ OpenRA.Game/Map/Map.cs
@@ -779,7 +779,7 @@ namespace OpenRA
 				}
 			}
 
-			var png = new Png(minimapData, SpriteFrameType.Bgra32, bitmapWidth, height);
+			var png = new Png(minimapData, SpriteFrameType.Rgba32, bitmapWidth, height);
 			return png.Save();
 		}
 
