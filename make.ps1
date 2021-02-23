9486f285cf324be1112d133e827f27e194b8e988
pizzaoverhead <pizzaoverhead@gmail.com>
2021-02-23 14:12:32 +0000

    Fix TLS error on Win7.

--- make.ps1
+++ make.ps1
@@ -24,6 +24,7 @@ function All-Command
 	{
 		echo "Downloading IP2Location GeoIP database."
 		$target = Join-Path $pwd.ToString() "IP2LOCATION-LITE-DB1.IPV6.BIN.ZIP"
+		[Net.ServicePointManager]::SecurityProtocol = 'Tls12'
 		(New-Object System.Net.WebClient).DownloadFile("https://github.com/OpenRA/GeoIP-Database/releases/download/monthly/IP2LOCATION-LITE-DB1.IPV6.BIN.ZIP", $target)
 	}
 }
