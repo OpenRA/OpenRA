fcfca869514e486bdc8f30aabac6483e5effe41f
filip12h <martello2311@gmail.com>
2021-06-23 17:29:26 +0200

    zenity dropdown replaced with radiobuttons

--- launch-game.sh
+++ launch-game.sh
@@ -17,12 +17,12 @@ if [ z"${*#*Game.Mod}" = z"$*" ]
 then
 	if command -v zenity > /dev/null
 	then
-		TITLE=$(zenity --forms --add-combo="" --combo-values="Red Alert|Tiberian Dawn|Dune 2000|Tiberian Sun" --text "Select mod" --title="" || echo "cancel")
-		if [ "$TITLE" = "cancel" ]; then exit 0
-		elif [ "$TITLE" = "Tiberian Dawn" ]; then MODARG='Game.Mod=cnc'
+		TITLE=$(zenity --title="" --list --radiolist --text 'Select game mod:' --column 'Select' --column 'Game mod' TRUE 'Red Alert' FALSE 'Tiberian Dawn' FALSE 'Dune 2000' FALSE 'Tiberian Sun' --height=240 || echo "cancel")
+		if [ "$TITLE" = "Tiberian Dawn" ]; then MODARG='Game.Mod=cnc'
 		elif [ "$TITLE" = "Dune 2000" ]; then MODARG='Game.Mod=d2k'
 		elif [ "$TITLE" = "Tiberian Sun" ]; then MODARG='Game.Mod=ts'
-		else MODARG='Game.Mod=ra'
+		elif [ "$TITLE" = "Red Alert" ]; then MODARG='Game.Mod=ra'
+		else exit 0
 		fi
 	else
 		echo "Please provide the Game.Mod=\$MOD argument (possible \$MOD values: ra, cnc, d2k, ts)"
