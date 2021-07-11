3e62952043f560a9002dbe08ba6a9706402685a2
MustaphaTR <mustafaoyunda37@gmail.com>
2021-07-11 13:58:43 +0200

    Add a condition per tileset.

---
+++ OpenRA.Mods.Common/Traits/Conditions/GrantConditionOnTileSet.cs
@@ -0,0 +1,46 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2021 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System.Linq;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Common.Traits
+{
+	public class GrantConditionOnTileSetInfo : TraitInfo
+	{
+		[FieldLoader.Require]
+		[GrantedConditionReference]
+		[Desc("Condition to grant.")]
+		public readonly string Condition = null;
+
+		[FieldLoader.Require]
+		[Desc("Tile set IDs to trigger the condition.")]
+		public readonly string[] TileSets = { };
+
+		public override object Create(ActorInitializer init) { return new GrantConditionOnTileSet(this); }
+	}
+
+	public class GrantConditionOnTileSet : INotifyCreated
+	{
+		readonly GrantConditionOnTileSetInfo info;
+
+		public GrantConditionOnTileSet(GrantConditionOnTileSetInfo info)
+		{
+			this.info = info;
+		}
+
+		void INotifyCreated.Created(Actor self)
+		{
+			if (info.TileSets.Contains(self.World.Map.Tileset))
+				self.GrantCondition(info.Condition);
+		}
+	}
+}
