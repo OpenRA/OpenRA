1ec1320d22f8536c0d766a4f9921ee4ad4443edc
abcdefg30 <alphabet@streber24.de>
2021-07-19 22:47:00 +0200

    Defer rollover checks while generating selection decorations

--- OpenRA.Mods.Common/Traits/World/Selection.cs
+++ OpenRA.Mods.Common/Traits/World/Selection.cs
@@ -19,7 +19,7 @@ namespace OpenRA.Mods.Common.Traits
 {
 	public class SelectionInfo : TraitInfo
 	{
-		public override object Create(ActorInitializer init) { return new Selection(this); }
+		public override object Create(ActorInitializer init) { return new Selection(); }
 	}
 
 	[TraitLocation(SystemActors.World | SystemActors.EditorWorld)]
@@ -29,12 +29,10 @@ namespace OpenRA.Mods.Common.Traits
 		public IEnumerable<Actor> Actors => actors;
 
 		readonly HashSet<Actor> actors = new HashSet<Actor>();
+		readonly List<Actor> rolloverActors = new List<Actor>();
 		World world;
-		IEnumerable<Actor> rolloverActors;
 
 		INotifySelection[] worldNotifySelection;
-
-		public Selection(SelectionInfo info) { }
 
 		void INotifyCreated.Created(Actor self)
 		{
@@ -157,12 +155,13 @@ namespace OpenRA.Mods.Common.Traits
 
 		public void SetRollover(IEnumerable<Actor> rollover)
 		{
-			rolloverActors = rollover;
+			rolloverActors.Clear();
+			rolloverActors.AddRange(rollover);
 		}
 
 		public bool RolloverContains(Actor a)
 		{
-			return rolloverActors != null && rolloverActors.Contains(a);
+			return rolloverActors.Contains(a);
 		}
 
 		void ITick.Tick(Actor self)
