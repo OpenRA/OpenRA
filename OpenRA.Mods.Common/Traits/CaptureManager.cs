e8bae2e50af2311433e9f097d43ea82c99584aa4
Vapre <anvilvapre@users.noreply.github.com>
2021-08-19 19:56:20 +0200

    Crash on capture fix. #19482.

--- OpenRA.Mods.Common/Traits/CaptureManager.cs
+++ OpenRA.Mods.Common/Traits/CaptureManager.cs
@@ -254,20 +254,25 @@ namespace OpenRA.Mods.Common.Traits
 			foreach (var w in progressWatchers)
 				w.Update(self, self, target, 0, 0);
 
-			foreach (var w in targetManager.progressWatchers)
-				w.Update(target, self, target, 0, 0);
+			if (targetManager != null)
+				foreach (var w in targetManager.progressWatchers)
+					w.Update(target, self, target, 0, 0);
 
 			if (capturingToken != Actor.InvalidConditionToken)
 				capturingToken = self.RevokeCondition(capturingToken);
 
-			if (targetManager.beingCapturedToken != Actor.InvalidConditionToken)
-				targetManager.beingCapturedToken = target.RevokeCondition(targetManager.beingCapturedToken);
+			if (targetManager != null)
+			{
+				if (targetManager.beingCapturedToken != Actor.InvalidConditionToken)
+					targetManager.beingCapturedToken = target.RevokeCondition(targetManager.beingCapturedToken);
+
+				targetManager.currentCaptors.Remove(self);
+			}
 
 			currentTarget = null;
 			currentTargetManager = null;
 			currentTargetDelay = 0;
 			enteringCurrentTarget = false;
-			targetManager.currentCaptors.Remove(self);
 		}
 
 		void ITick.Tick(Actor self)
