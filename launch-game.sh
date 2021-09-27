c7a092b974cef6ceb7e8b7b50f712b6a3be77302
Ivaylo Draganov <dragunoff@gmail.com>
2021-09-27 18:27:35 +0300

    Replace zenity select dropdown with a list

--- launch-game.sh
+++ launch-game.sh
@@ -17,12 +17,12 @@ if [ z"${*#*Game.Mod}" = z"$*" ]
 then
 	if command -v zenity > /dev/null
 	then
-		TITLE=$(zenity --forms --add-combo="" --combo-values="Red Alert|Tiberian Dawn|Dune 2000|Tiberian Sun" --text "Select mod" --title="" || echo "cancel")
-		if [ "$TITLE" = "cancel" ]; then exit 0
-		elif [ "$TITLE" = "Tiberian Dawn" ]; then MODARG='Game.Mod=cnc'
+		TITLE=$(zenity --title='Launch OpenRA' --list --hide-header --text 'Select game mod:' --column 'Game mod' 'Red Alert' 'Tiberian Dawn' 'Dune 2000' 'Tiberian Sun' || echo "cancel")
+		if [ "$TITLE" = "Tiberian Dawn" ]; then MODARG='Game.Mod=cnc'
 		elif [ "$TITLE" = "Dune 2000" ]; then MODARG='Game.Mod=d2k'
 		elif [ "$TITLE" = "Tiberian Sun" ]; then MODARG='Game.Mod=ts'
-		else MODARG='Game.Mod=ra'
+		elif [ "$TITLE" = "Red Alert" ]; then MODARG='Game.Mod=ra'
+		else exit 0
 		fi
 	else
 		echo "Please provide the Game.Mod=\$MOD argument (possible \$MOD values: ra, cnc, d2k, ts)"
