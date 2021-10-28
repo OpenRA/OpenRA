05420ab5ce12c4b07568eb107acbc7f391ef64cb
abcdefg30 <alphabet@streber24.de>
2021-10-28 13:30:08 +0200

    Fix the Windows packing script not working with wget only

--- packaging/windows/buildpackage.sh
+++ packaging/windows/buildpackage.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-command -v curl >/dev/null 2>&1 || { echo >&2 "Windows packaging requires curl."; exit 1; }
+command -v curl >/dev/null 2>&1 || command -v wget > /dev/null 2>&1 || { echo >&2 "Windows packaging requires curl or wget."; exit 1; }
 command -v makensis >/dev/null 2>&1 || { echo >&2 "Windows packaging requires makensis."; exit 1; }
 command -v convert >/dev/null 2>&1 || { echo >&2 "Windows packaging requires ImageMagick."; exit 1; }
 command -v python3 >/dev/null 2>&1 || { echo >&2 "Windows packaging requires python 3."; exit 1; }
