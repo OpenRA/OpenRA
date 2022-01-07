af07aee8b79d60f4a8e014c5cab8c4f06e86e99a
abcdefg30 <alphabet@streber24.de>
2022-01-07 02:36:27 +0100

    Remove AttackFrontal's FacingTolerance and define it explicitly in rules

--- OpenRA.Mods.Common/Traits/Attack/AttackFrontal.cs
+++ OpenRA.Mods.Common/Traits/Attack/AttackFrontal.cs
@@ -18,9 +18,6 @@ namespace OpenRA.Mods.Common.Traits
 	[Desc("Unit got to face the target")]
 	public class AttackFrontalInfo : AttackBaseInfo, Requires<IFacingInfo>
 	{
-		[Desc("Tolerance for attack angle. Range [0, 512], 512 covers 360 degrees.")]
-		public new readonly WAngle FacingTolerance = WAngle.Zero;
-
 		public override object Create(ActorInitializer init) { return new AttackFrontal(init.Self, this); }
 	}
 
