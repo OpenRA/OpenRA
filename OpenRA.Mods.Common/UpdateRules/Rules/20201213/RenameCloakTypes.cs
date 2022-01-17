6c33d47ef3cbd26ee08e30bb43c64a7a3cbe7508
dnqbob <dnqbob@outlook.com>
2022-01-17 23:05:40 +0800

    Update rule on renaming 'CloakTypes' to 'DetectionTypes'

---
+++ OpenRA.Mods.Common/UpdateRules/Rules/20201213/RenameCloakTypes.cs
@@ -0,0 +1,33 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2007-2022 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System.Collections.Generic;
+
+namespace OpenRA.Mods.Common.UpdateRules.Rules
+{
+	public class RenameCloakTypes : UpdateRule
+	{
+		public override string Name => "Rename 'CloakTypes' to 'DetectionTypes'";
+
+		public override string Description => "Rename 'CloakTypes' to 'DetectionTypes' in order to make it clearer as well as make space for 'CloakType' in Cloak";
+
+		public override IEnumerable<string> UpdateActorNode(ModData modData, MiniYamlNode actorNode)
+		{
+			foreach (var traitNode in actorNode.ChildrenMatching("Cloak"))
+				traitNode.RenameChildrenMatching("CloakTypes", "DetectionTypes");
+
+			foreach (var traitNode in actorNode.ChildrenMatching("DetectCloaked"))
+				traitNode.RenameChildrenMatching("CloakTypes", "DetectionTypes");
+
+			yield break;
+		}
+	}
+}
