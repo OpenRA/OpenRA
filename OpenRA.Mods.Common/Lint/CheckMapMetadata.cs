c40675cfba610ad91c85b14f27cb3b5c5c1b5361
Gustas <37534529+Punsho@users.noreply.github.com>
2022-01-21 00:03:43 +0200

    Include map.png into uid generation

--- OpenRA.Mods.Common/Lint/CheckMapMetadata.cs
+++ OpenRA.Mods.Common/Lint/CheckMapMetadata.cs
@@ -28,8 +28,8 @@ namespace OpenRA.Mods.Common.Lint
 
 		void Run(Action<string> emitError, int mapFormat, string author, string title, string[] categories)
 		{
-			if (mapFormat != Map.SupportedMapFormat)
-				emitError($"Map format {mapFormat} does not match the supported version {Map.SupportedMapFormat}.");
+			if (mapFormat < Map.SupportedMapFormat)
+				emitError($"Map format {mapFormat} does not match the supported version {Map.CurrentMapFormat}.");
 
 			if (author == null)
 				emitError("Map does not define a valid author.");
