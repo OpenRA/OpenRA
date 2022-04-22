4c08e449e0d29ccfe34131f34780e41c40e40d66
Matthias Mailänder <matthias@mailaender.name>
2022-04-22 18:51:22 +0200

    Fix master server respond not being localized.

--- OpenRA.Game/Server/Server.cs
+++ OpenRA.Game/Server/Server.cs
@@ -930,7 +930,7 @@ namespace OpenRA.Server
 			DispatchServerOrdersToClients(Order.FromTargetString("Message", text, true));
 
 			if (Type == ServerType.Dedicated)
-				WriteLineWithTimeStamp(ModData.Translation.GetFormattedMessage(text));
+				WriteLineWithTimeStamp(text);
 		}
 
 		public void SendLocalizedMessage(string key, Dictionary<string, object> arguments = null)
--- OpenRA.Mods.Common/ServerTraits/MasterServerPinger.cs
+++ OpenRA.Mods.Common/ServerTraits/MasterServerPinger.cs
@@ -1,6 +1,6 @@
 #region Copyright & License Information
 /*
- * Copyright 2007-2021 The OpenRA Developers (see AUTHORS)
+ * Copyright 2007-2022 The OpenRA Developers (see AUTHORS)
  * This file is part of OpenRA, which is free software. It is made
  * available to you under the terms of the GNU General Public License
  * as published by the Free Software Foundation, either version 3 of
@@ -92,7 +92,7 @@ namespace OpenRA.Mods.Common.Server
 
 			lock (masterServerMessages)
 				while (masterServerMessages.Count > 0)
-					server.SendMessage(masterServerMessages.Dequeue());
+					server.SendLocalizedMessage(masterServerMessages.Dequeue());
 		}
 
 		void INotifyServerStart.ServerStarted(S server)
