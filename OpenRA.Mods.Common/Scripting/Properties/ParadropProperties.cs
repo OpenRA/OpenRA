06e426bae8e39f411480133810561b411f76a13b
Robert O'Shea <robertoshea2k11@gmail.com>
2022-08-04 21:20:34 +0100

    Fixed compilation error

--- OpenRA.Mods.Common/Scripting/Properties/ParadropProperties.cs
+++ OpenRA.Mods.Common/Scripting/Properties/ParadropProperties.cs
@@ -33,7 +33,7 @@ namespace OpenRA.Mods.Common.Scripting
 		{
 			paradrop.SetLZ(cell, true);
 			Self.QueueActivity(new Fly(Self, Target.FromCell(Self.World, cell)));
-			Aircraft a = (Self.Trait<Aircraft>();
+			Aircraft a = Self.Trait<Aircraft>();
 			if (a.Info.IdleBehavior == IdleBehaviorType.LeaveMap ||
 				a.Info.IdleBehavior == IdleBehaviorType.LeaveMapAtClosestEdge)
 			{
