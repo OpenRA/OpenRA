fc5c2991fa08144e3ab5d0cfc324f286f9e00fdb
Shrooblord <jango.course@gmail.com>
2022-09-13 23:25:14 +0200

    add ability for Harvester to artificially wait after unloading

--- OpenRA.Mods.Common/Traits/Harvester.cs
+++ OpenRA.Mods.Common/Traits/Harvester.cs
@@ -38,6 +38,9 @@ namespace OpenRA.Mods.Common.Traits
 		[Desc("How fast it can dump it's carryage.")]
 		public readonly int BaleUnloadDelay = 4;
 
+		[Desc("Artificial delay to wait after unloading before continuing with next Activity.")]
+		public readonly int UnloadFinishedDelay = 0;
+
 		[Desc("How many bales can it dump at once.")]
 		public readonly int BaleUnloadAmount = 1;
 
@@ -123,6 +126,9 @@ namespace OpenRA.Mods.Common.Traits
 
 		[Sync]
 		int currentUnloadTicks;
+
+		[Sync]
+		int currentUnloadFinishedDelayTicks;
 
 		[Sync]
 		public int ContentHash
@@ -266,10 +272,15 @@ namespace OpenRA.Mods.Common.Traits
 						contents.Remove(resourceType);
 
 					currentUnloadTicks = Info.BaleUnloadDelay;
+					currentUnloadFinishedDelayTicks = Info.UnloadFinishedDelay;
 					UpdateCondition(self);
 					return false;
 				}
 			}
+
+			// Wait until artificial delay time has passed
+			if (--currentUnloadFinishedDelayTicks > 0)
+				return false;
 
 			return contents.Count == 0;
 		}
