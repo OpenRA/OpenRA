674ca8555b8be4b1d5aed9901e2068778c1d2a52
Gustas <37534529+PunkPun@users.noreply.github.com>
2022-10-06 14:11:46 +0300

    Fix contrail using the wrong colors

--- OpenRA.Mods.Common/Traits/Render/Contrail.cs
+++ OpenRA.Mods.Common/Traits/Render/Contrail.cs
@@ -73,7 +73,7 @@ namespace OpenRA.Mods.Common.Traits
 			this.info = info;
 
 			startcolor = info.StartColorUsePlayerColor ? Color.FromArgb(info.StartColorAlpha, self.Owner.Color) : Color.FromArgb(info.StartColorAlpha, info.StartColor);
-			endcolor = info.EndColorUsePlayerColor ? Color.FromArgb(info.EndColorAlpha, self.Owner.Color) : Color.FromArgb(info.EndColorAlpha, info.EndColor ?? info.StartColor);
+			endcolor = info.EndColorUsePlayerColor ? Color.FromArgb(info.EndColorAlpha, self.Owner.Color) : Color.FromArgb(info.EndColorAlpha, info.EndColor ?? startcolor);
 			trail = new ContrailRenderable(self.World, startcolor, endcolor, info.TrailWidth, info.TrailLength, info.TrailDelay, info.ZOffset);
 
 			body = self.Trait<BodyOrientation>();
