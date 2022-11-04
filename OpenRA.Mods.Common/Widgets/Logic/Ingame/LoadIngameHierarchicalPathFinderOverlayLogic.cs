18e36b96db2c6d9da2ac42d78862ff1f74fe0cdb
Ivaylo Draganov <dragunoff@gmail.com>
2022-11-04 13:33:38 +0200

    Add HPF overlay controls to observer chrome

---
+++ OpenRA.Mods.Common/Widgets/Logic/Ingame/LoadIngameHierarchicalPathFinderOverlayLogic.cs
@@ -0,0 +1,24 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2022 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using OpenRA.Widgets;
+
+namespace OpenRA.Mods.Common.Widgets.Logic
+{
+	public class LoadIngameHierarchicalPathFinderOverlayLogic : ChromeLogic
+	{
+		[ObjectCreator.UseCtor]
+		public LoadIngameHierarchicalPathFinderOverlayLogic(Widget widget, World world)
+		{
+			Game.LoadWidget(world, "HPF_OVERLAY", widget, new WidgetArgs());
+		}
+	}
+}
