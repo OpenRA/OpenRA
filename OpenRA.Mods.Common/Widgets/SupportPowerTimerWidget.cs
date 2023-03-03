2f7498a5693b3cd25827bf91ee9fff1568fab29f
Gustas <37534529+PunkPun@users.noreply.github.com>
2023-03-03 15:44:09 +0200

    Add player's name to super weapon timers

--- OpenRA.Mods.Common/Widgets/SupportPowerTimerWidget.cs
+++ OpenRA.Mods.Common/Widgets/SupportPowerTimerWidget.cs
@@ -21,7 +21,7 @@ namespace OpenRA.Mods.Common.Widgets
 	public class SupportPowerTimerWidget : Widget
 	{
 		public readonly string Font = "Bold";
-		public readonly string Format = "{0}: {1}";
+		public readonly string Format = "{0}'s {1}: {2}";
 		public readonly TextAlign Align = TextAlign.Left;
 		public readonly TimerOrder Order = TimerOrder.Descending;
 
@@ -54,7 +54,7 @@ namespace OpenRA.Mods.Common.Widgets
 			{
 				var self = p.Instances[0].Self;
 				var time = WidgetUtils.FormatTime(p.RemainingTicks, false, self.World.Timestep);
-				var text = Format.F(p.Info.Name, time);
+				var text = Format.F(self.Owner.PlayerName, p.Info.Name, time);
 				var playerColor = self.Owner.Color;
 
 				if (Game.Settings.Game.UsePlayerStanceColors)
