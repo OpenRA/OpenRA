1ab1b63c70ca5e0a07341b5964ad5353a0c1f341
dnqbob <dnqbob@outlook.com>
2023-04-07 00:30:45 +0800

    Add MaxBackwardCells for moving backward control

--- OpenRA.Mods.Common/Activities/Move/Move.cs
+++ OpenRA.Mods.Common/Activities/Move/Move.cs
@@ -160,7 +160,7 @@ namespace OpenRA.Mods.Common.Activities
 
 			var firstFacing = self.World.Map.FacingBetween(mobile.FromCell, nextCell.Value.Cell, mobile.Facing);
 
-			if (mobile.Info.CanMoveBackward && self.World.WorldTick - startTicks < mobile.Info.BackwardDuration && Math.Abs(firstFacing.Angle - mobile.Facing.Angle) > 256)
+			if (mobile.Info.CanMoveBackward && path.Count < mobile.Info.MaxBackwardCells && self.World.WorldTick - startTicks < mobile.Info.BackwardDuration && Math.Abs(firstFacing.Angle - mobile.Facing.Angle) > 256)
 			{
 				ActorFacingModifier = new WAngle(512);
 				firstFacing += ActorFacingModifier;
