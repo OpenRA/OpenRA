aa2888172625171fb6dfe2779de2b0f1a4a74e60
abcdefg30 <alphabet@streber24.de>
2023-05-16 19:20:55 +0200

    DefaultSpriteSequence: Use List.Find instead of LINQ's FirstOrDefault

--- OpenRA.Mods.Common/Graphics/DefaultSpriteSequence.cs
+++ OpenRA.Mods.Common/Graphics/DefaultSpriteSequence.cs
@@ -262,7 +262,7 @@ namespace OpenRA.Mods.Common.Graphics
 
 		protected static T LoadField<T>(string key, T fallback, MiniYaml data, MiniYaml defaults = null)
 		{
-			var node = data.Nodes.FirstOrDefault(n => n.Key == key) ?? defaults?.Nodes.FirstOrDefault(n => n.Key == key);
+			var node = data.Nodes.Find(n => n.Key == key) ?? defaults?.Nodes.Find(n => n.Key == key);
 			if (node == null)
 				return fallback;
 
@@ -276,7 +276,7 @@ namespace OpenRA.Mods.Common.Graphics
 
 		protected static T LoadField<T>(SpriteSequenceField<T> field, MiniYaml data, MiniYaml defaults, out MiniYamlNode.SourceLocation location)
 		{
-			var node = data.Nodes.FirstOrDefault(n => n.Key == field.Key) ?? defaults?.Nodes.FirstOrDefault(n => n.Key == field.Key);
+			var node = data.Nodes.Find(n => n.Key == field.Key) ?? defaults?.Nodes.Find(n => n.Key == field.Key);
 			if (node == null)
 			{
 				location = default;
@@ -412,7 +412,7 @@ namespace OpenRA.Mods.Common.Graphics
 			var offset = LoadField(Offset, data, defaults);
 			var blendMode = LoadField(BlendMode, data, defaults);
 
-			var combineNode = data.Nodes.FirstOrDefault(n => n.Key == Combine.Key);
+			var combineNode = data.Nodes.Find(n => n.Key == Combine.Key);
 			if (combineNode != null)
 			{
 				for (var i = 0; i < combineNode.Value.Nodes.Count; i++)
