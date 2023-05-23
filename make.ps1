36182fa7dcd69272a015f0b0da4335609cd51ad7
Matthias Mailänder <matthias.mailaender@lablicate.com>
2023-05-23 22:12:25 +0200

    Move unit testing into the make script.

--- make.ps1
+++ make.ps1
@@ -110,6 +110,13 @@ function Test-Command
 	InvokeCommand "$utilityPath ra --check-yaml"
 }
 
+function Tests-Command
+{
+	Write-Host "Running unit tests..." -ForegroundColor Cyan
+	dotnet build OpenRA.Test\OpenRA.Test.csproj -c Debug --nologo -p:TargetPlatform=win-x64
+	dotnet test bin\OpenRA.Test.dll --test-adapter-path:.
+}
+
 function Check-Command
 {
 	Write-Host "Compiling in Debug configuration..." -ForegroundColor Cyan
@@ -253,6 +260,7 @@ switch ($execute)
 	{"version",       "v"  -contains $_} { Version-Command }
 	{"clean",         "c"  -contains $_} { Clean-Command }
 	{"test",          "t"  -contains $_} { Test-Command }
+	{"tests",         "ut" -contains $_} { Tests-Command }
 	{"check",         "ck" -contains $_} { Check-Command }
 	{"check-scripts", "cs" -contains $_} { Check-Scripts-Command }
 	Default { Write-Host ("Invalid command '{0}'" -f $command) }
