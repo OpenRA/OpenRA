29eaab59bef238c941bad9bf498f2978b1ffacb5
Gustas <gustas.kazukauskas@gmail.com>
2023-08-28 23:42:39 +0300

    Add backup `ExplicitSequenceFilenames` to update rules

--- OpenRA.Mods.Common/UpdateRules/Rules/20230225/ExplicitSequenceFilenames.cs
+++ OpenRA.Mods.Common/UpdateRules/Rules/20230225/ExplicitSequenceFilenames.cs
@@ -92,19 +92,73 @@ namespace OpenRA.Mods.Common.UpdateRules.Rules
 					defaultSpriteExtension = defaultSpriteExtensionNode.Value.Value;
 				}
 
-				var tilesetExtensionsNode = spriteSequenceFormatNode.LastChildMatching("TilesetExtensions");
+				var fromBackup = false;
+
+				var tilesetExtensionsNode = spriteSequenceFormatNode.LastChildMatching("TilesetExtensions")?.Value?.Nodes;
+				if (tilesetExtensionsNode == null)
+				{
+					switch (modData.Manifest.Id)
+					{
+						case "cnc":
+							fromBackup = true;
+							tilesetExtensionsNode = new List<MiniYamlNodeBuilder>()
+							{
+								new MiniYamlNodeBuilder("TEMPERAT", ".tem"),
+								new MiniYamlNodeBuilder("SNOW", ".sno"),
+								new MiniYamlNodeBuilder("INTERIOR", ".int"),
+								new MiniYamlNodeBuilder("DESERT", ".des"),
+								new MiniYamlNodeBuilder("JUNGLE", ".jun"),
+							};
+							break;
+						case "ra":
+							fromBackup = true;
+							tilesetExtensionsNode = new List<MiniYamlNodeBuilder>()
+							{
+								new MiniYamlNodeBuilder("TEMPERAT", ".tem"),
+								new MiniYamlNodeBuilder("SNOW", ".sno"),
+								new MiniYamlNodeBuilder("INTERIOR", ".int"),
+								new MiniYamlNodeBuilder("DESERT", ".des"),
+							};
+							break;
+						case "ts":
+							fromBackup = true;
+							tilesetExtensionsNode = new List<MiniYamlNodeBuilder>()
+							{
+								new MiniYamlNodeBuilder("TEMPERATE", ".tem"),
+								new MiniYamlNodeBuilder("SNOW", ".sno"),
+							};
+							break;
+					}
+				}
+
 				if (tilesetExtensionsNode != null)
 				{
-					reportModYamlChanges = true;
-					foreach (var n in tilesetExtensionsNode.Value.Nodes)
+					if (!fromBackup)
+						reportModYamlChanges = true;
+
+					foreach (var n in tilesetExtensionsNode)
 						tilesetExtensions[n.Key] = n.Value.Value;
 				}
 
-				var tilesetCodesNode = spriteSequenceFormatNode.LastChildMatching("TilesetCodes");
+				fromBackup = false;
+
+				var tilesetCodesNode = spriteSequenceFormatNode.LastChildMatching("TilesetCodes")?.Value?.Nodes;
+				if (tilesetCodesNode == null && modData.Manifest.Id == "ts")
+				{
+					fromBackup = true;
+					tilesetCodesNode = new List<MiniYamlNodeBuilder>()
+					{
+						new MiniYamlNodeBuilder("TEMPERATE", "t"),
+						new MiniYamlNodeBuilder("SNOW", "a"),
+					};
+				}
+
 				if (tilesetCodesNode != null)
 				{
-					reportModYamlChanges = true;
-					foreach (var n in tilesetCodesNode.Value.Nodes)
+					if (!fromBackup)
+						reportModYamlChanges = true;
+
+					foreach (var n in tilesetCodesNode)
 						tilesetCodes[n.Key] = n.Value.Value;
 				}
 			}
