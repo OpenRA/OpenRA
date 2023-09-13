a47a7ad7e7c6b590818f61d8c736a33805255045
Gustas <gustas.kazukauskas@gmail.com>
2023-09-13 15:27:40 +0300

    Remove unused tint attribute from model shader

--- OpenRA.Game/Graphics/Model.cs
+++ OpenRA.Game/Graphics/Model.cs
@@ -49,7 +49,7 @@ namespace OpenRA.Graphics
 		IModel GetModel(string model);
 		IModel GetModelSequence(string model, string sequence);
 		bool HasModelSequence(string model, string sequence);
-		IVertexBuffer<Vertex> VertexBuffer { get; }
+		IVertexBuffer<ModelVertex> VertexBuffer { get; }
 	}
 
 	public interface IModelSequenceLoader
@@ -64,7 +64,7 @@ namespace OpenRA.Graphics
 
 		sealed class PlaceholderModelCache : IModelCache
 		{
-			public IVertexBuffer<Vertex> VertexBuffer => throw new NotImplementedException();
+			public IVertexBuffer<ModelVertex> VertexBuffer => throw new NotImplementedException();
 
 			public void Dispose() { }
 
