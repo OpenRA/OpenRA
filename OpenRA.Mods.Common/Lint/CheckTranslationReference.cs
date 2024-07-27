bd809e5af7f2c295aefd5f9c72acbbbd297a3d31
RoosterDragon <RoosterDragon@outlook.com>
2024-07-27 22:56:20 +0100

    Prevent community mods from warning on unused translations in the common assets
    Currently when linting translations, we check for any unused translation keys. This works fine for the default mods, which own the entire sets of translation files. For community mods, they often import the translation files from the common mod assets, but they may only use some of the translations provided. Currently, they would get warnings about not using translations from the common files they have imported.
    
    Since the community mods don't own those translations, getting warnings about it is annoying. To solve this issue, introduce a AllowUnusedTranslationsInExternalPackages in the mod.yaml which defaults to true. This will prevent reporting of unused translation keys from external assets. Keys that are used for external assets will still be validated, and keys from the mod assets will be both validated and unused keys will be reported.
    
    We default the new flag to true and don't provide an update rule. This means community mods will get the new behaviour. For the default mods, we do want to check the "external" assets, since we control those assets. So the default mods have their mod.yaml updated to disable the flag and retain the existing behaviour of checking everything.

--- OpenRA.Mods.Common/Lint/CheckTranslationReference.cs
+++ OpenRA.Mods.Common/Lint/CheckTranslationReference.cs
@@ -44,7 +44,12 @@ namespace OpenRA.Mods.Common.Lint
 
 			foreach (var language in GetTranslationLanguages(modData))
 			{
-				CheckKeys(modData.Manifest.Translations.Concat(mapTranslations), map.Open, usedKeys, language, false, emitError, emitWarning);
+				// Check keys and variables are not missing across all language files.
+				// But for maps we don't warn on unused keys. They might be unused on *this* map,
+				// but the mod or another map may use them and we don't have sight of that.
+				CheckKeys(
+					modData.Manifest.Translations.Concat(mapTranslations), map.Open, usedKeys,
+					language, _ => false, emitError, emitWarning);
 
 				var modTranslation = new Translation(language, modData.Manifest.Translations, modData.DefaultFileSystem, _ => { });
 				var mapTranslation = new Translation(language, mapTranslations, map, error => emitError(error.Message));
@@ -79,7 +84,13 @@ namespace OpenRA.Mods.Common.Lint
 				CheckModWidgets(modData, usedKeys, testedFields);
 
 				// With the fully populated keys, check keys and variables are not missing and not unused across all language files.
-				var keyWithAttrs = CheckKeys(modData.Manifest.Translations, modData.DefaultFileSystem.Open, usedKeys, language, true, emitError, emitWarning);
+				var keyWithAttrs = CheckKeys(
+					modData.Manifest.Translations, modData.DefaultFileSystem.Open, usedKeys,
+					language,
+					file =>
+						!modData.Manifest.AllowUnusedTranslationsInExternalPackages ||
+						!modData.DefaultFileSystem.IsExternalFile(file),
+					emitError, emitWarning);
 
 				foreach (var group in usedKeys.KeysWithContext)
 				{
@@ -322,7 +333,10 @@ namespace OpenRA.Mods.Common.Lint
 						CheckChrome(n, translationReferencesByWidgetField, usedKeys);
 		}
 
-		static HashSet<string> CheckKeys(IEnumerable<string> translationFiles, Func<string, Stream> openFile, TranslationKeys usedKeys, string language, bool checkUnusedKeys, Action<string> emitError, Action<string> emitWarning)
+		static HashSet<string> CheckKeys(
+			IEnumerable<string> translationFiles, Func<string, Stream> openFile, TranslationKeys usedKeys,
+			string language, Func<string, bool> checkUnusedKeysForFile,
+			Action<string> emitError, Action<string> emitWarning)
 		{
 			var keyWithAttrs = new HashSet<string>();
 			foreach (var file in translationFiles)
@@ -351,7 +365,7 @@ namespace OpenRA.Mods.Common.Lint
 						foreach (var (node, attributeName) in nodeAndAttributeNames)
 						{
 							keyWithAttrs.Add(attributeName == null ? key : $"{key}.{attributeName}");
-							if (checkUnusedKeys)
+							if (checkUnusedKeysForFile(file))
 								CheckUnusedKey(key, attributeName, file, usedKeys, emitWarning);
 							CheckVariables(node, key, attributeName, file, usedKeys, emitError, emitWarning);
 						}
