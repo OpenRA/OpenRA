OpenRA/OpenRA

commit 2c435c050628be41f91f44b2b05104d4059ec617

Ensure starting units or units granted by a crate are not isolated.
When spawning starting units, or spawning units when collecting a crate, nearby locations will be used. If a nearby location cannot be reached, e.g. it is on top of a cliff or blocked in by trees, then any unit spawned there will be isolated which is not ideal.

Use the PathMightExistForLocomotorBlockedByImmovable method to filter nearby locations to those where the spawned unit can path back to the original location. This ensures the spawned unit is not isolated.

OpenRA.Mods.Common/Traits/World/SpawnStartingUnits.cs
@@ -21,7 +21,7 @@ namespace OpenRA.Mods.Common.Traits
 	[TraitLocation(SystemActors.World)]
 	[Desc("Spawn base actor at the spawnpoint and support units in an annulus around the base actor. " +
 		"Both are defined at MPStartUnits. Attach this to the world actor.")]
-	public class SpawnStartingUnitsInfo : TraitInfo, Requires<StartingUnitsInfo>, NotBefore<LocomotorInfo>, ILobbyOptions
+	public class SpawnStartingUnitsInfo : TraitInfo, Requires<StartingUnitsInfo>, NotBefore<PathFinderInfo>, ILobbyOptions
 	{
 		public readonly string StartingUnitsClass = "none";
 
@@ -102,13 +102,29 @@ namespace OpenRA.Mods.Common.Traits
 			if (unitGroup.SupportActors.Length == 0)
 				return;
 
-			var supportSpawnCells = w.Map.FindTilesInAnnulus(p.HomeLocation, unitGroup.InnerSupportRadius + 1, unitGroup.OuterSupportRadius);
+			var supportSpawnCells = w.Map
+				.FindTilesInAnnulus(p.HomeLocation, unitGroup.InnerSupportRadius + 1, unitGroup.OuterSupportRadius)
+				.ToList();
 
+			var pathFinder = w.WorldActor.TraitOrDefault<IPathFinder>();
+			var locomotorsByName = w.WorldActor.TraitsImplementing<Locomotor>().ToDictionary(l => l.Info.Name);
 			foreach (var s in unitGroup.SupportActors)
 			{
 				var actorRules = w.Map.Rules.Actors[s.ToLowerInvariant()];
 				var ip = actorRules.TraitInfo<IPositionableInfo>();
-				var validCell = supportSpawnCells.Shuffle(w.SharedRandom).FirstOrDefault(c => ip.CanEnterCell(w, null, c));
+				var validCells = supportSpawnCells.Where(c => ip.CanEnterCell(w, null, c));
+
+				if (pathFinder != null)
+				{
+					var locomotorName = actorRules.TraitInfoOrDefault<MobileInfo>()?.Locomotor;
+					var locomotor = locomotorName != null ? locomotorsByName[locomotorName] : null;
+
+					if (locomotor != null)
+						validCells = validCells
+							.Where(c => pathFinder.PathMightExistForLocomotorBlockedByImmovable(locomotor, c, p.HomeLocation + unitGroup.BaseActorOffset));
+				}
+
+				var validCell = validCells.RandomOrDefault(w.SharedRandom);
 
 				if (validCell == CPos.Zero)
 				{

OpenRA.Mods.Common/Traits/Crates/DuplicateUnitCrateAction.cs
@@ -86,10 +86,26 @@ namespace OpenRA.Mods.Common.Traits
 			collector.World.AddFrameEndTask(w =>
 			{
 				var candidateCells = collector.World.Map.FindTilesInCircle(collector.Location, info.MaxRadius)
-					.Where(c => positionable.CanEnterCell(c)).Shuffle(collector.World.SharedRandom)
+					.Where(c => positionable.CanEnterCell(c));
+
+				var pathFinder = w.WorldActor.TraitOrDefault<IPathFinder>();
+				if (pathFinder != null)
+				{
+					var actorRules = w.Map.Rules.Actors[collector.Info.Name];
+					var locomotorName = actorRules.TraitInfoOrDefault<MobileInfo>()?.Locomotor;
+					if (locomotorName != null)
+					{
+						var locomotor = w.WorldActor.TraitsImplementing<Locomotor>().Single(l => l.Info.Name == locomotorName);
+						candidateCells = candidateCells
+							.Where(c => pathFinder.PathMightExistForLocomotorBlockedByImmovable(locomotor, c, collector.Location));
+					}
+				}
+
+				var shuffledCandidateCells = candidateCells
+					.Shuffle(collector.World.SharedRandom)
 					.ToArray();
 
-				var duplicates = Math.Min(candidateCells.Length, info.MaxAmount);
+				var duplicates = Math.Min(shuffledCandidateCells.Length, info.MaxAmount);
 
 				// Restrict duplicate count to a maximum value
 				if (info.MaxDuplicateValue > 0)
@@ -103,7 +119,7 @@ namespace OpenRA.Mods.Common.Traits
 				{
 					var actor = w.CreateActor(collector.Info.Name, new TypeDictionary
 					{
-						new LocationInit(candidateCells[i]),
+						new LocationInit(shuffledCandidateCells[i]),
 						new OwnerInit(info.Owner ?? collector.Owner.InternalName)
 					});
 

OpenRA.Mods.Common/Traits/Crates/GiveUnitCrateAction.cs
@@ -58,10 +58,12 @@ namespace OpenRA.Mods.Common.Traits
 			if (info.ValidFactions.Count > 0 && !info.ValidFactions.Contains(collector.Owner.Faction.InternalName))
 				return false;
 
+			var pathFinder = collector.World.WorldActor.TraitOrDefault<IPathFinder>();
+			var locomotorsByName = collector.World.WorldActor.TraitsImplementing<Locomotor>().ToDictionary(l => l.Info.Name);
 			foreach (var unit in info.Units)
 			{
 				// avoid dumping tanks in the sea, and ships on dry land.
-				if (!GetSuitableCells(collector.Location, unit).Any())
+				if (!GetSuitableCells(collector.Location, unit, pathFinder, locomotorsByName).Any())
 					return false;
 			}
 
@@ -80,9 +82,11 @@ namespace OpenRA.Mods.Common.Traits
 		{
 			collector.World.AddFrameEndTask(w =>
 			{
+				var pathFinder = w.WorldActor.TraitOrDefault<IPathFinder>();
+				var locomotorsByName = w.WorldActor.TraitsImplementing<Locomotor>().ToDictionary(l => l.Info.Name);
 				foreach (var unit in info.Units)
 				{
-					var location = ChooseEmptyCellNear(collector, unit);
+					var location = ChooseEmptyCellNear(collector, unit, pathFinder, locomotorsByName);
 					if (location != null)
 					{
 						var actor = w.CreateActor(unit, new TypeDictionary
@@ -101,19 +105,33 @@ namespace OpenRA.Mods.Common.Traits
 			base.Activate(collector);
 		}
 
-		IEnumerable<CPos> GetSuitableCells(CPos near, string unitName)
+		IEnumerable<CPos> GetSuitableCells(CPos near, string unitName, IPathFinder pathFinder, Dictionary<string, Locomotor> locomotorsByName)
 		{
-			var ip = self.World.Map.Rules.Actors[unitName].TraitInfo<IPositionableInfo>();
+			var actorRules = self.World.Map.Rules.Actors[unitName];
 
-			for (var i = -1; i < 2; i++)
-				for (var j = -1; j < 2; j++)
-					if (ip.CanEnterCell(self.World, self, near + new CVec(i, j)))
+			Locomotor locomotor = null;
+			if (pathFinder != null)
+			{
+				var locomotorName = actorRules.TraitInfoOrDefault<MobileInfo>()?.Locomotor;
+				locomotor = locomotorName != null ? locomotorsByName[locomotorName] : null;
+			}
+
+			var ip = actorRules.TraitInfo<IPositionableInfo>();
+			for (var i = -1; i <= 1; i++)
+			{
+				for (var j = -1; j <= 1; j++)
+				{
+					var cell = near + new CVec(i, j);
+					if (ip.CanEnterCell(self.World, self, cell) &&
+						(locomotor == null || pathFinder.PathMightExistForLocomotorBlockedByImmovable(locomotor, cell, near)))
 						yield return near + new CVec(i, j);
+				}
+			}
 		}
 
-		CPos? ChooseEmptyCellNear(Actor a, string unit)
+		CPos? ChooseEmptyCellNear(Actor a, string unit, IPathFinder pathFinder, Dictionary<string, Locomotor> locomotorsByName)
 		{
-			return GetSuitableCells(a.Location, unit)
+			return GetSuitableCells(a.Location, unit, pathFinder, locomotorsByName)
 				.Cast<CPos?>()
 				.RandomOrDefault(self.World.SharedRandom);
 		}

commit f83e60ee50a69f29615e56a6fddca10bf6aafacb

Allow SpawnStartingUnits to have an immovable BaseActor
Fixes a regression from 2c435c050628be41f91f44b2b05104d4059ec617 - where the support actors must be able to path to the base actor in order to prevent them from spawning in isolated areas. If the base actor is immovable, they cannot path onto it because the base actor blocks them, so no support actors will spawn.

Fix this by allowing the support actors to path back to any cell adjacent to an immovable base actor, rather than requiring them to be able to path to its location directly.

OpenRA.Mods.Common/Traits/World/SpawnStartingUnits.cs
@@ -86,10 +86,11 @@ namespace OpenRA.Mods.Common.Traits
 			if (unitGroup == null)
 				throw new InvalidOperationException($"No starting units defined for faction {p.Faction.InternalName} with class {spawnClass}");
 
+			CPos[] homeLocations;
 			if (unitGroup.BaseActor != null)
 			{
 				var facing = unitGroup.BaseActorFacing ?? new WAngle(w.SharedRandom.Next(1024));
-				w.CreateActor(unitGroup.BaseActor.ToLowerInvariant(), new TypeDictionary
+				var baseActor = w.CreateActor(unitGroup.BaseActor.ToLowerInvariant(), new TypeDictionary
 				{
 					new LocationInit(p.HomeLocation + unitGroup.BaseActorOffset),
 					new OwnerInit(p),
@@ -97,6 +98,25 @@ namespace OpenRA.Mods.Common.Traits
 					new FacingInit(facing),
 					new SpawnedByMapInit(),
 				});
+				var baseActorIsMovable =
+					baseActor.OccupiesSpace is Mobile mobile && !mobile.IsTraitDisabled && !mobile.IsTraitPaused && !mobile.IsImmovable;
+				if (baseActorIsMovable)
+				{
+					// If the base is movable, we want support actors to be able to path to its location.
+					homeLocations = new[] { baseActor.Location };
+				}
+				else
+				{
+					// For an immovable base, we want support actors to be able to path adjacent to it.
+					// They won't to able to path to its location, because it is immovable and blocks them.
+					var occupiedCells = baseActor.OccupiesSpace.OccupiedCells().Select(p => p.Cell).ToArray();
+					homeLocations = Util.ExpandFootprint(occupiedCells, true).Except(occupiedCells).ToArray();
+				}
+			}
+			else
+			{
+				// If there is no base actor, we want support actors to be able to path to the home location.
+				homeLocations = new[] { p.HomeLocation };
 			}
 
 			if (unitGroup.SupportActors.Length == 0)
@@ -121,7 +141,7 @@ namespace OpenRA.Mods.Common.Traits
 
 					if (locomotor != null)
 						validCells = validCells
-							.Where(c => pathFinder.PathMightExistForLocomotorBlockedByImmovable(locomotor, c, p.HomeLocation + unitGroup.BaseActorOffset));
+							.Where(c => homeLocations.Any(h => pathFinder.PathMightExistForLocomotorBlockedByImmovable(locomotor, c, h)));
 				}
 
 				var validCell = validCells.RandomOrDefault(w.SharedRandom);