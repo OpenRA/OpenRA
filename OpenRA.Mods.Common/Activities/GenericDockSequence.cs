91b8af5a72f5f42362e84bd553c84ea516bbf8cd
Gustas <gustas.kazukauskas@gmail.com>
2024-08-29 19:57:06 +0300

    Simplify IDockHost interface

--- OpenRA.Mods.Common/Activities/GenericDockSequence.cs
+++ OpenRA.Mods.Common/Activities/GenericDockSequence.cs
@@ -41,7 +41,8 @@ namespace OpenRA.Mods.Common.Activities
 
 		bool dockInitiated = false;
 
-		public GenericDockSequence(Actor self, DockClientManager client, Actor hostActor, IDockHost host)
+		public GenericDockSequence(Actor self, DockClientManager client, Actor hostActor, IDockHost host,
+			int dockWait, bool isDragRequired, WVec dragOffset, int dragLength)
 		{
 			dockingState = DockingState.Drag;
 
@@ -54,17 +55,12 @@ namespace OpenRA.Mods.Common.Activities
 			DockHostSpriteOverlay = hostActor.TraitOrDefault<WithDockingOverlay>();
 			notifyDockHosts = hostActor.TraitsImplementing<INotifyDockHost>().ToArray();
 
-			if (host is IDockHostDrag sequence)
-			{
-				IsDragRequired = sequence.IsDragRequired;
-				DragLength = sequence.DragLength;
-				StartDrag = self.CenterPosition;
-				EndDrag = hostActor.CenterPosition + sequence.DragOffset;
-			}
-			else
-				IsDragRequired = false;
+			IsDragRequired = isDragRequired;
+			DragLength = dragLength;
+			StartDrag = self.CenterPosition;
+			EndDrag = hostActor.CenterPosition + dragOffset;
 
-			QueueChild(new Wait(host.DockWait));
+			QueueChild(new Wait(dockWait));
 		}
 
 		public override bool Tick(Actor self)
