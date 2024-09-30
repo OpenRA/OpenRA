771b9ddfdad4a406be3d25946647fe1020b5e32c
Ivaylo Draganov <dragunoff@gmail.com>
2024-09-30 16:59:14 +0300

    Remove key handling from `ContainerWidget`
    It has no business handling key input. This was used only for opening the ingame chat and replacing it with a `LogicKeyListener` was trivial.

--- OpenRA.Game/Widgets/Widget.cs
+++ OpenRA.Game/Widgets/Widget.cs
@@ -632,8 +632,6 @@ namespace OpenRA.Widgets
 
 		public override string GetCursor(int2 pos) { return null; }
 		public override Widget Clone() { return new ContainerWidget(this); }
-		public Func<KeyInput, bool> OnKeyPress = _ => false;
-		public override bool HandleKeyPress(KeyInput e) { return OnKeyPress(e); }
 
 		public override bool HandleMouseInput(MouseInput mi)
 		{
