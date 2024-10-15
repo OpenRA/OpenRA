4f9f12131ae1a3bc6e97db827f28cdc6e6d3bb5d
Paul Chote <pchote@users.noreply.github.com>
2024-10-15 14:12:24 +0100

    Fix Lua Fluent string arguments.

--- OpenRA.Mods.Common/Scripting/Global/UserInterfaceGlobal.cs
+++ OpenRA.Mods.Common/Scripting/Global/UserInterfaceGlobal.cs
@@ -40,7 +40,7 @@ namespace OpenRA.Mods.Common.Scripting.Global
 		{
 			if (args != null)
 			{
-				var argumentDictionary = new Dictionary<string, object>();
+				var arguments = new List<object>();
 				foreach (var kv in args)
 				{
 					using (kv.Key)
@@ -51,11 +51,12 @@ namespace OpenRA.Mods.Common.Scripting.Global
 								"String arguments requires a table of [\"string\"]=value pairs. " +
 								$"Received {kv.Key.WrappedClrType().Name},{kv.Value.WrappedClrType().Name}");
 
-						argumentDictionary.Add(variable, value);
+						arguments.Add(variable);
+						arguments.Add(value);
 					}
 				}
 
-				return FluentProvider.GetString(key, argumentDictionary);
+				return FluentProvider.GetString(key, arguments.ToArray());
 			}
 
 			return FluentProvider.GetString(key);
