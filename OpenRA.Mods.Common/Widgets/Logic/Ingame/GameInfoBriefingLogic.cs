injiiiiil/888

Externalize briefing strings.

OpenRA.Mods.Common/Widgets/Logic/Ingame/GameInfoBriefingLogic.cs
@@ -29,7 +29,8 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 			var missionData = world.Map.Rules.Actors[SystemActors.World].TraitInfoOrDefault<MissionDataInfo>();
 			if (missionData != null)
 			{
-				var text = WidgetUtils.WrapText(missionData.Briefing?.Replace("\\n", "\n"), mapDescription.Bounds.Width, mapFont);
+				var briefing = missionData.Briefing != null ? previewWidget.Preview().GetLocalisedString(missionData.Briefing) : "";
+				var text = WidgetUtils.WrapText(briefing, mapDescription.Bounds.Width, mapFont);
 				mapDescription.GetText = () => text;
 				mapDescription.Bounds.Height = mapFont.Measure(text).Y;
 				mapDescriptionPanel.ScrollToTop();

OpenRA.Mods.Common/Widgets/Logic/MissionBrowserLogic.cs
@@ -282,13 +282,14 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 					infoVideo = missionData.BackgroundVideo;
 					infoVideoVisible = infoVideo != null;
 
-					var briefing = WidgetUtils.WrapText(missionData.Briefing?.Replace("\\n", "\n"), description.Bounds.Width, descriptionFont);
-					var height = descriptionFont.Measure(briefing).Y;
+					var briefing = missionData.Briefing != null ? selectedMap.GetLocalisedString(missionData.Briefing) : "";
+					var wrapped = WidgetUtils.WrapText(briefing, description.Bounds.Width, descriptionFont);
+					var height = descriptionFont.Measure(wrapped).Y;
 					Game.RunAfterTick(() =>
 					{
 						if (preview == selectedMap)
 						{
-							description.GetText = () => briefing;
+							description.GetText = () => wrapped;
 							description.Bounds.Height = height;
 							descriptionPanel.Layout.AdjustChildren();
 						}

OpenRA/OpenRA

OpenRA.Mods.Common/Widgets/Logic/Ingame/GameInfoBriefingLogic.cs
@@ -29,7 +29,8 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 			var missionData = world.Map.Rules.Actors[SystemActors.World].TraitInfoOrDefault<MissionDataInfo>();
 			if (missionData != null)
 			{
-				var text = WidgetUtils.WrapText(missionData.Briefing?.Replace("\\n", "\n"), mapDescription.Bounds.Width, mapFont);
+				var briefing = missionData.Briefing != null ? previewWidget.Preview().GetString(missionData.Briefing) : "";
+				var text = WidgetUtils.WrapText(briefing, mapDescription.Bounds.Width, mapFont);
 				mapDescription.GetText = () => text;
 				mapDescription.Bounds.Height = mapFont.Measure(text).Y;
 				mapDescriptionPanel.ScrollToTop();

OpenRA.Mods.Common/Widgets/Logic/MissionBrowserLogic.cs
@@ -279,13 +279,14 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 					infoVideo = missionData.BackgroundVideo;
 					infoVideoVisible = infoVideo != null;
 
-					var briefing = WidgetUtils.WrapText(missionData.Briefing?.Replace("\\n", "\n"), description.Bounds.Width, descriptionFont);
-					var height = descriptionFont.Measure(briefing).Y;
+					var briefing = missionData.Briefing != null ? selectedMap.GetString(missionData.Briefing) : "";
+					var wrapped = WidgetUtils.WrapText(briefing, description.Bounds.Width, descriptionFont);
+					var height = descriptionFont.Measure(wrapped).Y;
 					Game.RunAfterTick(() =>
 					{
 						if (preview == selectedMap)
 						{
-							description.GetText = () => briefing;
+							description.GetText = () => wrapped;
 							description.Bounds.Height = height;
 							descriptionPanel.Layout.AdjustChildren();
 							panel = PanelType.MissionInfo;